c919f04be4d4eacf7c2ec5073d6bb1a75f64b2cc
Dizz Err <dizzerr@gmail.com>
2011-12-13 18:55:44 +0300

    Changle: ResourceEditor camera control

--- Tools/ResourceEditor/Classes/SceneEditor/CameraController.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/CameraController.cpp
@@ -143,8 +143,9 @@ void WASDCameraController::Input(UIEvent * event)
     else if(event->phase == UIEvent::PHASE_DRAG || event->phase == UIEvent::PHASE_ENDED)
     {
         Vector2 dp = oldTouchPoint - event->point;
-        viewXAngle += dp.x * SystemTimer::Instance()->FrameDelta() * 1.5f;
-        viewYAngle += dp.y * SystemTimer::Instance()->FrameDelta();
+		dp *= 5.f;
+        viewXAngle -= dp.x * SystemTimer::Instance()->FrameDelta() * 1.5f;
+        viewYAngle -= dp.y * SystemTimer::Instance()->FrameDelta();
         oldTouchPoint = event->point;
         //ClampAngles();
         //LOG_AS_FLOAT(viewXAngle);
