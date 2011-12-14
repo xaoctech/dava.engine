3cc4cc9bfa22d975750da3ad4359efcbd3821851
VK <klesch@VKs-MacBook-Pro.local>
2011-12-14 18:24:45 +0200

    Git считает, что это я менял файлы и просит их закоммитить
    Git считает, что это я менял файлы и просит их закоммитить

---
+++ Tools/ResourceEditor/Classes/GameScene.h
@@ -0,0 +1,49 @@
+/*
+ *  GameScene.h
+ *  SceneEditor
+ *
+ *  Created by Yury Danilov on 14.12.11
+ *  Copyright 2011 DAVA. All rights reserved.
+ *
+ */
+
+#ifndef __GAME_SCENE_H__
+#define __GAME_SCENE_H__
+
+#include "DAVAEngine.h"
+#include "btBulletDynamicsCommon.h"
+
+using namespace DAVA;
+
+class GameScene : public Scene
+{
+public:
+    
+    GameScene();
+    ~GameScene();
+    
+    virtual void Update(float32 timeElapsed);
+    virtual void Draw();
+
+//    void AddTank(HittableObject *hittableObject);
+//    void AddBuilding(RealBuilding *building);
+	
+
+    btDiscreteDynamicsWorld *dynamicsWorld;
+    
+protected:
+
+    btDefaultCollisionConfiguration *dynCollisionConfiguration;
+	btCollisionDispatcher *dynDispatcher;
+	btAxisSweep3 *dynOverlappingPairCache;
+	btSequentialImpulseConstraintSolver* dynSolver;
+
+    btDefaultCollisionConfiguration* collisionConfiguration;
+	btCollisionDispatcher* dispatcher;
+	btAxisSweep3* broadphase;
+    
+
+//    Vector<RealBuilding*> buildingObjects;
+};
+
+#endif
