c0b8997a451e08b72eb5a02363b37660304d8ff9
VK <klesch@VKs-MacBook-Pro.local>
2011-12-15 04:07:33 +0200

    Created Files Navigation for LibraryControl
    Created Files Navigation for LibraryControl. Added DAE->SCE conversion

--- Tools/ResourceEditor/Classes/SceneEditor/LibraryControl.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/LibraryControl.cpp
@@ -1,10 +1,15 @@
 #include "LibraryControl.h"
+#include "./../Collada/ColladaConvert.h"
 
 
 LibraryControl::LibraryControl(const Rect & rect)
     :   UIControl(rect)
 {
+    scene = NULL;
+    controlDelegate = NULL;
     folderPath = "/";
+    selectedFileName = "";
+    selectedFileNameShort = "";
     
     fontLight = FTFont::Create("~res:/Fonts/MyriadPro-Regular.otf");
     fontLight->SetSize(12);
@@ -18,99 +23,73 @@ LibraryControl::LibraryControl(const Rect & rect)
     GetBackground()->SetDrawType(UIControlBackground::DRAW_FILL);
     GetBackground()->SetColor(Color(0.5f, 0.5f, 0.5f, 1.0f));
     
-    filesTree = new UIHierarchy(Rect(0, BUTTON_HEIGHT, rect.dx, rect.dy - BUTTON_HEIGHT));
-    filesTree->SetCellHeight(CELL_HEIGHT);
-    filesTree->SetDelegate(this);
-    filesTree->SetClipContents(true);
-    filesTree->GetBackground()->SetDrawType(UIControlBackground::DRAW_FILL);
-    filesTree->GetBackground()->SetColor(Color(0.92f, 0.92f, 0.92f, 1.0f));
-    AddControl(filesTree);
+    fileTreeControl = new UIFileTree(Rect(0, BUTTON_HEIGHT, rect.dx, rect.dy - BUTTON_HEIGHT - rect.dx));
+	fileTreeControl->SetDelegate(this);
+	fileTreeControl->SetFolderNavigation(true);
+	fileTreeControl->SetPath(folderPath, ".dae;.sce;.DAE;.SCE");
+    AddControl(fileTreeControl);
+
     
     //button
-    refreshButton = new UIButton(Rect(0, 0, rect.dx, BUTTON_HEIGHT));
-    refreshButton->SetStateDrawType(UIControl::STATE_NORMAL, UIControlBackground::DRAW_FILL);
-    refreshButton->SetStateDrawType(UIControl::STATE_PRESSED_INSIDE, UIControlBackground::DRAW_FILL);
-    refreshButton->GetStateBackground(UIControl::STATE_NORMAL)->SetColor(Color(0.0f, 0.0f, 0.0f, 0.5f));
-    refreshButton->GetStateBackground(UIControl::STATE_PRESSED_INSIDE)->SetColor(Color(0.5f, 0.5f, 0.5f, 0.5f));
-    refreshButton->SetStateFont(UIControl::STATE_NORMAL, fontLight);
-    refreshButton->SetStateFont(UIControl::STATE_PRESSED_INSIDE, fontLight);
-    refreshButton->SetStateText(UIControl::STATE_NORMAL, L"Refresh Library");
-    refreshButton->SetStateText(UIControl::STATE_PRESSED_INSIDE, L"Refresh Library");
+    refreshButton = CreateButton(Rect(0, 0, rect.dx, BUTTON_HEIGHT), L"Refresh Library");
+    refreshButton->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &LibraryControl::OnRefreshPressed));
     AddControl(refreshButton);
+    
+    panelDAE = CreatePanel(Rect(0, rect.dy - rect.dx, rect.dx, rect.dx));
+    btnConvert = CreateButton(Rect(0, 0, rect.dx, BUTTON_HEIGHT), L"Convert");
+    btnConvert->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &LibraryControl::OnConvertPressed));
+    panelDAE->AddControl(btnConvert);
+    
+    int32 btnwidth = (rect.dx - 2) / 2;
+    panelSCE = CreatePanel(Rect(0, rect.dy - rect.dx, rect.dx, rect.dx));
+    btnAdd = CreateButton(Rect(0, 0, btnwidth, BUTTON_HEIGHT), L"Add");
+    btnAdd->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &LibraryControl::OnAddPressed));
+    btnEdit = CreateButton(Rect(rect.dx - btnwidth, 0, btnwidth, BUTTON_HEIGHT), L"Edit");
+    btnEdit->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &LibraryControl::OnEditPressed));
+    preview = new UI3DView(Rect(0, BUTTON_HEIGHT, rect.dx, rect.dx - BUTTON_HEIGHT));
+    preview->SetDebugDraw(true);
+    preview->SetInputEnabled(false);
+    panelSCE->AddControl(btnAdd);
+    panelSCE->AddControl(btnEdit);
+    panelSCE->AddControl(preview);
+    
+//    scene = new Scene();
+//    // Camera setup
+//    Camera * cam = new Camera(scene);
+//    cam->SetName("editor-camera");
+//    cam->SetDebugFlags(SceneNode::DEBUG_DRAW_ALL);
+//    cam->SetUp(Vector3(0.0f, 0.0f, 1.0f));
+//    cam->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
+//    cam->SetTarget(Vector3(0.0f, 1.0f, 0.0f));
+//    cam->Setup(70.0f, 320.0f / 480.0f, 1.0f, 5000.0f); 
+//    
+//    scene->AddNode(cam);
+//    scene->AddCamera(cam);
+//    scene->SetCurrentCamera(cam);
+//    SafeRelease(cam);
+//
+//    preview->SetScene(scene);
 }
     
 LibraryControl::~LibraryControl()
 {
+    SafeRelease(scene);
+    
+    SafeRelease(btnAdd);
+    SafeRelease(btnConvert);
+    SafeRelease(btnEdit);
+    SafeRelease(preview);
+    
+    SafeRelease(panelDAE);
+    SafeRelease(panelSCE);
+    
+    
     SafeRelease(refreshButton);
     
-    SafeRelease(filesTree);
+    SafeRelease(fileTreeControl);
     
     SafeRelease(fontLight);
     SafeRelease(fontDark);
-}
-
-bool LibraryControl::IsNodeExpandable(UIHierarchy *forHierarchy, void *forNode)
-{
-    if(forHierarchy == filesTree)
-    {
-
-    }
-    
-    return false;
-}
-
-int32 LibraryControl::ChildrenCount(UIHierarchy *forHierarchy, void *forParent)
-{
-    if(forHierarchy == filesTree)
-    {
-    }
-    
-    return 0;
-}
-
-void * LibraryControl::ChildAtIndex(UIHierarchy *forHierarchy, void *forParent, int32 index)
-{
-    if(forHierarchy == filesTree)
-    {
-    }
-    
-    return NULL;
-}
-
-UIHierarchyCell * LibraryControl::CellForNode(UIHierarchy *forHierarchy, void *node)
-{
-    UIHierarchyCell *c = NULL;
-    if(forHierarchy == filesTree)
-    {
-        c = forHierarchy->GetReusableCell("Library cell"); //try to get cell from the reusable cells store
-        if(!c)
-        { //if cell of requested type isn't find in the store create new cell
-            Rect r = GetRect();
-            c = new UIHierarchyCell(Rect(0, 0, r.dx, CELL_HEIGHT), "Library cell");
-        }
-        
-        c->text->SetText(L"Cell");
-    }
-    
-    c->text->SetFont(fontDark);
-    c->text->SetAlign(ALIGN_LEFT|ALIGN_VCENTER);
-
-    Color color(0.1f, 0.5f, 0.05f, 1.0f);
-    c->openButton->SetStateDrawType(UIControl::STATE_NORMAL, UIControlBackground::DRAW_FILL);
-    c->openButton->SetStateDrawType(UIControl::STATE_PRESSED_INSIDE, UIControlBackground::DRAW_FILL);
-    c->openButton->SetStateDrawType(UIControl::STATE_HOVER, UIControlBackground::DRAW_FILL);
-    c->openButton->GetStateBackground(UIControl::STATE_NORMAL)->color = color;
-    c->openButton->GetStateBackground(UIControl::STATE_HOVER)->color = color + 0.1f;
-    c->openButton->GetStateBackground(UIControl::STATE_PRESSED_INSIDE)->color = color + 0.3f;
-
-    return c;
-}
-
-void LibraryControl::OnCellSelected(UIHierarchy *forHierarchy, UIHierarchyCell *selectedCell)
-{
-    if(forHierarchy == filesTree)
-    {
-    }
 }
 
 
@@ -121,15 +100,167 @@ void LibraryControl::Update(float32 timeElapsed)
 
 void LibraryControl::WillAppear()
 {
-    filesTree->Refresh();
+    RefreshTree();
 }
 
 void LibraryControl::SetPath(const String &path)
 {
-    folderPath = path;
+    folderPath = path + "/DataSource/3d";
     
     if(GetParent())
     {
-        filesTree->Refresh();
+        RefreshTree();
     }
+}
+
+UIButton * LibraryControl::CreateButton(Rect r, const WideString &text)
+{
+    UIButton *btn = new UIButton(r);
+    
+    btn->SetStateDrawType(UIControl::STATE_NORMAL, UIControlBackground::DRAW_FILL);
+    btn->SetStateDrawType(UIControl::STATE_PRESSED_INSIDE, UIControlBackground::DRAW_FILL);
+    btn->SetStateDrawType(UIControl::STATE_DISABLED, UIControlBackground::DRAW_FILL);
+    btn->SetStateDrawType(UIControl::STATE_SELECTED, UIControlBackground::DRAW_FILL);
+    
+    btn->GetStateBackground(UIControl::STATE_NORMAL)->SetColor(Color(0.0f, 0.0f, 0.0f, 0.5f));
+    btn->GetStateBackground(UIControl::STATE_PRESSED_INSIDE)->SetColor(Color(0.5f, 0.5f, 0.5f, 0.5f));
+    btn->GetStateBackground(UIControl::STATE_DISABLED)->SetColor(Color(0.2f, 0.2f, 0.2f, 0.2f));
+    btn->GetStateBackground(UIControl::STATE_SELECTED)->SetColor(Color(0.0f, 0.0f, 1.0f, 0.2f));
+    
+    
+    btn->SetStateFont(UIControl::STATE_PRESSED_INSIDE, fontLight);
+    btn->SetStateFont(UIControl::STATE_DISABLED, fontLight);
+    btn->SetStateFont(UIControl::STATE_NORMAL, fontLight);
+    btn->SetStateFont(UIControl::STATE_SELECTED, fontLight);
+    
+    btn->SetStateText(UIControl::STATE_PRESSED_INSIDE, text);
+    btn->SetStateText(UIControl::STATE_DISABLED, text);
+    btn->SetStateText(UIControl::STATE_NORMAL, text);
+    btn->SetStateText(UIControl::STATE_SELECTED, text);
+    return btn;
+}
+
+UIControl * LibraryControl::CreatePanel(Rect r)
+{
+    UIControl *ctrl = new UIControl(r);
+    ctrl->GetBackground()->color = Color(0.4f, 0.4f, 0.4f, 1.0f);
+    ctrl->GetBackground()->SetDrawType(UIControlBackground::DRAW_FILL);
+    
+    return ctrl;
+}
+
+void LibraryControl::OnAddPressed(DAVA::BaseObject *object, void *userData, void *callerData)
+{
+    if(controlDelegate)
+    {
+        controlDelegate->OnAddSCE(selectedFileName);
+    }
+}
+
+void LibraryControl::OnEditPressed(DAVA::BaseObject *object, void *userData, void *callerData)
+{
+    if(controlDelegate)
+    {
+        controlDelegate->OnEditSCE(selectedFileName, selectedFileNameShort);
+    }
+}
+
+void LibraryControl::OnConvertPressed(DAVA::BaseObject *object, void *userData, void *callerData)
+{
+    ConvertDaeToSce(selectedFileName);
+    RefreshTree();
+}
+
+void LibraryControl::OnRefreshPressed(DAVA::BaseObject *object, void *userData, void *callerData)
+{
+    RefreshTree();
+}
+
+void LibraryControl::OnCellSelected(DAVA::UIFileTree *tree, DAVA::UIFileTreeCell *selectedCell)
+{
+    UITreeItemInfo * itemInfo = selectedCell->GetItemInfo();
+	String extension = FileSystem::GetExtension(itemInfo->GetName());
+	if (extension == ".dae" || extension == ".DAE")
+	{
+        selectedFileName = itemInfo->GetPathname();
+        if(panelSCE->GetParent())
+        {
+            RemoveControl(panelSCE);
+        }
+        if(!panelDAE->GetParent())
+        {
+            AddControl(panelDAE);
+        }
+	}
+    else if(extension == ".sce" || extension == ".SCE")
+    {
+        selectedFileName = itemInfo->GetPathname();
+        selectedFileNameShort = itemInfo->GetName();
+        
+        if(panelDAE->GetParent())
+        {
+            RemoveControl(panelDAE);
+        }
+        
+        if(!panelSCE->GetParent())
+        {
+            AddControl(panelSCE);
+        }
+        else
+        {
+            scene->ReleaseRootNode(selectedFileName);
+        }
+
+        SafeRelease(scene);
+        scene = new Scene();
+        preview->SetScene(scene);
+        
+        SceneFile * file = new SceneFile();
+        file->SetDebugLog(true);
+        file->LoadScene(selectedFileName.c_str(), scene);
+        scene->AddNode(scene->GetRootNode(selectedFileName));
+        SafeRelease(file);
+        
+        if (scene->GetCamera(0))
+        {
+            scene->SetCurrentCamera(scene->GetCamera(0));
+        }
+        else
+        {
+            Camera * cam = new Camera(scene);
+            cam->SetName("editor-camera");
+            cam->SetDebugFlags(SceneNode::DEBUG_DRAW_ALL);
+            cam->SetUp(Vector3(0.0f, 0.0f, 1.0f));
+            cam->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
+            cam->SetTarget(Vector3(0.0f, 1.0f, 0.0f));
+            cam->Setup(70.0f, 320.0f / 480.0f, 1.0f, 5000.0f); 
+            
+            scene->AddNode(cam);
+            scene->AddCamera(cam);
+            scene->SetCurrentCamera(cam);
+            SafeRelease(cam);
+        }
+    }
+    else
+    {
+        if(panelDAE->GetParent())
+        {
+            RemoveControl(panelDAE);
+        }
+        if(panelSCE->GetParent())
+        {
+            RemoveControl(panelSCE);
+        }
+    }
+}
+
+void LibraryControl::RefreshTree()
+{
+    fileTreeControl->SetPath(folderPath, ".dae;.sce;.DAE;.SCE");
+    fileTreeControl->Refresh();
+}
+
+void LibraryControl::SetDelegate(LibraryControlDelegate *delegate)
+{
+    controlDelegate = delegate;
 }
