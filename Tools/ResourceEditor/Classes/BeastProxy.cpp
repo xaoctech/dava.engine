528968390f08c09bb24e2e6bda37f46b97fb8217
Dizz Err <dizzerr@gmail.com>
2011-12-15 21:04:04 +0300

    Fix: compilation errors

---
+++ Tools/ResourceEditor/Classes/BeastProxy.cpp
@@ -0,0 +1,15 @@
+#include "BeastProxy.h"
+
+BeastManager * BeastProxy::CreateManager()
+{
+	return 0;
+}
+
+void BeastProxy::SafeDeleteManager(BeastManager ** manager)
+{
+}
+
+void BeastProxy::ParseScene(BeastManager * manager, DAVA::Scene * scene)
+{
+
+}
