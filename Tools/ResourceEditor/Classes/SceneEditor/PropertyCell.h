69b76341611019de514b75f0f4f1102e7b1fa639
Hottych <alexey@davaconsulting.com>
2011-12-20 20:26:50 +0200

    Add: Editor: Property list added
    Former-commit-id: 546b5b65034c8b4b0bc75108f577bec276c2e8a6

---
+++ Tools/ResourceEditor/Classes/SceneEditor/PropertyCell.h
@@ -0,0 +1,68 @@
+/*
+ *  PropertyCell.h
+ *  SniperEditorMacOS
+ *
+ *  Created by Alexey Prosin on 12/13/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef PROPERTY_CELL
+#define PROPERTY_CELL
+
+#include "DAVAEngine.h"
+
+using namespace DAVA;
+
+class PropertyValue;
+
+class PropertyCellData;
+class PropertyCellDelegate
+{
+public:
+    virtual void OnPropertyChanged(PropertyCellData *changedProperty) = 0;
+};
+
+class PropertyCell : public UIListCell
+{
+public:
+    
+    enum CellType 
+    {
+        PROP_CELL_TEXT = 0,
+        PROP_CELL_DIGITS,
+        PROP_CELL_FILEPATH,
+        
+        PROP_CELL_COUNT
+    };
+    
+    PropertyCell(PropertyCellDelegate *propDelegate, const Rect &rect, Font *font, PropertyCellData *prop);
+
+    virtual void SetData(PropertyCellData *prop);
+
+    static String GetTypeName(int cellType);
+
+    PropertyCellData *property;
+    UIStaticText *keyName;
+    PropertyCellDelegate *propertyDelegate;
+};
+
+class PropertyTextCell : public PropertyCell, public UITextFieldDelegate
+{
+public:
+    PropertyTextCell(PropertyCellDelegate *propDelegate, Font *font, PropertyCellData *prop, float32 width);
+    
+    static float32 GetHeightForWidth(float32 currentWidth);
+    virtual void SetData(PropertyCellData *prop);
+
+    virtual void TextFieldShouldReturn(UITextField * textField);
+	virtual bool TextFieldKeyPressed(UITextField * textField, int32 replacementLocation, int32 replacementLength, const WideString & replacementString);
+    
+    virtual void DidAppear();
+
+    UITextField *editableText;
+    UIStaticText *uneditableText;
+    UIControl *uneditableTextContainer;
+};
+
+#endif
---
+++ Tools/ResourceEditor/Classes/SceneEditor/PropertyList.h
@@ -0,0 +1,75 @@
+/*
+ *  PropertyList.h
+ *  SniperEditorMacOS
+ *
+ *  Created by Alexey Prosin on 12/13/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef PROPERTY_LIST
+#define PROPERTY_LIST
+
+#include "DAVAEngine.h"
+#include "PropertyCellData.h"
+#include "PropertyCell.h"
+
+using namespace DAVA;
+
+class PropertyList;
+class PropertyListDelegate
+{
+public:
+    void OnStringPropertyChanged(PropertyList *forList, const String &forKey, const String &newValue){};
+    void OnFloatPropertyChanged(PropertyList *forList, const String &forKey, float newValue){};
+    void OnIntPropertyChanged(PropertyList *forList, const String &forKey, int newValue){};
+};
+
+
+
+class PropertyList : public UIControl, public UIListDelegate, public PropertyCellDelegate
+{
+public:
+    
+    enum editableType 
+    {
+        PROPERTY_IS_EDITABLE = 0,
+        PROPERTY_IS_READ_ONLY
+    };
+    
+    PropertyList(const Rect &rect, PropertyListDelegate *propertiesDelegate, Font *font);
+    ~PropertyList();
+    
+    void AddTextProperty(const String &propertyName, const String &currentText, editableType propEditType = PROPERTY_IS_EDITABLE);
+    void AddIntProperty(const String &propertyName, int32 currentIntValue, editableType propEditType = PROPERTY_IS_EDITABLE);
+    void AddFloatProperty(const String &propertyName, float32 currentFloatValue, editableType propEditType = PROPERTY_IS_EDITABLE);
+
+    void AddFilepathProperty(const String &propertyName, const String &currentFilepath, editableType propEditType = PROPERTY_IS_EDITABLE);
+
+    void SetTextPropertyValue(const String &propertyName, const String &newText);
+    void SetIntPropertyValue(const String &propertyName, int32 newIntValue);
+    void SetFloatPropertyValue(const String &propertyName, float32 newFloatValue);
+    
+    virtual int32 ElementsCount(UIList *forList);
+	virtual UIListCell *CellAtIndex(UIList *forList, int32 index);
+	virtual int32 CellWidth(UIList *forList, int32 index)//calls only for horizontal orientation
+	{return 20;};
+	virtual int32 CellHeight(UIList *forList, int32 index);//calls only for vertical orientation
+	virtual void OnCellSelected(UIList *forList, UIListCell *selectedCell);
+    
+    virtual void OnPropertyChanged(PropertyCellData *changedProperty);
+
+    
+protected:
+    
+    void AddProperty(PropertyCellData *newProp, const String &propertyName, editableType propEditType);
+    PropertyCellData *PropertyByName(const String &propertyName);
+    
+    PropertyListDelegate *delegate;
+    UIList *propsList;
+    Vector<PropertyCellData*> props;
+    Map<String, PropertyCellData*> propsMap;
+    Font *currentFont;
+};
+
+#endif
