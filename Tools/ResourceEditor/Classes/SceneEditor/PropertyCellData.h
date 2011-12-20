526590c56d1f886ca2ee447350d1a401baa1ac81
Hottych <alexey@davaconsulting.com>
2011-12-20 20:26:50 +0200

    Add: Editor: Property list added

---
+++ Tools/ResourceEditor/Classes/SceneEditor/PropertyCellData.h
@@ -0,0 +1,56 @@
+/*
+ *  PropertyValue.h
+ *  SniperEditorMacOS
+ *
+ *  Created by Alexey Prosin on 12/14/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef PROPERTY_VALUE
+#define PROPERTY_VALUE
+
+#include "DAVAEngine.h"
+
+using namespace DAVA;
+class PropertyCell;
+class PropertyCellData : public BaseObject
+{
+public:
+    enum ValueType 
+    {
+        PROP_VALUE_STRING = 0,
+        PROP_VALUE_INTEGER,
+        PROP_VALUE_FLOAT,
+        
+        PROP_VALUE_COUNT
+    };
+    
+    PropertyCellData(int _valueType);
+
+    int32 GetValueType();
+
+    
+    int32 GetInt();
+    float32 GetFloat();
+    String GetString();
+
+    void SetInt(int32 newInt);
+    void SetFloat(float32 newFloat);
+    void SetString(const String& newString);
+    
+    int32 cellType;
+    String key;
+    bool isEditable;
+    int index;
+    
+    PropertyCell *currentCell;
+protected:
+    int32 valueType;
+    
+    int32 intValue;
+    float32 floatValue;
+    String stringValue;
+};
+
+#endif
