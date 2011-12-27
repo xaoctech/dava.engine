266a4aa00794b8c44684c53d4c4130f534ba6489
VK <klesch@VKs-MacBook-Pro.local>
2011-12-27 17:48:41 +0200

    Remove: old commented code
    Removed old unused code
    
    
    Former-commit-id: 9d02a06b7a3723f745ac2296bb72ed5998ad76bc

--- Tools/ResourceEditor/Classes/SceneEditor/CreateNodeDialog.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/CreateNodeDialog.cpp
@@ -14,47 +14,31 @@ CreateNodeDialog::CreateNodeDialog(const Rect & rect)
     
     dialogDelegate = NULL;
     
-//    Rect r;
-//    r.dx = rect.dx / 2;
-//    r.dy = rect.dy / 2;
-//    
-//    r.x = rect.x + r.dx / 2;
-//    r.y = rect.y + r.dy / 2;
-//    UIControl *panel = ControlsFactory::CreatePanelControl(r);
-//    panel->SetInputEnabled(false, false);
-//    AddControl(panel);
-    
     header = new UIStaticText(Rect(0, 0, rect.dx, BUTTON_HEIGHT));
     Font *font = ControlsFactory::CreateFontLight();
     header->SetFont(font);
     header->SetAlign(ALIGN_HCENTER | ALIGN_VCENTER);
     SafeRelease(font);
     AddControl(header);
-//    panel->AddControl(header);
     
     int32 buttonY = rect.dy - BUTTON_HEIGHT - 2;
     int32 buttonX = (rect.dx - BUTTON_WIDTH * 2 - 2) / 2;
     
     UIButton *btnCancel = ControlsFactory::CreateButton(Rect(buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT), L"Cancel");
     btnCancel->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &CreateNodeDialog::OnCancel));
-//    panel->AddControl(btnCancel);
     AddControl(btnCancel);
     
     buttonX += BUTTON_WIDTH + 1;
     UIButton *btnOk = ControlsFactory::CreateButton(Rect(buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT), L"Ok");
     btnOk->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &CreateNodeDialog::OnOk));
-//    panel->AddControl(btnOk);
     AddControl(btnOk);
     
     Rect propertyRect(0, BUTTON_HEIGHT, rect.dx, buttonY - BUTTON_HEIGHT);
     propertyList = new PropertyList(propertyRect, this);
-//    panel->AddControl(propertyList);
     AddControl(propertyList);
-
     
     SafeRelease(btnCancel);
     SafeRelease(btnOk);
-//    SafeRelease(panel);
 }
     
 CreateNodeDialog::~CreateNodeDialog()
@@ -86,27 +70,6 @@ void CreateNodeDialog::OnOk(BaseObject * object, void * userData, void * callerD
         dialogDelegate->DialogClosed(RCODE_OK);
     }
 }
-
-//void CreateNodeDialog::OnStringPropertyChanged(PropertyList *forList, const String &forKey, const String &newValue)
-//{
-//    
-//}
-//
-//void CreateNodeDialog::OnFloatPropertyChanged(PropertyList *forList, const String &forKey, float newValue)
-//{
-//    
-//}
-//
-//void CreateNodeDialog::OnIntPropertyChanged(PropertyList *forList, const String &forKey, int newValue)
-//{
-//    
-//}
-//
-//void CreateNodeDialog::OnBoolPropertyChanged(PropertyList *forList, const String &forKey, bool newValue)
-//{
-//    
-//}
-
 
 SceneNode * CreateNodeDialog::GetSceneNode()
 {
