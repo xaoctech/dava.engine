537b12933badd7902d8945681a0aa11bb1e96d7d
binaryzebra <vitaliy@davaconsulting.com>
2012-01-10 16:53:31 +0300

    Fix: Selection now uses corner boxes again

--- Tools/ResourceEditor/Classes/EditorScene.cpp
+++ Tools/ResourceEditor/Classes/EditorScene.cpp
@@ -89,8 +89,8 @@ void EditorScene::CheckNodes(SceneNode * curr)
 void EditorScene::TrySelection(Vector3 from, Vector3 direction)
 {
 	if (selection)
-	//	selection->SetDebugFlags(selection->GetDebugFlags() & (~SceneNode::DEBUG_DRAW_AABOX_CORNERS));
-		selection->SetDebugFlags(selection->GetDebugFlags() & (~SceneNode::DEBUG_DRAW_AABBOX));
+		selection->SetDebugFlags(selection->GetDebugFlags() & (~SceneNode::DEBUG_DRAW_AABOX_CORNERS));
+	//	selection->SetDebugFlags(selection->GetDebugFlags() & (~SceneNode::DEBUG_DRAW_AABBOX));
 
 	btVector3 pos(from.x, from.y, from.z);
     btVector3 to(direction.x, direction.y, direction.z);
@@ -125,8 +125,8 @@ void EditorScene::TrySelection(Vector3 from, Vector3 direction)
 		selection = FindSelected(this, coll);
 	
 		if(selection)
-//			selection->SetDebugFlags(selection->GetDebugFlags() | (SceneNode::DEBUG_DRAW_AABOX_CORNERS));
-			selection->SetDebugFlags(selection->GetDebugFlags() | (SceneNode::DEBUG_DRAW_AABBOX));
+			selection->SetDebugFlags(selection->GetDebugFlags() | (SceneNode::DEBUG_DRAW_AABOX_CORNERS));
+//			selection->SetDebugFlags(selection->GetDebugFlags() | (SceneNode::DEBUG_DRAW_AABBOX));
 	}
 	else 
 	{
