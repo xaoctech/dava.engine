178da83de3879cdc58d0b742dd3f7513f2a9bcbc
VK <klesch@VKs-MacBook-Pro.local>
2012-01-19 17:20:17 +0200

    Add: lib png compiled as separate project for iOS and macos
    1. libpng projects
    2. Added viewport size selection for editor
    3. Added autosave.
    4. Added language selection
    
    
    Former-commit-id: 0444867acb59f1ee1f0826e4147fe8f81a36551d

--- Tools/ResourceEditor/Classes/SceneEditor/SceneEditorScreenMain.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/SceneEditorScreenMain.cpp
@@ -88,6 +88,8 @@ void SceneEditorScreenMain::LoadResources()
     AddControl(dataGraphButton);
     
     InitializeBodyList();
+    
+    SetupAnimation();
 }
 
 void SceneEditorScreenMain::UnloadResources()
@@ -160,6 +162,8 @@ void SceneEditorScreenMain::CreateTopMenu()
 #endif //#ifdef __DAVAENGINE_BEAST__
 	x += dx;
 	btnLandscape = ControlsFactory::CreateButton(Rect(x, y, dx, dy), LocalizedString(L"menu.landscape"));
+	x += dx;
+	btnViewPortSize = ControlsFactory::CreateButton(Rect(x, y, dx, dy), LocalizedString(L"menu.viewport"));
     
     
 
@@ -174,6 +178,7 @@ void SceneEditorScreenMain::CreateTopMenu()
 	AddControl(btnBeast);
 #endif
     AddControl(btnLandscape);
+    AddControl(btnViewPortSize);
     
 
     btnOpen->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &SceneEditorScreenMain::OnOpenPressed));
@@ -187,6 +192,7 @@ void SceneEditorScreenMain::CreateTopMenu()
 	btnBeast->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &SceneEditorScreenMain::OnBeastPressed));
 #endif// #ifdef __DAVAENGINE_BEAST__
 	btnLandscape->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &SceneEditorScreenMain::OnLandscapePressed));
+	btnViewPortSize->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &SceneEditorScreenMain::OnViewPortSize));
 }
 
 void SceneEditorScreenMain::ReleaseTopMenu()
@@ -202,6 +208,7 @@ void SceneEditorScreenMain::ReleaseTopMenu()
 	SafeRelease(btnBeast);
 #endif// #ifdef __DAVAENGINE_BEAST__
     SafeRelease(btnLandscape);
+    SafeRelease(btnViewPortSize);
 }
 
 void SceneEditorScreenMain::AddLineControl(DAVA::Rect r)
@@ -576,18 +583,21 @@ void SceneEditorScreenMain::OnCloseBody(BaseObject * owner, void * userData, voi
 
 void SceneEditorScreenMain::OnLibraryPressed(DAVA::BaseObject *obj, void *, void *)
 {
-    if(libraryControl->GetParent())
-    {
-        RemoveControl(libraryControl);
-    }
-    else
-    {
-        AddControl(libraryControl);
-    }
-
     BodyItem *iBody = FindCurrentBody();
-    iBody->bodyControl->ShowProperties(false);
-    iBody->bodyControl->UpdateLibraryState(libraryControl->GetParent(), libraryControl->GetRect().dx);
+    if(!iBody->bodyControl->ControlsAreLocked())
+    {
+        if(libraryControl->GetParent())
+        {
+            RemoveControl(libraryControl);
+        }
+        else
+        {
+            AddControl(libraryControl);
+        }
+        
+        iBody->bodyControl->ShowProperties(false);
+        iBody->bodyControl->UpdateLibraryState(libraryControl->GetParent(), libraryControl->GetRect().dx);
+    }
 }
 
 SceneEditorScreenMain::BodyItem * SceneEditorScreenMain::FindCurrentBody()
@@ -699,6 +709,21 @@ void SceneEditorScreenMain::MenuSelected(int32 menuID, int32 itemID)
             break;
         }
             
+        case MENUID_VIEWPORT:
+        {
+            BodyItem *iBody = FindCurrentBody();
+            
+            if(libraryControl->GetParent())
+            {
+                RemoveControl(libraryControl);
+            }
+            
+            iBody->bodyControl->UpdateLibraryState(libraryControl->GetParent(), libraryControl->GetRect().dx);
+            
+            iBody->bodyControl->SetViewPortSize(itemID);
+            break;
+        }
+            
             
         default:
             break;
@@ -778,6 +803,32 @@ WideString SceneEditorScreenMain::MenuItemText(int32 menuID, int32 itemID)
             break;
         }
             
+        case MENUID_VIEWPORT:
+        {
+            switch (itemID)
+            {
+                case EditorBodyControl::EVPID_IPHONE:
+                    text = LocalizedString("menu.viewport.iphone");
+                    break;
+
+                case EditorBodyControl::EVPID_RETINA:
+                    text = LocalizedString("menu.viewport.retina");
+                    break;
+
+                case EditorBodyControl::EVPID_IPAD:
+                    text = LocalizedString("menu.viewport.ipad");
+                    break;
+
+                case EditorBodyControl::EVPID_DEFAULT:
+                    text = LocalizedString("menu.viewport.default");
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        }
+            
         default:
             break;
     }
@@ -802,6 +853,10 @@ int32 SceneEditorScreenMain::MenuItemsCount(int32 menuID)
             retCount = ENMID_COUNT;
             break;
         }
+            
+        case MENUID_VIEWPORT:
+            retCount = EditorBodyControl::EVPID_COUNT;
+            break;
             
         default:
             break;
@@ -884,3 +939,34 @@ void SceneEditorScreenMain::OnSettingsPressed(BaseObject * obj, void *, void *)
     }
 }
 
+void SceneEditorScreenMain::AutoSaveLevel(BaseObject * obj, void *, void *)
+{
+    time_t now = time(0);
+    tm* utcTime = localtime(&now);
+    
+    String pathToFile = EditorSettings::Instance()->GetDataSourcePath();
+    pathToFile += Format("AutoSave_%04d.%02d.%02d_%02d_%02d.sc2",   utcTime->tm_year + 1900, utcTime->tm_mon + 1, utcTime->tm_mday, 
+                                                                utcTime->tm_hour, utcTime->tm_min);
+    
+    BodyItem *iBody = bodies[0];
+    Scene * scene = iBody->bodyControl->GetScene();
+    SceneFileV2 * file = new SceneFileV2();
+    file->EnableDebugLog(false);
+    file->SaveScene(pathToFile, scene);
+    SafeRelease(file);
+    
+    SetupAnimation();
+}
+
+void SceneEditorScreenMain::SetupAnimation()
+{
+    float32 minutes = EditorSettings::Instance()->GetAutosaveTime();
+    Animation * anim = WaitAnimation(minutes * 60.f); 
+    anim->AddEvent(Animation::EVENT_ANIMATION_END, Message(this, &SceneEditorScreenMain::AutoSaveLevel));
+}
+
+void SceneEditorScreenMain::OnViewPortSize(DAVA::BaseObject *obj, void *, void *)
+{
+    menuPopup->InitControl(MENUID_VIEWPORT, btnViewPortSize->GetRect());
+    AddControl(menuPopup);
+}
