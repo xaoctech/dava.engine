bd7b77058d1ec99d59c7b11d00b3311c855d24ec
VK <klesch@VKs-MacBook-Pro.local>
2012-02-09 18:51:34 +0300

    Add: xmllib for ios & macos

---
+++ Libs/xmllib/ios/xmllib_ios/xmllib_ios_sim/xmllib_ios_sim.m
@@ -0,0 +1,23 @@
+//
+//  xmllib_ios_sim.m
+//  xmllib_ios_sim
+//
+//  Created by VK on 2/9/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "xmllib_ios_sim.h"
+
+@implementation xmllib_ios_sim
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
