95cf9b02b38ea7fda57f8a2f9b0d847c70647217
VK <klesch@VKs-MacBook-Pro.local>
2012-03-19 12:41:10 +0300

    Add: Lost/invalidate

--- Sources/Internal/Render/Shader.h
+++ Sources/Internal/Render/Shader.h
@@ -104,6 +104,14 @@ public:
     //virtual uint32 GetVertexFormat();
     //virtual uint32 GetAttributeIndex(eVertexFormat fmt);
     
+#if defined(__DAVAENGINE_ANDROID__) || defined (__DAVAENGINE_MACOS__)
+	virtual void SaveToSystemMemory();
+	virtual void Lost();
+	virtual void Invalidate();
+    String relativeFileName;
+#endif //#if defined(__DAVAENGINE_ANDROID__) 
+
+    
 private:
 #if defined(__DAVAENGINE_DIRECTX9__)
     
