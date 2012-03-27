4ae6d107273f7873b1ef5a74b590011377082c9b
binaryzebra <vitaliy@davaconsulting.com>
2012-03-27 18:22:07 +0400

    Add: First iteration of intensity for dynamic lights

--- Sources/Internal/Render/Material.cpp
+++ Sources/Internal/Render/Material.cpp
@@ -176,6 +176,7 @@ void Material::RebuildShader()
     uniformMaterialLightDiffuseColor = -1;
     uniformMaterialLightSpecularColor = -1;
     uniformMaterialSpecularShininess = -1;
+    uniformMaterialLightIntensity0 = -1;
     
     String shaderCombileCombo = "MATERIAL_TEXTURE";
     
@@ -240,6 +241,7 @@ void Material::RebuildShader()
             uniformMaterialLightDiffuseColor = shader->FindUniformLocationByName("materialLightDiffuseColor");
             uniformMaterialLightSpecularColor = shader->FindUniformLocationByName("materialLightSpecularColor");
             uniformMaterialSpecularShininess = shader->FindUniformLocationByName("materialSpecularShininess");
+            uniformMaterialLightIntensity0 = shader->FindUniformLocationByName("lightIntensity0");
             break;
         case MATERIAL_PIXEL_LIT_NORMAL_DIFFUSE:
         case MATERIAL_PIXEL_LIT_NORMAL_DIFFUSE_SPECULAR:
@@ -490,6 +492,11 @@ void Material::Draw(PolygonGroup * group, InstanceMaterialState * instanceMateri
             if (uniformMaterialSpecularShininess != -1)
             {
                 shader->SetUniformValue(uniformMaterialSpecularShininess, shininess);
+            }
+            
+            if (uniformMaterialLightIntensity0 != -1)
+            {
+                shader->SetUniformValue(uniformMaterialLightIntensity0, lightNode0->GetIntensity());
             }
         }
     }
