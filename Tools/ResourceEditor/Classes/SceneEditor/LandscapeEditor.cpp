c75e5eb5593908c140db911cbbeb5af21f8c144a
klesch <victor@davaconsulting.com>
2012-03-28 17:58:48 +0300

    Fix: changed min and max values for landscape editor

--- Tools/ResourceEditor/Classes/SceneEditor/LandscapeEditor.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/LandscapeEditor.cpp
@@ -385,9 +385,9 @@ void LandscapeEditor::UpdateTileMask()
 	tileMaskEditorShader->SetUniformValue(colorTypeUniform, colorType);
 	int32 intensityUniform = tileMaskEditorShader->FindUniformLocationByName("intensity");
     
-    float32 intension = currentTool->intension * currentTool->intension;
-//	tileMaskEditorShader->SetUniformValue(intensityUniform, currentTool->intension);
-	tileMaskEditorShader->SetUniformValue(intensityUniform, intension);
+//    float32 intension = currentTool->intension * currentTool->intension;
+	tileMaskEditorShader->SetUniformValue(intensityUniform, currentTool->intension);
+//	tileMaskEditorShader->SetUniformValue(intensityUniform, intension);
     
 	RenderManager::Instance()->HWDrawArrays(PRIMITIVETYPE_TRIANGLESTRIP, 0, 4);
     
@@ -404,7 +404,7 @@ void LandscapeEditor::UpdateTileMaskTool()
 {
 	if(currentTool && currentTool->sprite && currentTool->zoom)
 	{
-		float32 scaleSize = currentTool->sprite->GetWidth() * ( currentTool->zoom );
+		float32 scaleSize = currentTool->sprite->GetWidth() * (currentTool->zoom * currentTool->zoom);
 		Vector2 deltaPos = endPoint - startPoint;
 		{
 			Vector2 pos = startPoint - Vector2(scaleSize, scaleSize)/2;
