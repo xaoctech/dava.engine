ac97a3011d8cbd4a5ced358567952367605c08f4
klesch <victor@davaconsulting.com>
2012-05-23 13:59:19 +0200

    Add: added teamcity scripts for ResourceEditor

--- Tools/ResourceEditor/Teamcity/verifyAndInstMagick.sh
+++ Tools/ResourceEditor/Teamcity/verifyAndInstMagick.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-DIR="ImageMagick-6.6.9"
+DIR="./../../../Libs/ImageMagick-6.6.9"
 ARCH="ImageMagick-6.6.9-0.tar.gz"
 
 if [ -d  $DIR ]
 then
 	echo "Magick exists"
 else
+	cd ./../../../Libs
 	tar -xf $ARCH
 	sh install_IM.sh
 fi
