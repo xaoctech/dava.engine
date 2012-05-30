2ae6f8b6519e163e3969781931758e59d6eb7bbf
klesch <victor@davaconsulting.com>
2012-05-30 11:04:36 +0300

    Added template specification for command line building

---
+++ Projects/PerformanceTest/test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by VK on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface test : SenTestCase
+
+@end
