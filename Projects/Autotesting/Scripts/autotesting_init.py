c9b8fe55f0bab04df41b57c4ee3e5cbb7ff72995
Dmitry Shpakov <dmitryshpakov@Mumzas-MacBook-Pro.local>
2012-06-13 16:57:30 +0300

    Fix: autotesting python scripts
    Former-commit-id: 13c07e61ebb5cbbf239c425432d63fc00f46c01e

---
+++ Projects/Autotesting/Scripts/autotesting_init.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2.5
+#
+#  autotesting.py
+#  DAVA SDK
+#
+#  Created by Dmitry Shpakov on 6/13/12.
+#  Copyright (c) 2008 DAVA Consulting, LLC. All rights reserved.
+#
+
+import os;
+import sys;
+import os.path;
+import string;
+import platform;
+import shutil;
+import random;
+
+print "*** DAVA Initializing autotesting"
+
+print "platform.system: " + platform.system()
+print sys.argv
+
+index_OS = 1;
+index_Project = 2;
+
+currentDir = os.getcwd(); 
+frameworkDir =  os.path.realpath(currentDir + "/../../../")
+projectDir = os.path.realpath(currentDir + "/../../../../" + sys.argv[index_Project])
+print "Framework directory:" + frameworkDir
+print "Project directory:" + projectDir
+
+autotestingConfigSrcPath = os.path.realpath(currentDir + "/../Data/Config.h")
+autotestingConfigDestPath = os.path.realpath(frameworkDir + "/Sources/Internal/Autotesting/Config.h")
+if os.path.exists(autotestingConfigDestPath):    
+    print "delete " + autotestingConfigDestPath
+    os.remove(autotestingConfigDestPath)
+print "copy " + autotestingConfigSrcPath + " to " + autotestingConfigDestPath
+shutil.copy(autotestingConfigSrcPath, autotestingConfigDestPath)
+
+autotestingSrcFolder = os.path.realpath(projectDir + "/Autotesting")
+autotestingDestFolder = os.path.realpath(projectDir + "/Data/Autotesting")
+    
+if (platform.system() == "Darwin"):
+    if (sys.argv[index_OS] == "iOS"):
+        print "copy iOS scripts from " + currentDir + " to " + autotestingSrcFolder
+        
+        scripts = ["/runOnDevice.sh", "/floatsign.sh", "/transporter_chief.rb", "/testRun.js"]
+        for scriptName in scripts:
+            scriptSrcPath = os.path.realpath(currentDir + scriptName)
+            scriptDestPath = os.path.realpath(autotestingSrcFolder + scriptName)
+            if os.path.exists(scriptDestPath):    
+                print "delete " + scriptDestPath
+                os.remove(scriptDestPath)
+            print "copy " + scriptSrcPath + " to " + scriptDestPath
+            shutil.copy(scriptSrcPath, scriptDestPath)
+
+if os.path.exists(autotestingDestFolder):    
+    print "Autotesting already exists - delete " + autotestingDestFolder
+    shutil.rmtree(autotestingDestFolder)
+print "copy " + autotestingSrcFolder + " to " + autotestingDestFolder
+shutil.copytree(autotestingSrcFolder, autotestingDestFolder)
+
+randomNumber = random.randint(0, 100000)
+print "randomNumber: " + str(randomNumber)
+idFilePath = os.path.realpath(projectDir + "/Data/Autotesting/id.txt")
+print "write to file " + idFilePath
+file=open(idFilePath,'w')
+file.write(str(randomNumber))
+file.close()
+   
+print "*** DAVA Initialized autotesting"
