fd661357dd132dbb0519d1b436e552c6a9236469
Dmitry Shpakov <mumza.12@gmail.com>
2012-06-13 11:29:28 +0300

    Add: autotesting system can run multiple tests without redeploy. tested on windows
    Former-commit-id: 911822e73741d53c7770d454c823b1cccce7bf40

--- Projects/Autotesting/Scripts/autotesting_windows.bat
+++ Projects/Autotesting/Scripts/autotesting_windows.bat
@@ -4,14 +4,32 @@ echo "go to %1"
 cd ..\..\..\..\%1
 echo "copy %3\%2.exe to current folder"
 copy /Y %3\%2.exe %2.exe
-for /f %%f in ('dir /b Tests\') do (
-echo "copy Tests\%%f to Data\Tests\autotesting.yaml"
-copy /Y Tests\%%f Data\Tests\autotesting.yaml
+echo "create Data\Autotesting"
+md Data\Autotesting
+md Data\Autotesting\Actions
+md Data\Autotesting\Tests
+
+cd Data\Autotesting
+del /f id.txt
+echo %random%>> id.txt
+cd ..\..
+
+for /f %%f in ('dir /b Autotesting\Tests\') do (
+echo "copy .\Autotesting\Tests\%%f to .\Data\Autotesting\Tests\%%f"
+copy /Y .\Autotesting\Tests\%%f .\Data\Autotesting\Tests\%%f
+)
+for /f %%f in ('dir /b Autotesting\Actions\') do (
+echo "copy .\Autotesting\Actions\%%f to .\Data\Autotesting\Actions\%%f"
+copy /Y .\Autotesting\Actions\%%f .\Data\Autotesting\Actions\%%f
+)
+for /f %%f in ('dir /b Autotesting\Tests\') do (
 echo "run %2.exe"
 start /WAIT %2.exe
 )
+
+echo "cleanup: delete Data\Autotesting and %2.exe"
+rd /s /q Data\Autotesting
 del /F %2.exe
-del /F Data\Tests\autotesting.yaml
 
 echo "go to dava.framework"
 cd ..\dava.framework\Projects\Autotesting\Scripts
