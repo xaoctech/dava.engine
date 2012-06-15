89c768e87d427f75af83d7ff69f04d9bf2be780d
d_shpakov <d_shpakov@wargaming.net>
2012-06-15 15:52:53 +0300

    removed unused autotesting scripts
    Former-commit-id: 4495be57e59481265e142f9d01c398a1f3628727

--- Projects/Autotesting/Scripts/copy_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#  copy_config.sh
-
-echo "copy_config.sh"
-cp -Rf ../../../Sources/Internal/Autotesting/Config.h ../Data/Config_backup.h
-cp -Rf ../Data/Config.h ../../../Sources/Internal/Autotesting/Config.h
-echo "copy_config.sh done"
