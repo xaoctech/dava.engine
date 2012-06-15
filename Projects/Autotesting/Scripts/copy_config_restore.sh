ec40847e07f7f20bc9551a26613699b8ca843939
d_shpakov <d_shpakov@wargaming.net>
2012-06-15 17:14:19 +0300

    deleted unused scripts

--- Projects/Autotesting/Scripts/copy_config_restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#  copy_config_restore.sh
-
-echo "copy_config_restore.sh"
-cp -Rf ../Data/Config_backup.h ../../../Sources/Internal/Autotesting/Config.h
-echo "copy_config_restore.sh done"
