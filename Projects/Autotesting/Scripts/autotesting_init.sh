81cbc583ad45591ef84d654dbc4915e6248b2507
d_shpakov <d_shpakov@wargaming.net>
2012-06-15 15:52:53 +0300

    removed unused autotesting scripts

--- Projects/Autotesting/Scripts/autotesting_init.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash 
-#  autotesting_init.sh
-
-echo "autotesting_init.sh"
-# copy Config.h to enable autotesting in framework
-cp -Rf ../Data/Config.h ../../../Sources/Internal/Autotesting/Config.h
--- Projects/Autotesting/Scripts/copy_config.bat
+++
@@ -1,5 +0,0 @@
-echo off
-echo "copy_config.bat"
-echo "copy Config.h"
-copy /Y ..\Data\Config.h ..\..\..\Sources\Internal\Autotesting\Config.h
-echo "copy_config.bat finished"
