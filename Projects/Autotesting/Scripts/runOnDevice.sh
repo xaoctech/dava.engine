c2e2dd4a71ed2a5601ed8995d32e939ae660a9ba
d_shpakov <d_shpakov@wargaming.net>
2012-07-06 14:47:18 +0300

    Fix: UIViewer scheme and app name. Add: Autotesting script gets device id from system_profiler
    Former-commit-id: 08d71f537e23cf6139fe9fc0be6fe7bef91ca3f1

--- Projects/Autotesting/Scripts/runOnDevice.sh
+++ Projects/Autotesting/Scripts/runOnDevice.sh
@@ -1,7 +1,10 @@
 # !/bin/bash
 
 # $1 - target product name
-# $2 - device id
 
-echo "run $1 on device $2"
-instruments -w $2 -t /Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate "$1" -e UIASCRIPT testRun.js
+echo "get udid device" 
+UDID_device=`system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' -e '/iPod/,/Serial/p' | grep "Serial Number:" | awk -F ": " '{print $2}'`
+echo "udid is ${UDID_device}"
+
+echo "run $1 on device"
+instruments -w ${UDID_device} -t /Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate "$1" -e UIASCRIPT testRun.js
