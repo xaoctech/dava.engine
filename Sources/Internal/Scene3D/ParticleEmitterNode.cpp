ac3e6eabf9d9dd324ddc0f089fff7c44c0995549
dizzer <dizzerr@gmail.com>
2012-07-13 16:53:35 +0300

    Add: interface for 3d particles in ResourceEditor
    Former-commit-id: 2e3c59e3c7bcdf6eff241ece4240f1b9ca023e96

--- Sources/Internal/Scene3D/ParticleEmitterNode.cpp
+++ Sources/Internal/Scene3D/ParticleEmitterNode.cpp
@@ -2,6 +2,7 @@
 #include "Render/RenderManager.h"
 #include "Scene3D/Scene.h"
 #include "Render/Material.h"
+#include "Particles/ParticleEmitter3D.h"
 
 namespace DAVA
 {
@@ -11,17 +12,12 @@ REGISTER_CLASS(ParticleEmitterNode);
 ParticleEmitterNode::ParticleEmitterNode()
 :	emitter(0)
 {
+	SetName("Particle emitter");
 }
 
 ParticleEmitterNode::~ParticleEmitterNode()
 {
 	SafeRelease(emitter);
-}
-
-void ParticleEmitterNode::SetEmitter(ParticleEmitter3D * _emitter)
-{
-	SafeRelease(emitter);
-	emitter = SafeRetain(_emitter);
 }
 
 void ParticleEmitterNode::Update(float32 timeElapsed)
@@ -41,16 +37,36 @@ void ParticleEmitterNode::Draw()
 	{
 		eBlendMode sblend = RenderManager::Instance()->GetSrcBlend();
 		eBlendMode dblend = RenderManager::Instance()->GetDestBlend();
+		RenderManager::Instance()->SetState(RenderStateBlock::DEFAULT_3D_STATE);
 
 		const Matrix4 & mv = RenderManager::Instance()->GetMatrix(RenderManager::MATRIX_MODELVIEW);
 		Vector3 up(mv._01, mv._11, mv._21);
 		Vector3 left(mv._00, mv._10, mv._20);
 
-		emitter->Draw(up, left);
+		ParticleEmitter3D * emitter3D = static_cast<ParticleEmitter3D*>(emitter);
+		emitter3D->Draw(up, left);
 
-		RenderManager::Instance()->SetState(RenderStateBlock::DEFAULT_3D_STATE);
+		
 		RenderManager::Instance()->SetBlendMode(sblend, dblend);
 	}
+}
+
+void ParticleEmitterNode::LoadFromYaml(String _yamlPath)
+{
+	yamlPath = _yamlPath;
+	SafeRelease(emitter);
+	emitter = new ParticleEmitter3D();
+	emitter->LoadFromYaml(yamlPath);
+}
+
+String ParticleEmitterNode::GetYamlPath()
+{
+	return yamlPath;
+}
+
+ParticleEmitter * ParticleEmitterNode::GetEmitter()
+{
+	return emitter;
 }
 
 };
