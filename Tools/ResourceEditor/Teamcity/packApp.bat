5818787e48e76828ce345ce3cc4181f34076a9e0
klesch <victor@davaconsulting.com>
2012-07-26 15:24:54 +0300

    Fix: added Qt dlls for win32 pack

--- Tools/ResourceEditor/Teamcity/packApp.bat
+++ Tools/ResourceEditor/Teamcity/packApp.bat
@@ -8,7 +8,9 @@ mkdir app\dava.resourceeditor.beast
 xcopy /e ..\Data\*.* app\1\2\3\Data 
 xcopy *.exe app\1\2\3
 xcopy ..\glew32.dll app\1\2\3
+xcopy %QT_HOME%\Desktop\Qt\4.8.1\msvc2010\lib\QtCore4.dll app\1\2\3
+xcopy %QT_HOME%\Desktop\Qt\4.8.1\msvc2010\lib\QtGui4.dll app\1\2\3
 xcopy ..\..\..\..\dava.resourceeditor.beast\beast\bin\beast32.dll app\1\2\3
 xcopy /e ..\..\..\..\dava.resourceeditor.beast\*.* app\dava.resourceeditor.beast
 
-wzzip -p -r ResourceEditor_win.zip app\*.*
+wzzip -p -r ResourceEditor_win.zip app\*.*	
