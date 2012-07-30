28a062a869fe4491fe92bfff3e16919ae6e8d9d4
dizzer <dizzerr@gmail.com>
2012-07-30 19:51:33 +0300

    Add: more render options
    Former-commit-id: 1bde981f5c4f5efaf7a4025e62f2b29cf09cbe93

--- Sources/Internal/Render/Material.cpp
+++ Sources/Internal/Render/Material.cpp
@@ -607,6 +607,11 @@ void Material::PrepareRenderState()
 
 void Material::Draw(PolygonGroup * group, InstanceMaterialState * instanceMaterialState)
 {
+	if(!RenderManager::Instance()->GetOptions()->IsOptionEnabled(RenderOptions::MATERIAL_DRAW))
+	{
+		return;
+	}
+
 	if(isOpaque && !RenderManager::Instance()->GetOptions()->IsOptionEnabled(RenderOptions::OPAQUE_DRAW))
 	{
 		return;
