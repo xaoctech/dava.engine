688e9bde86f8491ff8de7da3a74409c2223b5b75
Yury Danilov <yury.danilov@davaconsulting.com>
2012-08-01 17:22:38 +0300

    Fixed Collision flags in resource editor

--- Tools/ResourceEditor/Classes/SceneEditor/NodesPropertyControl.h
+++ Tools/ResourceEditor/Classes/SceneEditor/NodesPropertyControl.h
@@ -83,6 +83,7 @@ protected:
     bool createNodeProperties;
     
     UIButton *btnPlus;
+	UIButton *btnPlusCollision;
     UIButton *btnMinus;
 
     void OnSetDistancesForLodNodes(BaseObject * object, void * userData, void * callerData);
@@ -90,6 +91,7 @@ protected:
     
     void OnPlus(BaseObject * object, void * userData, void * callerData);
     void OnMinus(BaseObject * object, void * userData, void * callerData);
+	void OnPlusCollision(BaseObject * object, void * userData, void * callerData);
     
     CreatePropertyControl *propControl;
     SceneNode *currentSceneNode;
