e8735302219d2c170099f473d2462f0f8bc0ece7
klesch <victor@davaconsulting.com>
2012-09-04 12:34:01 +0300

    Fix: move resizing of particleemittercontrol from SetSize() to speed up resizing of widget
    Former-commit-id: 9d4f9213dbbf6982ac1c0f8bf15a26f870843942

--- Tools/ResourceEditor/Classes/SceneEditor/SceneEditorScreenMain.cpp
+++ Tools/ResourceEditor/Classes/SceneEditor/SceneEditorScreenMain.cpp
@@ -1281,6 +1281,9 @@ void SceneEditorScreenMain::EditParticleEmitter(ParticleEmitterNode * emitter)
 	//BodyItem *iBody = FindCurrentBody();
 	if (!particlesEditor->GetParent())
 	{
+		SafeRelease(particlesEditor);
+		particlesEditor = new ParticlesEditorControl();
+
 		particlesEditor->SetEmitter(emitter->GetEmitter());
 		AddControl(particlesEditor);
 	}
@@ -1603,11 +1606,11 @@ void SceneEditorScreenMain::SetSize(const Vector2 &newSize)
         bodies[i]->bodyControl->SetSize(bodySize);
     }
 
-    if(particlesEditor && !particlesEditor->GetParent())
-    {
-        SafeRelease(particlesEditor);
-        particlesEditor = new ParticlesEditorControl();
-    }
+//     if(particlesEditor && !particlesEditor->GetParent())
+//     {
+//         SafeRelease(particlesEditor);
+//         particlesEditor = new ParticlesEditorControl();
+//     }
     
 }
 #endif //#if defined (DAVA_QT)
