e5b34b8d42c45dea8f1ea980b31de64b18027e7a
klesch <victor@davaconsulting.com>
2012-09-12 13:20:38 +0300

    Version incremented
    Former-commit-id: 895e1bb5214119f92969d3ce90fc667e88adea42

--- Tools/ResourceEditor/Classes/FrameworkMain.cpp
+++ Tools/ResourceEditor/Classes/FrameworkMain.cpp
@@ -19,7 +19,7 @@
 
 using namespace DAVA;
 
-#define VERSION     "0.0.22"
+#define VERSION     "0.0.23"
  
 //void EntityTest();
 
@@ -51,7 +51,6 @@ void PrintUsage()
     printf("-sceneexporter -clean /Users/User/Project/Data/3d/\n");
     printf("-sceneexporter -export -indir /Users/User/Project/DataSource/3d -outdir /Users/User/Project/Data/3d/ -processdir Maps/objects/\n");
     printf("-sceneexporter -export -indir /Users/User/Project/DataSource/3d -outdir /Users/User/Project/Data/3d/ -processfile Maps/level.sc2 -force\n");
-    
 }
 
 
