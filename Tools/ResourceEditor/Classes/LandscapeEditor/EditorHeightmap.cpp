3ecd511518511e50f3fd8baf7239692b71edc658
klesch <victor@davaconsulting.com>
2012-09-12 15:36:42 +0300

    Fix: fixed downscale

--- Tools/ResourceEditor/Classes/LandscapeEditor/EditorHeightmap.cpp
+++ Tools/ResourceEditor/Classes/LandscapeEditor/EditorHeightmap.cpp
@@ -121,7 +121,7 @@ uint16 EditorHeightmap::GetVerticalValue(int32 posY, int32 muliplier)
     
     int32 firstY = posY * muliplier;
     int32 lastY = firstY + muliplier;
-    int32 index = savedHeightmap->Size() - 1;
+    int32 index = firstY * savedHeightmap->Size() + savedHeightmap->Size() - 1;
     for(int32 y = firstY; y < lastY; ++y)
     {
         sum += savedHeightmap->Data()[index];
