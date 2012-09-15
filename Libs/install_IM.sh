6f3df1ccb9b889bdbf93ce2a7b8bfc0fd20c3732
Ivan Petrochenko <dizzerr@gmail.com>
2012-09-15 19:21:00 +0300

    Fix: ImageMagick compilation options for MacOS
    Former-commit-id: 70a01721b8b7a4deee04be1817a8dd490acfba18

--- Libs/install_IM.sh
+++ Libs/install_IM.sh
@@ -10,12 +10,16 @@ tar -xf $imPath.tar.gz
 
 currPath=`pwd`
 
+mkdir -p $currPath/$installPath/delegates
+mkdir $currPath/$installPath/delegates/lib
+mkdir $currPath/$installPath/delegates/include
+
 cp $currPath/libs/libpng_macos.a $currPath/$installPath/delegates/lib/libpng-static.a
 cp $currPath/include/libpng/*.h $currPath/$installPath/delegates/include/
 
 cd $imPath
 
-./configure --prefix=$currPath/$installPath --disable-shared --without-dps --without-djvu --without-fontconfig --without-freetype --without-gslib --without-gvc --without-lcms --without-lcms2 --without-lqr --without-lzma --without-openexr --without-rsvg --without-webp --without-wmf --without-xml --disable-openmp --disable-opencl --with-x=no CPPFLAGS=-I$currPath/$installPath/delegates/include LDFLAGS=-L$currPath/$installPath/delegates/lib
+./configure --prefix=$currPath/$installPath --disable-shared --without-dps --without-djvu --without-fontconfig --without-gslib --without-gvc --without-lcms --without-lcms2 --without-lqr --without-lzma --without-openexr --without-rsvg --without-webp --without-wmf --without-xml --disable-openmp --disable-opencl --with-x=no --with-png=yes CPPFLAGS=-I$currPath/$installPath/delegates/include LDFLAGS=-L$currPath/$installPath/delegates/lib
 make
 make install
 cd ..
