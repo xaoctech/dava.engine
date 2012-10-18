de71498737e8aad7a13c0bdf160994e761f3325f
klesch <victor.kleschenko@gmail.com>
2012-10-18 11:25:45 +0300

    [DF-300] Removed creation of the descriptor file from class Texture

--- Sources/Internal/Render/Texture.h
+++ Sources/Internal/Render/Texture.h
@@ -276,7 +276,7 @@ public:							// properties for fast access
 
 	static const Map<String, Texture*> & GetTextureMap();
     
-    int32 GetDataSize();
+    int32 GetDataSize() const;
     
     void ReleaseTextureData();
 
