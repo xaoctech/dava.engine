e1e14f2c08b20ccb6fe2e2e1f6094d21916ebe8f
xrayman <xrayman64@gmail.com>
2012-10-26 13:47:38 +0300

    Fix color range
    Former-commit-id: d52f1fe95adb4587cbcd9d1df307463a40f97515

--- Tools/ResourceEditor/Classes/LandscapeEditor/NotPassableTerrain.cpp
+++ Tools/ResourceEditor/Classes/LandscapeEditor/NotPassableTerrain.cpp
@@ -155,10 +155,10 @@ void NotPassableTerrain::LoadColorsArray()
             
             Logger::Instance()->Debug("%d\n", colorNode->GetCount());
             
-            Color color(colorNode->Get(0)->AsFloat(),
-                        colorNode->Get(1)->AsFloat(),
-                        colorNode->Get(2)->AsFloat(),
-                        colorNode->Get(3)->AsFloat());
+            Color color(colorNode->Get(0)->AsFloat()/255.f,
+                        colorNode->Get(1)->AsFloat()/255.f,
+                        colorNode->Get(2)->AsFloat()/255.f,
+                        colorNode->Get(3)->AsFloat()/255.f);
             
             angleColor.push_back(TerrainColor(Vector2(tangentMin, tangentMax), color));
         }
