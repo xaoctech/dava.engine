59ab8182c05d0c2b0675be0138fbd80d52e6c9db
smile4u <s_zdanevich@wargaming.net>
2012-11-13 17:42:42 +0300

    new HashMap added

---
+++ Sources/Internal/Base/Hash.h
@@ -0,0 +1,95 @@
+/*
+    DAVA SDK
+    Hash 
+    Author: Sergey Zdanevich
+ */
+
+#ifndef __DAVAENGINE_HASH__
+#define __DAVAENGINE_HASH__
+
+#include <stddef.h>
+#include "Base/BaseTypes.h"
+
+namespace DAVA
+{
+	inline size_t dava_hash_string(const char* str)
+	{
+		size_t hash = 0; 
+		for (; *str; ++str)
+		{
+			hash = 5 * hash + *str;
+		}
+		return hash;
+	}
+
+	template <typename T>
+	struct Hash
+	{ };
+
+	template<> struct Hash <char *>
+	{
+		size_t operator()(const char *str) const
+		{
+			return dava_hash_string(str);
+		}
+
+		bool compare(const char *str1, const char *str2)
+		{
+			return (str1 == str2) || (0 == strcmp(str1, str2));
+		}
+	};
+
+	template<> struct Hash <const char *>
+	{
+		size_t operator()(const char *str) const
+		{
+			return dava_hash_string(str);
+		}
+
+		bool compare(const char *str1, const char *str2)
+		{
+			return (str1 == str2) || (0 == strcmp(str1, str2));
+		}
+	};
+
+	template<> struct Hash <DAVA::String>
+	{
+		size_t operator()(const DAVA::String &str) const
+		{
+			return dava_hash_string(str.c_str());
+		}
+
+		bool compare(const DAVA::String &str1, const DAVA::String &str2)
+		{
+			return (str1 == str2);
+		}
+	};
+
+	template<> struct Hash <DAVA::String *>
+	{
+		size_t operator()(const DAVA::String *str) const
+		{
+			return dava_hash_string(str->c_str());
+		}
+
+		bool compare(const DAVA::String *str1, const DAVA::String *str2)
+		{
+			return (str1 == str2) || 0 == str1->compare(str2->c_str());
+		}
+	};
+
+	template<> struct Hash <const DAVA::String *>
+	{
+		size_t operator()(const DAVA::String *str) const
+		{
+			return dava_hash_string(str->c_str());
+		}
+
+		bool compare(const DAVA::String *str1, const DAVA::String *str2)
+		{
+			return (str1 == str2) || 0 == str1->compare(str2->c_str());
+		}
+	};
+};
+
+#endif // __DAVAENGINE_HASH__
--- Sources/Internal/Base/HashMap.h
+++ Sources/Internal/Base/HashMap.h
@@ -1,207 +1,200 @@
-/*==================================================================================
-    Copyright (c) 2008, DAVA Consulting, LLC
-    All rights reserved.
+/*
+    DAVA SDK
+    HashMap
+    Author: Sergey Zdanevich
+ */
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-    * Neither the name of the DAVA Consulting, LLC nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTING, LLC AND CONTRIBUTORS "AS IS" AND
-    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL DAVA CONSULTING, LLC BE LIABLE FOR ANY
-    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-    Revision History:
-        * Created by Vitaliy Borodovsky 
-=====================================================================================*/
-#ifndef __DAVAENGINE_HASHMAP_H__
-#define __DAVAENGINE_HASHMAP_H__
+#ifndef __DAVAENGINE_HASH_MAP__
+#define __DAVAENGINE_HASH_MAP__
 
 #include "Base/BaseTypes.h"
-#include "Base/BaseObject.h"
-#include "Base/TemplateHelpers.h"
-#include "Base/FixedSizePoolAllocator.h"
+#include "Hash.h"
 
 namespace DAVA
 {
 
-template<class KeyType>
-struct HashFunc
-{
-    uint32 operator() (KeyType & value);
-};
-    
-template<class T>
-struct HashFunc<T*>
-{
-    uint32 operator() (const T* value)
-    {
-        pointer_size ptr = (pointer_size)value;
-        return (uint32)(ptr % 513);
-    };
-};
+// size = 2 ^ HASHMAP_DEFAULT_TABLE_SIZE_BITS
+#define HASHMAP_DEFAULT_TABLE_SIZE_BITS 10
+#define HASHMAP_DEFAULT_TABLE_SIZE (1 << HASHMAP_DEFAULT_TABLE_SIZE_BITS)
 
-
-template<class ItemType, class KeyType, class ValueType>
-struct HashMapIterator
+template <typename TKey, typename TValue>
+class HashMapN
 {
-    typedef typename Select<PointerTraits<ValueType>::result, ValueType, ValueType&>::Result ReturnType;
-    HashMapIterator(ValueType _value)
-    {
-        value = _value;
-    };
-    
-    bool operator == (const HashMapIterator<ItemType, KeyType, ValueType> & it) { return it.value == value; };
-    bool operator != (const HashMapIterator<ItemType, KeyType, ValueType> & it) { return it.value != value; };
-    
-    inline ReturnType operator * () { return value; };
 private:
-    ValueType value;
-};
-    
+	struct HashMapItemBase
+	{
+	};
 
-/** 
-    \ingroup baseobjects
-    \brief Class to work with hash.
- */
-template<class KeyType, class ValueType>
-class HashMap
-{
+	template <typename K, typename V>
+	struct HashMapItem : public HashMapItemBase
+	{
+		K key;
+		V value;
+		HashMapItem<K, V> *next;
+
+		HashMapItem(K k, V v)
+		{
+			next = NULL;
+			key = k;
+			value = v;
+		}
+	};
+
+	size_t sz;
+	size_t szTable;
+	HashMapItem<TKey, TValue>* table[HASHMAP_DEFAULT_TABLE_SIZE];
+	Hash<TKey> hashFn;
+	TValue defaultValue;
+
+	inline size_t getIndex(const TKey &key)
+	{
+		return hashFn(key) & (~(0xFFFFFFFF << HASHMAP_DEFAULT_TABLE_SIZE_BITS));
+	}
+
+	inline const HashMapItem<TKey, TValue>* getItem(const TKey &key)
+	{
+		size_t index = getIndex(key);
+
+		HashMapItem<TKey, TValue>* i = table[index];
+
+		while(NULL != i)
+		{
+			if(hashFn.compare(i->key, key))
+			{
+				break;
+			}
+
+			i = i->next;
+		}
+
+		return i;
+	}
+
 public:
-    struct Item
-    {
-        KeyType key;
-        ValueType value;
-        Item * next;
-    };
+	HashMapN(TValue defaultValue = TValue())
+		: sz(0)
+		, szTable(HASHMAP_DEFAULT_TABLE_SIZE)
+		, defaultValue(defaultValue)
+	{
+		for(size_t i = 0; i < szTable; ++i)
+		{
+			table[i] = NULL;
+		}
+	}
 
-    typedef HashMapIterator<Item, KeyType, ValueType> Iterator;
-    
-    HashMap(uint32 expectedItemsCount = 512);
-    ~HashMap();
-    
-    
-    Iterator & End() 
-    {  
-        static Iterator end(0);
-        return end;
-    };
-    
-    bool HasKey(const KeyType & key) const;
-    void Insert(KeyType key, const ValueType value);
-    void Remove(const KeyType & key);
-    Iterator Find(const KeyType & key) const;
-    
-    
-    static const uint32 HASH_ARRAY_SIZE = 513;
-    Item * hashArray[HASH_ARRAY_SIZE];
-    uint32 count;
-private:
-    FixedSizePoolAllocator pool;
+	~HashMapN()
+	{
+		Clear();
+	}
+
+	size_t Size()
+	{
+		return sz;
+	}
+
+	bool Empty()
+	{
+		return (0 == sz);
+	}
+
+	void Insert(const TKey &key, const TValue &value)
+	{
+		HashMapItem<TKey, TValue>* item = new HashMapItem<TKey, TValue>(key, value);
+
+		size_t index = getIndex(key);
+		if(NULL != table[index])
+		{
+			HashMapItem<TKey, TValue>* i = table[index];
+
+			while(NULL != i->next)
+			{
+				i = i->next;
+			}
+
+			i->next = item;
+		}
+		else
+		{
+			table[index] = item;
+		}
+
+		sz++;
+	}
+
+	void Remove(const TKey &key)
+	{
+		size_t index = getIndex(key);
+
+		HashMapItem<TKey, TValue>* item = table[index];
+		HashMapItem<TKey, TValue>* prev = NULL;
+
+		while(NULL != item)
+		{
+			if(hashFn.compare(item->key, key))
+			{
+				break;
+			}
+
+			prev = item;
+			item = item->next;
+		}
+		
+		if(NULL != item)
+		{
+			if(NULL != prev)
+			{
+				prev->next = item->next;
+			}
+			else
+			{
+				table[index] = NULL;
+			}
+
+			sz--;
+			delete item;
+		}
+	}
+
+	void Clear()
+	{
+		const HashMapItem<TKey, TValue>* item;
+		const HashMapItem<TKey, TValue>* next = NULL;
+
+		for(size_t i = 0; i < szTable; ++i)
+		{
+			item = table[i];
+			table[i] = NULL;
+			while(NULL != item)
+			{
+				next = item->next;
+				delete item;
+				item = next;
+			}
+		}
+	}
+
+	bool HasKey(const TKey &key)
+	{
+		return (NULL != getItem(key));
+	}
+
+	TValue Value(const TKey &key)
+	{
+		const HashMapItem<TKey, TValue>* item = getItem(key);
+		if(NULL != item)
+		{
+			return item->value;
+		}
+
+		return defaultValue;
+	}
+
+	TValue operator[](const TKey &key)
+	{
+		return value(key);
+	}
 };
-    
-template<class KeyType, class ValueType>
-HashMap<KeyType, ValueType>::HashMap(uint32 expectedItemsCount)
-    : pool(sizeof(Item), expectedItemsCount)
-    , count(0)
-{
-    for (uint32 k = 0; k < HASH_ARRAY_SIZE; ++k)
-        hashArray[k] = 0;
-}
 
-template<class KeyType, class ValueType>
-HashMap<KeyType, ValueType>::~HashMap()
-{
-    
-}
+};
 
-template<class KeyType, class ValueType>
-void HashMap<KeyType, ValueType>::Insert(KeyType key, const ValueType value)
-{
-    HashFunc<KeyType> func;
-    uint32 itemHashIndex = func(key);
-    
-    Item * currentItem = hashArray[itemHashIndex];
-    
-    Item * newItem = (Item*)pool.New();
-    newItem->key = key;
-    newItem->value = value;
-    newItem->next = currentItem;
-    
-    hashArray[itemHashIndex] = newItem;
-}
-
-template<class KeyType, class ValueType>
-void HashMap<KeyType, ValueType>::Remove(const KeyType & key)
-{
-    HashFunc<KeyType> func;
-    uint32 itemHashIndex = func(key);
-
-    Item * currentItem = hashArray[itemHashIndex];
-    Item * prevItem = 0;
-    while(currentItem)
-    {
-        // TODO: think how to deal with char* strings, because pointers is not a good way to compare strings
-        if (currentItem->key == key)    
-        {
-            if (prevItem == 0)
-            {
-                hashArray[itemHashIndex] = currentItem->next;
-            }else
-            {
-                prevItem->next = currentItem->next; 
-            }
-        }
-        prevItem = currentItem;
-        currentItem = currentItem->next;
-    }
-}
-    
-template<class KeyType, class ValueType>
-bool HashMap<KeyType, ValueType>::HasKey(const KeyType & key) const
-{
-    HashFunc<KeyType> func;
-    uint32 itemHashIndex = func(key);
-    Item * currentItem = hashArray[itemHashIndex];
-    while(currentItem)
-    {
-        if (currentItem->key == key)return true;
-        currentItem = currentItem->next;
-    }
-    return false;
-}
-
-template<class KeyType, class ValueType>
-typename HashMap<KeyType, ValueType>::Iterator HashMap<KeyType, ValueType>::Find(const KeyType & key) const
-{
-    HashFunc<KeyType> func;
-    uint32 itemHashIndex = func(key);
-    Item * currentItem = hashArray[itemHashIndex];
-    while(currentItem)
-    {
-        if (currentItem->key == key)return Iterator(currentItem->value);
-        currentItem = currentItem->next;
-    }
-    // Think should I call End() function here? Is it make sense from performance reasons.
-    return Iterator(0);
-}
-
-
-}; 
-
-
-#endif // __DAVAENGINE_HASHMAP_H__
-
+#endif
