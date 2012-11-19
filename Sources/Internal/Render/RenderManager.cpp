d930a16680350f36a5b56227a2b5d951fabcd5fe
klesch <victor.kleschenko@gmail.com>
2012-11-19 16:12:11 +0300

    [DF-185] Disabled using of notsupported OpenGL code for android.

--- Sources/Internal/Render/RenderManager.cpp
+++ Sources/Internal/Render/RenderManager.cpp
@@ -201,7 +201,7 @@ void RenderManager::Init(int32 _frameBufferWidth, int32 _frameBufferHeight)
     hardwareState.Reset(true);
 
 #if defined(__DAVAENGINE_OPENGL__)
-#if !defined(__DAVAENGINE_IPHONE__)//Dizz: glDisableClientState functions are not supported by GL ES 2.0
+#if !defined(__DAVAENGINE_IPHONE__) && !defined(__DAVAENGINE_ANDROID__)//Dizz: glDisableClientState functions are not supported by GL ES 2.0
     glDisableClientState(GL_VERTEX_ARRAY);
     oldVertexArrayEnabled = 0;                      
 
