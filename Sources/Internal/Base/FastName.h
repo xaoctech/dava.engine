2b6a4911983a4c52fe3f0591ed696717f568977d
smile4u <s_zdanevich@wargaming.net>
2012-11-26 17:31:41 +0300

    [DF-376] fixes static initialization order. (temp fix)
    Now FastName static variables will be always initilized before use

--- Sources/Internal/Base/FastName.h
+++ Sources/Internal/Base/FastName.h
@@ -9,17 +9,27 @@
 
 #include "HashMap.h"
 #include "Debug/DVAssert.h"
+#include "Base/StaticSingleton.h"
 
 namespace DAVA
 {
 
+class FastNameData : public StaticSingleton<FastNameData>
+{
+public:
+	Vector<const char *> namesTable;
+	Vector<int> namesRefCounts;
+	Vector<int> namesEmptyIndexes;
+	HashMap<const char *, int> namesHashs;
+
+	FastNameData()
+		: namesHashs(HashMap<const char *, int>(4096, -1))
+	{ }
+};
+
 class FastName
 {
 private:
-	static Vector<const char *> namesTable;
-	static Vector<int> namesRefCounts;
-	static Vector<int> namesEmptyIndexes;
-	static HashMap<const char *, int> namesHash;
 
 public:
 	FastName();
@@ -29,21 +39,21 @@ public:
 	FastName(const FastName &_name)
 	{
 		index = _name.index;
-		namesRefCounts[index]++;
+		FastNameData::Instance()->namesRefCounts[index]++;
 	}
 
 	FastName& operator=(const FastName &_name)
 	{
 		index = _name.index;
-		namesRefCounts[index]++;
+		FastNameData::Instance()->namesRefCounts[index]++;
 		return *this;
 	}
 
 	const char* operator*() const
 	{
-		DVASSERT((size_t) index < namesTable.size());
-		DVASSERT(NULL != namesTable[index]);
-		return namesTable[index];
+		DVASSERT((size_t) index < FastNameData::Instance()->namesTable.size());
+		DVASSERT(NULL != FastNameData::Instance()->namesTable[index]);
+		return FastNameData::Instance()->namesTable[index];
 	}
 
 	bool operator==(const FastName &_name) const
