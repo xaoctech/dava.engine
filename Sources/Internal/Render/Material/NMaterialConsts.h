f1278a465d6f8265d020b095e93367df52762831
binaryzebra <vitaliy.borodovsky@gmail.com>
2012-12-04 19:10:58 +0300

    [DF-43] Missing files, for prev. commits
    Former-commit-id: aac755999547e7c393c694180038f2836081f2ae

---
+++ Sources/Internal/Render/Material/NMaterialConsts.h
@@ -0,0 +1,58 @@
+/*==================================================================================
+    Copyright (c) 2008, DAVA Consulting, LLC
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    * Neither the name of the DAVA Consulting, LLC nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTING, LLC AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL DAVA CONSULTING, LLC BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    Revision History:
+        * Created by Vitaliy Borodovsky 
+=====================================================================================*/
+#ifndef __DAVAENGINE_NMATERIAL_CONSTS_H__
+#define __DAVAENGINE_NMATERIAL_CONSTS_H__
+
+#include "Base/BaseTypes.h"
+#include "Base/BaseMath.h"
+#include "Scene3D/DataNode.h"
+#include "Render/RenderStateBlock.h"
+#include "Render/ShaderUniformArray.h"
+
+namespace DAVA
+{
+    // Should be convertex to FNAMEs
+
+    static const char * TEXTURE_MAP_BASE_TEXTURE = "baseTextureMap";
+    static const char * TEXTURE_MAP_NORMAL = "normalMap";
+    static const char * TEXTURE_MAP_LIGHTMAP = "lightMap";
+    static const char * TEXTURE_MAP_TILE0 = "tile0Map";
+    static const char * TEXTURE_MAP_TILE1 = "tile1Map";
+    static const char * TEXTURE_MAP_TILE2 = "tile2Map";
+    static const char * TEXTURE_MAP_TILE3 = "tile3Map";
+
+    
+    static const char * UNIFORM_GLOBAL_TIME = "globalTime";
+    
+
+};
+
+#endif // __DAVAENGINE_NMATERIAL_CONSTS_H__
+
