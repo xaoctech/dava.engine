89aaed1233945623f50945d72a6a66e6da8d38c5
klesch <victor.kleschenko@gmail.com>
2012-12-20 11:37:47 +0300

    [DF-477] Added android pure-native template project

--- Projects/TemplateProject/android_native/jni/Android.mk
+++ Projects/TemplateProject/android_native/jni/Android.mk
@@ -4,11 +4,15 @@
 #set local path
 LOCAL_PATH := $(call my-dir)
 
+MY_PROJECT_ROOT := $(LOCAL_PATH)/../..
+DAVA_ROOT := $(MY_PROJECT_ROOT)/../..
+
+
 # clear all variables
 include $(CLEAR_VARS)
 
 # set module name
-LOCAL_MODULE := ViewerLib
+LOCAL_MODULE := TemplateProjectNative
 
 # set path for includes
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
@@ -20,24 +24,29 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 # set source files
 LOCAL_SRC_FILES :=  \
-                    AndroidLayer.cpp \
-                    AndroidDelegate.cpp \
+                    main.cpp \
                     \
                     ../../Classes/FrameworkMain.cpp \
                     ../../Classes/GameCore.cpp \
                     ../../Classes/TestScreen.cpp \
 
-LOCAL_CFLAGS := -g -O2
+
+LOCAL_CFLAGS := -g -O2 -Wno-deprecated -Wno-psabi -DOC_NEW_STYLE_INCLUDES -DOC_FACTOR_INTO_H_AND_CC
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -lz
 LOCAL_LDLIBS += -lOpenSLES
+LOCAL_LDLIBS += -fuse-ld=gold
+
+
+# LOCAL_ARM_MODE := arm
 
 # set included libraries
-LOCAL_STATIC_LIBRARIES := libInternal libbox2d
+LOCAL_STATIC_LIBRARIES += libInternal
 
 # build shared library
 include $(BUILD_SHARED_LIBRARY)
 
 # include modules
+$(call import-add-path,$(DAVA_ROOT)/Sources)
 $(call import-module,Internal)
