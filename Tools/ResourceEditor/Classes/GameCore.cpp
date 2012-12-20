aec2c62243816d2281fa39ae75596428bc917b6c
klesch <victor.kleschenko@gmail.com>
2012-12-20 16:36:04 +0300

    [DF-448] Moved splitting image on channels to ImageSplitter

--- Tools/ResourceEditor/Classes/GameCore.cpp
+++ Tools/ResourceEditor/Classes/GameCore.cpp
@@ -25,6 +25,8 @@
 #include "SceneEditor/CommandLineTool.h"
 #include "SceneEditor/ExporterScreen.h"
 
+#include "ImageSplitter/ImageSplitterScreen.h"
+
 #include "TextureBrowser/TextureConvertor.h"
 
 
@@ -75,19 +77,25 @@ void GameCore::OnAppStarted()
 	resourcePackerScreen = new ResourcePackerScreen();
     sceneEditorScreenMain = new SceneEditorScreenMain();
     exporterScreen = new ExporterScreen();
+    imageSplitterScreen = new ImageSplitterScreen();
 
     Texture::SetDefaultFileFormat((ImageFileFormat)EditorSettings::Instance()->GetTextureViewFileFormat());
 
 	UIScreenManager::Instance()->RegisterScreen(SCREEN_RESOURCE_PACKER, resourcePackerScreen);
     UIScreenManager::Instance()->RegisterScreen(SCREEN_SCENE_EDITOR_MAIN, sceneEditorScreenMain);
-    
     UIScreenManager::Instance()->RegisterScreen(SCREEN_EXPORTER, exporterScreen);
+    UIScreenManager::Instance()->RegisterScreen(SCREEN_IMAGE_SPLITTER, imageSplitterScreen);
 
+    
     if(CommandLineTool::Instance() && CommandLineTool::Instance()->CommandIsFound(String("-sceneexporter")))
     {
         UIScreenManager::Instance()->SetFirst(SCREEN_EXPORTER);
     }
-    else 
+    else if(CommandLineTool::Instance() && CommandLineTool::Instance()->CommandIsFound(String("-imagesplitter")))
+    {
+        UIScreenManager::Instance()->SetFirst(SCREEN_IMAGE_SPLITTER);
+    }
+    else
     {
         UIScreenManager::Instance()->SetFirst(SCREEN_SCENE_EDITOR_MAIN);
     }
@@ -103,6 +111,7 @@ void GameCore::OnAppFinished()
 	SafeRelease(resourcePackerScreen);
     SafeRelease(sceneEditorScreenMain);
     SafeRelease(exporterScreen);
+    SafeRelease(imageSplitterScreen);
 }
 
 void GameCore::OnSuspend()
