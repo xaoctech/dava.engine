6b0cc1e1cae3f2220d71b399633dee7fd1504ca5
klesch <victor.kleschenko@gmail.com>
2012-12-20 17:55:40 +0300

    Updated TeamCity script

--- Tools/ResourceEditor/Teamcity/packAppQt.bat
+++ Tools/ResourceEditor/Teamcity/packAppQt.bat
@@ -9,6 +9,7 @@ call git log --since=3.days --branches="development" --pretty=format:"%%%%s (%%%
 xcopy /e ..\Data\*.* app\1\2\3\Data 
 xcopy *.exe app\1\2\3
 xcopy ..\glew32.dll app\1\2\3
+xcopy ..\Teamcity\imagesplitter\*.bat app\1\2\3
 xcopy %QT_HOME%\Desktop\Qt\4.8.1\msvc2010\lib\QtCore4.dll app\1\2\3
 xcopy %QT_HOME%\Desktop\Qt\4.8.1\msvc2010\lib\QtGui4.dll app\1\2\3
 xcopy ..\..\..\..\dava.resourceeditor.beast\beast\bin\beast32.dll app\1\2\3
