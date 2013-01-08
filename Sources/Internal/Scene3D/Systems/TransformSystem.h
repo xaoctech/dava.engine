ec6be3942da4987fa79fd42ab29aa32810222968
binaryzebra <vitaliy.borodovsky@gmail.com>
2013-01-09 01:38:24 +0300

    [DF-47] Global refactoring of new render system. Basic culling enabled now.
    Former-commit-id: 9aa9630ee9ea7b0dee306755dafe2bbb475b9831

---
+++ Sources/Internal/Scene3D/Systems/TransformSystem.h
@@ -0,0 +1,48 @@
+#ifndef __DAVAENGINE_TRANSFORM_SYSTEM_H__
+#define __DAVAENGINE_TRANSFORM_SYSTEM_H__
+
+#include "Base/BaseTypes.h"
+#include "Math/MathConstants.h"
+#include "Math/Matrix4.h"
+#include "Base/Singleton.h"
+#include "Entity/SceneSystem.h"
+
+namespace DAVA 
+{
+
+class SceneNode;
+class Transform;
+
+class TransformSystem : public SceneSystem
+{
+public:
+	TransformSystem();
+	~TransformSystem();
+
+    Transform * CreateTransform();
+
+	virtual void ImmediateEvent(SceneNode * entity, uint32 event);
+
+    void DeleteTransform(Transform * transform);
+    void LinkTransform(int32 parentIndex, int32 childIndex);
+	void UnlinkTransform(int32 childIndex);
+    
+    virtual void Process();
+
+private:
+    void SortAndThreadSplit();
+    
+	Vector<SceneNode*> updatableEntities;
+
+	void HierahicNeedUpdate(SceneNode * entity);
+	void HierahicAddToUpdate(SceneNode * entity);
+
+	void HierahicFindUpdatableTransform(SceneNode * entity);
+
+	int32 passedNodes;
+	int32 multipliedNodes;
+};
+
+};
+
+#endif //__DAVAENGINE_TRANSFORM_SYSTEM_H__
