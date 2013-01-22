0a52fb812ef10deeab2b4785b05d6765a084667c
klesch <victor.kleschenko@gmail.com>
2013-01-22 17:32:48 +0300

    [DF-535] Added commen to install script for ImageMagick

--- Libs/install_IM.sh
+++ Libs/install_IM.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# MacOS:
+# It is strongly recommended that the X11R6 package be installed since this enables 
+# ImageMagick's X11 support (animate, display, and import will work) and it includes
+# the Freetype v2 DLL required to support TrueType and Postscript Type 1 fonts.
+#
+# Make sure that /usr/X11R6/bin is in your PATH prior to running configure.
+#
+
 
 installPath="ImageMagick-6.7.4"
 imPath="ImageMagick-6.7.4-10"
