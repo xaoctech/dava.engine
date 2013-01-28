69865a619a8563f7d09ecf938ebb4908c88812b7
adebt <adebt@yandex.ru>
2013-01-28 17:08:22 +0200

    [DF-559] fix crash.
    Former-commit-id: 4723e15b01017da21fd5307498e7beb2526ea82d

--- Tools/ResourceEditor/Classes/Qt/DockParticleEditor/BaseParticleEditorContentWidget.h
+++ Tools/ResourceEditor/Classes/Qt/DockParticleEditor/BaseParticleEditorContentWidget.h
@@ -8,8 +8,15 @@ using namespace DAVA;
 class BaseParticleEditorContentWidget
 {
 public:
+	BaseParticleEditorContentWidget();
+	
 	virtual void StoreVisualState(KeyedArchive* visualStateProps) = 0;
 	virtual void RestoreVisualState(KeyedArchive* visualStateProps) = 0;
+	
+	ParticleEmitter* GetEmitter() const {return emitter;};
+	
+protected:
+	ParticleEmitter* emitter;
 };
 
 #endif /* defined(__RESOURCEEDITORQT__BASEPARTICLEEDITORCONTENTWIDGET__) */
