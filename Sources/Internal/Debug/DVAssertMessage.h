60fe4bd9393b5936b4941e089529eb9f50c302cc
amakovii <andrii.makovii@gmail.com>
2013-02-08 18:03:53 +0200

    [DF-517] Refactoring for different OS
    Former-commit-id: 38f0c1b8239be380a1ce0c99b87810629e5b907c

--- Sources/Internal/Debug/DVAssertMessage.h
+++ Sources/Internal/Debug/DVAssertMessage.h
@@ -11,6 +11,9 @@ namespace DAVA
 
 		static void ShowMessage(const char8 * text, ...);
 
+	protected:
+
+		static void InnerShow(const char* content);
 	};
 };
 #endif // __DAVAENGINE_DVASSERT_MESSAGE_H__
