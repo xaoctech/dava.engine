42c06634e5f09e399335bfd54cad16e265d11370
smile4u <s_zdanevich@wargaming.net>
2013-02-13 18:38:05 +0300

    [DF-576] Added serializable interface

---
+++ Sources/Internal/Base/Serializable.h
@@ -0,0 +1,22 @@
+#ifndef __DAVAENGINE_SIRIALIZABLE__H__
+#define __DAVAENGINE_SIRIALIZABLE__H__
+
+#include "Base/BaseTypes.h"
+
+namespace DAVA
+{
+
+class KeyedArchive;
+
+class Serializable
+{
+public:
+	virtual ~Serializable() {};
+
+	virtual void Serialize(KeyedArchive *archive) = 0;
+	virtual void Deserialize(KeyedArchive *archive) = 0;
+};
+
+} // namespace DAVA
+
+#endif // __DAVAENGINE_SIRIALIZABLE__H__
