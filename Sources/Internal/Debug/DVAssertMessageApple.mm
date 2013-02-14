cac6abefad0b7b4e872580a04f413a58daa9014e
adebt <adebt@yandex.ru>
2013-02-14 18:06:49 +0200

    Merge
    Former-commit-id: 9329b4f937fc0068df62abc0596969d9574bdb26

--- Sources/Internal/Debug/DVAssertMessageApple.mm
+++ Sources/Internal/Debug/DVAssertMessageApple.mm
@@ -6,7 +6,7 @@ using namespace DAVA;
 #import <Foundation/Foundation.h>
 #include <AppKit/NSAlert.h>
 #elif defined(__DAVAENGINE_IPHONE__)
-#include <UIKit.h>
+#include <UIKit/UIKit.h>
 #include "UI/UIScreenManager.h"
 #import "UIAlertView_Modal.h"
 #endif
