2d1d5bb19bb4e74ceae5d58227597c3bd7ffe598
Yuri Coder <yuri.coder@gmail.com>
2013-02-18 17:16:41 +0200

    [DF-589] is implemented - MacOS only.

--- Projects/UnitTests/Classes/GameCore.cpp
+++ Projects/UnitTests/Classes/GameCore.cpp
@@ -46,6 +46,7 @@
 #include "KeyedArchiveYamlTest.h"
 #include "CloneTest.h"
 #include "DLCSystemTests.h"
+#include "InputTest.h"
 
 using namespace DAVA;
 
@@ -77,6 +78,7 @@ void GameCore::OnAppStarted()
 //    new HashMapTest();
 //    new SoundTest();
 //    new SplitTest();
+	new InputTest();
     new MaterialCompilerTest();
     new CloneTest();
     new PVRTest();
