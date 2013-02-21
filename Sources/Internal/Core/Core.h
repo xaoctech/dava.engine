ea4b9f72277cdb6d80b652085e20ba62af90f85d
adebt <adebt@yandex.ru>
2013-02-21 10:42:16 +0200

    [DF-598] get dpi android.
    Former-commit-id: 85168a1ec31f6117514e5bfeb4151d98ca318072

--- Sources/Internal/Core/Core.h
+++ Sources/Internal/Core/Core.h
@@ -250,6 +250,7 @@ public:
 	virtual int32 GetDesirableResourceIndex();
 	virtual int32 GetBaseResourceIndex();
 	
+    virtual uint32 GetScreenDPI();
 	
 	/*
 		\brief Mouse cursor for the platforms where it make sense (Win32, MacOS X) 
