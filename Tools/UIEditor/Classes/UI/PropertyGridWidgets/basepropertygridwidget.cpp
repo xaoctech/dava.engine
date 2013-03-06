6fc7a98fc58147348710787a460f03063009c1eb
amakovii <andrii.makovii@gmail.com>
2013-03-06 12:47:09 +0200

    [DF-766] fix current color in color selection dialog
    Former-commit-id: a57763ed8a2cfc7b63099818ab11056247712ed0

--- Tools/UIEditor/Classes/UI/PropertyGridWidgets/basepropertygridwidget.cpp
+++ Tools/UIEditor/Classes/UI/PropertyGridWidgets/basepropertygridwidget.cpp
@@ -467,16 +467,19 @@ void BasePropertyGridWidget::OnColorButtonClicked()
         return;
     }
 
-    QColor color = QColorDialog::getColor(Qt::white, this, "Select a color",  QColorDialog::DontUseNativeDialog);
-    if (color.isValid() == false)
-    {
-        return;
-    }
-        
-    PROPERTYGRIDWIDGETSITER iter = propertyGridWidgetsMap.find(senderWidget);
-    if (iter == propertyGridWidgetsMap.end())
+	PROPERTYGRIDWIDGETSITER iter = propertyGridWidgetsMap.find(senderWidget);
+
+	if (iter == propertyGridWidgetsMap.end())
     {
         Logger::Error("OpenSelectColorDialog - unable to find attached property in the propertyGridWidgetsMap!");
+        return;
+    }
+
+	QColor propertyValue = PropertiesHelper::GetPropertyValue<QColor>(this->activeMetadata, iter->second.getProperty().name(), false);
+
+    QColor color = QColorDialog::getColor(propertyValue, this, "Select a color",  QColorDialog::DontUseNativeDialog);
+    if (color.isValid() == false)
+    {
         return;
     }
         
