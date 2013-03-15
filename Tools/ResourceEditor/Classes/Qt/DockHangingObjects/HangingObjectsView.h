5c1f9a4f133f846993cf38e7b15f1d2110faa2bf
amakovii <andrii.makovii@gmail.com>
2013-03-15 15:16:22 +0200

    [DF-563] hanging objects: ui infrastructure
    Former-commit-id: b431784640b6b32f29eb6baeecd20682fe868996

---
+++ Tools/ResourceEditor/Classes/Qt/DockHangingObjects/HangingObjectsView.h
@@ -0,0 +1,33 @@
+#ifndef __RESOURCEEDITORQT__HANGING_OBJECTS_VIEW_H__
+#define __RESOURCEEDITORQT__HANGING_OBJECTS_VIEW_H__
+
+#include <QWidget>
+#include "Base/BaseTypes.h"
+//#include "Classes/Commands/SetSwitchIndexCommands.h"
+
+namespace Ui
+{
+	class HangingObjectsView;
+}
+
+class HangingObjectsView: public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit HangingObjectsView(QWidget* parent = 0);
+	~HangingObjectsView();
+
+	void Init();
+/*
+signals:
+	void Clicked(DAVA::uint32 value, DAVA::SetSwitchIndexHelper::eSET_SWITCH_INDEX selectionState);
+
+private slots:
+	void Clicked();
+*/
+private:
+	Ui::HangingObjectsView *ui;
+};
+
+#endif /* defined(__RESOURCEEDITORQT__HANGING_OBJECTS_VIEW_H__) */
