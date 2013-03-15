edfafaa372a4207e92eb740254d95c77c8ea7be9
klesch <victor.kleschenko@gmail.com>
2013-03-15 18:07:36 +0300

    [DF-811] Added save_to_folder and command line for this operation
    Former-commit-id: db1698110f46e86afd80745ab6635c1ae8ed787c

---
+++ Tools/ResourceEditor/Classes/SceneEditor/SceneSaver.h
@@ -0,0 +1,38 @@
+#ifndef __SCENE_SAVER_H__
+#define __SCENE_SAVER_H__
+
+#include "DAVAEngine.h"
+#include "SceneUtils.h"
+
+using namespace DAVA;
+
+class SceneSaver: public DAVA::Singleton<SceneSaver>
+{
+public:
+
+	SceneSaver();
+	virtual ~SceneSaver();
+    
+    void SetInFolder(const String &folderPathname);
+    void SetOutFolder(const String &folderPathname);
+    
+    void SaveFile(const String &fileName, Set<String> &errorLog);
+    void SaveScene(Scene *scene, const String &fileName, Set<String> &errorLog);
+    
+protected:
+    
+    void ReleaseTextures();
+
+    void CopyTextures(Scene *scene, Set<String> &errorLog);
+    void CopyTexture(const String &texturePathname, Set<String> &errorLog);
+
+protected:
+    
+    SceneUtils sceneUtils;
+    
+    Map<String, Texture *> texturesForSave;
+};
+
+
+
+#endif // __SCENE_SAVER_H__
