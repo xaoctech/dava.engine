3eea953d82135aa3634bb3ad3d8999c2a434ec9f
Igor Solovey <salikq@gmail.com>
2013-04-01 19:43:19 +0300

    [DF-451] Completed
    Former-commit-id: cec0516a71c17af49758a556027f27fbab00efa1

--- Tools/ResourceEditor/Classes/Qt/Main/QtMainWindowHandler.h
+++ Tools/ResourceEditor/Classes/Qt/Main/QtMainWindowHandler.h
@@ -64,6 +64,11 @@ public:
 	void RegisterSetSwitchIndexWidgets(QSpinBox*, QRadioButton*, QRadioButton*, QPushButton*);
     void SetSwitchIndexWidgetsState(bool state);
 
+	//material view options
+	void RegisterMaterialViewOptionsWidgets(QComboBox*);
+	void SetMaterialViewOptionsWidgetsState(bool state);
+	void SelectMaterialViewOption(Material::eViewOptions value);
+
 	//hanging objects
 	void RegisterHangingObjectsWidgets(QCheckBox*, QDoubleSpinBox*, QPushButton*);
     void SetHangingObjectsWidgetsState(bool state);
@@ -122,6 +127,9 @@ public slots:
     
 	//set switch index
 	void ToggleSetSwitchIndex(DAVA::uint32  value, DAVA::SetSwitchIndexHelper::eSET_SWITCH_INDEX state);
+
+	//material view options
+	void MaterialViewOptionChanged(int index);
 
 	//hanging objects
 	void ToggleHangingObjects(float value, bool isEnabled);
@@ -184,6 +192,9 @@ private:
 	QSpinBox*		editSwitchIndexValue;
 	QRadioButton*	rBtnSelection;
 	QRadioButton*	rBtnScene;
+
+	//material view options
+	QComboBox* comboMaterialViewOption;
 
 	//hanging objects
 	QPushButton*	hangingObjectsToggleButton;
