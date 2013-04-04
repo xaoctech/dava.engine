61efa0c48175f779af2161ae95f8d483940355e6
smile4u <s_zdanevich@wargaming.net>
2013-04-04 17:42:05 +0300

    [DF-895] bullet words created/released

--- Tools/ResourceEditor/Classes/Qt/Scene/System/SceneCollisionSystem.cpp
+++ Tools/ResourceEditor/Classes/Qt/Scene/System/SceneCollisionSystem.cpp
@@ -1,15 +1,34 @@
 #include "Scene/System/SceneCollisionSystem.h"
-
+#include "Scene3D/Components/ComponentHelpers.h"
 
 SceneCollisionSystem::SceneCollisionSystem(DAVA::Scene * scene)
 	: DAVA::SceneSystem(scene)
 {
+	btVector3 worldMin(-1000,-1000,-1000);
+	btVector3 worldMax(1000,1000,1000);
 
+	objectsCollConf = new btDefaultCollisionConfiguration();
+	objectsCollDisp = new btCollisionDispatcher(objectsCollConf);
+	objectsBroadphase = new btAxisSweep3(worldMin,worldMax);
+	objectsCollWorld = new btCollisionWorld(objectsCollDisp, objectsBroadphase, objectsCollConf);
+
+	landCollConf = new btDefaultCollisionConfiguration();
+	landCollDisp = new btCollisionDispatcher(landCollConf);
+	landBroadphase = new btAxisSweep3(worldMin,worldMax);
+	landCollWorld = new btCollisionWorld(landCollDisp, landBroadphase, landCollConf);
 }
 
 SceneCollisionSystem::~SceneCollisionSystem()
 {
+	DAVA::SafeDelete(objectsCollWorld);
+	DAVA::SafeDelete(objectsBroadphase);
+	DAVA::SafeDelete(objectsCollDisp);
+	DAVA::SafeDelete(objectsCollConf);
 
+	DAVA::SafeDelete(landCollWorld); 
+	DAVA::SafeDelete(landBroadphase);
+	DAVA::SafeDelete(landCollDisp);
+	DAVA::SafeDelete(landCollConf);
 }
 
 void SceneCollisionSystem::Update(DAVA::float32 timeElapsed)
@@ -31,9 +50,9 @@ void SceneCollisionSystem::AddEntity(DAVA::Entity * entity)
 {
 	if(NULL != entity)
 	{
+		DAVA::Landscape *landscape = DAVA::GetLandscape(entity);
 
 	}
-	// check if this entity has render component
 }
 
 void SceneCollisionSystem::RemoveEntity(DAVA::Entity * entity)
