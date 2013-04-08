ac5da511c02ac9cb9978956c670de99bcc63d471
klesch <victor.kleschenko@gmail.com>
2013-04-08 23:21:45 +0300

    [DF-956] Fixed incorrect sprite reloading.

--- Sources/Internal/Render/Texture.cpp
+++ Sources/Internal/Render/Texture.cpp
@@ -766,12 +766,6 @@ void Texture::ReloadAs(ImageFileFormat fileFormat)
 
 void Texture::ReloadAs(DAVA::ImageFileFormat fileFormat, const TextureDescriptor *descriptor)
 {
-    if(descriptor->IsCompressedFile())
-    {
-        return;
-    }
-    
-    
     ReleaseTextureData();
 	
 	DVASSERT(NULL != descriptor);
