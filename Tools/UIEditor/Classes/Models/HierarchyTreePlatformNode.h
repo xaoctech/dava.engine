e3cfdc63398654ab242edacef9f283414f27801b
amakovii <andrii.makovii@gmail.com>
2013-04-10 18:50:24 +0300

    Fix add dialogs

--- Tools/UIEditor/Classes/Models/HierarchyTreePlatformNode.h
+++ Tools/UIEditor/Classes/Models/HierarchyTreePlatformNode.h
@@ -51,6 +51,8 @@ public:
 	
 	virtual void SetParent(HierarchyTreeNode* node, HierarchyTreeNode* insertAfter);
 
+	bool IsAggregatorNamePresent(const QString& candidatName);
+
 private:
 	int width;
 	int height;
