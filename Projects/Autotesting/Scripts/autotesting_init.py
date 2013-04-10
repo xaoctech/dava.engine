cec92abe1111b63dd0d49f6def38d8b02e59d2b1
Aliaksey Baradulkin <aliaksey.baradulkin@gmail.com>
2013-04-10 13:00:52 +0300

    Add dbCollectionName parameter to autotesting_init script
    Former-commit-id: dfabb5c00528b545b0216c2345d7bbc124773a6f

--- Projects/Autotesting/Scripts/autotesting_init.py
+++ Projects/Autotesting/Scripts/autotesting_init.py
@@ -18,8 +18,8 @@ import filecmp
 
 arguments = sys.argv[1:]
 
-if len(arguments) < 2 or 3 < len(arguments):
-    print 'Usage: ./autotesting_init.py [PlatformName] [ProjectName] [TestsGroupName]'
+if len(arguments) < 3 or 4 < len(arguments):
+    print 'Usage: ./autotesting_init.py PlatformName ProjectName [TestsGroupName] [dbCollectionName]'
     exit(1)
 
 def copy_file(srcFolder, destFolder, fileName):
@@ -35,11 +35,15 @@ print "*** DAVA Initializing autotesting"
 platformName = arguments[0]
 projectName = arguments[1]
 testsGroupName = "default"
+dbCollectionName = projectName
 
 testsSrcFolder = "/Tests"
-if 3 == len(arguments):
+if 3 <= len(arguments):
     testsGroupName = arguments[2]
     testsSrcFolder = testsSrcFolder + "/" + testsGroupName
+	
+if 4 == len(arguments):
+	dbCollectionName = arguments[3]
 
 print "platform.system: " + platform.system()
 
@@ -117,7 +121,7 @@ params = ["python", "./copy_tests.py", testsSrcFolder, autotestingDestFolder]
 print "subprocess.call " + "[%s]" % ", ".join(map(str, params))
 subprocess.call(params)
 
-params = ["python", "./generate_id.py", projectName, autotestingDestFolder, testsGroupName]
+params = ["python", "./generate_id.py", dbCollectionName, autotestingDestFolder, testsGroupName]
 print "subprocess.call " + "[%s]" % ", ".join(map(str, params))
 subprocess.call(params)
 
