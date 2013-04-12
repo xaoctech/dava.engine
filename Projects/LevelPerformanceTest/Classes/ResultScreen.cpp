8c6ebae5c0297f7aa7cd7ada5d7f92d3d9372e9f
Igor Solovey <salikq@gmail.com>
2013-04-12 15:07:23 +0300

    [DF-1013] Add Texture files size to LevelPerformanceTest Report

--- Projects/LevelPerformanceTest/Classes/ResultScreen.cpp
+++ Projects/LevelPerformanceTest/Classes/ResultScreen.cpp
@@ -63,9 +63,10 @@ void ResultScreen::SaveResults()
     ImageLoader::Save(image, saveFileName);
     
     Map<String, String> results;
-    results["TextureMemorySize"] = Format("%.2f Mb", testData.GetTextureMemorySize()/(1024.f * 1024.f));
+	Map<String, Texture *> textures;
+	results["TextureMemorySize"] = Format("%.2f Mb", testData.GetTextureMemorySize()/(1024.f * 1024.f));
+	results["TextureFilesSize"] = Format("%.2f Mb", testData.GetTexturesFilesSize()/(1024.f * 1024.f));
     results["SceneFileSize"] = Format("%.2f Mb", testData.GetSceneFileSize()/(1024.f * 1024.f));
-    
     
     String documentsPath = FileSystem::Instance()->SystemPathForFrameworkPath("~doc:");
     String folderPathname = documentsPath + "PerformanceTestResult";
