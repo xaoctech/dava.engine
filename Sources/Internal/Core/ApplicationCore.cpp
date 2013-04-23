92bd3c3577448c168d926bb0d1dffcdb155b0adc
Yuri Coder <yuri.coder@gmail.com>
2013-04-23 17:17:52 +0300

    [DF-1067] fix for non-iOS projects.

--- Sources/Internal/Core/ApplicationCore.cpp
+++ Sources/Internal/Core/ApplicationCore.cpp
@@ -101,9 +101,13 @@ bool ApplicationCore::OnQuit()
 	return false;
 }
 
+#if defined (__DAVAENGINE_IPHONE__) || defined (__DAVAENGINE_ANDROID__) 
+	
 void ApplicationCore::OnForeground()
 {
 	// Default implementation is empty.
 }
+
+#endif
 
 };
