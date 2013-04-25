3ab8cda8739b60349a2a5af5bd3c9f47d5182011
smile4u <s_zdanevich@wargaming.net>
2013-04-25 13:12:15 +0300

    commented debug code
    Former-commit-id: 65f49032006e8a976131eb34eafc851cafd4a457

--- Tools/ResourceEditor/Classes/Qt/Scene/SceneTabWidget.cpp
+++ Tools/ResourceEditor/Classes/Qt/Scene/SceneTabWidget.cpp
@@ -46,7 +46,7 @@ SceneTabWidget::SceneTabWidget(QWidget *parent)
 	SetTabScene(oldTabIndex, NULL);
 	// <--
 
-	OpenTab("/Projects/dava.wot.art/DataSource/3d/Maps/dike_village/dike_village.sc2");
+	//OpenTab("/Projects/dava.wot.art/DataSource/3d/Maps/dike_village/dike_village.sc2");
 	//AddTab("/Projects/dava.wot.art/DataSource/3d/Maps/desert_train/desert_train.sc2");
 
 	QObject::connect(tabBar, SIGNAL(currentChanged(int)), this, SLOT(TabBarCurrentChanged(int)));
