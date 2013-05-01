d58db74b5a593ad8fe56554a7fef07514d6fa31a
Valentine Ivanov <valentin_ivanov@wargaming.net>
2013-05-02 01:10:01 +0300

    merged code from main development branch

--- Projects/LevelPerformanceTest/Classes/ResultScreen.cpp
+++ Projects/LevelPerformanceTest/Classes/ResultScreen.cpp
@@ -69,7 +69,7 @@ void ResultScreen::SaveResults()
 	results["TextureMemorySize"] = Format("%.2f Mb", testData.GetTextureMemorySize()/(1024.f * 1024.f));
 	results["TextureFilesSize"] = Format("%.2f Mb", testData.GetTexturesFilesSize()/(1024.f * 1024.f));
 
-	String filePath = testData.GetSceneFilePath().ResolvePathname();
+	String filePath = testData.GetSceneFilePath().GetAbsolutePathname();
     results["SceneFilePath"] = filePath.substr(filePath.find("Maps"));
     
 	FilePath folderPathname("~doc:/PerformanceTestResult/");
--- Projects/LevelPerformanceTest/Classes/Test.cpp
+++ Projects/LevelPerformanceTest/Classes/Test.cpp
@@ -28,6 +28,11 @@ void Test::LoadResources()
 	Scene *scene = new Scene();
 	scene->AddNode(scene->GetRootNode(fullName));
 	DVASSERT_MSG(scene, "Could not load the scene");
+    
+    /*SceneFileV2 *file = new SceneFileV2();
+    file->EnableDebugLog(false);
+    file->SaveScene(FilePath("~res:/3d/Maps/desert_train/a_level.sc2"), scene);
+    SafeRelease(file);*/
 
 	Camera* cam = new Camera();
 	scene->AddCamera(cam);
@@ -35,7 +40,7 @@ void Test::LoadResources()
 	Core* core = DAVA::Core::Instance();
 	float32 aspect = core->GetVirtualScreenHeight() / core->GetVirtualScreenWidth();
 
-	cam->Setup(70.f, aspect, 1.f, 5000.f);
+	cam->SetupPerspective(70.f, aspect, 1.f, 5000.f);
 	cam->SetLeft(Vector3(1, 0, 0));
 	cam->SetUp(Vector3(0, 0, 1));
     
@@ -48,7 +53,7 @@ void Test::LoadResources()
 	SafeRelease(sceneView);
 
 	Landscape* landscape = GetLandscape();
-	DVASSERT_MSG(scene, "There is no landscape in a scene");
+	DVASSERT_MSG(landscape, "There is no landscape in a scene");
 
 	uint32 textureMemory = TextureHelper::GetSceneTextureMemory(scene);
 	testData.SetTextureMemorySize(textureMemory);
