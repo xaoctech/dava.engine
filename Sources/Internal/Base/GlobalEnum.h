bcd23a35cfe36f2db4ce1e29b45e8f9bf25d5e19
smile4u <s_zdanevich@wargaming.net>
2013-05-15 16:00:59 +0300

    [DF-1131] Added globalEnum class for enumerating enums

--- Sources/Internal/Base/GlobalEnum.h
+++ Sources/Internal/Base/GlobalEnum.h
@@ -2,43 +2,130 @@
 #define __DAVAENGINE_GLOBAL_ENUM_H__
 
 #include "Base/BaseTypes.h"
-#include "Base/StaticSingleton.h"
-#include "Base/Meta.h"
+#include "Debug/DVAssert.h"
 
-class GlobalEnum : public DAVA::StaticSingleton<GlobalEnum>
+template<typename T>
+class GlobalEnum
 {
 public:
-	GlobalEnum();
+	explicit GlobalEnum();
 	~GlobalEnum();
 
-	template<typename T> void Add(int value, const char *str);
+	static bool ToValue(const char *, int &e);
+	static const char* ToString(const int e);
 
-	template<typename T> int StringToValue(const char *str);
-	template<typename T> const char* ValueToString(int value);
+	static int GetCount();
+	static bool GetValue(size_t index, int &e);
 
-	template<typename T> int GetCount() const;
-	template<typename T> int GetValue(int index) const ;
-	template<typename T> const char* GetString(int index) const;
+private:
+	typedef DAVA::Map<int, DAVA::String> ETOSMap;
+	static ETOSMap& GetMap();
 
-protected:
-	struct EnumValues
-	{
-		DAVA::Map<int, const char *> valueToString;
-	};
-
-	void Add(const DAVA::MetaInfo *meta, int value, const char *str);
-
-	int StringToValue(const DAVA::MetaInfo *meta, const char *str);
-	const char* ValueToString(const DAVA::MetaInfo *meta, int value);
-
-	int GetCount(const DAVA::MetaInfo *meta) const;
-	int GetValue(const DAVA::MetaInfo *meta, int index) const ;
-	const char* GetString(const DAVA::MetaInfo *meta, int index) const;
-
-	DAVA::Map<const DAVA::MetaInfo *, EnumValues*> enumValues;
+	static void RegisterAll();
+	static void Register(const int e, const char *);
 };
 
-#define DECLARE_ENUM 
+template<typename T>
+void GlobalEnum<T>::Register(const int e, const char* s)
+{
+	ETOSMap& etos = GetMap();
 
+	DVASSERT(!etos.count(e));
+	etos[e] = DAVA::String(s);
+}
+
+template<typename T>
+bool GlobalEnum<T>::ToValue(const char* s, int &e)
+{
+	bool ret = false;
+	ETOSMap &etos = GetMap();
+
+	ETOSMap::iterator i = etos.begin();
+	ETOSMap::iterator end = etos.end();
+	DAVA::String str(s);
+
+	for(; i != end; ++i)
+	{
+		if(i->second == str)
+		{
+			e = i->first;
+			ret = true;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+template<typename T>
+const char* GlobalEnum<T>::ToString(const int e)
+{
+	const char* ret = NULL;
+	ETOSMap &etos = GetMap();
+
+	if(etos.count(e))
+	{
+		ret = etos[e].c_str();
+	}
+
+	return ret;
+}
+
+template<typename T>
+int GlobalEnum<T>::GetCount()
+{
+	return GetMap().size();
+}
+
+template<typename T>
+bool GlobalEnum<T>::GetValue(size_t index, int &e)
+{
+	bool ret = false;
+	ETOSMap &etos = GetMap();
+	ETOSMap::iterator i = etos.begin();
+
+	if(index < etos.size())
+	{
+		while(index > 0)
+		{
+			index--;
+			i++;
+		}
+
+		if(i != etos.end())
+		{
+			e = i->first;
+			ret = true;
+		}
+	}
+
+	return ret;
+}
+
+template<typename T>
+typename GlobalEnum<T>::ETOSMap& GlobalEnum<T>::GetMap()
+{
+	static ETOSMap etosMap;
+	static bool initialized = false;
+
+	if(!initialized)
+	{
+		initialized = true;
+		RegisterAll();
+	}
+
+	return etosMap;
+}
+
+#define ENUM_DECLARE(eType) template<> static void GlobalEnum<eType>::RegisterAll()
+#define ENUM_ADD(eValue) Register(eValue, #eValue)
+#define ENUM_ADD_DESCR(eValue, eDescr) Register(eValue, eDescr)
+
+// Usage:
+//	ENUM_DECLARE(AnyEnumType)
+//	{
+//		ENUM_ADDS(AnyEnumType::Value1);
+//		ENUM_ADD_DESCR(AnyEnumType::Value2, "Value2");
+//	}
 
 #endif // __DAVAENGINE_GLOBAL_ENUM_H__
