15d11fd683a4f70ac05ecb50e174e589df2414ed
amakovii <andrii.makovii@gmail.com>
2013-05-24 12:29:50 +0300

    [DF-1152] implement undo for setSwitchIndexCommand

--- Tools/ResourceEditor/Classes/Commands/SetSwitchIndexCommands.h
+++ Tools/ResourceEditor/Classes/Commands/SetSwitchIndexCommands.h
@@ -15,8 +15,10 @@ public:
 protected:
 	DAVA::uint32	value;
 	DAVA::SetSwitchIndexHelper::eSET_SWITCH_INDEX	swtichState;
+	DAVA::Map<SwitchComponent *, int32> originalIndexes;
 
     virtual void Execute();
+	virtual void Cancel();
 };
 
 #endif // #ifndef __RESOURCE_EDITOR_SWITCH_INDEX_COMMANDS_H__
