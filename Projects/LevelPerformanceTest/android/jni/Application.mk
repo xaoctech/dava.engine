fd5c38271c82553ab904cc4824ba006cdbffc7ae
Igor Solovey <salikq@gmail.com>
2013-06-05 11:53:34 +0300

    [DF-1068] Fix LevelPerformanceTest for Android

--- Projects/LevelPerformanceTest/android/jni/Application.mk
+++ Projects/LevelPerformanceTest/android/jni/Application.mk
@@ -4,14 +4,14 @@ APP_GNUSTL_FORCE_CPP_FEATURES := rtti
 APP_CFLAGS = -marm -g
 
 #debug
-APP_CFLAGS += -DNDK_DEBUG=1 -O0
-APP_CFLAGS += -D__DAVAENGINE_DEBUG__
-APP_OPTIM := debug
+#APP_CFLAGS += -DNDK_DEBUG=1 -O0
+#APP_CFLAGS += -D__DAVAENGINE_DEBUG__
+#APP_OPTIM := debug
 #APP_CFLAGS += -DUSE_LOCAL_RESOURCES #use local resources
 
 #release
-#APP_CFLAGS += -O2
-#APP_OPTIM := release
+APP_CFLAGS += -O2
+APP_OPTIM := release
 
 APP_CFLAGS += -Wno-invalid-offsetof
 
