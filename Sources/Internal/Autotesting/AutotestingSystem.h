9ab6586c8f485bd59a3d0507d6e6127b5f40b13c
Aliaksey Baradulkin <aliaksey.baradulkin@gmail.com>
2013-06-10 16:42:11 +0300

    Use DB at by2-buildmachine.wargaming.net
    Former-commit-id: 903d225a6cfed9c76a9758501ce94f67d2d978f9

--- Sources/Internal/Autotesting/AutotestingSystem.h
+++ Sources/Internal/Autotesting/AutotestingSystem.h
@@ -41,8 +41,8 @@
 
 #include "Autotesting/MongodbUpdateObject.h"
 
-#define AUTOTESTING_DB_HOST    "10.128.19.33"
-//#define AUTOTESTING_DB_HOST    "by2-buildmachine.wargaming.net"
+//#define AUTOTESTING_DB_HOST    "10.128.19.33"
+#define AUTOTESTING_DB_HOST    "by2-buildmachine.wargaming.net"
 //#define AUTOTESTING_DB_HOST    "10.128.128.5"
 //#define AUTOTESTING_DB_HOST    "192.168.1.2"
 #define AUTOTESTING_DB_PORT  27017
