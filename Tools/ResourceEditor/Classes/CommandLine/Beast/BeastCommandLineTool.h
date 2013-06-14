e94134c00cb01ee4bd14b8cd7e5900145cd12de7
klesch <victor.kleschenko@gmail.com>
2013-06-14 13:46:51 +0300

    [DF-1331] Added new Beast Command for new command line tools
    Former-commit-id: c3a474c26d4dad81dc8471e42b33ebe74edbec8a

---
+++ Tools/ResourceEditor/Classes/CommandLine/Beast/BeastCommandLineTool.h
@@ -0,0 +1,24 @@
+#ifndef __BEAST_COMMAND_LINE_TOOL_H__
+#define __BEAST_COMMAND_LINE_TOOL_H__
+
+#include "../CommandLineTool.h"
+
+class BeastCommandLineTool: public CommandLineTool
+{
+public:
+
+    virtual DAVA::String GetCommandLineKey();
+    virtual bool InitializeFromCommandLine();
+    virtual void Process();
+    virtual void PrintUsage();
+
+    const DAVA::FilePath & GetScenePathname() const;
+    
+protected:
+
+    DAVA::FilePath scenePathname;
+    
+};
+
+
+#endif // __BEAST_COMMAND_LINE_TOOL_H__
