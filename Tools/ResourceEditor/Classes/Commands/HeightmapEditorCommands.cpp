7f91a31b299e61a8a7b6d9b668349f0c0c6e83d5
xrayman <xrayman64@gmail.com>
2013-06-18 12:31:06 +0300

    [DF-1294] Fix heightmap editor commands to store only changed rect of the texture.

--- Tools/ResourceEditor/Classes/Commands/HeightmapEditorCommands.cpp
+++ Tools/ResourceEditor/Classes/Commands/HeightmapEditorCommands.cpp
@@ -320,35 +320,68 @@ CommandModifyHeightmap::CommandModifyHeightmap(HeightmapProxy* heightmapProxy,
 	
 	if (originalHeightmap && heightmapProxy)
 	{
-		undoFilename = SaveHeightmap(originalHeightmap);
-		redoFilename = SaveHeightmap(heightmapProxy);
+		undoRegion = GetHeightmapRegion(originalHeightmap);
+		redoRegion = GetHeightmapRegion(heightmapProxy);
 	}
 }
 
 CommandModifyHeightmap::~CommandModifyHeightmap()
 {
-	FileSystem::Instance()->DeleteFile(undoFilename);
-	FileSystem::Instance()->DeleteFile(redoFilename);
+	SafeDeleteArray(undoRegion);
+	SafeDeleteArray(redoRegion);
 }
 
 void CommandModifyHeightmap::Execute()
 {
-	Heightmap* h = new Heightmap();
-	h->Load(redoFilename);
-	
-	h->Clone(heightmapProxy);
-	heightmapProxy->UpdateRect(updatedRect);
-	
-	SafeRelease(h);
+	ApplyHeightmapRegion(redoRegion);
 }
 
 void CommandModifyHeightmap::Cancel()
 {
-	Heightmap* h = new Heightmap();
-	h->Load(undoFilename);
-	
-	h->Clone(heightmapProxy);
+	ApplyHeightmapRegion(undoRegion);
+}
+
+uint16* CommandModifyHeightmap::GetHeightmapRegion(Heightmap* heightmap)
+{
+	int32 size = heightmap->Size();
+	int32 width = (int32)updatedRect.dx;
+	int32 height = (int32)updatedRect.dy;
+	int32 xOffset = (int32)updatedRect.x;
+	int32 yOffset = (int32)updatedRect.y;
+
+	DVASSERT((xOffset + width) <= size && (yOffset + height) <= size);
+
+	uint16* newData = new uint16[width * height];
+	uint16* oldData = heightmap->Data();
+
+	for (int32 i = 0; i < height; ++i)
+	{
+		uint16* src = oldData + (yOffset + i) * size + xOffset;
+		uint16* dst = newData + i * width;
+		memcpy(dst, src, sizeof(uint16) * width);
+	}
+
+	return newData;
+}
+
+void CommandModifyHeightmap::ApplyHeightmapRegion(uint16* region)
+{
+	int32 size = heightmapProxy->Size();
+	int32 width = (int32)updatedRect.dx;
+	int32 height = (int32)updatedRect.dy;
+	int32 xOffset = (int32)updatedRect.x;
+	int32 yOffset = (int32)updatedRect.y;
+
+	DVASSERT((xOffset + width) <= size && (yOffset + height) <= size);
+
+	uint16* data = heightmapProxy->Data();
+
+	for (int32 i = 0; i < height; ++i)
+	{
+		uint16* src = region + i * width;
+		uint16* dst = data + (yOffset + i) * size + xOffset;
+		memcpy(dst, src, sizeof(uint16) * width);
+	}
+
 	heightmapProxy->UpdateRect(updatedRect);
-	
-	SafeRelease(h);
 }
