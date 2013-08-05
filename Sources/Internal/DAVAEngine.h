cafa96148c6b2cceee0b6538e50a4d5cb43a94c9
Igor Solovey <salikq@gmail.com>
2013-08-05 15:30:48 +0300

    [MOBWOT-732] SiluetteVolume moved to game project

--- Sources/Internal/DAVAEngine.h
+++ Sources/Internal/DAVAEngine.h
@@ -229,7 +229,6 @@
 #include "Render/Highlevel/SpriteObject.h"
 #include "Render/Highlevel/RenderObject.h"
 #include "Render/Highlevel/RenderFastNames.h"
-#include "Render/Highlevel/SiluetteVolume.h"
 
 #include "Scene3D/ShadowVolumeNode.h"
 #include "Scene3D/LodNode.h"
