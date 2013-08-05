854766b70cc89d8b09baaddb0eecfb7165ab09ca
adebt <adebt@adebts-MacBook-Pro.local>
2013-08-05 15:24:02 +0300

    [DF-1620] add device name for android.
    Former-commit-id: bae963154328a7547e005e44898fb17e48b78fa4

--- Projects/UnitTests/Classes/DeviceInfoTest.cpp
+++ Projects/UnitTests/Classes/DeviceInfoTest.cpp
@@ -29,6 +29,7 @@ void DeviceInfoTest::TestFunction(TestTemplate<DeviceInfoTest>::PerfFuncData *da
     String region = DeviceInfo::GetRegion();
     String timezone = DeviceInfo::GetTimeZone();
     String udid = DeviceInfo::GetUDID();
+    WideString name = DeviceInfo::GetName();
 
 	Logger::Debug("********** Device info **********");
 	Logger::Debug("Platform: %s", platform.c_str());
@@ -39,6 +40,7 @@ void DeviceInfoTest::TestFunction(TestTemplate<DeviceInfoTest>::PerfFuncData *da
 	Logger::Debug("Region: %s", region.c_str());
 	Logger::Debug("Time zone: %s", timezone.c_str());
     Logger::Debug("UDID: %s", udid.c_str());
+    Logger::Debug("Name: %s", WStringToString(name).c_str());
 	Logger::Debug("********** Device info **********");
 
 	data->testData.message = "DeviceInfo test - passed";
