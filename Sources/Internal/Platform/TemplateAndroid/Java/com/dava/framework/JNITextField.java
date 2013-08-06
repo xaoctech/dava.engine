287e45f66b52d4a4fedea7e05c436a1e88e15c1d
adebt <adebt@adebts-MacBook-Pro.local>
2013-08-06 18:14:53 +0300

    [DF-1639] add password style to text feild

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNITextField.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNITextField.java
@@ -1,54 +1,58 @@
 package com.dava.framework;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+
 public class JNITextField {
 	final static String TAG = "JNITextField";
 	
 	public static void ShowField(final float x, final float y, final float dx, final float dy, final String defaultText)
 	{
-		final JNIActivity activity = JNIActivity.GetActivity();
-		final Object sync = new Object();
-		synchronized (sync) {
-			activity.runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					synchronized (sync) {
-						activity.ShowEditText(x, y, dx, dy, defaultText);
-						sync.notify();
-					}
-				}
-			});
-			try {
-				sync.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		final FutureTask<Void> task = new FutureTask<Void> (new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				JNIActivity.GetActivity().ShowEditText(x, y, dx, dy, defaultText);
+				return null;
 			}
-		}
+		});
+		
+		JNIActivity.GetActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				task.run();
+			}
+		});
+		
+		while (!task.isDone())
+			Thread.yield();
+		TextFieldKeyPressed(0, 0, defaultText);
 	}
 	
+	static String text;
 	public static void HideField()
 	{
-		final Object sync = new Object();
-		synchronized (sync) {
-			final JNIActivity activity = JNIActivity.GetActivity();
-			String text = activity.GetEditText();
-			activity.runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					synchronized (sync) {
-						activity.HideEditText();
-						sync.notify();
-					}
-				}
-			});
-
-			try {
-				sync.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		final FutureTask<Void> task = new FutureTask<Void> (new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				JNIActivity activity = JNIActivity.GetActivity();
+				text = activity.GetEditText();
+				activity.HideEditText();
+				return null;
 			}
-			
-			FieldHiddenWithText(text);
-		};
+		});
+		
+		JNIActivity.GetActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				task.run();
+			}
+		});
+		
+		while (!task.isDone())
+			Thread.yield();
+		
+		FieldHiddenWithText(text);
 	}
 	
 	public static native void FieldHiddenWithText(String text);
