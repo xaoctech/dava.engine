a44fbbafcdac56711e75795502365a72ba087080
smile4u <s_zdanevich@wargaming.net>
2013-08-14 17:37:30 +0300

    [DF-1519] Added command to modify meta-object value
    Former-commit-id: d4f038ac1a0fad6482525325412eddffe4e6fcf7

--- Tools/ResourceEditor/Classes/Commands2/MetaObjModifyCommand.cpp
+++ Tools/ResourceEditor/Classes/Commands2/MetaObjModifyCommand.cpp
@@ -14,57 +14,35 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-#ifndef __COMMAND_ID_H__
-#define __COMMAND_ID_H__
+#include "Commands2/MetaObjModifyCommand.h"
 
-enum CommandID
+MetaObjModifyCommand::MetaObjModifyCommand(DAVA::MetaInfo *_info, void *_object, const DAVA::VariantType &_newValue)
+	: Command2(CMDID_META_OBJ_MODIFY, "Modify value")
+	, info(_info)
+	, object(_object)
+	, newValue(_newValue)
 {
-	CMDID_UNKNOWN	= -1,
-	CMDID_BATCH		=  0,
+	if(NULL != info && NULL != object)
+	{
+		oldValue = DAVA::VariantType::LoadData(object, info);
+	}
+}
 
-	CMDID_TRANSFORM,
+MetaObjModifyCommand::~MetaObjModifyCommand()
+{ }
 
-	CMDID_ENTITY_REMOVE,
-	CMDID_ENTITY_MOVE,
-	CMDID_PARTICLE_LAYER_REMOVE,
-	CMDID_PARTICLE_LAYER_MOVE,
-	CMDID_PARTICLE_FORCE_REMOVE,
-	CMDID_PARTICLE_FORCE_MOVE,
-	CMDID_ADD_ENTITY,
+void MetaObjModifyCommand::Undo()
+{
+	if(NULL != info && NULL != object)
+	{
+		DAVA::VariantType::SaveData(object, info, oldValue);
+	}
+}
 
-	CMDID_META_OBJ_MODIFY,
-
-	CMDID_MODIFY_CUSTOM_COLORS,
-	CMDID_SET_VISIBILITY_POINT,
-	CMDID_SET_VISIBILITY_AREA,
-	CMDID_DRAW_HEIGHTMAP,
-	CMDID_COPY_PASTE_HEIGHTMAP,
-	CMDID_MODIFY_TILEMASK,
-	
-	CMDID_UPDATE_PARTICLE_EFFECT,
-	CMDID_UPDATE_PARTICLE_EMITTER,
-	CMDID_UPDATE_PARTICLE_LAYER,
-	CMDID_UPDATE_PARTILCE_LAYER_TIME,
-	CMDID_UPDATE_PARTICLE_LAYER_ENABLED,
-	CMDID_UPDATE_PARTICLE_FORCE,
-
-	CMDID_ADD_PARTICLE_EMITTER,
-
-	CMDID_START_STOP_PARTICLE_EFFECT,
-	CMDID_RESTART_PARTICLE_EFFECT,
-
-	CMDID_ADD_PARTICLE_EMITTER_LAYER,
-	CMDID_REMOVE_PARTICLE_EMITTER_LAYER,
-	CMDID_CLONE_PARTICLE_EMITTER_LAYER,
-	CMDID_ADD_PARTICLE_EMITTER_FORCE,
-	CMDID_REMOVE_PARTICLE_EMITTER_FORCE,
-	
-	CMDID_LOAD_PARTICLE_EMITTER_FROM_YAML,
-	CMDID_SAVE_PARTICLE_EMITTER_TO_YAML,
-
-	CMDID_DAE_CONVERT,
-
-	CMDID_USER		= 0xF000
-};
-
-#endif // __COMMAND_ID_H__
+void MetaObjModifyCommand::Redo()
+{
+	if(NULL != info && NULL != object)
+	{
+		DAVA::VariantType::SaveData(object, info, newValue);
+	}
+}
