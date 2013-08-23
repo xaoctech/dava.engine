1904509c85a4ec489ac66f593cce37ff0159934a
IgorSolovey <igorsolovey@Igors-MacBook-Pro-4.local>
2013-08-23 13:56:41 +0300

    [DF-1434] Fix launcher crash on wg-guest

--- Tools/Launcher/Launcher/main.cpp
+++ Tools/Launcher/Launcher/main.cpp
@@ -21,14 +21,16 @@ int main(int argc, char *argv[])
 /////////TEMP!!! Delete previous Launcher version
 /////////Remove in next version
     QString appDir = FileManager::Instance()->GetLauncherDirectory();
-    FileManager::Instance()->DeleteDirectory(appDir + "Dependencies");
-    FileManager::Instance()->DeleteDirectory(appDir + "Development");
-    FileManager::Instance()->DeleteDirectory(appDir + "Downloads");
-    FileManager::Instance()->DeleteDirectory(appDir + "Launcherold");
+    FileManager::Instance()->DeleteDirectory(appDir + "../Dependencies");
+    FileManager::Instance()->DeleteDirectory(appDir + "../Development");
+    FileManager::Instance()->DeleteDirectory(appDir + "../Downloads");
+    FileManager::Instance()->DeleteDirectory(appDir + "../Launcherold");
     FileManager::Instance()->DeleteDirectory(appDir + "Launcher.appold");
-    FileManager::Instance()->DeleteDirectory(appDir + "QA");
-    FileManager::Instance()->DeleteDirectory(appDir + "Stable");
-    FileManager::Instance()->DeleteDirectory(appDir + "ToMaster");
+    FileManager::Instance()->DeleteDirectory(appDir + "../QA");
+    FileManager::Instance()->DeleteDirectory(appDir + "../Stable");
+    FileManager::Instance()->DeleteDirectory(appDir + "../ToMaster");
+    QFile file(appDir + "settings.yaml");
+    file.remove();
 //////////////////////////////////////////////////
 
     return a.exec();
