77ba4498d782c7b9a0cbbea1ffb11e5433ef1d29
xKernel <Kernel@tut.by>
2013-08-28 12:45:44 +0300

    DF-1819 added performance settings to project, fixed lods for single particle

---
+++ Sources/Internal/Core/PerformanceSettings.cpp
@@ -0,0 +1,56 @@
+#include "Core/PerformanceSettings.h"
+namespace DAVA 
+{
+
+PerformanceSettings::PerformanceSettings()
+{
+	//init with some default values
+	psPerformanceMinFPS = 3;
+	psPerformanceMaxFPS = 10;
+	psPerformanceLodOffset = 0;
+	psPerformanceLodMult = 1;
+	psPerformanceSpeedMult =1;
+}
+float32 PerformanceSettings::GetPsPerformanceMinFPS()
+{
+	return psPerformanceMinFPS;
+}
+float32 PerformanceSettings::GetPsPerformanceMaxFPS()
+{
+	return psPerformanceMaxFPS;
+}
+float32 PerformanceSettings::GetPsPerformanceSpeedMult()
+{
+	return psPerformanceSpeedMult;
+}
+float32 PerformanceSettings::GetPsPerformanceLodOffset()
+{
+	return psPerformanceLodOffset;
+}
+float32 PerformanceSettings::GetPsPerformanceLodMult()
+{
+	return psPerformanceLodMult;
+}
+
+void PerformanceSettings::SetPsPerformanceMinFPS(float32 minFPS)
+{
+	psPerformanceMinFPS = minFPS;
+}
+void PerformanceSettings::SetPsPerformanceMaxFPS(float32 maxFPS)
+{
+	psPerformanceMaxFPS = maxFPS;
+}
+void PerformanceSettings::SetPsPerformanceSpeedMult(float32 speedMult)
+{
+	psPerformanceSpeedMult = speedMult;
+}
+void PerformanceSettings::SetPsPerformanceLodOffset(float32 lodOffset)
+{
+	psPerformanceLodOffset = lodOffset;
+}
+void PerformanceSettings::SetPsPerformanceLodMult(float32 lodMult)
+{
+	psPerformanceLodMult = lodMult;
+}
+
+}
