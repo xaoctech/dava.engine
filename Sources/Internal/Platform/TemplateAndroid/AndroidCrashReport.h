be223f88b448c8148929f37583b4be42f4125d38
adebt <adebt@adebts-MacBook-Pro.local>
2013-08-29 13:04:24 +0300

    [DF-993] android cpp crash report [DF-1603] generate Java exption with CPP stack

--- Sources/Internal/Platform/TemplateAndroid/AndroidCrashReport.h
+++ Sources/Internal/Platform/TemplateAndroid/AndroidCrashReport.h
@@ -28,31 +28,22 @@ namespace DAVA
 
 class File;
 
-class CustomReport
-{
-public:
-	virtual void WriteCustomReport(File*, int signal, siginfo_t *info, void *uapVoid);
-};
-
 class AndroidCrashReport
 {
 public:
 	static void Init();
-	static void SetCustomReport(CustomReport* );
 
 private:
 	static void SignalHandler(int signal, siginfo_t *info, void *uapVoid);
-	static void WriteCStack(File*, void *uapVoid);
 
 private:
 	static stack_t s_sigstk;
-	static CustomReport* s_customReport;
 };
 
 class JniCrashReporter: public JniExtension
 {
 public:
-	String GetReportFile();
+	void ThrowJavaExpetion(const String& cppSignal);
 
 protected:
 	virtual jclass GetJavaClass() const;
