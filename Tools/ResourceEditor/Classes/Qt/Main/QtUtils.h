bdbe632105a7db69d7b38191639fc8d33188b5bf
Valentine Ivanov <valentin_ivanov@wargaming.net>
2013-09-03 13:28:33 +0300

    [DF-1857] separate project for image tools
    Former-commit-id: 64f0862fb74db56c8177b798771bf70697721aa7

--- Tools/ResourceEditor/Classes/Qt/Main/QtUtils.h
+++ Tools/ResourceEditor/Classes/Qt/Main/QtUtils.h
@@ -57,9 +57,6 @@ enum eMessageBoxFlags
 int ShowQuestion(const DAVA::String &header, const DAVA::String &question, int buttons, int defaultButton);
 int ShowSaveSceneQuestion(DAVA::Scene *scene);
 
-DAVA_DEPRECATED(void DeleteOldPVRTextureIfPowerVr_IOS(const DAVA::TextureDescriptor *descriptor, const DAVA::eGPUFamily gpu));
-DAVA_DEPRECATED(void DeleteOldDXTTextureIfTegra(const DAVA::TextureDescriptor *descriptor, const DAVA::eGPUFamily gpu));
-
 #ifdef __DAVAENGINE_WIN32__
 	const Qt::WindowFlags WINDOWFLAG_ON_TOP_OF_APPLICATION = Qt::Window;
 #else
