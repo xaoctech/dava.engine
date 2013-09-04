6971315e4d26d27970a251918425db5e2b945469
smile4u <s_zdanevich@wargaming.net>
2013-09-04 14:47:39 +0300

    [DF-185] Project fixes after folder renamed

--- Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyData/QtPropertyDataInspColl.h
+++ Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyData/QtPropertyDataInspColl.h
@@ -20,21 +20,17 @@
 #include "Base/Introspection.h"
 #include "../QtPropertyData.h"
 
-class QtPropertyDataIntroCollection : public QtPropertyData
+class QtPropertyDataInspColl : public QtPropertyData
 {
 public:
-	QtPropertyDataIntroCollection(void *_object, const DAVA::InspColl *_collection, int hasAllFlags = DAVA::I_NONE);
-	virtual ~QtPropertyDataIntroCollection();
+	QtPropertyDataInspColl(void *_object, const DAVA::InspColl *_collection, int hasAllFlags = DAVA::I_NONE);
+	virtual ~QtPropertyDataInspColl();
 
 protected:
 	void *object;
 	const DAVA::InspColl *collection;
 
-	//QMap<QtPropertyDataDavaVariant*, int> childVariantIndexes;
-
 	virtual QVariant GetValueInternal();
-	//virtual void ChildChanged(const QString &key, QtPropertyData *data);
-	//virtual void ChildNeedUpdate();
 };
 
 #endif // __QT_PROPERTY_DATA_INTRO_COLLECTION_H__
