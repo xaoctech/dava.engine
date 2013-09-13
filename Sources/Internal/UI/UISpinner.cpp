befdbedcb2e61bab7b3cfc239d9173f26cd6fc7f
Yuri Coder <yuri.coder@gmail.com>
2013-09-13 17:26:14 +0300

    [DF-1992] is fixed - hierarchy for "embedded" controls is now saved.
    Former-commit-id: 00e5daed99565289a67cd74c229c66265ba72c27

--- Sources/Internal/UI/UISpinner.cpp
+++ Sources/Internal/UI/UISpinner.cpp
@@ -353,12 +353,12 @@ YamlNode * UISpinner::SaveToYamlNode(UIYamlLoader * loader)
 
 	//Control Type
 	SetPreferredNodeType(node, "UISpinner");
-	
+
 	// "Prev/Next" buttons have to be saved too.
-	YamlNode* prevButtonNode = buttonPrevious->SaveToYamlNode(loader);
-	YamlNode* nextButtonNode = buttonNext->SaveToYamlNode(loader);
-	YamlNode* contentNode = content->SaveToYamlNode(loader);
-	
+	YamlNode* prevButtonNode = SaveToYamlNodeRecursive(loader, buttonPrevious);
+	YamlNode* nextButtonNode = SaveToYamlNodeRecursive(loader, buttonNext);
+	YamlNode* contentNode = SaveToYamlNodeRecursive(loader, content);
+
 	node->AddNodeToMap(UISPINNER_BUTTON_PREVIOUS_NAME, prevButtonNode);
 	node->AddNodeToMap(UISPINNER_BUTTON_NEXT_NAME, nextButtonNode);
 	node->AddNodeToMap(UISPINNER_CONTENT_NAME, contentNode);
@@ -383,7 +383,8 @@ List<UIControl* > UISpinner::GetSubcontrols()
 	// Lookup for the contols by their names.
 	AddControlToList(subControls, UISPINNER_BUTTON_PREVIOUS_NAME);
 	AddControlToList(subControls, UISPINNER_BUTTON_NEXT_NAME);
-    
+	AddControlToList(subControls, UISPINNER_CONTENT_NAME);
+
 	return subControls;
 }
 
