8cd8c2d94e8b65ad56a3ab6a730af4ef065a519b
Aliaksey Baradulkin <a_baradulkin@wargaming.net>
2013-09-16 13:22:42 +0300

    [DF-291] Commit changes of new autotesting framework version
    Former-commit-id: 4849d4b93c490e925131f1e2ed3fd1ae661e46af

--- Sources/Internal/Autotesting/AutotestingDB.h
+++ Sources/Internal/Autotesting/AutotestingDB.h
@@ -20,18 +20,69 @@
 
 #ifdef __DAVAENGINE_AUTOTESTING__
 
+//#define AUTOTESTING_DB_HOST    "10.128.19.33"
+#define AUTOTESTING_DB_HOST    "by2-buildmachine.wargaming.net"
+//#define AUTOTESTING_DB_HOST    "10.128.128.5"
+//#define AUTOTESTING_DB_HOST    "192.168.1.2"
+#define AUTOTESTING_DB_PORT  27017
+#define AUTOTESTING_DB_NAME  "Autotesting"
+
 #include "Database/MongodbClient.h"
 
 #include "Autotesting/MongodbUpdateObject.h"
+#include "AutotestingSystem.h"
 
 namespace DAVA
 {
-
+class Image;
 
 class AutotestingDB : public Singleton<AutotestingDB>
 {
 public:
-	KeyedArchive *GetArchive(const String &name);
+	AutotestingDB();
+	~AutotestingDB();
+
+	bool ConnectToDB(const String &name);
+	
+
+	// Work with log object in DB
+	KeyedArchive *FindRunArchive(MongodbUpdateObject* dbUpdateObject, const String &auxArg);
+	KeyedArchive *FindOrInsertRunArchive(MongodbUpdateObject* dbUpdateObject, const String &auxArg);
+
+	KeyedArchive *InsertTestArchive(MongodbUpdateObject* dbUpdateObject, const String &testId, const String &testName, bool needClearGroup);
+	KeyedArchive *InsertStepArchive(KeyedArchive *testArchive, const String &stepId, const String &description);
+
+	KeyedArchive *FindOrInsertTestArchive(MongodbUpdateObject *dbUpdateObject, const String &testId);
+	KeyedArchive *FindOrInsertTestStepArchive(KeyedArchive *testArchive, const String &stepId);
+	KeyedArchive *FindOrInsertTestStepLogEntryArchive(KeyedArchive *testStepArchive, const String &logId);
+
+	// Getting and Setting data from/in DB
+	bool SaveToDB(MongodbUpdateObject *dbUpdateObject);
+
+	void Log(const String &level, const String &message);
+
+	String GetStringTestParameter(const String & deviceName, const String & parameter);
+	int32 GetIntTestParameter(const String & deviceName, const String & parameter);
+
+	String ReadString(const String & name);
+	void WriteString(const String & name, const String & text);
+
+	bool SaveKeyedArchiveToDB(const String &archiveName, KeyedArchive *archive, const String &docName);
+
+	void UploadScreenshot(const String & name, Image *image);
+
+	// multiplayer api
+	void WriteState(const String & device, const String & state);
+	void WriteCommand(const String & device, const String & state);
+
+	String ReadState(const String & device);
+	String ReadCommand(const String & device);
+
+
+protected:
+	MongodbClient *dbClient;
+
+
 };
 
 
