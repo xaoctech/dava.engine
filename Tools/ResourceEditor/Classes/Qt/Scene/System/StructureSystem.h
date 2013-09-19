34849973c633325e925c2789bb1d037cc09b24d1
smile4u <s_zdanevich@wargaming.net>
2013-09-19 19:13:17 +0300

    [DF-2018] Fixed bug, when reaload was done for each second item

--- Tools/ResourceEditor/Classes/Qt/Scene/System/StructureSystem.h
+++ Tools/ResourceEditor/Classes/Qt/Scene/System/StructureSystem.h
@@ -59,7 +59,7 @@ public:
 	void RemoveLayer(const DAVA::Vector<DAVA::ParticleLayer *> &layers);
 	void MoveForce(const DAVA::Vector<DAVA::ParticleForce *> &forces, const DAVA::Vector<DAVA::ParticleLayer *> &oldLayers, DAVA::ParticleLayer *newLayer);
 	void RemoveForce(const DAVA::Vector<DAVA::ParticleForce *> &forces, const DAVA::Vector<DAVA::ParticleLayer *> &layers);
-	void Reload(const EntityGroup *entityGroup, const DAVA::FilePath &newModelPath = "", bool saveLightmapSettings = false);
+	void Reload(const EntityGroup& entityGroup, const DAVA::FilePath &newModelPath = "", bool saveLightmapSettings = false);
 	void Add(const DAVA::FilePath &newModelPath, const DAVA::Vector3 pos = DAVA::Vector3());
 
 	void LockSignals(bool locked);
