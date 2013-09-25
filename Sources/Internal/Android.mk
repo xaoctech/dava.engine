68bfb6f3df02f5e59b7ff359efb9173824336d87
Aliaksey Baradulkin <a_baradulkin@wargaming.net>
2013-09-25 19:05:33 +0300

    [DF-291] Add autotesting to android project

--- Sources/Internal/Android.mk
+++ Sources/Internal/Android.mk
@@ -40,6 +40,7 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_SRC_FILES := \
                      $(subst $(LOCAL_PATH)/,, \
+					 $(wildcard $(LOCAL_PATH)/Autotesting/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Animation/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Base/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Collision/*.cpp) \
