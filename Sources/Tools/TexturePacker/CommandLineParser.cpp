01c8d73458da1667fab8924347430e0fe236829b
IgorSolovey <igorsolovey@Igors-MacBook-Pro-5.local>
2013-09-25 12:06:21 +0300

    [DF-1407] Update ResourcePacker executable for MacOS

--- Sources/Tools/TexturePacker/CommandLineParser.cpp
+++ Sources/Tools/TexturePacker/CommandLineParser.cpp
@@ -73,6 +73,7 @@ void CommandLineParser::SetArguments(const Vector<String> &arguments)
 void CommandLineParser::AddArgument(const String & arg)
 {
     flags.push_back(arg);
+    params.push_back(arg);
 }
     
 void CommandLineParser::SetArguments(int argc, char * argv[])
