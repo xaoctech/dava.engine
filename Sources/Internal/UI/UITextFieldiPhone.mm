7861e30ec9df880657dd6fada691384ff4980c59
Alexander Volchkevich <alexander.volchkevich@gmail.com>
2013-10-09 19:11:08 +0300

    [DF-2318] fixed UITextView focus

--- Sources/Internal/UI/UITextFieldiPhone.mm
+++ Sources/Internal/UI/UITextFieldiPhone.mm
@@ -106,6 +106,14 @@ float GetUITextViewSizeDivider()
 	return self;
 }
 
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if (cppTextField)
+    {
+        DAVA::UIControlSystem::Instance()->SetFocusedControl(cppTextField, false);
+    }
+}
+
 -(id)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
     id hitView = [super hitTest:point withEvent:event];
