10752e08c2ae9d55c3ccd3c90633bef3796cedd3
xKernel <Kernel@tut.by>
2013-10-16 15:55:39 +0300

    DF-2071 object updated no only climbs up the tree - moving down is now lazly done in tree update

--- Sources/Internal/Render/Highlevel/SpatialTree.h
+++ Sources/Internal/Render/Highlevel/SpatialTree.h
@@ -57,6 +57,7 @@ class QuadTree : public AbstractSpatialTree
 	Frustum *currFrustum;
 
 	List<int32> dirtyZNodes;    
+	List<RenderObject *> dirtyObjects;    
 	
 	/*to compare*/
 	int32 objFrustrumCalls;	
@@ -75,8 +76,10 @@ class QuadTree : public AbstractSpatialTree
 	uint16 FindObjectAddNode(uint16 startNodeId, const AABBox3& objBox);
 	
 	static const int32 RECALCULATE_Z_PER_FRAME = 10;
+	static const int32 RECALCULATE_OBJECTS_PER_FRAME = 10;
 	void RecalculateNodeZLimits(uint16 nodeId);
 	void MarkNodeDirty(uint16 nodeId);
+	void MarkObjectDirty(RenderObject *object);
 
 public:
 	QuadTree(const AABBox3 &worldBox, int32 maxTreeDepth);
