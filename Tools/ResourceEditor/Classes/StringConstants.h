0523c62d5787ce1767d14c1b19fcc9f3a5f33af1
xrayman <xrayman64@gmail.com>
2013-10-17 15:28:04 +0300

    DF-2363. Remove pick color button from tile preview widget

--- Tools/ResourceEditor/Classes/StringConstants.h
+++ Tools/ResourceEditor/Classes/StringConstants.h
@@ -203,6 +203,8 @@ namespace ResourceEditor
 
 	static const String SLIDER_WIDGET_CHANGE_VALUE_TOOLTIP			= "Double click to change value";
 	static const String SLIDER_WIDGET_CURRENT_VALUE					= "Current value";
+
+	static const String TILE_TEXTURE_PREVIEW_CHANGE_COLOR_TOOLTIP	= "Double click to change color";
 }
 
 #endif //#ifndef __STRING_CONSTANTS_H__
