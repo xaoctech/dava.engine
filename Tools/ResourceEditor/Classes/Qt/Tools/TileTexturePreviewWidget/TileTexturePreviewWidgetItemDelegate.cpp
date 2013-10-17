8a166065345bd81fc76ca82be552e366e7b116a3
xrayman <xrayman64@gmail.com>
2013-10-17 13:15:05 +0300

    DF-2362. Move tile texture preview widget to the Tools folder. Fix input to the color field

---
+++ Tools/ResourceEditor/Classes/Qt/Tools/TileTexturePreviewWidget/TileTexturePreviewWidgetItemDelegate.cpp
@@ -0,0 +1,31 @@
+#include "TileTexturePreviewWidgetItemDelegate.h"
+
+#include <QLineEdit>
+
+const QString TileTexturePreviewWidgetItemDelegate::TILE_COLOR_VALIDATE_REGEXP = "#[A-F0-9]{6}";
+
+TileTexturePreviewWidgetItemDelegate::TileTexturePreviewWidgetItemDelegate(QObject* parent /* = 0 */)
+:	QItemDelegate(parent)
+{
+}
+
+TileTexturePreviewWidgetItemDelegate::~TileTexturePreviewWidgetItemDelegate()
+{
+}
+
+QWidget* TileTexturePreviewWidgetItemDelegate:: createEditor(QWidget* parent,
+															 const QStyleOptionViewItem& option,
+															 const QModelIndex& index) const
+{
+	QWidget* widget = QItemDelegate::createEditor(parent, option, index);
+
+	QLineEdit* edit = qobject_cast<QLineEdit*>(widget);
+	if (edit != 0)
+	{
+		QRegExpValidator* validator = new QRegExpValidator();
+		validator->setRegExp(QRegExp(TILE_COLOR_VALIDATE_REGEXP, Qt::CaseInsensitive));
+		edit->setValidator(validator);
+	}
+
+	return widget;
+}
---
+++ Tools/ResourceEditor/Classes/Qt/Tools/TileTexturePreviewWidget/TileTexturePreviewWidgetItemDelegate.h
@@ -0,0 +1,20 @@
+#ifndef __RESOURCEEDITORQT__TILETEXTUREPREVIEWWIDGETITEMDELEGATE__
+#define __RESOURCEEDITORQT__TILETEXTUREPREVIEWWIDGETITEMDELEGATE__
+
+#include <QWidget>
+#include <QItemDelegate>
+
+class TileTexturePreviewWidgetItemDelegate: public QItemDelegate
+{
+public:
+	static const QString TILE_COLOR_VALIDATE_REGEXP;
+
+	TileTexturePreviewWidgetItemDelegate(QObject* parent = 0);
+	~TileTexturePreviewWidgetItemDelegate();
+
+	virtual QWidget* createEditor(QWidget* parent,
+								  const QStyleOptionViewItem& option,
+								  const QModelIndex& index) const;
+};
+
+#endif /* defined(__RESOURCEEDITORQT__TILETEXTUREPREVIEWWIDGETITEMDELEGATE__) */
