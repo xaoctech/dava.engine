301f34dd7d00b418e5b9867de2d1faf21f504a4d
dbespalov <mavisson@yandex.ru>
2013-10-18 11:47:56 +0300

    [DF-1627] Removed test data.
    Former-commit-id: fda0bfda64ec244715a41a9499957889ee220d8e

--- Projects/UnitTests/Classes/AlignTest.cpp
+++ Projects/UnitTests/Classes/AlignTest.cpp
@@ -179,8 +179,8 @@ void AlignTest::MakeScreenShot()
 void AlignTest::OnScreenShot(Image *testImage)
 {
 	//Use this code to generate new reference screenshots
-	FilePath workingPath = FileSystem::Instance()->GetCurrentWorkingDirectory();
-	ImageLoader::Save(testImage, workingPath + Format("Data/test%d.png", currenTestIndex));
+//	FilePath workingPath = FileSystem::Instance()->GetCurrentWorkingDirectory();
+//	ImageLoader::Save(testImage, workingPath + Format("Data/test%d.png", currenTestIndex));
 	VerifyTestImage(testImage);
 }
 
