c3eac69716c7ee2b9499470e59302f1f1ad0b68b
Valentine Ivanov <valentin_ivanov@wargaming.net>
2013-10-22 18:17:19 +0300

    [DF-29] no need to build shader to determine if the material has lighting
    Former-commit-id: 151e6f9b3f7fc6e845cabca58bd3c718d86323f1

--- Sources/Internal/Render/Highlevel/SpriteRenderBatch.cpp
+++ Sources/Internal/Render/Highlevel/SpriteRenderBatch.cpp
@@ -130,8 +130,10 @@ void SpriteRenderBatch::Draw(const FastName & ownerRenderPass, Camera * camera)
 
 	RenderManager::Instance()->SetMatrix(RenderManager::MATRIX_MODELVIEW, finalMatrix);
 
-	RenderManager::Instance()->SetRenderData(renderDataObject);
+	
 	material->BindMaterialTechnique(ownerRenderPass, camera);
+	RenderManager::Instance()->SetRenderData(renderDataObject);
+	RenderManager::Instance()->AttachRenderData();
 
 	RenderManager::Instance()->HWDrawArrays(PRIMITIVETYPE_TRIANGLESTRIP, spriteObject->GetFrame() * 4, 4);
 }
