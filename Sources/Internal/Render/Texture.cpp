2d7f001dac354683a8ff6b87d61cbd6253780b8f
klesch <victor.kleschenko@gmail.com>
2013-10-23 17:54:06 +0300

    [DF-185] Refactored CreatePink()
    Former-commit-id: 386a9a548e63d2dada8cc588bd9c510e367d7b2a

--- Sources/Internal/Render/Texture.cpp
+++ Sources/Internal/Render/Texture.cpp
@@ -173,9 +173,8 @@ Texture * Texture::Get(const FilePath & pathName)
 	return 0;
 }
 
-void Texture::AddToMap( Texture *tex, const FilePath & pathname)
+void Texture::AddToMap(Texture *tex)
 {
-    tex->relativePathname = pathname;
     if(!tex->relativePathname.IsEmpty())
     {
         textureMap[tex->relativePathname.GetAbsolutePathname()] = tex;
@@ -270,10 +269,15 @@ Texture * Texture::CreateTextFromData(PixelFormat format, uint8 * data, uint32 w
 	RenderManager::Instance()->UnlockNonMain();
     
 	if (!addInfo)
-        AddToMap(tx, Format("Text texture %d", textureFboCounter));
+    {
+        tx->relativePathname = Format("Text texture %d", textureFboCounter);
+    }
 	else
-        AddToMap(tx, Format("Text texture %d info:%s", textureFboCounter, addInfo));
-
+    {
+        tx->relativePathname = Format("Text texture %d info:%s", textureFboCounter, addInfo);
+    }
+    AddToMap(tx);
+    
 	textureFboCounter++;
 	return tx;
 }
@@ -675,7 +679,10 @@ Texture * Texture::CreateFromFile(const FilePath & pathName, TextureType typeHin
 	Texture * texture = PureCreate(pathName);
 	if(!texture)
 	{
-		texture = CreatePink(pathName, typeHint);
+		texture = CreatePink(typeHint);
+        texture->relativePathname = pathName;
+        
+        AddToMap(texture);
 	}
 
 	return texture;
@@ -698,7 +705,8 @@ Texture * Texture::PureCreate(const FilePath & pathName)
 	if(texture)
 	{
 		texture->loadedAsFile = gpuForLoading;
-		AddToMap(texture, descriptorPathname);
+        texture->relativePathname = descriptorPathname;
+		AddToMap(texture);
 	}
 
 	descriptor->Release();
@@ -823,7 +831,8 @@ Texture * Texture::CreateFBO(uint32 w, uint32 h, PixelFormat format, DepthFormat
 
 
     tx->isRenderTarget = true;
-	AddToMap(tx, Format("FBO texture %d", textureFboCounter));
+    tx->relativePathname = Format("FBO texture %d", textureFboCounter);
+	AddToMap(tx);
 	
 	textureFboCounter++;
 	
@@ -1057,12 +1066,10 @@ int32 Texture::GetDataSize() const
     return allocSize;
 }
 
-Texture * Texture::CreatePink(const FilePath &path, TextureType requestedType)
+Texture * Texture::CreatePink(TextureType requestedType)
 {
     Texture *tex = new Texture();
     tex->MakePink(requestedType);
-	
-    AddToMap(tex, path);
     
 	return tex;
 }
