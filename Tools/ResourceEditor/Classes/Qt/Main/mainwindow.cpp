bb52f8112a015a959b9807cf170e7e6f6622a87b
smile4u <s_zdanevich@wargaming.net>
2013-10-25 12:14:48 +0300

    [DF-185] hanging objects: decoration for button added

--- Tools/ResourceEditor/Classes/Qt/Main/mainwindow.cpp
+++ Tools/ResourceEditor/Classes/Qt/Main/mainwindow.cpp
@@ -463,6 +463,7 @@ void QtMainWindow::SetupToolBars()
 		hangingBtn->setMinimumWidth(ResourceEditor::DEFAULT_TOOLBAR_CONTROL_SIZE_WITH_ICON);
 		ui->sceneToolBar->addSeparator();
 		ui->sceneToolBar->addWidget(hangingBtn);
+		hangingBtn->setAutoRaise(false);
 	}
 
 	// outline by object type
@@ -482,8 +483,9 @@ void QtMainWindow::SetupToolBars()
 		}
 
 		objectTypesWidget->setCurrentIndex(ResourceEditor::ESOT_NONE + 1);
-
 		QObject::connect(objectTypesWidget, SIGNAL(currentIndexChanged(int)), this, SLOT(OnObjectsTypeChanged(int)));
+
+		ui->sceneToolBar->addSeparator();
 		ui->sceneToolBar->addWidget(objectTypesWidget);
 	}
 }
