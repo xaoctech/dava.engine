9e8cbbb558c603b32c59a75fa6923f81b13fca66
klesch <victor.kleschenko@gmail.com>
2013-11-06 10:57:49 +0300

    [DF-2660] Added colored background for short emitters
    Former-commit-id: c7af4319cec6081fe3cb8a566d41e78c08d2f878

--- Tools/ResourceEditor/Classes/Qt/DockSceneTree/SceneTreeItem.cpp
+++ Tools/ResourceEditor/Classes/Qt/DockSceneTree/SceneTreeItem.cpp
@@ -60,6 +60,9 @@ QVariant SceneTreeItem::data(int role) const
 	case EIDR_Data:
 		v = ItemData();
 		break;
+    case Qt::BackgroundColorRole:
+        v = ItemBackgroundColor();
+        break;
 	default:
 		break;
 	}
@@ -82,6 +85,12 @@ QIcon SceneTreeItem::ItemIcon() const
 {
 	static QIcon icon = QIcon(":/QtIcons/node.png");
 	return icon;
+}
+
+
+QVariant SceneTreeItem::ItemBackgroundColor() const
+{
+	return QStandardItem::data(Qt::BackgroundColorRole);
 }
 
 // =========================================================================================
@@ -197,6 +206,21 @@ QIcon SceneTreeItemEntity::ItemIcon() const
 
 	return ret;
 }
+
+QVariant SceneTreeItemEntity::ItemBackgroundColor() const
+{
+    if(NULL != entity)
+	{
+        DAVA::ParticleEmitter *emitter = DAVA::GetEmitter(entity);
+        if(emitter && emitter->IsShortEffect())
+        {
+            return QColor(245, 215, 210);
+        }
+    }
+    
+    return SceneTreeItem::ItemBackgroundColor();
+}
+
 
 void SceneTreeItemEntity::DoSync(QStandardItem *rootItem, DAVA::Entity *entity)
 {
