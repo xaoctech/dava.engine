78b7a8c3a2e40e16d3b4d4cfa3aea845a7668552
klesch <victor.kleschenko@gmail.com>
2013-11-22 18:05:31 +0300

    [DF-2720] Added Unit Test for Add/Remove Component functionality
    Former-commit-id: c85c0a97df083909123f71468fa6cde0dffe9edb

--- Projects/UnitTests/Classes/GameCore.cpp
+++ Projects/UnitTests/Classes/GameCore.cpp
@@ -88,38 +88,39 @@ void GameCore::OnAppStarted()
     CreateDocumentsFolder();
 
 	new ComponentsTest();
-// 	new UIMovieTest();
-// 	new InputTest();
-//     new FormatsTest();
-// 	new DeviceInfoTest();
-// 	new TransparentWebViewTest();
-//     new FilePathTest();
-//     new FileListTest();
-//     new FileSystemTest();
-//     new LocalizationTest();
-// 
-// 	new SampleTest();
-// 	new EntityTest(); 
-// 	new MemoryAllocatorsTest();
-// 	new HashMapTest();
-// //	new SoundTest();
-// 	new SplitTest();
-// 	new AlignTest();
-// 	new EMailTest();
-// 	new DPITest();
-// 	new MaterialCompilerTest();
-// 	new CloneTest();
-// 	new PVRTest();
-// 	new DXTTest();
-// 	new EntityTest();	
-// 	new MemoryAllocatorsTest();
-// 	new HashMapTest();
-// 	new KeyedArchiveYamlTest();
-// 	new DLCTest();
-// 	new UIListTest();
-// 	new UIScrollViewTest();
-// 
-//     new ThreadSyncTest();
+    
+ 	new UIMovieTest();
+ 	new InputTest();
+     new FormatsTest();
+ 	new DeviceInfoTest();
+ 	new TransparentWebViewTest();
+     new FilePathTest();
+     new FileListTest();
+     new FileSystemTest();
+     new LocalizationTest();
+ 
+ 	new SampleTest();
+ 	new EntityTest(); 
+ 	new MemoryAllocatorsTest();
+ 	new HashMapTest();
+ //	new SoundTest();
+ 	new SplitTest();
+ 	new AlignTest();
+ 	new EMailTest();
+ 	new DPITest();
+ 	new MaterialCompilerTest();
+ 	new CloneTest();
+ 	new PVRTest();
+ 	new DXTTest();
+ 	new EntityTest();	
+ 	new MemoryAllocatorsTest();
+ 	new HashMapTest();
+ 	new KeyedArchiveYamlTest();
+ 	new DLCTest();
+ 	new UIListTest();
+ 	new UIScrollViewTest();
+ 
+     new ThreadSyncTest();
     
     errors.reserve(TestCount());
 
