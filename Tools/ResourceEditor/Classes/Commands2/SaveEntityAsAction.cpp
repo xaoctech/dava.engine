b6fa2d9145555e9c2ce537f933597e122bb994cf
klesch <victor.kleschenko@gmail.com>
2013-11-22 15:07:18 +0300

    [DF-2747] Removed Reference to Owner during save entity as
    Former-commit-id: 2ce994f2171eaf6f1c779d181b2dfb3d17a3c826

--- Tools/ResourceEditor/Classes/Commands2/SaveEntityAsAction.cpp
+++ Tools/ResourceEditor/Classes/Commands2/SaveEntityAsAction.cpp
@@ -32,6 +32,7 @@
 #include "Scene3D/SceneFileV2.h"
 
 #include "Classes/Qt/Scene/SceneHelper.h"
+#include "Classes/StringConstants.h"
 
 
 SaveEntityAsAction::SaveEntityAsAction(const EntityGroup *_entities, const DAVA::FilePath &_path)
@@ -54,6 +55,12 @@ void SaveEntityAsAction::Redo()
 			DAVA::Entity *entity = entities->GetEntity(i);
 			DAVA::Entity *clone = entity->Clone();
 
+            DAVA::KeyedArchive *props = clone->GetCustomProperties();
+            if(props && props->IsKeyExists(ResourceEditor::EDITOR_REFERENCE_TO_OWNER))
+            {
+                props->DeleteKey(ResourceEditor::EDITOR_REFERENCE_TO_OWNER);
+            }
+            
 			scene->AddNode(clone);
 		}
 
