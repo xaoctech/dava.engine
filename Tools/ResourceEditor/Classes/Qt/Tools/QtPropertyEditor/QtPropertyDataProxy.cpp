388d4e48e5e33afdc7f12f5fa16ac1aa412e2da9
smile4u <s_zdanevich@wargaming.net>
2013-12-10 16:24:47 +0300

    [DF-2876] Added propertyEditor mode switching (normal/advances/favoritesOnly view, favorites edit On/Off)
    Former-commit-id: 7a97e25864069f39b31ae3492df809aae5b3755f

--- Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyDataProxy.cpp
+++ Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyDataProxy.cpp
@@ -25,3 +25,22 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
+
+#include "QtPropertyDataProxy.h"
+
+QtPropertyDataProxy::QtPropertyDataProxy(QtPropertyData *_original)
+: original(_original)
+{ }
+
+QtPropertyDataProxy::~QtPropertyDataProxy()
+{ }
+
+QtPropertyData* QtPropertyDataProxy::GetProxyOriginal()
+{
+	return original;
+}
+
+QVariant QtPropertyDataProxy::GetValueInternal() const
+{
+	return QString("Proxy");
+}
