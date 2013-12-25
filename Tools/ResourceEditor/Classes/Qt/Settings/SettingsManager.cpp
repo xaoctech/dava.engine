0b240d4925519c2e801281aecf07e74bb7fd0b96
amakovii <andrii.makovii@gmail.com>
2013-12-25 13:15:40 +0200

    [DF-2382] small refactoring of settings manager
    Former-commit-id: 8afa85f68dfef75f149d2ddd4f14dacf2db58caf

--- Tools/ResourceEditor/Classes/Qt/Settings/SettingsManager.cpp
+++ Tools/ResourceEditor/Classes/Qt/Settings/SettingsManager.cpp
@@ -51,6 +51,104 @@ SettingsManager::~SettingsManager()
 	SafeRelease(settings);
 }
 
+void SettingsManager::Initialize()
+{
+	settings->DeleteAllKeys();
+	KeyedArchive* generalSettings = new KeyedArchive();
+	KeyedArchive* defaultSettings = new KeyedArchive();
+	KeyedArchive* internalSettings = new KeyedArchive();
+	KeyedArchive* mutableLengthSettings = new KeyedArchive();
+	
+	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_GENERAL_MAP) / sizeof(SettingRow)); ++i )
+	{
+		generalSettings->SetVariant( SETTINGS_GROUP_GENERAL_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_GENERAL_MAP[i].defValue) );
+	}
+	
+	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_DEFAULT_MAP) / sizeof(SettingRow)); ++i )
+	{
+		defaultSettings->SetVariant( SETTINGS_GROUP_DEFAULT_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_DEFAULT_MAP[i].defValue) );
+	}
+	
+	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_INTERNAL_MAP) / sizeof(SettingRow)); ++i )
+	{
+		internalSettings->SetVariant( SETTINGS_GROUP_INTERNAL_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_INTERNAL_MAP[i].defValue) );
+	}
+	
+	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_VARIABLE_LENGHT_MAP) / sizeof(SettingRow)); ++i )
+	{
+		mutableLengthSettings->SetVariant( SETTINGS_GROUP_VARIABLE_LENGHT_MAP[i].key + MUTABLE_LENGTH_COUNT_SUFFIX,
+										  DAVA::VariantType(SETTINGS_GROUP_VARIABLE_LENGHT_MAP[i].defValue) );
+	}
+	
+	settings->SetArchive(GetNameOfGroup(GENERAL), generalSettings);
+	settings->SetArchive(GetNameOfGroup(DEFAULT), defaultSettings);
+	settings->SetArchive(GetNameOfGroup(INTERNAL), internalSettings);
+	settings->SetArchive(GetNameOfGroup(VARIABLE_LENGTH_SET), mutableLengthSettings);
+	settings->SetUInt32(SETTINGS_VERSION_KEY, SETTINGS_VERSION_NUMBER);
+	
+	LoadSettings();
+}
+
+void SettingsManager::LoadSettings()
+{
+	KeyedArchive* loadedArchive = new KeyedArchive();
+	if(!loadedArchive->Load(CONFIG_FILE))
+	{
+		return;
+	}
+	bool isSettingsFileVersionActual = loadedArchive->IsKeyExists(SETTINGS_VERSION_KEY);
+	// perform synchronization: update present in settings elements from file
+	for (uint32 i = 0; i < GROUPS_COUNT; ++i)
+	{
+		KeyedArchive* presentSettingsGroup = GetSettingsGroup((eSettingsGroups)i);
+		KeyedArchive* archiveFromFile = isSettingsFileVersionActual ?
+		loadedArchive->GetArchive(GetNameOfGroup((eSettingsGroups)i)) : loadedArchive;
+		
+		if(NULL == archiveFromFile)
+		{
+			continue;
+		}
+		
+		Map<String, VariantType*> predefinedSettingsGroupMap = presentSettingsGroup->GetArchieveData();
+		DAVA::Map<DAVA::String, DAVA::VariantType*>::iterator it = predefinedSettingsGroupMap.begin();
+		DAVA::Map<DAVA::String, DAVA::VariantType*>::iterator end = predefinedSettingsGroupMap.end();
+		
+		for(; it != end; ++it)
+		{
+			if(archiveFromFile->IsKeyExists(it->first))
+			{
+				presentSettingsGroup->SetVariant(it->first, *archiveFromFile->GetVariant(it->first));
+			}
+		}
+		//in case of mutable section, presentSettingsGroup contain only counter rows like "LastOpenedFilesCount"
+		if(VARIABLE_LENGTH_SET == (eSettingsGroups)i && presentSettingsGroup->Count())
+		{
+			Map<String, VariantType*> predefinedMapUpdated = presentSettingsGroup->GetArchieveData();
+			it = predefinedMapUpdated.begin();
+			end = predefinedMapUpdated.end();
+			Vector<std::pair<String, VariantType> > setToInsert;
+			for(; it != end; ++it)
+			{
+				int32 subsectionLength = it->second->AsInt32();
+				String subSectionName = it->first.substr(0, it->first.size() - strlen(MUTABLE_LENGTH_COUNT_SUFFIX));
+				for (uint32 j = 0; j < subsectionLength; ++j)
+				{
+					String keyToInsert(Format((subSectionName + "_%d").c_str(), j));
+					setToInsert.push_back(std::make_pair(keyToInsert, *archiveFromFile->GetVariant(keyToInsert)));
+				}
+			}
+			Vector<std::pair<String, VariantType> >::iterator iterToInsert = setToInsert.begin();
+			Vector<std::pair<String, VariantType> >::iterator endToInsert = setToInsert.end();
+			for(; iterToInsert != endToInsert; ++iterToInsert)
+			{
+				presentSettingsGroup->SetVariant(iterToInsert->first, iterToInsert->second);
+			}
+		}
+	}
+	
+	SafeRelease(loadedArchive);
+}
+
 DAVA::VariantType* SettingsManager::GetValue(const DAVA::String& _name, eSettingsGroups _group)
 {
 	KeyedArchive* foundedGroupSettings = settings->GetArchive(GetNameOfGroup(_group));
@@ -106,104 +204,6 @@ void SettingsManager::UpdateVariableSectionIfNeeded(const String& name,  const D
 			mutableSection->DeleteKey(keyToDelete);
 		}
 	}
-}
-
-void SettingsManager::Initialize()
-{
-	settings->DeleteAllKeys();
-	KeyedArchive* generalSettings = new KeyedArchive();
-	KeyedArchive* defaultSettings = new KeyedArchive();
-	KeyedArchive* internalSettings = new KeyedArchive();
-	KeyedArchive* mutableLengthSettings = new KeyedArchive();
-	
-	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_GENERAL_MAP) / sizeof(SettingRow)); ++i )
-	{
-		generalSettings->SetVariant( SETTINGS_GROUP_GENERAL_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_GENERAL_MAP[i].defValue) );
-	}
-
-	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_DEFAULT_MAP) / sizeof(SettingRow)); ++i )
-	{
-		defaultSettings->SetVariant( SETTINGS_GROUP_DEFAULT_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_DEFAULT_MAP[i].defValue) );
-	}
-
-	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_INTERNAL_MAP) / sizeof(SettingRow)); ++i )
-	{
-		internalSettings->SetVariant( SETTINGS_GROUP_INTERNAL_MAP[i].key, DAVA::VariantType(SETTINGS_GROUP_INTERNAL_MAP[i].defValue) );
-	}
-	
-	for (uint32 i = 0; i < (sizeof(SETTINGS_GROUP_VARIABLE_LENGHT_MAP) / sizeof(SettingRow)); ++i )
-	{
-		mutableLengthSettings->SetVariant( SETTINGS_GROUP_VARIABLE_LENGHT_MAP[i].key + MUTABLE_LENGTH_COUNT_SUFFIX,
-			DAVA::VariantType(SETTINGS_GROUP_VARIABLE_LENGHT_MAP[i].defValue) );
-	}
-	
-	settings->SetArchive(GetNameOfGroup(GENERAL), generalSettings);
-	settings->SetArchive(GetNameOfGroup(DEFAULT), defaultSettings);
-	settings->SetArchive(GetNameOfGroup(INTERNAL), internalSettings);
-	settings->SetArchive(GetNameOfGroup(VARIABLE_LENGTH_SET), mutableLengthSettings);
-	settings->SetUInt32(SETTINGS_VERSION_KEY, SETTINGS_VERSION_NUMBER);
-
-	LoadSettings();
-}
-
-void SettingsManager::LoadSettings()
-{
-	KeyedArchive* loadedArchive = new KeyedArchive();
-	if(!loadedArchive->Load(CONFIG_FILE))
-	{
-		return;
-	}
-	bool isSettingsFileVersionActual = loadedArchive->IsKeyExists(SETTINGS_VERSION_KEY);
-	// perform synchronization: update present in settings elements from file
-	for (uint32 i = 0; i < GROUPS_COUNT; ++i)
-	{
-		KeyedArchive* presentSettingsGroup = GetSettingsGroup((eSettingsGroups)i);
-		KeyedArchive* archiveFromFile = isSettingsFileVersionActual ? 
-			 loadedArchive->GetArchive(GetNameOfGroup((eSettingsGroups)i)) : loadedArchive;
-	
-		if(NULL == archiveFromFile)
-		{
-			continue;
-		}
-		
-		Map<String, VariantType*> predefinedSettingsGroupMap = presentSettingsGroup->GetArchieveData();
-		DAVA::Map<DAVA::String, DAVA::VariantType*>::iterator it = predefinedSettingsGroupMap.begin();
-		DAVA::Map<DAVA::String, DAVA::VariantType*>::iterator end = predefinedSettingsGroupMap.end();
-	
-		for(; it != end; ++it)
-		{
-			if(archiveFromFile->IsKeyExists(it->first))
-			{
-				presentSettingsGroup->SetVariant(it->first, *archiveFromFile->GetVariant(it->first));
-			}
-		}
-		//in case of mutable section, presentSettingsGroup contain only counter rows like "LastOpenedFilesCount"
-		if(VARIABLE_LENGTH_SET == (eSettingsGroups)i && presentSettingsGroup->Count())
-		{
-			Map<String, VariantType*> predefinedMapUpdated = presentSettingsGroup->GetArchieveData();
-			it = predefinedMapUpdated.begin();
-			end = predefinedMapUpdated.end();
-			Vector<std::pair<String, VariantType> > setToInsert;
-			for(; it != end; ++it)
-			{
-				int32 subsectionLength = it->second->AsInt32();
-				String subSectionName = it->first.substr(0, it->first.size() - strlen(MUTABLE_LENGTH_COUNT_SUFFIX));
-				for (uint32 j = 0; j < subsectionLength; ++j)
-				{
-					String keyToInsert(Format((subSectionName + "_%d").c_str(), j));
-					setToInsert.push_back(std::make_pair(keyToInsert, *archiveFromFile->GetVariant(keyToInsert)));
-				}
-			}
-			Vector<std::pair<String, VariantType> >::iterator iterToInsert = setToInsert.begin();
-			Vector<std::pair<String, VariantType> >::iterator endToInsert = setToInsert.end();
-			for(; iterToInsert != endToInsert; ++iterToInsert)
-			{
-				presentSettingsGroup->SetVariant(iterToInsert->first, iterToInsert->second);
-			}
-		}
-	}
-
-	SafeRelease(loadedArchive);
 }
 
 void SettingsManager::SaveSettings()
