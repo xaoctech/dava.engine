61005d107a6be0e54e61012599afecffb06fc946
smile4u <s_zdanevich@wargaming.net>
2013-12-27 16:08:00 +0300

    [DF-2842] Restored functionality after material changes
    Former-commit-id: 429cc1e90916105a2b9dbb96d24e2e212417af43

--- Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyData/QtPropertyDataIntrospection.cpp
+++ Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyData/QtPropertyDataIntrospection.cpp
@@ -71,10 +71,6 @@ const DAVA::MetaInfo * QtPropertyDataIntrospection::MetaInfo() const
 
 QtPropertyData * QtPropertyDataIntrospection::CreateMemberData(void *_object, const DAVA::InspMember *member)
 {
-	DVASSERT(false && "Implement for new materials!");
-	return NULL;
-	
-	/*
 	void *memberObject = member->Data(_object);
 	int memberFlags = member->Flags();
 	const DAVA::MetaInfo *memberMetaInfo = member->Type();
@@ -169,7 +165,6 @@ QtPropertyData * QtPropertyDataIntrospection::CreateMemberData(void *_object, co
 	}
 
 	return retData;
-	 */
 }
 
 void QtPropertyDataIntrospection::AddMember(const DAVA::InspMember *member)
