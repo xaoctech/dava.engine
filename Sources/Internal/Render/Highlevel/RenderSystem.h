8684f45e44d0e034b8e36f395781384f194275d4
binaryzebra <vitaliy.borodovsky@gmail.com>
2013-12-31 11:21:19 +0300

    [DF-3037] Changed GetShadowRectColor from non-const to const
    Former-commit-id: 5ef80ff96f688746b735b14f052e6bc89a1cb708

--- Sources/Internal/Render/Highlevel/RenderSystem.h
+++ Sources/Internal/Render/Highlevel/RenderSystem.h
@@ -64,11 +64,11 @@ public:
     /**
         \brief Get Render Layer Manager to have ability to get all render layers from RenderSystem.
      */
-    RenderLayerManager * GetRenderLayerManager() { return &renderLayerManager; }
+    const RenderLayerManager * GetRenderLayerManager() const { return &renderLayerManager; }
     /**
         \brief Get Render Pass Manager to have ability to get all render passes from RenderSystem.
      */
-    RenderPassManager * GetRenderPassManager() { return &renderPassManager; };
+    const RenderPassManager * GetRenderPassManager() const { return &renderPassManager; };
     /**
         \brief Get Render Hierarchy. It allow you to work with current render hierarchy and perform all main tasks with geometry on the level.
      */
@@ -139,7 +139,7 @@ public:
 	//RenderPass * GetRenderPass(const FastName & passName);
     
     void SetShadowRectColor(const Color &color);
-    const Color & GetShadowRectColor();
+    const Color & GetShadowRectColor() const;
 	void SetShadowBlendMode(ShadowPassBlendMode::eBlend blendMode);
 	ShadowPassBlendMode::eBlend GetShadowBlendMode();
 	
