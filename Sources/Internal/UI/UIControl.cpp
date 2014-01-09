b70c0fa4bcc2c9e3d5e84e0ec74a9a0267d57bca
mavisson <mavisson@yandex.ru>
2014-01-09 12:18:52 +0200

    [ WOTGM-476] Now we do not draw child controls with "invisible" parent. Also we disable input for these controls.
    Former-commit-id: 24dee4a76988c0ca148cc3e9cec43a2f35773a41

--- Sources/Internal/UI/UIControl.cpp
+++ Sources/Internal/UI/UIControl.cpp
@@ -1585,13 +1585,18 @@ namespace DAVA
 		}
 		DrawPivotPoint(unrotatedRect);
 		
-		isIteratorCorrupted = false;
-		List<UIControl*>::iterator it = childs.begin();
-        List<UIControl*>::iterator itEnd = childs.end();
-		for(; it != itEnd; ++it)
+		
+		// Do not draw child controls if parent is not visible
+		if (visible && visibleForUIEditor)
 		{
-			(*it)->SystemDraw(drawData);
-			DVASSERT(!isIteratorCorrupted);
+			isIteratorCorrupted = false;
+			List<UIControl*>::iterator it = childs.begin();
+			List<UIControl*>::iterator itEnd = childs.end();
+			for(; it != itEnd; ++it)
+			{
+				(*it)->SystemDraw(drawData);
+				DVASSERT(!isIteratorCorrupted);
+			}
 		}
 		
 		if(visible && visibleForUIEditor)
@@ -1944,36 +1949,39 @@ namespace DAVA
 					return false;
 				}
 			}
-
-			List<UIControl*>::reverse_iterator it = childs.rbegin();
-			List<UIControl*>::reverse_iterator itEnd = childs.rend();
-			for(; it != itEnd; ++it)
+			// Do not handle input for child controls which are insibible from parent
+			if (visible)
 			{
-				(*it)->isUpdated = false;
-			}
-			
-			it = childs.rbegin();
-			itEnd = childs.rend();
-			while(it != itEnd)
-			{
-				isIteratorCorrupted = false;
-				UIControl *current = *it;
-				if(!current->isUpdated)
+				List<UIControl*>::reverse_iterator it = childs.rbegin();
+				List<UIControl*>::reverse_iterator itEnd = childs.rend();
+				for(; it != itEnd; ++it)
 				{
-					current->Retain();
-					if(current->SystemInput(currentInput))
-					{
-						current->Release();
-						return true;
-					}
-					current->Release();
-					if(isIteratorCorrupted)
-					{
-						it = childs.rbegin();
-						continue;
-					}
+					(*it)->isUpdated = false;
 				}
-				++it;
+			
+				it = childs.rbegin();
+				itEnd = childs.rend();
+				while(it != itEnd)
+				{
+					isIteratorCorrupted = false;
+					UIControl *current = *it;
+					if(!current->isUpdated)
+					{
+						current->Retain();
+						if(current->SystemInput(currentInput))
+						{
+							current->Release();
+							return true;
+						}
+						current->Release();
+						if(isIteratorCorrupted)
+						{
+							it = childs.rbegin();
+							continue;
+						}
+					}
+					++it;
+				}
 			}
 		}
 		return SystemProcessInput(currentInput);
