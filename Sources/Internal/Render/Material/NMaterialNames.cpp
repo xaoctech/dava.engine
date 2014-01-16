79803c310e16763d9953bfcb61c89fe9a990c49b
Valentine Ivanov <valentin_ivanov@wargaming.net>
2014-01-16 13:32:28 +0300

    [DF-3061] material names to separate file

---
+++ Sources/Internal/Render/Material/NMaterialNames.cpp
@@ -0,0 +1,68 @@
+/*==================================================================================
+ Copyright (c) 2008, binaryzebra
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the binaryzebra nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE binaryzebra AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL binaryzebra BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ =====================================================================================*/
+
+#include "Render/Material/NMaterialNames.h"
+
+namespace DAVA
+{
+    const FastName NMaterialName::DECAL_ALPHABLEND = FastName("~res:/Materials/Legacy/Decal.Alphablend.material");
+    const FastName NMaterialName::PIXELLIT_SPECULARMAP_ALPHATEST = FastName("~res:/Materials/Legacy/PixelLit.SpecularMap.Alphatest.material");
+    const FastName NMaterialName::TEXTURED_ALPHABLEND = FastName("~res:/Materials/Legacy/Textured.Alphablend.material");
+    const FastName NMaterialName::DECAL_ALPHATEST = FastName("~res:/Materials/Legacy/Decal.Alphatest.material");
+    const FastName NMaterialName::PIXELLIT_SPECUALRMAP_OPAQUE = FastName("~res:/Materials/Legacy/PixelLit.SpecularMap.Opaque.material");
+    const FastName NMaterialName::TEXTURED_ALPHATEST = FastName("~res:/Materials/Legacy/Textured.Alphatest.material");
+    const FastName NMaterialName::DECAL_OPAQUE = FastName("~res:/Materials/Legacy/Decal.Opaque.material");
+    const FastName NMaterialName::SHADOWRECT_ALPHA = FastName("~res:/Materials/Legacy/ShadowRectAlpha.material");
+    const FastName NMaterialName::TEXTURED_OPAQUE = FastName("~res:/Materials/Legacy/Textured.Opaque.material");
+    const FastName NMaterialName::DETAIL_ALPHABLEND = FastName("~res:/Materials/Legacy/Detail.Alphablend.material");
+    const FastName NMaterialName::SHADOWRECT_MULTIPLY = FastName("~res:/Materials/Legacy/ShadowRectMultiply.material");
+    const FastName NMaterialName::TILE_MASK = FastName("~res:/Materials/Legacy/TileMask.material");
+    const FastName NMaterialName::DETAIL_ALPHATEST = FastName("~res:/Materials/Legacy/Detail.Alphatest.material");
+    const FastName NMaterialName::SHADOW_VOLUME = FastName("~res:/Materials/Legacy/ShadowVolume.material");
+    const FastName NMaterialName::VERTEXCOLOR_ALPHABLEND = FastName("~res:/Materials/Legacy/VertexColor.Alphablend.material");
+    const FastName NMaterialName::DETAIL_OPAQUE = FastName("~res:/Materials/Legacy/Detail.Opaque.material");
+    const FastName NMaterialName::SILHOUETTE = FastName("~res:/Materials/Legacy/Silhouette.material");
+    const FastName NMaterialName::VERTEXCOLOR_FRAMEBLEND_ALPHABLEND = FastName("~res:/Materials/Legacy/VertexColor.FrameBlend.Alphablend.material");
+    const FastName NMaterialName::SKYBOX = FastName("~res:/Materials/Legacy/Skybox.material");
+    const FastName NMaterialName::VERTEXCOLOR_FRAMEBLEND_OPAQUE = FastName("~res:/Materials/Legacy/VertexColor.FrameBlend.Opaque.material");
+    const FastName NMaterialName::PIXELLIT_ALPHATEST = FastName("~res:/Materials/Legacy/PixelLit.Alphatest.material");
+    const FastName NMaterialName::SPEEDTREE_LEAF = FastName("~res:/Materials/Legacy/SpeedTreeLeaf.material");
+    const FastName NMaterialName::VERTEXCOLOR_OPAQUE = FastName("~res:/Materials/Legacy/VertexColor.Opaque.material");
+    const FastName NMaterialName::PIXELLIT_OPAQUE = FastName("~res:/Materials/Legacy/PixelLit.Opaque.material");
+    const FastName NMaterialName::TEXTURE_LIGHTMAP_ALPHABLEND = FastName("~res:/Materials/Legacy/TextureLightmap.Alphablend.material");
+    const FastName NMaterialName::VERTEXLIT_ALPHATEST = FastName("~res:/Materials/Legacy/VertexLit.Alphatest.material");
+    const FastName NMaterialName::PIXELLIT_SPECULAR_ALPHATEST = FastName("~res:/Materials/Legacy/PixelLit.Specular.Alphatest.material");
+    const FastName NMaterialName::TEXTURE_LIGHTMAP_ALPHATEST = FastName("~res:/Materials/Legacy/TextureLightmap.Alphatest.material");
+    const FastName NMaterialName::VERTEXLIT_OPAQUE = FastName("~res:/Materials/Legacy/VertexLit.Opaque.material");
+    const FastName NMaterialName::PIXELLIT_SPECULAR_OPAQUE = FastName("~res:/Materials/Legacy/PixelLit.Specular.Opaque.material");
+    const FastName NMaterialName::TEXTURE_LIGHTMAP_OPAQUE = FastName("~res:/Materials/Legacy/TextureLightmap.Opaque.material");
+    
+    const FastName NMaterialName::PARTICLES = FastName("~res:/Materials/Legacy/Particles/Particles.material");
+    const FastName NMaterialName::PARTICLES_FRAMEBLEND = FastName("~res:/Materials/Legacy/Particles/ParticlesFrameBlend.material");
+  
+};
