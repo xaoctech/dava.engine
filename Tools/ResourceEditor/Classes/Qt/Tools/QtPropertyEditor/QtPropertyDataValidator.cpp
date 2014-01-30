bc22b2b76cb8f7abf963a5e9c4e212cddb10633c
amakovii <andrii.makovii@gmail.com>
2014-01-30 17:03:10 +0200

    [DF-2722] fix review issues: move new class to necessary folder

--- Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyDataValidator.cpp
+++ Tools/ResourceEditor/Classes/Qt/Tools/QtPropertyEditor/QtPropertyDataValidator.cpp
@@ -26,35 +26,14 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-
-
-#ifndef __INT_RANGE_VALIDATOR_H__
-#define __INT_RANGE_VALIDATOR_H__
-
 #include "QtPropertyDataValidator.h"
-#include <QIntValidator>
 
-class IntRangeValidator : public QtPropertyDataValidator
+bool QtPropertyDataValidator::Validate(const QVariant &v) const
 {
-public:
-    
-    IntRangeValidator(int minValue, int maxValue);
-	
-	void SetRange(int minValue, int maxValue);
-	
-	int GetMaximum() const;
-	void SetMaximum(int maxValue);
-	
-	int GetMinimum() const;
-	void SetMinimum(int minValue);
-   
-protected:
-
-    virtual bool ValidateInternal(const QVariant &v) const;
-
-private:
-    
-    QIntValidator  innerValidator;
-};
-
-#endif // __INT_RANGE_VALIDATOR_H__
+    bool ret = ValidateInternal(v);
+    if(!ret)
+    {
+        ErrorNotifyInternal(v);
+    }
+    return ret;
+}
