d8e9b8ac753459b2efb1c8c310b79b53f21fd32f
Hottych <alexey@davaconsulting.com>
2014-02-06 11:18:45 +0200

    DF-3431 inputs in UIControls optimized
    Former-commit-id: 9e1991479bfc74cd816baf867a742f81af70e913

--- Sources/Internal/UI/UIHierarchy.cpp
+++ Sources/Internal/UI/UIHierarchy.cpp
@@ -542,7 +542,7 @@ void UIHierarchy::Input(UIEvent *currentInput)
 
 bool UIHierarchy::SystemInput(UIEvent *currentInput)
 {
-    if(!inputEnabled || !visible || controlState & STATE_DISABLED)
+    if(!GetInputEnabled() || !visible || controlState & STATE_DISABLED)
     {
         return false;
     }
