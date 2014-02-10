3483b7e333ebcbb7d8052f5c3a176e9f3899bd3b
d_shpakov <d_shpakov@wargaming.net>
2014-02-10 18:08:38 +0300

    DF-3475 replaced String name with FastName name, added find and operator < to FastName to use it instead of string

--- Sources/Internal/Scene3D/SceneFileV2.cpp
+++ Sources/Internal/Scene3D/SceneFileV2.cpp
@@ -733,7 +733,7 @@ bool SceneFileV2::RemoveEmptyHierarchy(Entity * currentNode)
             if (parent)
             {
                 Entity * childNode = SafeRetain(currentNode->GetChild(0));
-                String currentName = currentNode->GetName();
+                FastName currentName = currentNode->GetName();
 				KeyedArchive * currentProperties = currentNode->GetCustomProperties();
                 
                 //Logger::FrameworkDebug("remove node: %s %p", currentNode->GetName().c_str(), currentNode);
@@ -831,7 +831,7 @@ bool SceneFileV2::ReplaceNodeAfterLoad(Entity * node)
             }
         }
         
-        mesh->SetOwnerDebugInfo(oldMeshInstanceNode->GetName());
+        mesh->SetOwnerDebugInfo(String(oldMeshInstanceNode->GetName().c_str()));
         
         //
         Entity * parent = oldMeshInstanceNode->GetParent();
@@ -853,7 +853,7 @@ bool SceneFileV2::ReplaceNodeAfterLoad(Entity * node)
                 newShadowVolume->SetPolygonGroup(pg);
                 mesh->AddRenderBatch(newShadowVolume);
                 
-                mesh->SetOwnerDebugInfo(oldMeshInstanceNode->GetName() + " shadow:" + oldShadowVolumeNode->GetName());
+                mesh->SetOwnerDebugInfo(Format("%s shadow:%s", oldMeshInstanceNode->GetName().c_str(), oldShadowVolumeNode->GetName().c_str()));
                 
                 parent->RemoveNode(oldShadowVolumeNode);
                 SafeRelease(newShadowVolume);
