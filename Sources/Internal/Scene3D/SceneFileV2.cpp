754a9304db7d6549c5bfebb8925971f04b840c6e
klesch <victor@davaconsulting.com>
2014-02-19 15:23:42 +0300

    [DF-3599] Fixed opeing of switch nodes from Editor
    Former-commit-id: 4221c70e42357978b3c80d41819b00e54b79ac7b

--- Sources/Internal/Scene3D/SceneFileV2.cpp
+++ Sources/Internal/Scene3D/SceneFileV2.cpp
@@ -1192,6 +1192,11 @@ void SceneFileV2::OptimizeScene(Entity * rootNode)
     Logger::FrameworkDebug("nodes removed: %d before: %d, now: %d, diff: %d", removedNodeCount, beforeCount, nowCount, beforeCount - nowCount);
 }
 
+void SceneFileV2::SetVersion( int32 version )
+{
+	header.version = version;
+}
+
 
 
 SceneArchive::~SceneArchive()
