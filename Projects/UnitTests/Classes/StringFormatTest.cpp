3bcd6169cfc5da482271ae773a9a4af85b699f1b
Alexey Strokachuk <leronxp@gmail.com>
2014-03-14 13:08:57 +0300

    [DF-3105] Added unit test for DAVA::Format

--- Projects/UnitTests/Classes/StringFormatTest.cpp
+++ Projects/UnitTests/Classes/StringFormatTest.cpp
@@ -48,41 +48,29 @@ void StringFormatTest::IntegerTestFunction( PerfFuncData * data )
 void StringFormatTest::FloatTestFunction( PerfFuncData * data )
 {
     WideString formatStr1 = L"%f"   ;
-    WideString formatStr2 = L"%10.f";
-    WideString formatStr3 = L" %9.f";
-    WideString formatStr4 = L" %8.f";
-    WideString formatStr5 = L" %7.f";
-    WideString formatStr6 = L" %6.f";
-    WideString formatStr7 = L" %5.f";
-    WideString formatStr8 = L" %4.f";
-    WideString formatStr9 = L" %3.f";
-    WideString formatStr10= L" %2.f";
-    WideString formatStr11= L" %1.f";
-    WideString formatStr12= L" %0.f";
-    WideString formatStr13= L"%.10f";
-    WideString formatStr14= L" %.9f";
-    WideString formatStr15= L" %.8f";
-    WideString formatStr16= L" %.7f";
-    WideString formatStr17= L" %.6f";
-    WideString formatStr18= L" %.5f";
-    WideString formatStr19= L" %.4f";
-    WideString formatStr20= L" %.3f";
-    WideString formatStr21= L" %.2f";
-    WideString formatStr22= L" %.1f";
-    WideString formatStr23= L" %.0f";
-    WideString formatStr24= L"%10.10f";
-    WideString formatStr25= L" %9.9f";
-    WideString formatStr26= L" %8.8f";
-    WideString formatStr27= L" %7.7f";
-    WideString formatStr28= L" %6.6f";
-    WideString formatStr29= L" %5.5f";
-    WideString formatStr30= L" %4.4f";
-    WideString formatStr31= L" %3.3f";
-    WideString formatStr32= L" %2.2f";
-    WideString formatStr33= L" %1.1f";
-    WideString formatStr34= L" %0.0f";
-    float32 value1 = 123456789.123456789f;
-    float32 value2 = 987654321.987654321f;
+    WideString formatStr2 = L"%6.f";
+    WideString formatStr3 = L"%5.f";
+    WideString formatStr4 = L"%4.f";
+    WideString formatStr5 = L"%3.f";
+    WideString formatStr6 = L"%2.f";
+    WideString formatStr7 = L"%1.f";
+    WideString formatStr8 = L"%0.f";
+    WideString formatStr9 = L"%.6f";
+    WideString formatStr10= L"%.5f";
+    WideString formatStr11= L"%.4f";
+    WideString formatStr12= L"%.3f";
+    WideString formatStr13= L"%.2f";
+    WideString formatStr14= L"%.1f";
+    WideString formatStr15= L"%.0f";
+    WideString formatStr16= L"%6.6f";
+    WideString formatStr17= L"%5.5f";
+    WideString formatStr18= L"%4.4f";
+    WideString formatStr19= L"%3.3f";
+    WideString formatStr20= L"%2.2f";
+    WideString formatStr21= L"%1.1f";
+    WideString formatStr22= L"%0.0f";
+    float32 value1 = 1234.1234f;
+    float32 value2 = 876.876f;
 
     TEST_VERIFY( Format(formatStr1.c_str() , value1) == StringToWString( Format( WStringToString(formatStr1 ).c_str(), value1) ) );
     TEST_VERIFY( Format(formatStr2.c_str() , value1) == StringToWString( Format( WStringToString(formatStr2 ).c_str(), value1) ) );
@@ -106,19 +94,7 @@ void StringFormatTest::FloatTestFunction( PerfFuncData * data )
     TEST_VERIFY( Format(formatStr20.c_str(), value1) == StringToWString( Format( WStringToString(formatStr20).c_str(), value1) ) );
     TEST_VERIFY( Format(formatStr21.c_str(), value1) == StringToWString( Format( WStringToString(formatStr21).c_str(), value1) ) );
     TEST_VERIFY( Format(formatStr22.c_str(), value1) == StringToWString( Format( WStringToString(formatStr22).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr23.c_str(), value1) == StringToWString( Format( WStringToString(formatStr23).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr24.c_str(), value1) == StringToWString( Format( WStringToString(formatStr24).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr25.c_str(), value1) == StringToWString( Format( WStringToString(formatStr25).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr26.c_str(), value1) == StringToWString( Format( WStringToString(formatStr26).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr27.c_str(), value1) == StringToWString( Format( WStringToString(formatStr27).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr28.c_str(), value1) == StringToWString( Format( WStringToString(formatStr28).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr29.c_str(), value1) == StringToWString( Format( WStringToString(formatStr29).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr30.c_str(), value1) == StringToWString( Format( WStringToString(formatStr30).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr31.c_str(), value1) == StringToWString( Format( WStringToString(formatStr31).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr32.c_str(), value1) == StringToWString( Format( WStringToString(formatStr32).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr33.c_str(), value1) == StringToWString( Format( WStringToString(formatStr33).c_str(), value1) ) );
-    TEST_VERIFY( Format(formatStr34.c_str(), value1) == StringToWString( Format( WStringToString(formatStr34).c_str(), value1) ) );
-
+    
     TEST_VERIFY( Format(formatStr1.c_str() , value2) == StringToWString( Format( WStringToString(formatStr1 ).c_str(), value2) ) );
     TEST_VERIFY( Format(formatStr2.c_str() , value2) == StringToWString( Format( WStringToString(formatStr2 ).c_str(), value2) ) );
     TEST_VERIFY( Format(formatStr3.c_str() , value2) == StringToWString( Format( WStringToString(formatStr3 ).c_str(), value2) ) );
@@ -141,26 +117,14 @@ void StringFormatTest::FloatTestFunction( PerfFuncData * data )
     TEST_VERIFY( Format(formatStr20.c_str(), value2) == StringToWString( Format( WStringToString(formatStr20).c_str(), value2) ) );
     TEST_VERIFY( Format(formatStr21.c_str(), value2) == StringToWString( Format( WStringToString(formatStr21).c_str(), value2) ) );
     TEST_VERIFY( Format(formatStr22.c_str(), value2) == StringToWString( Format( WStringToString(formatStr22).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr23.c_str(), value2) == StringToWString( Format( WStringToString(formatStr23).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr24.c_str(), value2) == StringToWString( Format( WStringToString(formatStr24).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr25.c_str(), value2) == StringToWString( Format( WStringToString(formatStr25).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr26.c_str(), value2) == StringToWString( Format( WStringToString(formatStr26).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr27.c_str(), value2) == StringToWString( Format( WStringToString(formatStr27).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr28.c_str(), value2) == StringToWString( Format( WStringToString(formatStr28).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr29.c_str(), value2) == StringToWString( Format( WStringToString(formatStr29).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr30.c_str(), value2) == StringToWString( Format( WStringToString(formatStr30).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr31.c_str(), value2) == StringToWString( Format( WStringToString(formatStr31).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr32.c_str(), value2) == StringToWString( Format( WStringToString(formatStr32).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr33.c_str(), value2) == StringToWString( Format( WStringToString(formatStr33).c_str(), value2) ) );
-    TEST_VERIFY( Format(formatStr34.c_str(), value2) == StringToWString( Format( WStringToString(formatStr34).c_str(), value2) ) );
-
+ 
     float32 value3 = 0.1234f;
     float32 value4 = 0.2567f;
     float32 value5 = 0.5f;
     float32 value6 = 0.7543f;
-    TEST_VERIFY( Format(formatStr23.c_str(), value3) == StringToWString( Format( WStringToString(formatStr23).c_str(), value3) ) );
-    TEST_VERIFY( Format(formatStr23.c_str(), value4) == StringToWString( Format( WStringToString(formatStr23).c_str(), value4) ) );
-    TEST_VERIFY( Format(formatStr23.c_str(), value5) == StringToWString( Format( WStringToString(formatStr23).c_str(), value5) ) );
-    TEST_VERIFY( Format(formatStr23.c_str(), value6) == StringToWString( Format( WStringToString(formatStr23).c_str(), value6) ) );
+    TEST_VERIFY( Format(formatStr15.c_str(), value3) == StringToWString( Format( WStringToString(formatStr15).c_str(), value3) ) );
+    TEST_VERIFY( Format(formatStr15.c_str(), value4) == StringToWString( Format( WStringToString(formatStr15).c_str(), value4) ) );
+    TEST_VERIFY( Format(formatStr15.c_str(), value5) == StringToWString( Format( WStringToString(formatStr15).c_str(), value5) ) );
+    TEST_VERIFY( Format(formatStr15.c_str(), value6) == StringToWString( Format( WStringToString(formatStr15).c_str(), value6) ) );
 
 }
