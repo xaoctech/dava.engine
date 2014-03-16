7cb3bb84e848c819f4460e3829d0b343cb2eba63
klesch <victor.kleschenko@gmail.com>
2014-03-16 23:45:54 +0300

    [DF-185] Fixed crash with setting quality for cubemap

--- Sources/Internal/Render/Texture.cpp
+++ Sources/Internal/Render/Texture.cpp
@@ -620,36 +620,34 @@ bool Texture::LoadImages(eGPUFamily gpu, Vector<Image *> * images)
                 // to use texture group qualities
                 // -->
                 
-                int baselevel = curTxQuality->albedoBaseMipMapLevel;
-                
+                int32 baselevel = curTxQuality->albedoBaseMipMapLevel;
                 if(baselevel > 0)
                 {
-                    int leaveCount = images->size() - baselevel;
+                    int32 faceCount = (TEXTURE_CUBE == textureType) ? CUBE_FACE_MAX_COUNT : 1;
                     
-                    // we should leave at last one last image
-                    if(leaveCount < 1)
+                    uint32 count = images->size();
+                    baselevel = Min(baselevel, (int32)(count / faceCount) - 1);
+                    if(baselevel > 0)
                     {
-                        leaveCount = 1;
+                        for(int32 i = 0; i < count; ++i)
+                        {
+                            if(images->operator[](i)->mipmapLevel < baselevel)
+                            {
+                                SafeRelease(images->at(i));
+
+                                images->operator[](i) = images->operator[](count - 1);
+                                --i;
+                                --count;
+                            }
+                        }
+
+                        images->resize(count);
+                        for(uint32 i = 0; i < count; ++i)
+                        {
+                            images->operator[](i)->mipmapLevel -= baselevel;
+                        }
                     }
-                    
-                    int leaveOffset = images->size() - leaveCount;
-                    
-                    // release all images, except last one
-                    for(int i = 0; i < leaveOffset; ++i)
-                    {
-                        SafeRelease(images->at(i));
-                    }
-                    
-                    // move last items to the beginning of the vector vector
-                    for(int i = 0; i < leaveCount; ++i)
-                    {
-                        images->operator[](i) = images->operator[](leaveOffset + i);
-                        images->operator[](i)->mipmapLevel = i;
-                    }
-                    
-                    images->resize(leaveCount);
                 }
-                
                 // <-
             }
         }
