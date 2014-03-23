b9dca8e02f0bd185c26e6380bf78d107e3f4237e
LeRoN <leronxp@gmail.com>
2014-03-24 01:09:15 +0300

    [DF-3797] Fixed recursive visibility for native controls. Fix issues in UIEditor.
    Former-commit-id: 071822096983f23fc446bc18c138e34fa1334613

--- Sources/Internal/Render/RenderHelper.cpp
+++ Sources/Internal/Render/RenderHelper.cpp
@@ -334,7 +334,7 @@ void RenderHelper::DrawPoint(const Vector3 & pt, float32 ptSize, UniqueHandle re
 void RenderHelper::DrawCircle(const Vector2 & center, float32 radius, UniqueHandle renderState)
 {
 	Polygon2 pts;
-    float32 angle = SEGMENT_LENGTH / radius;
+    float32 angle = Min(PI/6.0f, SEGMENT_LENGTH / radius);// minimum angle 30 degrees
 	int ptsCount = (int)(2 * PI / angle) + 1;
 	
     pts.points.reserve(ptsCount);
