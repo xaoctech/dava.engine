0ba18cf6b0ef09014694ec801e94f387b17329c8
binaryzebra <vitaliy.borodovsky@gmail.com>
2014-03-24 15:48:18 +0300

    [DF-2911] Fixes in static occlusion

--- Sources/Internal/Render/Highlevel/StaticOcclusionRenderPass.cpp
+++ Sources/Internal/Render/Highlevel/StaticOcclusionRenderPass.cpp
@@ -79,18 +79,86 @@ StaticOcclusionRenderPass::~StaticOcclusionRenderPass()
 {
     
 }
-
+    
+bool StaticOcclusionRenderPass::CompareFunction(const RenderBatch * a, const RenderBatch *  b)
+{
+    return a->layerSortingKey < b->layerSortingKey;
+}
+    
 void StaticOcclusionRenderPass::Draw(Camera * camera, RenderPassBatchArray * renderPassBatchArray)
 {
+    Vector<RenderBatch*> terrainBatches;
+    Vector<RenderBatch*> batches;
+    
+    terrainBatches.clear();
+    batches.clear();
+    
     uint32 size = (uint32)renderLayers.size();
     for (uint32 k = 0; k < size; ++k)
     {
         RenderLayer * layer = renderLayers[k];
         RenderLayerBatchArray * renderLayerBatchArray = renderPassBatchArray->Get(layer->GetRenderLayerID());
-        if (renderLayerBatchArray)
+    
+        uint32 batchCount = (uint32)renderLayerBatchArray->GetRenderBatchCount();
+        for (uint32 batchIndex = 0; batchIndex < batchCount; ++batchIndex)
         {
-            layer->Draw(name, camera, renderLayerBatchArray);
+            RenderBatch * batch = renderLayerBatchArray->Get(batchIndex);
+            if (batch->GetRenderObject()->GetType() == RenderObject::TYPE_LANDSCAPE)
+            {
+                terrainBatches.push_back(batch);
+            }else
+            {
+                batches.push_back(batch);
+            }
         }
+    }
+
+    OcclusionQueryManager & manager = occlusion->GetOcclusionQueryManager();
+    size = (uint32)terrainBatches.size();
+    for (uint32 k = 0; k < size; ++k)
+    {
+        OcclusionQueryManagerHandle handle = manager.CreateQueryObject();
+        OcclusionQuery & query = manager.Get(handle);
+        
+        RenderBatch * batch = terrainBatches[k];
+        
+        query.BeginQuery();
+        batch->Draw(name, camera);
+        query.EndQuery();
+        
+        occlusion->RecordFrameQuery(batch, handle);
+    }
+    
+    
+    Vector3 cameraPosition = camera->GetPosition();
+
+    size = (uint32)batches.size();
+    for (uint32 k = 0; k < size; ++k)
+    {
+        RenderBatch * batch = batches[k];
+        RenderObject * renderObject = batch->GetRenderObject();
+        Vector3 position = renderObject->GetWorldBoundingBox().GetCenter();
+        float realDistance = (position - cameraPosition).Length();
+        uint32 distance = ((uint32)(realDistance * 100.0f));
+        uint32 distanceBits = distance;
+        
+        batch->layerSortingKey = distanceBits;
+    }
+
+    std::sort(batches.begin(), batches.end(), CompareFunction);
+    
+    for (uint32 k = 0; k < size; ++k)
+    {
+        OcclusionQueryManagerHandle handle = manager.CreateQueryObject();
+        OcclusionQuery & query = manager.Get(handle);
+        
+        RenderBatch * batch = batches[k];
+        
+        query.BeginQuery();
+        batch->Draw(name, camera);
+        query.EndQuery();
+        
+        occlusion->RecordFrameQuery(batch, handle);
     }
 }
 
