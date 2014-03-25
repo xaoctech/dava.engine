3a0fdb1d1396e16f53ed548c8825452baa8e4042
adebt <adebt@adebts-MacBook-Pro.local>
2014-03-25 16:58:19 +0200

    [DF-399] restore resources after recreate gl context

--- Sources/Internal/Render/Shader.cpp
+++ Sources/Internal/Render/Shader.cpp
@@ -42,6 +42,7 @@
 #include "Utils/CRC32.h"
 #endif
 #include "Utils/Utils.h"
+#include "Render/ShaderCache.h"
 
 namespace DAVA
 {
@@ -1262,6 +1263,10 @@ void Shader::Invalidate()
 {
     RenderResource::Invalidate();
     Recompile();
+    
+    ShaderAsset* asset = ShaderCache::Instance()->Get(assetName);
+    if (asset)
+        asset->BindShaderDefaults(this);
 }
 #endif //#if defined(__DAVAENGINE_ANDROID__)
 
