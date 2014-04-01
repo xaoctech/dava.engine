e1da9d6d119f414492a6ce49e8d8a6340f794fbf
xrayman <xrayman64@gmail.com>
2014-04-01 16:52:02 +0300

    [DF-2803] Revert previous fix
    Former-commit-id: d38c92da40796a2e55b210695be629325bed3677

--- Tools/ResourceEditor/Classes/Qt/Scene/System/EditorParticlesSystem.cpp
+++ Tools/ResourceEditor/Classes/Qt/Scene/System/EditorParticlesSystem.cpp
@@ -120,7 +120,7 @@ void EditorParticlesSystem::Draw()
         DAVA::Matrix3 effectMatrix(selectedEffectEntity->GetWorldTransform());
         ParticleEffectComponent * effect = GetEffectComponent(selectedEffectEntity);
         DAVA::Vector3 center =effect->GetSpawnPosition(effect->GetEmitterId(selectedEmitter));
-//        TransformPerserveLength(center, effectMatrix);
+        TransformPerserveLength(center, effectMatrix);
         center+=selectedEffectEntity->GetWorldTransform().GetTranslationVector();
         
         DAVA::RenderManager::Instance()->SetColor(DAVA::Color(0.7f, 0.0f, 0.0f, 0.25f));
