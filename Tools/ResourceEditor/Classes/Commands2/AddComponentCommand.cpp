4efbe6895e2a4b1a34c80b2030b36b1da99d2ca7
Dmitriy Sazonov <d_sazonov@wargaming.net>
2014-04-15 12:14:54 +0300

    [DF-4087] Fixed crash on scene closing

--- Tools/ResourceEditor/Classes/Commands2/AddComponentCommand.cpp
+++ Tools/ResourceEditor/Classes/Commands2/AddComponentCommand.cpp
@@ -53,6 +53,7 @@ AddComponentCommand::~AddComponentCommand()
 void AddComponentCommand::Redo()
 {
     componentType = backup->GetType();
+    backup->SetEntity(entityToAdd);
 	entityToAdd->AddComponent(backup);
     original = backup;
 }
@@ -63,6 +64,7 @@ void AddComponentCommand::Undo()
     DAVA::Component * component = entityToAdd->GetComponent(componentType, nComponents - 1);
     DVASSERT(component);
     backup = component->Clone(entityToAdd);
+    backup->SetEntity(NULL);
     original = 0;
     entityToAdd->RemoveComponent(component);
 }
