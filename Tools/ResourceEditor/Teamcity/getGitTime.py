991c48890f3639e989242c7d4b62725e51fdc68d
klesch <victor.kleschenko@gmail.com>
2014-04-16 16:54:30 +0300

    [DF-185] Updated teamcity script

--- Tools/ResourceEditor/Teamcity/getGitTime.py
+++ Tools/ResourceEditor/Teamcity/getGitTime.py
@@ -1,6 +1,7 @@
 import os
+import sys
 os.chdir('../')
-os.system("git log -1 --format=\"#define RESOURCE_EDITOR_VERSION %x22%ci%x22\" > version.h")
+os.system("git log -1 --date=local --format=\"#define RESOURCE_EDITOR_VERSION %x22%cd | " + sys.argv[1] + "%x22\" > version.h")
 
 os.chdir('../../')
-os.system("git log -1 --format=\"%ci\" > gitTime.txt")
+os.system("git log -1 --date=local --format=\"%cd\" > gitTime.txt")
