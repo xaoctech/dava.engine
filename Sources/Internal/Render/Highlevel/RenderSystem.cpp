ca3ee1a13f0dee2596118f62400fc0f3d3a6b290
Valentine Ivanov <valentin_ivanov@wargaming.net>
2014-04-21 15:18:26 +0300

    [DF-4120] possible fix for updating lights for all LODs
    Former-commit-id: 7a907de1c785b0d11e09bd764f5358b9b443b59c

--- Sources/Internal/Render/Highlevel/RenderSystem.cpp
+++ Sources/Internal/Render/Highlevel/RenderSystem.cpp
@@ -253,10 +253,10 @@ void RenderSystem::FindNearestLights(RenderObject * renderObject)
 {
 	//do not calculate nearest lights for non-lit objects
 	bool needUpdate = false;
-	uint32 renderBatchCount = renderObject->GetActiveRenderBatchCount();
+	uint32 renderBatchCount = renderObject->GetRenderBatchCount();
     for (uint32 k = 0; k < renderBatchCount; ++k)
     {
-        RenderBatch * batch = renderObject->GetActiveRenderBatch(k);
+        RenderBatch * batch = renderObject->GetRenderBatch(k);
         NMaterial * material = batch->GetMaterial();
         if (material && material->IsDynamicLit())
         {
@@ -301,7 +301,7 @@ void RenderSystem::FindNearestLights(RenderObject * renderObject)
     
     for (uint32 k = 0; k < renderBatchCount; ++k)
     {
-        RenderBatch * batch = renderObject->GetActiveRenderBatch(k);
+        RenderBatch * batch = renderObject->GetRenderBatch(k);
         NMaterial * material = batch->GetMaterial();
         if (material)
         {
