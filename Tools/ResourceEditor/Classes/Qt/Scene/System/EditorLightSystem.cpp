b371799be194a775a6df2b8ac719cb6d115f3641
Igor Solovey <salikq@gmail.com>
2014-05-06 13:06:59 +0300

    [DF-3732] Fix compilation error
    Former-commit-id: 0650aafdb1164fa113bfe494c86f5c64ea9b9a27

--- Tools/ResourceEditor/Classes/Qt/Scene/System/EditorLightSystem.cpp
+++ Tools/ResourceEditor/Classes/Qt/Scene/System/EditorLightSystem.cpp
@@ -50,7 +50,7 @@ EditorLightSystem::EditorLightSystem(DAVA::Scene * scene)
 	light->Release();
 
 	lightCountOnScene = 0;
-	SetComponentsFlags(1 << Component::LIGHT_COMPONENT);
+	SetRequiredComponents(1 << Component::LIGHT_COMPONENT);
 
 	isEnabled = true;
 	AddCameraLightOnScene();
