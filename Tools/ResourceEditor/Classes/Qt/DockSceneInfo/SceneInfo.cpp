de004f9b4c0a7ba486c11bf7fd2f5040ab3f8506
Kernel <Kernel@tut.by>
2014-05-07 12:07:14 +0300

    DF-4010 fixes after merge

--- Tools/ResourceEditor/Classes/Qt/DockSceneInfo/SceneInfo.cpp
+++ Tools/ResourceEditor/Classes/Qt/DockSceneInfo/SceneInfo.cpp
@@ -393,11 +393,11 @@ void SceneInfo::CollectLODDataInFrame()
 {
     lodInfoInFrame.Clear();
 
-    if(!activeScene||!activeScene->renderSystem||!activeScene->renderSystem->IsRenderHierarchyInitialized()||!activeScene->GetClipCamera())
+    if(!activeScene||!activeScene->renderSystem||!activeScene->renderSystem->IsRenderHierarchyInitialized()||!activeScene->GetCurrentCamera())
         return;
 
     visibilityArray.Clear();
-    activeScene->renderSystem->GetRenderHierarchy()->Clip(activeScene->GetClipCamera(), &visibilityArray, RenderObject::CLIPPING_VISIBILITY_CRITERIA);
+    activeScene->renderSystem->GetRenderHierarchy()->Clip(activeScene->GetCurrentCamera(), &visibilityArray, RenderObject::CLIPPING_VISIBILITY_CRITERIA);
 
     uint32 size = (uint32)visibilityArray.visibilityArray.size();
     for (uint32 ro = 0; ro < size; ++ro)
