6cd167fd63e5ca551d772d2a69f7cc98cb84b7fb
Dmitriy Sazonov <d_sazonov@wargaming.net>
2014-05-15 12:14:04 +0300

    [DF-185] Fixed typo in Entity.cpp
    Former-commit-id: 9facc282c9953185659c1ef7ad3e95f70c76c439

--- Sources/Internal/Scene3D/Entity.cpp
+++ Sources/Internal/Scene3D/Entity.cpp
@@ -169,7 +169,7 @@ void Entity::RemoveAllComponents()
 		if(components[i])
 		{
 			CleanupComponent(components[i], 0);
-            SafeRelease(components[i]);
+            SafeDelete(components[i]);
 		}
 	}
 
@@ -188,7 +188,7 @@ void Entity::RemoveAllComponents()
 			{
 				componentCount--;
 				CleanupComponent(*compIt, componentCount);
-                SafeRelease(*compIt);
+                SafeDelete(*compIt);
             }
 		}
 
