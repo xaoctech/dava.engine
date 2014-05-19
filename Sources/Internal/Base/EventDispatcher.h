ca058649dd02e4fca5f257259c75cf4bf6ec9bc0
Alexey Strokachuk <leronxp@gmail.com>
2014-05-19 15:45:33 +0300

    [DF-4218] new implementation for safe EventDispatcher.

--- Sources/Internal/Base/EventDispatcher.h
+++ Sources/Internal/Base/EventDispatcher.h
@@ -114,14 +114,16 @@ protected:
 	class Event 
 	{
 	public:
-		int32	eventType;
-		Message msg;
-		Event()
-		{
-		}
+        Event() : eventType(0), needDelete(false){}
+        static bool IsEventToDelete(const Event &event){ return event.needDelete; }
+
+        int32   eventType : 31;
+        bool    needDelete : 1;
+        Message msg;
 	};
 	
 	List<Event> events;
+    bool eraseLocked;
 };
 	
 /**
