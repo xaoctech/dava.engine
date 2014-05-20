b714afabd3913a7bf287e05194a597c99889e5d9
d_shpakov <d_shpakov@wargaming.net>
2014-05-20 12:45:51 +0300

    DF-4237 disabled editing on localization value field
    Former-commit-id: 9259fde11eac5dac8721329fdeece64defdd69cd

--- Tools/UIEditor/Classes/UI/PropertyGridWidgets/uistatictextpropertygridwidget.cpp
+++ Tools/UIEditor/Classes/UI/PropertyGridWidgets/uistatictextpropertygridwidget.cpp
@@ -119,6 +119,9 @@ void UIStaticTextPropertyGridWidget::Initialize(BaseMetadata* activeMetadata)
     ui->localizationKeyWidget->setVisible(!isUITextField);
     ui->localizationValueWidget->setVisible(!isUITextField);
     
+    ui->localizationKeyTextLineEdit->setEnabled(false);
+    ui->localizationKeyTextLineEdit->setReadOnly(true);
+
     UpdateLocalizationValue();
     
     RegisterGridWidgetAsStateAware();
