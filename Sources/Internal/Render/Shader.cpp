56874c0097a19fa8bc244ed0dcfdcefef3a7571d
Igor Solovey <salikq@gmail.com>
2014-05-22 18:54:55 +0300

    [DF-3732] tree animation params through autobinds

--- Sources/Internal/Render/Shader.cpp
+++ Sources/Internal/Render/Shader.cpp
@@ -1203,7 +1203,28 @@ void Shader::BindDynamicParameters()
                 }
                 break;
             }
-
+            case PARAM_SPEED_TREE_TRUNK_OSCILLATION:
+            {
+                pointer_size _updateSemantic = GET_DYNAMIC_PARAM_UPDATE_SEMANTIC(currentUniform->shaderSemantic);
+                if (_updateSemantic != currentUniform->updateSemantic)
+                {
+                    Vector2 * param = (Vector2*)RenderManager::GetDynamicParam(currentUniform->shaderSemantic);
+                    SetUniformValueByUniform(currentUniform, *param);
+                    currentUniform->updateSemantic = _updateSemantic;
+                }
+                break;
+            }
+            case PARAM_SPEED_TREE_LEAFS_OSCILLATION:
+            {
+                pointer_size _updateSemantic = GET_DYNAMIC_PARAM_UPDATE_SEMANTIC(currentUniform->shaderSemantic);
+                if (_updateSemantic != currentUniform->updateSemantic)
+                {
+                    Vector2 * param = (Vector2*)RenderManager::GetDynamicParam(currentUniform->shaderSemantic);
+                    SetUniformValueByUniform(currentUniform, *param);
+                    currentUniform->updateSemantic = _updateSemantic;
+                }
+                break;
+            }
             case PARAM_COLOR:
             {
                 const Color & c = RenderManager::Instance()->GetColor();
