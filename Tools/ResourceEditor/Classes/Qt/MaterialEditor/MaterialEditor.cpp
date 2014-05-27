2e3be71b2406f731cf40f2936e5156c165a20c98
smile4u <s_zdanevich@wargaming.net>
2014-05-27 13:02:05 +0300

    [DF-185] Added red highlight for properties, that aren't used in shader
    Conflicts:
    	Tools/ResourceEditor/Classes/Qt/MaterialEditor/MaterialEditor.cpp
    
    Former-commit-id: 9908d6f2cbb7ea035a16f9fb08b24e262282683c

--- Tools/ResourceEditor/Classes/Qt/MaterialEditor/MaterialEditor.cpp
+++ Tools/ResourceEditor/Classes/Qt/MaterialEditor/MaterialEditor.cpp
@@ -496,12 +496,18 @@ void MaterialEditor::UpdateAddRemoveButtonState(QtPropertyDataInspDynamic *data)
             editEnabled = true;
             addRemoveButton->setIcon(QIcon(":/QtIcons/cminus.png"));
             addRemoveButton->setToolTip("Remove property");
+
+        // isn't set in parent or shader
+        if(!(memberFlags & DAVA::I_VIEW) && !(memberFlags & DAVA::I_SAVE))
+        {
+            bgColor = QBrush(QColor(255, 0, 0, 25));
+        }
         }
         // inherited from parent property - should be add button
     else
         {
             editEnabled = false;
-            bgColor = QBrush(QColor(0, 0, 0, 10));
+        bgColor = QBrush(QColor(0, 0, 0, 25));
             addRemoveButton->setIcon(QIcon(":/QtIcons/cplus.png"));
             addRemoveButton->setToolTip( "Add property" );
         }
