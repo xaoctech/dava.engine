43be0b2208f81ebbd62b9641a4f44e3036bfcb0f
d_shpakov <d_shpakov@wargaming.net>
2014-06-06 15:10:56 +0300

    DF-185 FORMAT_STRING_MAX_LEN = 512

--- Sources/Internal/Utils/StringFormat.cpp
+++ Sources/Internal/Utils/StringFormat.cpp
@@ -37,7 +37,7 @@
 namespace DAVA
 {
 static const int32 FORMAT_STRING_SIZE = 2048;
-static const int32 FORMAT_STRING_MAX_LEN = 256;
+static const int32 FORMAT_STRING_MAX_LEN = 512;
 
 //static char8 formatString8[FORMAT_STRING_SIZE];
 //static char16 formatString16[FORMAT_STRING_SIZE];
