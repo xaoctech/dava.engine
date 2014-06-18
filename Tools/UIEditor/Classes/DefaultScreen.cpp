bb8ba6cc77a60cc7214bad1f1af01aa7f2f16064
Vladimir Bondarenko <dominik@Vladimirs-Mac-mini.local>
2014-06-18 12:25:33 +0300

    DF-3694 - Partial fix for guidelines sticking issue (90, 180 and 270 degrees)

--- Tools/UIEditor/Classes/DefaultScreen.cpp
+++ Tools/UIEditor/Classes/DefaultScreen.cpp
@@ -1462,7 +1462,30 @@ int32 DefaultScreen::CalculateStickToGuidesDrag(Vector2& offset) const
         HierarchyTreeControlNode* controlNode = (*iter);
         if (controlNode && controlNode->GetUIObject())
         {
-            controlRects.push_back(controlNode->GetUIObject()->GetRect(true));
+            Rect rectControl = controlNode->GetUIObject()->GetRect(true);
+            Rect rectFinal = rectControl;
+            float32 fAngle = controlNode->GetUIObject()->GetAngle();
+            if(fAngle != 0)
+            {
+                // Complex case - angle is not 0. Particular fix for 90, 180 and 270 degrees goes here
+                if(fAngle == PI_05)
+                {
+                    // 90
+                    rectFinal = Rect(rectControl.x - rectControl.dy, rectControl.y, rectControl.dy, rectControl.dx);
+                }
+                else if(fAngle == PI)
+                {
+                    // 180
+                    rectFinal = Rect(rectControl.x - rectControl.dx, rectControl.y - rectControl.dy, rectControl.dx, rectControl.dy);
+                }
+                else if(fAngle == (PI+PI_05))
+                {
+                    // 270
+                    rectFinal = Rect(rectControl.x, rectControl.y - rectControl.dx, rectControl.dy, rectControl.dx);
+                }
+                
+            }
+            controlRects.push_back(rectFinal);
         }
     }
     
