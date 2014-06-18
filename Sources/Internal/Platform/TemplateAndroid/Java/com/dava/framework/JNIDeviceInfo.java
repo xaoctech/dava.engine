98f028ce2d728b59ba2c07dba4eea286c218d1f0
adebt <adebt@adebts-MacBook-Pro.local>
2014-06-18 15:45:23 +0300

    [DF-4346] review fix

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNIDeviceInfo.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNIDeviceInfo.java
@@ -74,34 +74,28 @@ public class JNIDeviceInfo {
 		return JNIConfigChooser.GetDepthBufferSize();
 	}
 	
+	static int GPU_UNKNOWN = -1;
+	static int GPU_POWERVR_IOS = 0;
+	static int GPU_POWERVR_ANDROID = 1;
+	static int GPU_TEGRA = 2;
+	static int GPU_MALI = 3;
+	static int GPU_ADRENO = 4;
+	
 	protected static void SetGPUFamily(GL10 gl)
 	{
-		/*
-		enum eGPUFamily
-		{
-		    GPU_UNKNOWN = -1,
-		    
-		    GPU_POWERVR_IOS     =   0,
-		    GPU_POWERVR_ANDROID,
-		    GPU_TEGRA,
-		    GPU_MALI,
-		    GPU_ADRENO,
-		    
-		    GPU_FAMILY_COUNT
-		};ig
-		 */
+		
 		String extensions = gl.glGetString(GL10.GL_EXTENSIONS);
 		
 		if (extensions.indexOf("GL_IMG_texture_compression_pvrtc") >= 0)
-			gpuFamily = 1;
+			gpuFamily = GPU_POWERVR_ANDROID;
 		else if (extensions.indexOf("GL_NV_draw_texture") >= 0)
-			gpuFamily = 2;
+			gpuFamily = GPU_TEGRA;
 		else if (extensions.indexOf("GL_AMD_compressed_ATC_texture") >= 0)
-			gpuFamily = 4;
+			gpuFamily = GPU_ADRENO;
 		else if (extensions.indexOf("GL_OES_compressed_ETC1_RGB8_texture") >= 0)
-			gpuFamily = 3;
+			gpuFamily = GPU_MALI;
 		else
-			gpuFamily = -1;
+			gpuFamily = GPU_UNKNOWN;
 	}
 	
 	public static int GetGPUFamily()
