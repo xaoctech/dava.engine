486163587c4f1d79a106aab50b03e5c0958b5a70
Vladimir Bondarenko <dominik@Vladimirs-Mac-mini.local>
2014-07-03 12:12:26 +0300

    DF-3694 - fixed sticking to gridlines.

--- Tools/UIEditor/Classes/DefaultScreen.cpp
+++ Tools/UIEditor/Classes/DefaultScreen.cpp
@@ -1472,17 +1472,17 @@ int32 DefaultScreen::CalculateStickToGuidesDrag(Vector2& offset) const
             if(fAngle != 0)
             {
                 // Complex case - angle is not 0. Particular fix for 90, 180 and 270 degrees goes here
-                if(FLOAT_EQUAL(fAngle, PI_05))
+                if(FLOAT_EQUAL_EPS(fAngle, PI_05, 1e-4f))
                 {
                     // 90
                     rectFinal = Rect(rectControl.x - rectControl.dy, rectControl.y, rectControl.dy, rectControl.dx);
                 }
-                else if(FLOAT_EQUAL(fAngle, PI))
+                else if(FLOAT_EQUAL_EPS(fAngle, PI, 1e-4f))
                 {
                     // 180
                     rectFinal = Rect(rectControl.x - rectControl.dx, rectControl.y - rectControl.dy, rectControl.dx, rectControl.dy);
                 }
-                else if(FLOAT_EQUAL(fAngle, (PI+PI_05)))
+                else if(FLOAT_EQUAL_EPS(fAngle, (PI+PI_05), 1e-4f))
                 {
                     // 270
                     rectFinal = Rect(rectControl.x, rectControl.y - rectControl.dx, rectControl.dy, rectControl.dx);
