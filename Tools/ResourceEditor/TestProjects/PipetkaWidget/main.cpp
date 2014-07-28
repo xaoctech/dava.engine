2a285a4ca7e745786785da7325a444ecf851303f
SaZ <dsazonoff@gmail.com>
2014-07-28 15:00:51 +0300

    [DF-1785] Test project for color picker

---
+++ Tools/ResourceEditor/TestProjects/PipetkaWidget/main.cpp
@@ -0,0 +1,10 @@
+#include "pipetkawidget.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    PipetkaWidget w;
+    w.show();
+    return a.exec();
+}
