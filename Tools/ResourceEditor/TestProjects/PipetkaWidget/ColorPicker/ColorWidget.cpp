18155db2b480f3aebf87b3877860ad79610e421b
SaZ <dsazonoff@gmail.com>
2014-07-29 16:36:23 +0300

    [DF-1785] Expander widget experiments
    Former-commit-id: b65d0bf414a0fb19be1fe25ad7a7d3b8d2bfcbdb

--- Tools/ResourceEditor/TestProjects/PipetkaWidget/ColorPicker/ColorWidget.cpp
+++ Tools/ResourceEditor/TestProjects/PipetkaWidget/ColorPicker/ColorWidget.cpp
@@ -1,11 +1,18 @@
 #include "ColorWidget.h"
 #include "ui_ColorWidget.h"
 
+#include <QVBoxLayout>
+#include <QLabel>
+
+
 ColorWidget::ColorWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ColorWidget())
 {
     ui->setupUi(this);
+    {
+        //ui->widget->setExpanderTitle( "Test" );
+    }
 }
 
 ColorWidget::~ColorWidget()
