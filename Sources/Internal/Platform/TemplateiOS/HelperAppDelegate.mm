7f4063aaa0e9d26ac689ed8e0c77a866c9c34428
klesch <victor@davaconsulting.com>
2014-08-05 16:02:20 +0300

    [DF-4622] Changed order of calls of FrameworkDidLaunched for iOS
    Former-commit-id: 6682f16b01d9b676241e7e59ba65fff6fdcd1157

--- Sources/Internal/Platform/TemplateiOS/HelperAppDelegate.mm
+++ Sources/Internal/Platform/TemplateiOS/HelperAppDelegate.mm
@@ -30,38 +30,38 @@ int DAVA::Core::Run(int argc, char * argv[], AppHandle handle)
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	DAVA::Core * core = new DAVA::Core();
 	core->CreateSingletons();
-	FrameworkDidLaunched();
 	
-	{//detecting physical screen size and initing core system with this size
-		
-		::UIScreen* mainScreen = [::UIScreen mainScreen];
-		unsigned int width = [mainScreen bounds].size.width;
-		unsigned int height = [mainScreen bounds].size.height;
-		eScreenOrientation orientation = Instance()->GetScreenOrientation();
+	//detecting physical screen size and initing core system with this size
+	::UIScreen* mainScreen = [::UIScreen mainScreen];
+	unsigned int width = [mainScreen bounds].size.width;
+	unsigned int height = [mainScreen bounds].size.height;
+	eScreenOrientation orientation = Instance()->GetScreenOrientation();
 
-		//if (UIDeviceOrientationIsLandscape([[UIDevice currentDevice] orientation]))
-		if ((orientation==SCREEN_ORIENTATION_LANDSCAPE_LEFT)||(orientation==SCREEN_ORIENTATION_LANDSCAPE_RIGHT)||(orientation==SCREEN_ORIENTATION_LANDSCAPE_AUTOROTATE))
-		{
-			width = [mainScreen bounds].size.height;
-			height = [mainScreen bounds].size.width;
-		}
-		
-		unsigned int scale = 1;
-		
-		if (DAVA::Core::IsAutodetectContentScaleFactor())
-		{
-			if ([::UIScreen instancesRespondToSelector: @selector(scale) ]
-				&& [::UIView instancesRespondToSelector: @selector(contentScaleFactor) ]) 
-			{
-				scale = (unsigned int)[[::UIScreen mainScreen] scale];
-			}
-		}
-
-        DAVA::DeviceInfo::SetScreenInfo(width, height, scale);
-
-		DAVA::UIControlSystem::Instance()->SetInputScreenAreaSize(width, height);
-		DAVA::Core::Instance()->SetPhysicalScreenSize(width*scale, height*scale);
+	if ((orientation==SCREEN_ORIENTATION_LANDSCAPE_LEFT)||
+		(orientation==SCREEN_ORIENTATION_LANDSCAPE_RIGHT)||
+		(orientation==SCREEN_ORIENTATION_LANDSCAPE_AUTOROTATE))
+	{
+		width = [mainScreen bounds].size.height;
+		height = [mainScreen bounds].size.width;
 	}
+		
+	unsigned int scale = 1;
+		
+	if (DAVA::Core::IsAutodetectContentScaleFactor())
+	{
+		if ([::UIScreen instancesRespondToSelector: @selector(scale) ]
+			&& [::UIView instancesRespondToSelector: @selector(contentScaleFactor) ]) 
+		{
+			scale = (unsigned int)[[::UIScreen mainScreen] scale];
+		}
+	}
+
+        DeviceInfo::SetScreenInfo(width, height, scale);
+
+	FrameworkDidLaunched();
+
+	DAVA::UIControlSystem::Instance()->SetInputScreenAreaSize(width, height);
+	DAVA::Core::Instance()->SetPhysicalScreenSize(width*scale, height*scale);
 		
 	int retVal = UIApplicationMain(argc, argv, nil, nil);
 	
