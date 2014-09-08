e69bcd995cdf68da729dd0aa9611ead40ccf3e56
Yury Danilov <yury.danilov@davaconsulting.com>
2014-09-08 17:14:27 +0300

    DF-4918 initial version of AnimationComponent and AnimationSystem
    Former-commit-id: 1b7e686d52c18d11d050b6598f970d7792e3342d

---
+++ Sources/Internal/Scene3D/Components/AnimationComponent.h
@@ -0,0 +1,94 @@
+/*==================================================================================
+    Copyright (c) 2014, thorin
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    * Neither the name of the binaryzebra nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE binaryzebra AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL binaryzebra BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=====================================================================================*/
+
+
+
+#ifndef __DAVAENGINE_ANIMATION_COMPONENT_H__
+#define __DAVAENGINE_ANIMATION_COMPONENT_H__
+
+#include "Base/BaseTypes.h"
+#include "Scene3D/Systems/AnimationSystem.h"
+#include "Entity/Component.h"
+#include "Scene3D/SceneFile/SerializationContext.h"
+
+namespace DAVA 
+{
+
+class Entity;
+
+class AnimationSampler
+{
+public:
+    virtual float32 GetTimeLimit(void) = 0;
+    virtual float32 GetValue(float32 time) = 0;
+};
+
+class AnimationComponent : public Component
+{
+protected:
+    virtual ~AnimationComponent();
+public:
+    AnimationComponent();
+
+    IMPLEMENT_COMPONENT_TYPE(ANIMATION_COMPONENT);
+
+    virtual Component * Clone(Entity * toEntity);
+	virtual void Serialize(KeyedArchive *archive, SerializationContext *serializationContext);
+	virtual void Deserialize(KeyedArchive *archive, SerializationContext *serializationContext);
+
+    enum eSamplerType
+    {
+        EST_TRANSLATION_X,
+        EST_TRANSLATION_Y,
+        EST_TRANSLATION_Z,
+        EST_ROTATION_X,
+        EST_ROTATION_Y,
+        EST_ROTATION_Z,
+        EST_COUNT
+    };
+
+private:
+
+	friend class AnimationSystem;
+    AnimationSampler * samplers[EST_COUNT];
+    Matrix4 originalLocalTransform;
+
+public:
+
+//     INTROSPECTION_EXTEND(TransformComponent, Component,
+//         MEMBER(localMatrix, "Local Transform", I_SAVE | I_VIEW)
+//         MEMBER(worldMatrix, "World Transform", I_SAVE | I_VIEW)
+//         MEMBER(parentMatrix, "Parent Matrix", I_SAVE)
+//     );
+};
+
+
+
+};
+
+#endif //__DAVAENGINE_ANIMATION_COMPONENT_H__
---
+++ Sources/Internal/Scene3D/Systems/AnimationSystem.cpp
@@ -0,0 +1,120 @@
+/*==================================================================================
+    Copyright (c) 2014, thorin
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    * Neither the name of the binaryzebra nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE binaryzebra AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL binaryzebra BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=====================================================================================*/
+
+
+
+#include "Scene3D/Systems/AnimationSystem.h"
+#include "Scene3D/Components/AnimationComponent.h"
+#include "Scene3D/Entity.h"
+#include "Debug/DVAssert.h"
+#include "Scene3D/Systems/EventSystem.h"
+#include "Scene3D/Scene.h"
+#include "Scene3D/Systems/GlobalEventSystem.h"
+#include "Debug/Stats.h"
+
+namespace DAVA
+{
+
+AnimationSystem::AnimationSystem(Scene * scene)
+:	SceneSystem(scene)
+{
+// 	scene->GetEventSystem()->RegisterSystemForEvent(this, EventSystem::LOCAL_TRANSFORM_CHANGED);
+// 	scene->GetEventSystem()->RegisterSystemForEvent(this, EventSystem::TRANSFORM_PARENT_CHANGED);
+}
+
+AnimationSystem::~AnimationSystem()
+{
+}
+
+void AnimationSystem::Process(float32 timeElapsed)
+{
+    TIME_PROFILE("TransformSystem::Process");
+    
+//     passedNodes = 0;
+//     multipliedNodes = 0;
+//     
+//     uint32 size = updatableEntities.size();
+//     for(uint32 i = 0; i < size; ++i)
+//     {
+//         //HierahicFindUpdatableTransform(updatableEntities[i]);
+//         FindNodeThatRequireUpdate(updatableEntities[i]);
+//     }
+//     
+//     GlobalEventSystem::Instance()->GroupEvent(GetScene(), sendEvent, EventSystem::WORLD_TRANSFORM_CHANGED);
+//     sendEvent.clear();
+//     
+//     updatableEntities.clear();
+//     
+//     if(passedNodes)
+//     {
+//         //		Logger::Info("TransformSystem %d passed %d multiplied", passedNodes, multipliedNodes);
+//     }
+}
+
+void AnimationSystem::ImmediateEvent(Entity * entity, uint32 event)
+{
+	switch(event)
+	{
+	case EventSystem::LOCAL_TRANSFORM_CHANGED:
+	case EventSystem::TRANSFORM_PARENT_CHANGED:
+// 		EntityNeedUpdate(entity);
+// 		HierahicAddToUpdate(entity);
+		break;
+	}
+}
+
+
+void AnimationSystem::AddEntity(Entity * entity)
+{
+// 	TransformComponent * transform = (TransformComponent*)entity->GetComponent(Component::TRANSFORM_COMPONENT);
+// 	if (!transform) return; //just in case
+// 	if(transform->parentMatrix)	
+// 		transform->worldMatrix = transform->localMatrix * *(transform->parentMatrix);
+// 	else
+// 		transform->worldMatrix = transform->localMatrix;
+}
+
+void AnimationSystem::RemoveEntity(Entity * entity)
+{
+	//TODO: use hashmap
+// 	uint32 size = updatableEntities.size();
+// 	for(uint32 i = 0; i < size; ++i)
+// 	{
+// 		if(updatableEntities[i] == entity)
+// 		{
+// 			entity->RemoveFlag(Entity::TRANSFORM_NEED_UPDATE);
+// 			entity->RemoveFlag(Entity::TRANSFORM_DIRTY);
+// 
+// 			updatableEntities[i] = updatableEntities[size-1];
+// 			updatableEntities.pop_back();
+// 			return;
+// 		}
+// 	}
+}
+
+};
