191816c84f089e4c217a902921d043b333053fab
Yury Danilov <yury.danilov@davaconsulting.com>
2014-09-17 16:50:43 +0300

    DF-4918 updated AnimationSystem logic according review
    Former-commit-id: 9084806f1c8768bc5643b2784edb976d1f266531

--- Sources/Internal/Scene3D/Components/AnimationComponent.cpp
+++ Sources/Internal/Scene3D/Components/AnimationComponent.cpp
@@ -1,5 +1,5 @@
 /*==================================================================================
-    Copyright (c) 2014, thorin
+    Copyright (c) 2008, binaryzebra
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -32,6 +32,9 @@
 #include "Scene3D/Entity.h"
 #include "Scene3D/Scene.h"
 #include "Scene3D/AnimationData.h"
+#include "Scene3D/Systems/GlobalEventSystem.h"
+#include "Scene3D/Systems/EventSystem.h"
+#include "Scene3D/Components/ComponentHelpers.h"
 
 namespace DAVA
 {
@@ -40,10 +43,10 @@ REGISTER_CLASS(AnimationComponent)
     
 AnimationComponent::AnimationComponent()
 :time(0.0f),
-isPlaying(false),
 animation(NULL),
-autoStart(true),
-repeat(true)
+repeatsCount(1),
+currRepeatsCont(0),
+state(STATE_STOPPED)
 {
 }
 
@@ -57,14 +60,12 @@ Component * AnimationComponent::Clone(Entity * toEntity)
 	AnimationComponent * newAnimation = new AnimationComponent();
 
 	newAnimation->time = time;
-	newAnimation->isPlaying = false;
 	newAnimation->animation = animation ? animation->Clone() : NULL;
-	newAnimation->autoStart = autoStart;
-	newAnimation->repeat = repeat;
+	newAnimation->repeatsCount = repeatsCount;
+	newAnimation->currRepeatsCont = 0;
+    newAnimation->state = STATE_STOPPED; //for another state we need add this one to AnimationSystem
 	return newAnimation;
 }
-
-
 
 void AnimationComponent::Serialize(KeyedArchive *archive, SerializationContext *serializationContext)
 {
@@ -83,8 +84,7 @@ void AnimationComponent::Serialize(KeyedArchive *archive, SerializationContext *
 			archive->SetVector3(Format("key_%i_scale", keyIndex), animation->keys[keyIndex].scale);
 			archive->SetVector4(Format("key_%i_rotation", keyIndex), Vector4(animation->keys[keyIndex].rotation.x, animation->keys[keyIndex].rotation.y, animation->keys[keyIndex].rotation.z, animation->keys[keyIndex].rotation.w));
 		}
-		archive->SetBool("autostart", autoStart);
-		archive->SetBool("repeat", repeat);
+		archive->SetUInt32("repeatsCount", repeatsCount);
 	}
 }
 
@@ -113,8 +113,7 @@ void AnimationComponent::Deserialize(KeyedArchive *archive, SerializationContext
 			animation->SetKey(keyIndex, key);
 		}
 
-		autoStart = archive->GetBool("autostart", true);
-		repeat = archive->GetBool("repeat", true);
+		repeatsCount = archive->GetUInt32("repeatsCount", 1);
 	}
 
 	Component::Deserialize(archive, sceneFile);
@@ -131,18 +130,34 @@ void AnimationComponent::SetAnimation(AnimationData* _animation)
 
 void AnimationComponent::SetIsPlaying( bool value )
 {
-	isPlaying = value;
-
-	if (!isPlaying)
-	{
-        animationTransform.Identity();
-	}
-	time = 0.0f;
+    if (state == STATE_STOPPED && value)
+        Start();
+    if (state == STATE_PLAYING && !value)
+        Stop();
 }
 
 bool AnimationComponent::GetIsPlaying() const
 {
-	return isPlaying;
+	return state == STATE_PLAYING;
+}
+
+void AnimationComponent::Start()
+{
+    GlobalEventSystem::Instance()->Event(GetEntity(), EventSystem::START_ANIMATION);
+}
+
+void AnimationComponent::StopAfterNRepeats( int32 numberOfRepeats )
+{
+    repeatsCount = numberOfRepeats;
+}
+
+
+void AnimationComponent::Stop()
+{
+    if (state == STATE_STOPPED) 
+        return;
+    GlobalEventSystem::Instance()->Event(GetEntity(), EventSystem::STOP_ANIMATION);
+    animationTransform.Identity();
 }
 
 };
