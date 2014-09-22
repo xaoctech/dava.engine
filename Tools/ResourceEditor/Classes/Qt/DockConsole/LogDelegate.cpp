5d962a500a9cce68f61bcd9b5e8387f9ab6d1b5e
SaZ <dsazonoff@gmail.com>
2014-09-22 14:06:29 +0300

    [DF-2474] Added copy support
    Former-commit-id: 40d352c4e6b92574b3a595c82d1a6f37e97bf342

--- Tools/ResourceEditor/Classes/Qt/DockConsole/LogDelegate.cpp
+++ Tools/ResourceEditor/Classes/Qt/DockConsole/LogDelegate.cpp
@@ -1,6 +1,10 @@
 #include "LogDelegate.h"
 
 #include <QAbstractItemView>
+#include <QEvent>
+#include <QMouseEvent>
+#include <QMenu>
+#include <QCursor>
 
 
 LogDelegate::LogDelegate(QAbstractItemView* _view, QObject* parent)
@@ -13,4 +17,30 @@ LogDelegate::LogDelegate(QAbstractItemView* _view, QObject* parent)
 
 LogDelegate::~LogDelegate()
 {
+}
+
+bool LogDelegate::editorEvent(QEvent* event, QAbstractItemModel* model, QStyleOptionViewItem const& option, QModelIndex const& index)
+{
+    switch (event->type())
+    {
+    case QEvent::MouseButtonRelease:
+        {
+            QMouseEvent *me = static_cast<QMouseEvent *>(event);
+            if ( me->button() == Qt::RightButton )
+            {
+                QMenu menu;
+                QAction *copy = new QAction("Copy", this);
+                connect( copy, SIGNAL( triggered() ), SIGNAL(copyRequest()) );
+                menu.addAction(copy);
+                menu.exec(QCursor::pos());
+                copy->deleteLater();
+            }
+        }
+        break;
+
+    default:
+        break;
+    }
+
+    return QStyledItemDelegate::editorEvent(event, model, option, index);
 }
