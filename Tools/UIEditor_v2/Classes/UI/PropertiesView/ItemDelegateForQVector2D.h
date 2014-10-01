643caa75ab0c8e34fbc181e20106d5880bf3a462
Alexey Strokachuk <leronxp@gmail.com>
2014-10-01 14:29:02 +0300

    [DF-5048] fix project files
    Former-commit-id: e79fe5e36d14a97e25cc36485a89a02915904ff7

--- Tools/UIEditor_v2/Classes/UI/PropertiesView/ItemDelegateForQVector2D.h
+++ Tools/UIEditor_v2/Classes/UI/PropertiesView/ItemDelegateForQVector2D.h
@@ -3,20 +3,18 @@
 
 #include <QStyledItemDelegate>
 
-class PropertiesTreeQVariantItemDelegate: public QStyledItemDelegate
+class ItemDelegateForQVector2D: public QStyledItemDelegate
 {
     friend class PropertiesTreeItemDelegate;
     Q_OBJECT
 public:
-    explicit PropertiesTreeQVariantItemDelegate(QObject *parent = NULL);
-    ~PropertiesTreeQVariantItemDelegate();
+    explicit ItemDelegateForQVector2D(QObject *parent = NULL);
+    ~ItemDelegateForQVector2D();
 
     virtual QWidget * createEditor( QWidget * parent, const QStyleOptionViewItem & option, const QModelIndex & index ) const override;
     virtual void setEditorData ( QWidget * editor, const QModelIndex & index ) const override;
     virtual void setModelData ( QWidget * editor, QAbstractItemModel * model, const QModelIndex & index ) const override;
 public:
-    bool IsEditorDataModified(QWidget *editor) const;
-    //virtual bool eventFilter(QObject *object, QEvent *event);
 
 private slots:
     void OnCommitData(QWidget *editor);
