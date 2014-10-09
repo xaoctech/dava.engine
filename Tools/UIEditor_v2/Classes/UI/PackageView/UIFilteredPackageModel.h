a52bcaf83dbbe67f79368a459a32f37f32871cbe
Alexey Strokachuk <leronxp@gmail.com>
2014-10-09 11:14:09 +0300

    [DF-5048] remove loggin and fix win project.

--- Tools/UIEditor_v2/Classes/UI/PackageView/UIFilteredPackageModel.h
+++ Tools/UIEditor_v2/Classes/UI/PackageView/UIFilteredPackageModel.h
@@ -18,10 +18,7 @@ class UIFilteredPackageModel: public QSortFilterProxyModel
 public:
     UIFilteredPackageModel(QObject *parent = NULL);
     ~UIFilteredPackageModel();
-    
-    virtual Qt::DropActions supportedDropActions() const;
-    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
-    
+
 protected:
     virtual bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
     virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
