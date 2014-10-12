ffb83be58936e7e89261d90f311faaef6d7adbcd
Dima Belsky <dima.belsky@gmail.com>
2014-10-12 14:26:05 +0300

    [DF-5048] refactoring loading packages
    Former-commit-id: c8e562d80d106d865aa4ee683cd0246ae51014d5

---
+++ Tools/UIEditor_v2/Classes/UIControls/EditorUIPackageBuilder.h
@@ -0,0 +1,14 @@
+//
+//  EditorUIPackageBuilder.h
+//  UIEditor
+//
+//  Created by Dmitry Belsky on 11.10.14.
+//
+//
+
+#ifndef __UIEditor__EditorUIPackageBuilder__
+#define __UIEditor__EditorUIPackageBuilder__
+
+#include <stdio.h>
+
+#endif /* defined(__UIEditor__EditorUIPackageBuilder__) */
