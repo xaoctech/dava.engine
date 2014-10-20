abf76a36753d7aba6cb25eec9c368623f252c828
y_drazdouski <y_drazdouski@wargaming.net>
2014-10-20 13:45:28 +0300

    DF-4794 local notifications for android
    Former-commit-id: 7fa55fff1427fab0de7f6f4774ec903c78d633f3

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/ScheduledNotificationReceiver.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/ScheduledNotificationReceiver.java
@@ -13,9 +13,11 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder.setContentTitle("Title");
-        builder.setContentText("Text");
+        String uid = intent.getStringExtra("uid");
+        builder.setContentTitle(intent.getStringExtra("title"));
+        builder.setContentText(intent.getStringExtra("text"));
+        builder.setSmallIcon(intent.getIntExtra("icon", 0));
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());
+        notificationManager.notify(uid, 0, builder.build());
     }
 }
