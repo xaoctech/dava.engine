e53bfead30d4e2194d3a614ef4047ddddbed1930
binaryzebra <vitaliy.borodovsky@gmail.com>
2014-10-27 21:56:17 +0300

    [DF-3576] Pixel lit fake enable
    Former-commit-id: 8699f4738d505300d2107f8e4a9b75ba6e8f1bb0

--- Sources/Internal/Render/Highlevel/Landscape.cpp
+++ Sources/Internal/Render/Highlevel/Landscape.cpp
@@ -130,7 +130,7 @@ Landscape::Landscape()
     prevLodLayer = -1;
     
 	NMaterial* landscapeParent = NMaterial::CreateMaterial(FastName("Landscape_Tilemask_Material"),
-                                                            NMaterialName::TILE_MASK_SPECULAR,
+                                                            NMaterialName::TILE_MASK_PIXEL_LIT,
                                                             NMaterial::DEFAULT_QUALITY_NAME);
     
 	tileMaskMaterial = 	NMaterial::CreateMaterialInstance();
