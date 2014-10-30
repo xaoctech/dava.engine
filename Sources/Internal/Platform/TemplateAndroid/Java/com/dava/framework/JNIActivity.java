bd325c8cab4f175af92203a46fd8a12e98561d55
Yaroslav.Zablotskiy <Yaroslav.Zablotskiy@softsculptor.net>
2014-10-30 11:22:26 +0200

    [DF-5211] fix device sleep after lock/unlock device
    Former-commit-id: b30ee4f2099b45f5fa84b1671a2f6c47b0b9a0e9

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNIActivity.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNIActivity.java
@@ -177,6 +177,8 @@ public abstract class JNIActivity extends Activity implements JNIAccelerometer.J
         }
         
         Log.i(JNIConst.LOG_TAG, "[Activity::onResume] finish");
+        
+        JNIUtils.onResume();
     }
 
     
