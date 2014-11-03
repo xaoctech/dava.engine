25792829b958a26b9ab1f061378bdb3a46aa6adc
Mikhail Polubisok <m_polubisok@wargaming.net>
2014-11-03 17:48:40 +0300

    [DF-5106] Add android, maces and ios projects and binaries of libunibreak

---
+++ Libs/libunibreak/android/jni/Android.mk
@@ -0,0 +1,31 @@
+#-----------------------------
+# libunibrake lib
+
+# set local path for lib
+LOCAL_PATH := $(call my-dir)
+
+# clear all variables
+include $(CLEAR_VARS)
+
+# set module name
+LOCAL_MODULE := libunibreak_android
+
+# set path for includes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../src
+
+# set exported includes
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+
+# set source files
+LOCAL_SRC_FILES :=  \
+                  ../../src/linebreak.c \
+                  ../../src/linebreakdata.c \
+                  ../../src/linebreakdef.c \
+                  ../../src/wordbreak.c \
+                  ../../src/wordbreakdata.c \ss
+                  
+LOCAL_CFLAGS := -O2
+
+# build static library
+include $(BUILD_STATIC_LIBRARY)
---
+++ Libs/libunibreak/android/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_MODULES := libunibreak_android
+
+APP_ABI := armeabi-v7a x86
+APP_PLATFORM := android-10
+APP_OPTIM := release
