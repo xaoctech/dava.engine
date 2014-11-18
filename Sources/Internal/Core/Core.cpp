a145b51301a3fb5c5070f722916e345eb9de8f11
leanid <leanid.chaika@gmail.com>
2014-11-18 09:22:32 +0300

    DF-5129 use framework func Split
    Former-commit-id: 912647fe36552be8f323223e8f06c0c2140aa821

--- Sources/Internal/Core/Core.cpp
+++ Sources/Internal/Core/Core.cpp
@@ -799,12 +799,8 @@ void Core::SetCommandLine(int argc, char *argv[])
 
 void Core::SetCommandLine(const DAVA::String& cmdLine)
 {
-	std::stringstream ss(cmdLine);
-	std::string param;
-	while (std::getline(ss, param, ' '))
-	{
-		commandLine.push_back(param);
-	}
+    commandLine.clear();
+    Split(cmdLine, "\n", commandLine);
 }
 
 Vector<String> & Core::GetCommandLine()
