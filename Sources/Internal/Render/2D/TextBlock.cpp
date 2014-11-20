eb79442b6dc2c569a132becc792ba8e717e1f916
d_shpakov <d_shpakov@wargaming.net>
2014-11-20 14:54:10 +0300

    DF-5194 fixed size calculation in case of fribidi marks present in text

--- Sources/Internal/Render/2D/TextBlock.cpp
+++ Sources/Internal/Render/2D/TextBlock.cpp
@@ -44,6 +44,8 @@
 #include "Render/2D/TextBlockDistanceRender.h"
 
 #include "Utils/StringUtils.h"
+#include "fribidi/fribidi-bidi-types.h"
+#include "fribidi/fribidi-unicode.h"
 
 namespace DAVA 
 {
@@ -947,6 +949,12 @@ void TextBlock::SplitTextToStrings(const WideString& string, Vector2 const& targ
 
     Vector<float32> sizes;
     font->GetStringSize(string, &sizes);
+    for(uint32 i = 0; i < sizes.size(); ++i)
+        if (FRIBIDI_IS_EXPLICIT_OR_BN (fribidi_get_bidi_type (string[i]))
+	    || string[i] == FRIBIDI_CHAR_LRM || string[i] == FRIBIDI_CHAR_RLM)
+        {
+            sizes[i] = 0.0f;
+        }
     if (sizes.size() != string.length())
     {
         return;
