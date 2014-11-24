9f2a0323b0a58a21b52d13518c5c257f902fcc27
y_drazdouski <y_drazdouski@wargaming.net>
2014-11-24 15:13:29 +0300

    DF-4794 incorrect notification appearance when application was killed

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNINotificationProvider.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/JNINotificationProvider.java
@@ -91,6 +91,9 @@ public class JNINotificationProvider {
         intent.putExtra("icon", icon);
         intent.putExtra("title", title);
         intent.putExtra("text", text);
+		if(JNIActivity.GetActivity() != null) {
+			intent.putExtra("activityClassName", JNIActivity.GetActivity().getClass().getName());
+		}
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0 /*(int) System.currentTimeMillis()*/, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         alarmManager.set(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis() + delaySeconds * 1000, pendingIntent);
     }
