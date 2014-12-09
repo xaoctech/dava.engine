bcd119ca38b6593e8822f9cc0fa6a1875040d788
Igor.Bratchik <stinvi@rambler.ru>
2014-12-09 14:55:11 +0300

    DF-5481 set path to dll
    Former-commit-id: d47d421a97085051d11558ba11795b0125c47a3e

--- Sources/CMake/Modules/GlobalVariables.cmake
+++ Sources/CMake/Modules/GlobalVariables.cmake
@@ -11,6 +11,9 @@ endif ()
 set ( DAVA_LIBRARY                     "DavaFramework" )
 set ( DAVA_ROOT_DIR                    "${CMAKE_CURRENT_LIST_DIR}/../../.." )
 
+get_filename_component( DAVA_ROOT_DIR ${DAVA_ROOT_DIR} ABSOLUTE )
+
+set ( DAVA_TOOLS_BIN_DIR               "${DAVA_ROOT_DIR}/Tools/Bin" )
 set ( DAVA_TOOLS_DIR                   "${DAVA_ROOT_DIR}/Sources/Tools" )
 set ( DAVA_ENGINE_DIR                  "${DAVA_ROOT_DIR}/Sources/Internal" )
 set ( DAVA_THIRD_PARTY_ROOT_PATH       "${DAVA_ROOT_DIR}/Libs" )
@@ -22,11 +25,11 @@ set ( DAVA_THIRD_PARTY_INCLUDES_PATH   "${DAVA_THIRD_PARTY_ROOT_PATH}/include"
                                        "${DAVA_THIRD_PARTY_ROOT_PATH}/fmod/include" 
                                       ) 
 
-set ( DAVA_TOOL_DIR                   "${DAVA_ENGINE_DIR}/../Tools" )
-
 set( DAVA_SPEEDTREE_ROOT_DIR            "${DAVA_ROOT_DIR}/../dava.speedtree" )                                      
-set( DAVA_RESOURCEEDITOR_BEAST_ROOT_DIR "${DAVA_ROOT_DIR}/../dava.resourceeditor.beast" )                                      
-
+set( DAVA_RESOURCEEDITOR_BEAST_ROOT_DIR "${DAVA_ROOT_DIR}/../dava.resourceeditor.beast" ) 
+                                   
+get_filename_component( DAVA_SPEEDTREE_ROOT_DIR ${DAVA_SPEEDTREE_ROOT_DIR} ABSOLUTE )
+get_filename_component( DAVA_RESOURCEEDITOR_BEAST_ROOT_DIR ${DAVA_RESOURCEEDITOR_BEAST_ROOT_DIR} ABSOLUTE )
 
 if     ( ANDROID )
     set ( DAVA_THIRD_PARTY_LIBRARIES_PATH  "${DAVA_THIRD_PARTY_ROOT_PATH}/lib_CMake/android/${ANDROID_NDK_ABI_NAME}" ) 
