71db6ade07eead1d00824b558c84b850806f1362
LeRoN <leronxp@gmail.com>
2014-12-09 23:56:01 +0300

    [DF-3824] Fix for UIWebView.
    Former-commit-id: a9687eee4ebea658aae02056dd4ec37862aaca6c

--- Sources/Internal/UI/UIWebView.h
+++ Sources/Internal/UI/UIWebView.h
@@ -62,7 +62,7 @@ public:
     void OpenFromBuffer(const String& string, const FilePath& basePath);
     
 	// Overloaded virtual methods.
-	virtual void SetPosition(const Vector2 &position, bool positionInAbsoluteCoordinates = false);
+	virtual void SetPosition(const Vector2 &position);
 	virtual void SetSize(const Vector2 &newSize);
 
     virtual void LoadFromYamlNode(const YamlNode * node, UIYamlLoader * loader);
