dc5626030b5e2eb82b638870a90e4c1bc4db4ba4
Leanid Chaika <leanid.chaika@gmail.com>
2014-12-12 16:48:04 +0300

    DF-5423 rename remove _ in filename
    Former-commit-id: f64bd7a4d935420ea844313494d91f9ce7e055eb

--- Sources/Internal/Debug/UIAlertViewModal.m
+++ Sources/Internal/Debug/UIAlertViewModal.m
@@ -14,7 +14,7 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-#import "UIAlertView_Modal.h"
+#import "UIAlertViewModal.h"
 
 NSInteger returnButtonIndex = -1;
 
