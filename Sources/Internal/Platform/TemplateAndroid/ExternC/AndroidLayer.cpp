652c1cee75e498a8b9ef401e90481f9777b438bf
Aleksei Kanash <aleksei.kanash@gmail.com>
2014-12-23 16:50:17 +0300

    [DF-5228] JniMovieView refactored.
    Former-commit-id: 2f38e01de2009ad5af0d4dead740e61a5a9b8899

--- Sources/Internal/Platform/TemplateAndroid/ExternC/AndroidLayer.cpp
+++ Sources/Internal/Platform/TemplateAndroid/ExternC/AndroidLayer.cpp
@@ -39,11 +39,9 @@
 #include "Input/AccelerometerAndroid.h"
 #include "AndroidDelegate.h"
 #include "AndroidCrashReport.h"
-
 #include "JniExtensions.h"
 #include "Platform/TemplateAndroid/DateTimeAndroid.h"
 #include "Platform/TemplateAndroid/AndroidCrashReport.h"
-#include "Platform/TemplateAndroid/MovieViewControlAndroid.h"
 #include "FileSystem/LocalizationAndroid.h"
 #include "Platform/TemplateAndroid/FileListAndroid.h"
 #include "Utils/UTF8Utils.h"
@@ -106,7 +104,6 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved)
 		LOGE("Failed get java environment");
 		return -1;
 	}
-	DAVA::JniExtension::SetJavaClass(env, "com/dava/framework/JNIMovieViewControl", &DAVA::JniMovieViewControl::gJavaClass, &DAVA::JniMovieViewControl::gJavaClassName);
 	DAVA::JniExtension::SetJavaClass(env, "com/dava/framework/JNILocalization", &DAVA::JniLocalization::gJavaClass, &DAVA::JniLocalization::gJavaClassName);
 	DAVA::JniExtension::SetJavaClass(env, "com/dava/framework/JNIFileList", &DAVA::JniFileList::gJavaClass, &DAVA::JniFileList::gJavaClassName);
 	DAVA::JniExtension::SetJavaClass(env, "com/dava/framework/JNIDateTime", &DAVA::JniDateTime::gJavaClass, &DAVA::JniDateTime::gJavaClassName);
