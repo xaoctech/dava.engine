14f928d9a7ea0a015752be879f79cef2ead80d4b
Dima Belsky <dima.belsky@gmail.com>
2015-01-30 11:08:50 +0300

    [DF-5527] quicked: refactoring packages information
    Former-commit-id: a3985ef7cb82c8a04e41ecbb8515122b9163489c

---
+++ Tools/QuickEd/Classes/UIControls/PackageHierarchy/ControlPrototype.cpp
@@ -0,0 +1,9 @@
+//
+//  ControlPrototype.cpp
+//  QuickEd
+//
+//  Created by Dmitry Belsky on 29.1.15.
+//
+//
+
+#include "ControlPrototype.h"
---
+++ Tools/QuickEd/Classes/UIControls/PackageHierarchy/ControlPrototype.h
@@ -0,0 +1,14 @@
+//
+//  ControlPrototype.h
+//  QuickEd
+//
+//  Created by Dmitry Belsky on 29.1.15.
+//
+//
+
+#ifndef __QuickEd__ControlPrototype__
+#define __QuickEd__ControlPrototype__
+
+#include <stdio.h>
+
+#endif /* defined(__QuickEd__ControlPrototype__) */
