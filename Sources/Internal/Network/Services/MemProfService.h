aa4b5dfd257a6b3f0959d5cccb5830a4299013a9
max <kkdaemon@gmail.com>
2015-02-09 00:01:57 +0300

    add MemProfService class
    Former-commit-id: f9d0386ac3a2d3c77832c232512f3dcd34f2917a

--- Sources/Internal/Network/Services/MemProfService.h
+++ Sources/Internal/Network/Services/MemProfService.h
@@ -5,16 +5,20 @@
 #include "Base/BaseTypes.h"
 #include "Network/NetService.h"
 
+#include "memprof/mem_profiler_types.h"
+
 namespace DAVA
 {
 namespace Net
 {
 
 class MemProfService : public NetService
-{
+{    
 public:
     MemProfService();
     virtual ~MemProfService();
+
+    void OnUpdate(float32 timeElapsed);
 
     void ChannelOpen() override;
     void ChannelClosed(const char8* message) override;
@@ -23,6 +27,18 @@ public:
     void PacketDelivered() override;
     
 private:
+    void SendNextRecord();
+    
+    bool Enqueue(net_mem_stat_t* h);
+    net_mem_stat_t* GetFirstMessage();
+    void RemoveFirstMessage();
+    
+private:
+    uint32 period;
+    uint32 passed;
+    size_t maxQueueSize;
+    uint32 timestamp;
+    Deque<net_mem_stat_t*> queue;
 };
 
 }   // namespace Net
