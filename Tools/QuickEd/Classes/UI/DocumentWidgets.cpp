cd0156186ad376cb5d6d2c04a7f6d3241fcf8c52
Dima Belsky <dima.belsky@gmail.com>
2015-02-12 21:40:55 +0300

    [DF-5527] quicked: refactoring PreviewWidget
    Former-commit-id: bea4b5d4be4505d1ee51b1a5197487c607d9eed2

---
+++ Tools/QuickEd/Classes/UI/DocumentWidgets.cpp
@@ -0,0 +1,9 @@
+//
+//  DocumentWidgets.cpp
+//  QuickEd
+//
+//  Created by Dmitry Belsky on 12.2.15.
+//
+//
+
+#include "DocumentWidgets.h"
---
+++ Tools/QuickEd/Classes/UI/DocumentWidgets.h
@@ -0,0 +1,14 @@
+//
+//  DocumentWidgets.h
+//  QuickEd
+//
+//  Created by Dmitry Belsky on 12.2.15.
+//
+//
+
+#ifndef __QuickEd__DocumentWidgets__
+#define __QuickEd__DocumentWidgets__
+
+#include <stdio.h>
+
+#endif /* defined(__QuickEd__DocumentWidgets__) */
