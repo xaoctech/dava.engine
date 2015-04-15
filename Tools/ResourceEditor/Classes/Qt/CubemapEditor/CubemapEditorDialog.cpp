3202912a0127c4f36cc256852ffbb66d7bfd2f91
klesch <victor.kleschenko@gmail.com>
2015-04-15 14:33:59 +0300

    [DF-6043] Changed loading of images in cubemap editor
    Former-commit-id: 7911b4f6857714a9aefbf08e4ed66f2bba7351d5

--- Tools/ResourceEditor/Classes/Qt/CubemapEditor/CubemapEditorDialog.cpp
+++ Tools/ResourceEditor/Classes/Qt/CubemapEditor/CubemapEditorDialog.cpp
@@ -37,6 +37,7 @@
 #include "Project/ProjectManager.h"
 
 #include "Tools/PathDescriptor/PathDescriptor.h"
+#include "ImageTools/ImageTools.h"
 
 
 #include <QMouseEvent>
@@ -134,7 +135,7 @@ bool CubemapEditorDialog::LoadImageTo(const DAVA::FilePath& filePath, int face, 
     QString errorString;
     if (VerifyImage(filePath, face, errorString))
     {
-        QImage faceImage(fileName);
+        QImage faceImage = ImageTools::FromDavaImage(filePath);
 
         ClickableQLabel* label = GetLabelForFace(face);
         QImage scaledFace = faceImage.scaled(label->width(), label->height());
@@ -401,7 +402,7 @@ void CubemapEditorDialog::SaveCubemap(const QString& path)
 			{
 				QTransform transform;
 				transform.rotate(faceLabel->GetRotation());
-				QImage qimg(targetFullPath.c_str());
+                QImage qimg = ImageTools::FromDavaImage(targetFullPath.c_str());
 				QImage rotatedImage = qimg.transformed(transform);
 				rotatedImage.save(targetFullPath.c_str());
                 faceLabel->SetRotation(0);
