b9d6b709bc9cc305430b9977cca81351671cfe89
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-05-13 13:24:58 +0300

    Added c_str() to Format
    Former-commit-id: 85a1cfc90ee367682aad18256387954d0ad3aae9

--- Tools/QuickEd/Classes/Project/EditorFontSystem.cpp
+++ Tools/QuickEd/Classes/Project/EditorFontSystem.cpp
@@ -65,13 +65,13 @@ void EditorFontSystem::SetFont(const String &presetName, const String &locale, F
 {
     if (nullptr != font)
     {
-        DVASSERT_MSG(false, Format("wrong argument: font = nullptr"));
+        DVASSERT_MSG(false, "wrong argument: font = nullptr");
         return;
     }
     auto fonstIt = localizedFonts.find(locale);
     if (fonstIt == localizedFonts.end())
     {
-        DVASSERT_MSG(false, Format("wrong argument: locale = %s passed to this function not found", locale.c_str()));
+        DVASSERT_MSG(false, Format("wrong argument: locale = %s passed to this function not found", locale.c_str()).c_str());
         return;
     }
     auto &fonts = fonstIt->second;
@@ -79,7 +79,7 @@ void EditorFontSystem::SetFont(const String &presetName, const String &locale, F
 
     if(it != fonts.end())
     {
-        DVASSERT_MSG(false, Format("wrong argument: presetName = %s passed to this function not found for locale %s", presetName.c_str(), locale.c_str()));
+        DVASSERT_MSG(false, Format("wrong argument: presetName = %s passed to this function not found for locale %s", presetName.c_str(), locale.c_str()).c_str());
         return;
     }
 
