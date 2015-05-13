66dca929d44cbe3a8e9a6fe17be2d4c683fc52cd
Aleksandr Klubovich <avklubovich@gmail.com>
2015-05-13 14:10:18 +0300

    [DF-6532] fix in WayEditSystem
    Former-commit-id: 47af5e07512ed810608a1a411a6df79b6d1a5269

--- Tools/ResourceEditor/Classes/Qt/Scene/System/WayEditSystem.h
+++ Tools/ResourceEditor/Classes/Qt/Scene/System/WayEditSystem.h
@@ -72,7 +72,7 @@ protected:
     void ProcessCommand(const Command2 *command, bool redo);
 
     DAVA::Entity* CreateWayPoint(DAVA::Entity *parent, DAVA::Vector3 pos);
-    DAVA::Entity* CopyWayPoint(DAVA::Entity* waypoint);
+    //DAVA::Entity* CopyWayPoint(DAVA::Entity* waypoint);
 
     void RemoveEdge(DAVA::Entity* entity, DAVA::EdgeComponent * edgeComponent);
 
