ffe5fec7ec0ae97a0811e8fe83751d6dfd40f917
Kernel <Kernel@tut.by>
2015-05-27 12:30:52 +0300

    DF-6367 removed vertex fog removal
    Former-commit-id: bf3df8eafef3f4f7756db81a364dd9688179ed0d

--- Sources/Internal/Scene3D/SceneFileV2.cpp
+++ Sources/Internal/Scene3D/SceneFileV2.cpp
@@ -480,9 +480,7 @@ SceneFileV2::eError SceneFileV2::LoadScene(const FilePath & filename, Scene * _s
             if (name == "GlobalMaterial")
             {
                 uint64 globalMaterialId = archive->GetUInt64("globalMaterialId");
-                NMaterial * globalMaterial = static_cast<NMaterial*>(serializationContext.GetDataBlock(globalMaterialId));
-
-                globalMaterial->RemoveFlag(NMaterialFlagName::FLAG_VERTEXFOG);
+                NMaterial * globalMaterial = static_cast<NMaterial*>(serializationContext.GetDataBlock(globalMaterialId));                
 
                 scene->SetGlobalMaterial(globalMaterial);
                 serializationContext.SetGlobalMaterialKey(globalMaterialId);
