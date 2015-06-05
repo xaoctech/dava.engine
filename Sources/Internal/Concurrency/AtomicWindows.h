ef0340f4d73aed949b6939bffaeeb5708ac7f230
Andrey Derkach <a_derkach2@wargaming.net>
2015-06-05 21:32:30 +0300

    [DF-6650] Some pull request remarks fix
    Former-commit-id: 7e487238b31d7432e76756cc47685356601f451e

--- Sources/Internal/Concurrency/AtomicWindows.h
+++ Sources/Internal/Concurrency/AtomicWindows.h
@@ -32,6 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "Base/Platform.h"
 #ifndef USE_CPP11_CONCURRENCY
 
+#include <intrin.h>
 #include "Concurrency/Atomic.h"
 
 namespace DAVA
@@ -49,64 +50,82 @@ template <size_t N>
 struct TypeSelector;
 
 template <>
+struct TypeSelector<sizeof(CHAR)> { using Type = CHAR; };
+template <>
 struct TypeSelector<sizeof(SHORT)> { using Type = SHORT; };
 template <>
 struct TypeSelector<sizeof(LONG)> { using Type = LONG; };
 template <>
 struct TypeSelector<sizeof(LONGLONG)> { using Type = LONGLONG; };
 
-//atomic increment overloads 
-inline SHORT AtomicIncrement(SHORT* value)
+//atomic increment overloads
+inline CHAR AtomicIncrement(volatile CHAR* value)
+{
+    return ::_InterlockedExchangeAdd8(value, 1);
+}
+inline SHORT AtomicIncrement(volatile SHORT* value)
 {
     return ::InterlockedIncrement16(value);
 }
-inline LONG AtomicIncrement(LONG* value)
+inline LONG AtomicIncrement(volatile LONG* value)
 {
     return ::InterlockedIncrement(value);
 }
-inline LONGLONG AtomicIncrement(LONGLONG* value)
+inline LONGLONG AtomicIncrement(volatile LONGLONG* value)
 {
     return ::InterlockedIncrement64(value);
 }
 
 //atomic decrement overloads 
-inline short AtomicDecrement(short* value)
+inline CHAR AtomicDecrement(volatile CHAR* value)
+{
+    return ::_InterlockedExchangeAdd8(value, -1);
+}
+inline SHORT AtomicDecrement(volatile SHORT* value)
 {
     return ::InterlockedDecrement16(value);
 }
-inline LONG AtomicDecrement(LONG* value)
+inline LONG AtomicDecrement(volatile LONG* value)
 {
     return ::InterlockedDecrement(value);
 }
-inline LONGLONG AtomicDecrement(LONGLONG* value)
+inline LONGLONG AtomicDecrement(volatile LONGLONG* value)
 {
     return ::InterlockedDecrement64(value);
 }
 
 //atomic swap overloads 
-inline SHORT AtomicSwap(SHORT* target, SHORT desired)
+inline CHAR AtomicSwap(volatile CHAR* target, CHAR desired)
+{
+    return ::InterlockedExchange8(target, desired);
+}
+inline SHORT AtomicSwap(volatile SHORT* target, SHORT desired)
 {
     return ::InterlockedExchange16(target, desired);
 }
-inline LONG AtomicSwap(LONG* target, LONG desired)
+inline LONG AtomicSwap(volatile LONG* target, LONG desired)
 {
     return ::InterlockedExchange(target, desired);
 }
-inline LONGLONG AtomicSwap(LONGLONG* target, LONGLONG desired)
+inline LONGLONG AtomicSwap(volatile LONGLONG* target, LONGLONG desired)
 {
     return ::InterlockedExchange64(target, desired);
 }
 
 //atomic cas overloads
-inline SHORT AtomicCAS(SHORT* target, SHORT expected, SHORT desired)
+inline CHAR AtomicCAS(volatile CHAR* target, CHAR expected, CHAR desired)
+{
+    return ::_InterlockedCompareExchange8(target, desired, expected);
+}
+inline SHORT AtomicCAS(volatile SHORT* target, SHORT expected, SHORT desired)
 {
     return ::InterlockedCompareExchange16(target, desired, expected);
 }
-inline LONG AtomicCAS(LONG* target, LONG expected, LONG desired)
+inline LONG AtomicCAS(volatile LONG* target, LONG expected, LONG desired)
 {
     return ::InterlockedCompareExchange(target, desired, expected);
 }
-inline LONGLONG AtomicCAS(LONGLONG* target, LONGLONG expected, LONGLONG desired)
+inline LONGLONG AtomicCAS(volatile LONGLONG* target, LONGLONG expected, LONGLONG desired)
 {
     return ::InterlockedCompareExchange64(target, desired, expected);
 }
@@ -134,45 +153,60 @@ T Atomic<T>::Get() const DAVA_NOEXCEPT
 template <typename T>
 T Atomic<T>::Increment() DAVA_NOEXCEPT
 {
-    using Type = Detail::TypeSelector<sizeof(T)>::Type;
+    using Type = typename Detail::TypeSelector<sizeof(T)>::Type;
     Type* val = reinterpret_cast<Type*>(&value);
 
-    T result = static_cast<T>(Detail::AtomicIncrement(val));
+    T result = Cast(Detail::AtomicIncrement(val));
     return result;
 }
 
 template <typename T>
 T Atomic<T>::Decrement() DAVA_NOEXCEPT
 {
-    using Type = Detail::TypeSelector<sizeof(T)>::Type;
+    using Type = typename Detail::TypeSelector<sizeof(T)>::Type;
     Type* val = reinterpret_cast<Type*>(&value);
 
-    T result = static_cast<T>(Detail::AtomicDecrement(val));
+    T result = Cast(Detail::AtomicDecrement(val));
     return result;
 }
 
 template <typename T>
 T Atomic<T>::Swap(T desired) DAVA_NOEXCEPT
 {
-    using Type = Detail::TypeSelector<sizeof(T)>::Type;
+    using Type = typename Detail::TypeSelector<sizeof(T)>::Type;
     Type* val = reinterpret_cast<Type*>(&value);
     Type des = static_cast<Type>(desired);
 
-    T result = static_cast<T>(Detail::AtomicSwap(val, des));
+    T result = Cast(Detail::AtomicSwap(val, des));
     return result;
 }
 
 template <typename T>
 bool Atomic<T>::CompareAndSwap(T expected, T desired) DAVA_NOEXCEPT
 {
-    using Type = Detail::TypeSelector<sizeof(T)>::Type;
+    using Type = typename Detail::TypeSelector<sizeof(T)>::Type;
     Type* val = reinterpret_cast<Type*>(&value);
     Type exp = static_cast<Type>(expected);
     Type des = static_cast<Type>(desired);
 
-    T result = static_cast<T>(Detail::AtomicCAS(val, exp, des));
+    T result = Cast(Detail::AtomicCAS(val, exp, des));
     return result == expected;
 }
+
+template <typename T>
+template <typename Y> 
+T Atomic<T>::Cast(Y val)
+{
+    return static_cast<T>(val);
+}
+
+template <>
+template <typename Y>
+bool Atomic<bool>::Cast(Y val)
+{
+    return val != 0;
+}
+
 
 #endif //  __DAVAENGINE_WINDOWS__
 
