fa5babb2d89d85adc45b25e95c9db595defe3131
Kernel <Kernel@tut.by>
2015-06-05 14:26:00 +0300

    fixed variables after merge
    Former-commit-id: 29dcc0a229cb92c322d68da7e322a9c1e365240c

--- Sources/Internal/Render/Texture.cpp
+++ Sources/Internal/Render/Texture.cpp
@@ -440,10 +440,10 @@ void Texture::FlushDataToRenderer(Vector<Image *> * images)
     descriptor.type = ((*images)[0]->cubeFaceID == Texture::INVALID_CUBEMAP_FACE) ? rhi::TEXTURE_TYPE_2D : rhi::TEXTURE_TYPE_CUBE;
     descriptor.format = formatDescriptor.format;
 
-    descriptor.mipCount = (descriptor.type == rhi::TEXTURE_TYPE_CUBE) ? images->size() / 6 : images->size();
+    descriptor.levelCount = (descriptor.type == rhi::TEXTURE_TYPE_CUBE) ? images->size() / 6 : images->size();
 
     for (Image * img : (*images))
-        descriptor.mipCount = Max(descriptor.mipCount, img->mipmapLevel + 1);
+        descriptor.levelCount = Max(descriptor.levelCount, img->mipmapLevel + 1);
 
     DVASSERT(descriptor.format != -1);//unsupported format
     handle = rhi::CreateTexture(descriptor);
