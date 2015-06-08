4a5aa9d5c647d09728dbdef24ee9ca1f699b1688
Aleksey Podverbnyy <a_podverbnyy@wgs-macbook-pro.corp.wargaming.local>
2015-06-08 13:33:56 +0300

    DF-6367 corrected Texture creation;
    Former-commit-id: b39dbeb11cd1a32470c81895b3997b4fcd3ccad5

--- Sources/Internal/Render/Texture.cpp
+++ Sources/Internal/Render/Texture.cpp
@@ -417,6 +417,7 @@ void Texture::FlushDataToRenderer(Vector<Image *> * images)
     rhi::Texture::Descriptor descriptor;
     descriptor.autoGenMipmaps = false;
     descriptor.isRenderTarget = false;
+    descriptor.levelCount = ((*images)[0]->cubeFaceID == Texture::INVALID_CUBEMAP_FACE)  ? images->size()  : images->size()/6;
     descriptor.width = (*images)[0]->width;
     descriptor.height = (*images)[0]->height;
     descriptor.type = ((*images)[0]->cubeFaceID == Texture::INVALID_CUBEMAP_FACE) ? rhi::TEXTURE_TYPE_2D : rhi::TEXTURE_TYPE_CUBE;
