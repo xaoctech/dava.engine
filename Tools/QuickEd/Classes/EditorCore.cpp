263d0f1a074be79039ea5623cee19acf9ce12d12
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-07-02 15:36:57 +0300

    [DF-6973] details from review fixed
    Former-commit-id: 52ca1d4041c493b5d4f4d13a1436442c8a70dd94

--- Tools/QuickEd/Classes/EditorCore.cpp
+++ Tools/QuickEd/Classes/EditorCore.cpp
@@ -127,7 +127,7 @@ void EditorCore::OnOpenPackageFile(const QString &path)
 void EditorCore::OnProjectPathChanged(const QString &projectPath)
 {
     QRegularExpression searchOption("gfx\\d*$", QRegularExpression::CaseInsensitiveOption);
-    dialogReloadSprites->GetSpritesPacker()->ClearTasks();
+    mainWindow->GetDialogReloadSprites()->GetSpritesPacker()->ClearTasks();
     QDirIterator it(projectPath + "/DataSource");
     while (it.hasNext())
     {
@@ -142,7 +142,7 @@ void EditorCore::OnProjectPathChanged(const QString &projectPath)
             }
             outputPath.replace(outputPath.lastIndexOf("DataSource"), QString("DataSource").size(), "Data");
             QDir outputDir(outputPath);
-            dialogReloadSprites->GetSpritesPacker()->AddTask(fileInfo.absoluteFilePath(), outputDir);
+            mainWindow->GetDialogReloadSprites()->GetSpritesPacker()->AddTask(fileInfo.absoluteFilePath(), outputDir);
         }
     }
 }
