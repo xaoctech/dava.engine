2a0cfe2ec795bdd1f7ed15b6457cd1d61ac9fefb
Igor.Bratchik <stinvi@rambler.ru>
2015-07-15 17:10:49 +0300

    DF-7055 fix cmake building
    Former-commit-id: b8eeec8ca57a6a3793b73fcce17b466f3ddfab29

--- Sources/CMake/Modules/FileTreeCheck.cmake
+++ Sources/CMake/Modules/FileTreeCheck.cmake
@@ -1,31 +1,29 @@
-include ( GlobalVariables      )
+MACRO ( FILE_TREE_CHECK arg_folders ) 
+    find_package( PythonInterp   )
 
-FIND_PROGRAM( PYTHON_BINARY python )
-
-MACRO( FILE_TREE_CHECK arg_folders ) 
-
-    if( PYTHON_BINARY AND NOT IGNORE_FILE_TREE_CHECK )
-
+    if( PYTHONINTERP_FOUND AND NOT IGNORE_FILE_TREE_CHECK )
+        set( TARGET_FILE_TREE_FOUND true )
         EXECUTE_PROCESS(
-            COMMAND ${PYTHON_BINARY} "${DAVA_SCRIPTS_FILES_PATH}/file_tree_hash.py" ${arg_folders}
+            COMMAND ${PYTHON_EXECUTABLE} "${DAVA_SCRIPTS_FILES_PATH}/file_tree_hash.py" ${arg_folders}
             OUTPUT_VARIABLE FILE_TREE_HASH
         )
 
         string(REPLACE "\n" "" FILE_TREE_HASH ${FILE_TREE_HASH})
         string(REPLACE ";" "," folders "${arg_folders}" )
 
-        add_custom_target ( FILE_TREE ALL 
-            COMMAND python ${DAVA_SCRIPTS_FILES_PATH}/versions_check.py ${CMAKE_CURRENT_BINARY_DIR} "${folders}" ${FILE_TREE_HASH}
+        add_custom_target ( __FILE_TREE ALL 
+            COMMAND ${PYTHON_EXECUTABLE} ${DAVA_SCRIPTS_FILES_PATH}/versions_check.py ${CMAKE_CURRENT_BINARY_DIR} "${folders}" ${FILE_TREE_HASH}
         )
 
-        set_target_properties( FILE_TREE PROPERTIES FOLDER ${DAVA_PREDEFINED_TARGETS_FOLDER} )         
+        set_target_properties( __FILE_TREE PROPERTIES FOLDER ${DAVA_PREDEFINED_TARGETS_FOLDER} )         
 
         FOREACH( item ${arg_folders} )
             message( " - ${item}" )        
         ENDFOREACH()
 
         message( "hash  ${FILE_TREE_HASH}" )        
-       
+
+
     endif()    
 
 endmacro( FILE_TREE_CHECK )
