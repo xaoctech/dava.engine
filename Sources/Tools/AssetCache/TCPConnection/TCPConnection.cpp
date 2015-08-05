408b23121fa994f6bf49a533b0564ae82181a609
klesch <victor.kleschenko@gmail.com>
2015-08-05 18:37:05 +0300

    [DF-6961] Removed comment
    Former-commit-id: d18cffb3458fbb1c78e6f842736aa81cf8c1ca43

--- Sources/Tools/AssetCache/TCPConnection/TCPConnection.cpp
+++ Sources/Tools/AssetCache/TCPConnection/TCPConnection.cpp
@@ -73,7 +73,6 @@ TCPConnection::~TCPConnection()
     if(Net::NetCore::INVALID_TRACK_ID != controllerId && Net::NetCore::Instance())
     {
         Disconnect();
-        //need wait for diconnecting ?
     }
 }
     
