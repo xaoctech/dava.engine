22c8bc6f0ca9026ef8236275beae0189dd2d3400
Sergey Zdanevich <s_zdanevich@wargaming.net>
2015-08-06 15:25:46 +0300

    [DF-185] Fixed MakeSharedObject for DAVA::BaseObject
    Former-commit-id: e36051ac1ffecc96c20f81eda74065498edf77d2

--- Sources/Internal/Base/BaseObject.h
+++ Sources/Internal/Base/BaseObject.h
@@ -173,6 +173,7 @@ template<typename T>
 auto MakeSharedObject(T *obj) -> typename std::enable_if<std::is_base_of<BaseObject, T>::value, std::shared_ptr<T>>::type
 {
     DVASSERT(nullptr != obj);
+    obj->Retain();
     return std::shared_ptr<T>(obj, [](T *obj) { obj->Release(); });
 }
 
