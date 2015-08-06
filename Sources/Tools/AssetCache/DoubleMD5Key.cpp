b6674a538db95b839719012fc30ddf1340648197
klesch <victor.kleschenko@gmail.com>
2015-08-06 18:37:31 +0300

    [DF-6961] refactored key
    Former-commit-id: e07116598d87f9056a9e75966f8c8cf310093f77

---
+++ Sources/Tools/AssetCache/DoubleMD5Key.cpp
@@ -0,0 +1,64 @@
+/*==================================================================================
+    Copyright (c) 2008, binaryzebra
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    * Neither the name of the binaryzebra nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE binaryzebra AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL binaryzebra BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=====================================================================================*/
+
+#include "AssetCache/DoubleMD5Key.h"
+#include "Debug/DVAssert.h"
+
+namespace DAVA
+{
+namespace AssetCache
+{
+        
+String KeyToString(const DoubleMD5Key &key)
+{
+    static const DAVA::uint32 bufferSize = HASH_SIZE * 2;
+    Array<DAVA::char8, bufferSize> buffer;
+    
+    MD5::HashToChar(key.data(), buffer.data(), bufferSize);
+    MD5::HashToChar(key.data() + MD5::DIGEST_SIZE, buffer.data() + HASH_SIZE, bufferSize);
+    
+    return String(buffer.data(), bufferSize);
+}
+  
+DoubleMD5Key StringToKey(const String & string)
+{
+    DVASSERT(string.length() == HASH_SIZE * 2);
+    
+    DoubleMD5Key key;
+    MD5::CharToHash(string.data(), key.data());
+    MD5::CharToHash(string.data() + HASH_SIZE, key.data() + MD5::DIGEST_SIZE);
+    
+    return key;
+}
+
+        
+} // end of namespace AssetCache
+} // end of namespace DAVA
+
+
+
