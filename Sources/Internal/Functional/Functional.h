25a478d88c0f8d25e274d1d973972dbbadfdb3f1
Sergey Zdanevich <s_zdanevich@wargaming.net>
2015-08-07 17:53:16 +0300

    [DF-185] Added independent module header Functional.h
    Former-commit-id: cd263b3aa59c829def6ce67da409164076d33773

--- Sources/Internal/Functional/Functional.h
+++ Sources/Internal/Functional/Functional.h
@@ -26,70 +26,10 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-#ifndef __DAVA_SIGNAL_BASE_H__
-#define __DAVA_SIGNAL_BASE_H__
+#ifndef __DAVA_FUNCTIONAL_H__
+#define __DAVA_FUNCTIONAL_H__
 
-#include "Base/BaseTypes.h"
+#include "Functional/Function.h"
+#include "Functional/Signal.h"
 
-namespace DAVA {
-
-class TrackedObject;
-
-class SignalBase
-{
-public:
-    virtual ~SignalBase() = default;
-    virtual void Disconnect(TrackedObject*) = 0;
-};
-
-class TrackedObject
-{
-public:
-    ~TrackedObject()
-    {
-        while (trackedSignals.size() > 0)
-        {
-            auto it = trackedSignals.begin();
-            (*it)->Disconnect(this);
-        }
-    }
-
-    void Track(SignalBase *signal)
-    {
-        trackedSignals.insert(signal);
-    }
-
-    void Untrack(SignalBase *signal)
-    {
-        trackedSignals.erase(signal);
-    }
-    
-    template<typename T>
-    static TrackedObject* Cast(T *t)
-    {
-        return Detail<std::is_base_of<TrackedObject, T>::value>::Cast(t);
-    }
-
-protected:
-    Set<SignalBase*> trackedSignals;
-
-    template<bool is_derived_from_tracked_obj>
-    struct Detail;
-};
-    
-template<>
-struct TrackedObject::Detail<false>
-{
-    static TrackedObject* Cast(void* t) { return nullptr; }
-};
-
-template<>
-struct TrackedObject::Detail<true>
-{
-    template<typename T>
-    static TrackedObject* Cast(T* t) { return static_cast<TrackedObject *>(t); }
-};
-
-} // namespace DAVA
-
-#endif // __DAVA_SIGNAL_BASE_H__
+#endif // __DAVA_FUNCTIONAL_H__
