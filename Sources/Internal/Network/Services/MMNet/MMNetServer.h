9e9d71606ca45885f2b3596acb3c2d4f2c2b0f89
Maksim Lazarev <m_lazarev@wargaming.net>
2015-08-13 17:06:47 +0300

    DF-7369 make code responsible for sending memory stat look better
    Former-commit-id: bd9388a5897f7065ca02cf2062029eac986e5a09

--- Sources/Internal/Network/Services/MMNet/MMNetServer.h
+++ Sources/Internal/Network/Services/MMNet/MMNetServer.h
@@ -69,6 +69,7 @@ private:
     void AutoReplyStat(uint64 curTimestamp);
     void SendPacket(MMNetProto::Packet&& packet);
 
+    MMNetProto::Packet ObtainStatPacket();
     MMNetProto::Packet CreateHeaderOnlyPacket(uint16 type, uint16 status);
     MMNetProto::Packet CreateReplyTokenPacket(bool newSession);
     MMNetProto::Packet CreateReplyStatPacket(uint32 maxItems);
@@ -76,32 +77,38 @@ private:
     bool GetAndSaveSnapshot(uint64 curTimestamp);
 
 private:
-    uint32 connToken = 0;
     bool tokenRequested = false;
-    uint64 timerBegin;
-    uint64 lastStatTimestamp = 0;
-    uint64 statPeriod = 250;
+    uint32 connToken = 0;
+    uint64 baseTimePoint = 0;                       // Timestamp when server was started (it is approximate time when app was started)
+    uint32 statGatherFreq = 100;                    // Memory statistics gathering frequency (zero means each frame), ms
+    uint32 statSendFreq = 500;                      // Memory statistics sending frequency (zero means as soon as gathered), ms
+    uint64 lastGatheredStatTimestamp = 0;           // Timestamp at which the latest memory statistics have been gathered
+    uint64 statSentStatTimestamp = 0;               // Timestamp at which the latest memory statistics have been sent
 
-    List<MMNetProto::Packet> packetQueue;
-    MMNetProto::Packet curStatPacket;
-    uint32 maxStatItemsPerPacket = 30;
-    uint32 statItemsInPacket = 0;
-    uint64 statTimestamp = 0;
+    MMNetProto::Packet curStatPacket;               // Packet to collect memory stat to send it in one operation
+    uint32 statItemsInPacket = 0;                   // Number of memory stat items already stored in curStatPacket
+    uint32 maxStatItemsPerPacket = 0;               // Max number of stat items that curStatPacket can hold
 
-    Vector<MMNetProto::Packet> cachedStatPackets;
-    uint32 maxCachedPackets = 4;
-    uint32 freeCachedPackets = 0;
+    // Memory statistics are gathered at statGatherFreq frequency and placed into curStatPacket.
+    // Memory statistics are sent over network when one of the following conditions is met:
+    //  - difference between statSentStatTimestamp and current time exceeded statSendFreq, or
+    //  - statItemsInPacket reached maxStatItemsPerPacket
 
-    uint32 statItemSize = 0;
-    uint32 configSize = 0;
-    uint64 lastManualSnapshotTimestamp = 0;
+    static const uint32 MAX_POOL_SIZE = 4;
+    std::array<MMNetProto::Packet, MAX_POOL_SIZE> packetPool;   // Pool of packets for sending memory stat items to reduce memory allocations
+                                                                // Each packet can hold MAX_STATITEMS_PER_PACKET stat items
+    uint32 totalPoolEntries = 0;                                // Total number of entries in packet pool
+    uint32 freePoolEntries = 0;                                 // Number of free entries in packet pool
 
-    std::unique_ptr<MMAnotherService> anotherService;
+    uint32 statItemSize = 0;                        // Size of MemoryManager's memory statistics item
+    uint32 configSize = 0;                          // Size of MemoryManager's configuration block
+
+    List<MMNetProto::Packet> packetQueue;               // Queue of outgoing packets
+    std::unique_ptr<MMAnotherService> anotherService;   // Special service for uploading memory snapshots and other big data
 };
 
 }   // namespace Net
 }   // namespace DAVA
 
 #endif  // defined(DAVA_MEMORY_PROFILING_ENABLE)
-
 #endif  // __DAVAENGINE_MMNETSERVER_H__
