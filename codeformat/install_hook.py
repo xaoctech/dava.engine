9771b2bf134ac736c04a3c4b8b713d2739204cfd
Ivan Petrochenko <i_petrochenko@wargaming.net>
2015-08-31 14:48:10 +0300

    DF-7696 clang-format and config for windows
    Former-commit-id: 737d8ba18ea5d87b45fe95b4c7ab873cce7ae056

---
+++ codeformat/install_hook.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import shutil
+import os
+import errno
+
+def main(): 
+	os.mkdir('../.git/hooks')
+	try:
+		os.mkdir('../.git/hooks')
+	except OSError as exc: 
+		#already exists
+	  if exc.errno == errno.EEXIST:
+	    #already exists
+	    pass
+	  else: raise
+
+	shutil.copy('pre-commit', '../.git/hooks/pre-commit')
+
+
+if __name__ == '__main__':
+  main()
