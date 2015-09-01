205933e92a8f7d42581c63bdf1c84394e0bf2586
klesch <victor@davaconsulting.com>
2015-09-01 12:02:13 +0300

    [DF-7700] Fixed saving of recursive innerEmitters
    Former-commit-id: 3647832a3b723b700c939a8e7203c3c7fc4b066d

--- Tools/ResourceEditor/Classes/Qt/Main/mainwindow.cpp
+++ Tools/ResourceEditor/Classes/Qt/Main/mainwindow.cpp
@@ -336,6 +336,26 @@ QString GetSaveFolderForEmitters()
     return particlesPath;
 }
 
+
+
+void QtMainWindow::CollectEmittersForSave(ParticleEmitter *topLevelEmitter, DAVA::List<EmitterDescriptor> &emitters, const String &entityName) const
+{
+    DVASSERT(topLevelEmitter != nullptr);
+
+    for (auto & layer : topLevelEmitter->layers)
+    {
+        if (nullptr != layer->innerEmitter)
+        {
+            CollectEmittersForSave(layer->innerEmitter, emitters, entityName);
+            emitters.emplace_back(EmitterDescriptor(layer->innerEmitter, layer, layer->innerEmitter->configPath, entityName));
+        }
+    }
+
+    emitters.emplace_back(EmitterDescriptor(topLevelEmitter, nullptr, topLevelEmitter->configPath, entityName));
+}
+
+
+
 bool QtMainWindow::SaveAllSceneEmitters(SceneEditor2 *scene) const
 {
     DVASSERT(nullptr != scene);
@@ -345,19 +365,6 @@ bool QtMainWindow::SaveAllSceneEmitters(SceneEditor2 *scene) const
     if (effectEntities.empty())
         return true;
 
-    struct EmitterDescriptor
-    {
-        EmitterDescriptor(ParticleEmitter * _emitter, ParticleLayer *layer, FilePath path, String name)
-            : emitter(_emitter), ownerLayer(layer), yamlPath(path), entityName(name)
-        {
-        }
-
-        ParticleEmitter * emitter = nullptr;
-        ParticleLayer *ownerLayer = nullptr;
-        FilePath yamlPath;
-        String entityName;
-    };
-
     DAVA::List<EmitterDescriptor> emittersForSave;
     for (auto & entityWithEffect : effectEntities)
     {
@@ -365,16 +372,7 @@ bool QtMainWindow::SaveAllSceneEmitters(SceneEditor2 *scene) const
         ParticleEffectComponent *effect = GetEffectComponent(entityWithEffect);
         for (int32 i = 0, sz = effect->GetEmittersCount(); i < sz; ++i)
         {
-            ParticleEmitter* emitter = effect->GetEmitter(i);
-            for (auto & layer : emitter->layers)
-            {
-                if (nullptr != layer->innerEmitter)
-                {
-                    emittersForSave.emplace_back(EmitterDescriptor(layer->innerEmitter, layer, layer->innerEmitter->configPath, entityName));
-                }
-            }
-
-            emittersForSave.emplace_back(EmitterDescriptor(emitter, nullptr, emitter->configPath, entityName));
+            CollectEmittersForSave(effect->GetEmitter(i), emittersForSave, entityName);
         }
     }
 
