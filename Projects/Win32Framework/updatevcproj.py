79eb9fad7ca1d82e6c15f8534c212afe7163b29e
Igor.Bratchik <stinvi@rambler.ru>
2015-09-09 16:45:11 +0300

    [DF-7818] deletet box2d for mac
    Former-commit-id: ef847e85fb2c8ec826f8e5a81247a57c3ee183d8

--- Projects/Win32Framework/updatevcproj.py
+++ Projects/Win32Framework/updatevcproj.py
@@ -11,7 +11,7 @@ import subprocess;
 import platform;
 import re;
 
-excludeDirs = ["External\Freetype"]
+excludeDirs = ["Box2D", "External\Freetype"]
 includePaths = {}
 export_script_dir = os.getcwd() + "/../../Sources/";
 
