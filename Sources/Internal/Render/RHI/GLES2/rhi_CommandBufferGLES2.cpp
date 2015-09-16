c506437da8e21fb0fc1aa451ddfbfc389e8e3347
Igor Solovey <i_solovey@wargaming.net>
2015-09-16 16:52:34 +0300

    DF-6367 fix compilation
    Former-commit-id: c72ae2a8e57729f08de4d9a61ec55add1e3780c5

--- Sources/Internal/Render/RHI/GLES2/rhi_CommandBufferGLES2.cpp
+++ Sources/Internal/Render/RHI/GLES2/rhi_CommandBufferGLES2.cpp
@@ -1,23 +1,41 @@
-﻿#include <GLES2/gl2.h>
-#include <iterator>
-#include <vector>
+﻿/*==================================================================================
+ Copyright (c) 2008, binaryzebra
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the binaryzebra nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE binaryzebra AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL binaryzebra BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ =====================================================================================*/
 
-#include "../../../../../../ADT/android-ndk-r10b/platforms/android-14/arch-arm/usr/include/string.h"
-#include "../../../Base/BaseTypes.h"
-#include "../../../Base/Message.h"
-#include "../../../Concurrency/Mutex.h"
-#include "../../../Concurrency/Spinlock.h"
-#include "../../../Debug/DVAssert.h"
-#include "../../../Math/Math2D.h"
-#include "../Common/dbg_StatSet.h"
-#include "../Common/rhi_Impl.h"
-#include "../Common/rhi_Pool.h"
-#include "../Common/rhi_Private.h"
-#include "../Common/rhi_RingBuffer.h"
-#include "../rhi_Type.h"
-#include "android_gl.h"
-#include "rhi_GLES2.h"
-#include "rhi_ProgGLES2.h"
+    #include "../Common/rhi_Pool.h"
+    #include "rhi_GLES2.h"
+    #include "rhi_ProgGLES2.h"
+
+    #include "../Common/rhi_Private.h"
+    #include "../Common/rhi_RingBuffer.h"
+    #include "../Common/dbg_StatSet.h"
+
+    #include "Debug/DVAssert.h"
+    #include "FileSystem/Logger.h"
 
     using DAVA::Logger;
     #include "Concurrency/Thread.h"
