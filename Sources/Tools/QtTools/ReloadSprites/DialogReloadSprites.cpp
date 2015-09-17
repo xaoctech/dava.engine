e3ee5c6a28568cb8e78897be93f49b25a8629de9
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-09-17 15:29:36 +0300

    extra quit and wait removed
    Former-commit-id: 66cd54d27b6dc237d243857d0e189a7574cb20af

--- Sources/Tools/QtTools/ReloadSprites/DialogReloadSprites.cpp
+++ Sources/Tools/QtTools/ReloadSprites/DialogReloadSprites.cpp
@@ -185,12 +185,10 @@ void DialogReloadSprites::BlockingStop()
     QEventLoop loop;
     connect(spritesPacker, &SpritesPacker::Finished, &loop, &QEventLoop::quit);
     spritesPacker->Cancel();
-    workerThread.quit();
     if (spritesPacker->IsRunning())
     {
         loop.exec();
     }
-    workerThread.wait();
     QApplication::restoreOverrideCursor();
     workerThread.quit();
     this->setEnabled(true);
