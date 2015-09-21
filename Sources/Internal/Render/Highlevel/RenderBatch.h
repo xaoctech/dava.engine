3ae957be89af2b34879839da8aa312b8696132d0
Sergey Reznik <s_reznik@wargaming.net>
2015-09-21 14:26:51 +0300

    Line endings in RenderBatch reverted
    Former-commit-id: 08cf5e37af553fe9394661c9621e98462f7055df

--- Sources/Internal/Render/Highlevel/RenderBatch.h
+++ Sources/Internal/Render/Highlevel/RenderBatch.h
@@ -26,9 +26,8 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-
 #ifndef __DAVAENGINE_SCENE3D_RENDER_BATCH_H__
-#define	__DAVAENGINE_SCENE3D_RENDER_BATCH_H__
+#define __DAVAENGINE_SCENE3D_RENDER_BATCH_H__
 
 #include "Base/BaseTypes.h"
 #include "Base/BaseObject.h"
@@ -43,7 +42,6 @@
 #include "Scene3D/SceneFile/SerializationContext.h"
 namespace DAVA
 {
-
 /*
 class RenderCallInstance
 {
@@ -65,59 +63,58 @@ class NMaterial;
 class NMaterialInstance;
 class OcclusionQuery;
 
-    
 /*
     Not finished. We'll return to it when we start batching functionality.
  */
 class IRenderBatchDataSource
 {
 public:
-    virtual void InitBatch(RenderBatch * renderBatch) = 0;
-    virtual void FillData(RenderBatch * renderBatch) = 0;
-    virtual void ReleaseBatch(RenderBatch * renderBatch) = 0;
+    virtual void InitBatch(RenderBatch* renderBatch) = 0;
+    virtual void FillData(RenderBatch* renderBatch) = 0;
+    virtual void ReleaseBatch(RenderBatch* renderBatch) = 0;
 };
 
 class RenderBatch : public BaseObject
 {
 protected:
     virtual ~RenderBatch();
+
 public:
     RenderBatch();
-    
-    void SetPolygonGroup(PolygonGroup * _polygonGroup);
-    inline PolygonGroup * GetPolygonGroup();        
-    
-    void SetMaterial(NMaterial * _material);
-    inline NMaterial * GetMaterial();    
-    
-	void SetRenderObject(RenderObject * renderObject);
-	inline RenderObject * GetRenderObject() const;        
-    
-    inline void SetStartIndex(uint32 _startIndex);
-    inline void SetIndexCount(uint32 _indexCount);    
-    
-    const AABBox3 & GetBoundingBox() const;
 
-	virtual void GetDataNodes(Set<DataNode*> & dataNodes);
-	virtual RenderBatch * Clone(RenderBatch * destination = 0);
-	virtual void Save(KeyedArchive *archive, SerializationContext *serializationContext);
-	virtual void Load(KeyedArchive *archive, SerializationContext *serializationContext);
-    
+    void SetPolygonGroup(PolygonGroup* _polygonGroup);
+    inline PolygonGroup* GetPolygonGroup();
+
+    void SetMaterial(NMaterial* _material);
+    inline NMaterial* GetMaterial();
+
+    void SetRenderObject(RenderObject* renderObject);
+    inline RenderObject* GetRenderObject() const;
+
+    inline void SetStartIndex(uint32 _startIndex);
+    inline void SetIndexCount(uint32 _indexCount);
+
+    const AABBox3& GetBoundingBox() const;
+
+    virtual void GetDataNodes(Set<DataNode*>& dataNodes);
+    virtual RenderBatch* Clone(RenderBatch* destination = 0);
+    virtual void Save(KeyedArchive* archive, SerializationContext* serializationContext);
+    virtual void Load(KeyedArchive* archive, SerializationContext* serializationContext);
+
     /*
         \brief This is additional sorting key. It should be from 0 to 15.
-     */   
+     */
     void SetSortingKey(uint32 key);
     inline uint32 GetSortingKey() const;
 
     /*sorting offset allowed in 0..31 range, 15 default, more - closer to camera*/
     void SetSortingOffset(uint32 offset);
-    inline uint32 GetSortingOffset();    
-
+    inline uint32 GetSortingOffset();
 
     void BindGeometryData(rhi::Packet& packet);
-    
-	void UpdateAABBoxFromSource();
-	
+
+    void UpdateAABBoxFromSource();
+
     pointer_size layerSortingKey;
 
     rhi::HVertexBuffer vertexBuffer;
@@ -131,80 +128,74 @@ public:
     uint32 vertexLayoutId;
 
 private:
-    PolygonGroup * dataSource;
-    
-    NMaterial * material;                    // Should be replaced to NMaterial
-	RenderObject * renderObject;
-    Matrix4 *sortingTransformPtr;
-    
-    uint32 sortingKey; //oooookkkk -where o is offset, k is key    
+    PolygonGroup* dataSource;
+
+    NMaterial* material; // Should be replaced to NMaterial
+    RenderObject* renderObject;
+    Matrix4* sortingTransformPtr;
+
+    uint32 sortingKey; //oooookkkk -where o is offset, k is key
 
     const static uint32 SORTING_KEY_MASK = 0x0f;
     const static uint32 SORTING_OFFSET_MASK = 0x1f0;
     const static uint32 SORTING_OFFSET_SHIFT = 4;
     const static uint32 SORTING_KEY_DEF_VALUE = 0xf8;
-    
-	AABBox3 aabbox;
 
-	void InsertDataNode(DataNode *node, Set<DataNode*> & dataNodes);
-    
+    AABBox3 aabbox;
+
+    void InsertDataNode(DataNode* node, Set<DataNode*>& dataNodes);
 
 public:
-    
     INTROSPECTION_EXTEND(RenderBatch, BaseObject,
-        MEMBER(dataSource, "Data Source", I_SAVE | I_VIEW | I_EDIT)
+                         MEMBER(dataSource, "Data Source", I_SAVE | I_VIEW | I_EDIT)
 
-        MEMBER(startIndex, "Start Index", I_SAVE)
-        MEMBER(indexCount, "Index Count", I_SAVE)
-        //MEMBER(primitiveType, InspDesc("primitiveType", GlobalEnumMap<rhi::PrimitiveType>::Instance()), I_VIEW | I_EDIT | I_SAVE)
-                         
-        MEMBER(aabbox, "AABBox",  I_SAVE | I_VIEW | I_EDIT )
-        MEMBER(material, "Material", I_VIEW | I_EDIT)
-                         
-        PROPERTY("sortingKey", "Key for the sorting inside render layer", GetSortingKey, SetSortingKey, I_SAVE | I_VIEW | I_EDIT)
-    );
+                         MEMBER(startIndex, "Start Index", I_SAVE)
+                         MEMBER(indexCount, "Index Count", I_SAVE)
+                         //MEMBER(primitiveType, InspDesc("primitiveType", GlobalEnumMap<rhi::PrimitiveType>::Instance()), I_VIEW | I_EDIT | I_SAVE)
+
+                         MEMBER(aabbox, "AABBox", I_SAVE | I_VIEW | I_EDIT)
+                         MEMBER(material, "Material", I_VIEW | I_EDIT)
+
+                         PROPERTY("sortingKey", "Key for the sorting inside render layer", GetSortingKey, SetSortingKey, I_SAVE | I_VIEW | I_EDIT));
 };
-    
 
-inline PolygonGroup * RenderBatch::GetPolygonGroup()
+inline PolygonGroup* RenderBatch::GetPolygonGroup()
 {
     return dataSource;
 }
-    
 
-inline NMaterial * RenderBatch::GetMaterial()
+inline NMaterial* RenderBatch::GetMaterial()
 {
     return material;
 }
-    
-inline RenderObject * RenderBatch::GetRenderObject() const
+
+inline RenderObject* RenderBatch::GetRenderObject() const
 {
-	return renderObject;
+    return renderObject;
 }
 
 inline void RenderBatch::SetStartIndex(uint32 _startIndex)
 {
     startIndex = _startIndex;
 }
-    
+
 inline void RenderBatch::SetIndexCount(uint32 _indexCount)
 {
     indexCount = _indexCount;
 }
-    
+
 inline uint32 RenderBatch::GetSortingKey() const
 {
-    return sortingKey&SORTING_KEY_MASK;
+    return sortingKey & SORTING_KEY_MASK;
 }
 
 inline uint32 RenderBatch::GetSortingOffset()
 {
-    return ((sortingKey&SORTING_OFFSET_MASK)>>SORTING_OFFSET_SHIFT);
+    return ((sortingKey & SORTING_OFFSET_MASK) >> SORTING_OFFSET_SHIFT);
 }
 
-
 inline void RenderBatch::BindGeometryData(rhi::Packet& packet)
-{    
+{
     if (dataSource)
     {
         packet.vertexStreamCount = 1;
@@ -215,7 +206,7 @@ inline void RenderBatch::BindGeometryData(rhi::Packet& packet)
         packet.primitiveType = dataSource->primitiveType;
         packet.primitiveCount = GetPrimitiveCount(dataSource->indexCount, dataSource->primitiveType); //later move it into pg!
         packet.vertexLayoutUID = dataSource->vertexLayoutId;
-        packet.startIndex = 0;        
+        packet.startIndex = 0;
     }
     else
     {
@@ -227,11 +218,10 @@ inline void RenderBatch::BindGeometryData(rhi::Packet& packet)
         packet.primitiveType = primitiveType;
         packet.primitiveCount = GetPrimitiveCount(indexCount, primitiveType);
         packet.vertexLayoutUID = vertexLayoutId;
-        packet.startIndex = startIndex;        
+        packet.startIndex = startIndex;
     }
 }
-    
+
 } //
 
-#endif	/* __DAVAENGINE_SCENE3D_RENDER_BATCH_H__ */
-
+#endif /* __DAVAENGINE_SCENE3D_RENDER_BATCH_H__ */
