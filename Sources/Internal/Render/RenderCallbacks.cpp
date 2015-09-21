49cd7c9c93dc5a922861b4dc138c5f778ffefcac
Kernel <Kernel@tut.by>
2015-09-21 18:45:05 +0300

    DF-6367 post restore callbacks added
    Former-commit-id: b1180899825863dbbe4d5fbfb0177de1154213d1

--- Sources/Internal/Render/RenderCallbacks.cpp
+++ Sources/Internal/Render/RenderCallbacks.cpp
@@ -37,6 +37,7 @@ namespace
 {
     Mutex callbackListMutex;
     Vector<Function<void()>> resourceRestoreCallbacks;    
+    Vector<Function<void()>> postRestoreCallbacks;
 
     struct SyncCallback
     {
@@ -44,6 +45,8 @@ namespace
         Function < void(rhi::HSyncObject) > callback;
     };
     Vector<SyncCallback> syncCallbacks;
+
+    bool isInRestore = false;
 }
 
 namespace RenderCallbacks
@@ -70,19 +73,52 @@ void UnRegisterResourceRestoreCallback(Function<void()> callback)
     DVASSERT_MSG(false, "trying to unregister callback that was not perviously registered");
 }
 
+void RegisterPostRestoreCallback(Function<void()> callback)
+{
+    DVASSERT(callback.IsTrivialTarget());
+    LockGuard<Mutex> guard(callbackListMutex);
+    postRestoreCallbacks.push_back(callback);
+}
+void UnRegisterPostRestoreCallback(Function<void()> callback)
+{
+    DVASSERT(callback.IsTrivialTarget());
+    LockGuard<Mutex> guard(callbackListMutex);
+    for (size_t i = 0, sz = postRestoreCallbacks.size(); i < sz; ++i)
+    {
+        if (postRestoreCallbacks[i].Target() == callback.Target())
+        {
+            RemoveExchangingWithLast(postRestoreCallbacks, i);
+            return;
+        }
+    }
+    DVASSERT_MSG(false, "trying to unregister callback that was not perviously registered");
+}
+
 void ProcessFrame()
 {    
     if (rhi::NeedRestoreResources())
     {        
+        isInRestore = true;
         LockGuard<Mutex> guard(callbackListMutex);
         for (auto& callback : resourceRestoreCallbacks)
         {
             callback();
         }
         Logger::Debug("Resources still need restore: ");
-        rhi::NeedRestoreResources();
-        //DVASSERT_MSG(!rhi::NeedRestoreResources(), "some of resorces are still not restored yet marked as requireRestore");
+        rhi::NeedRestoreResources();        
     }    
+    else
+    {
+        if (isInRestore)
+        {
+            isInRestore = false;
+            LockGuard<Mutex> guard(callbackListMutex);
+            for (auto& callback : postRestoreCallbacks)
+            {
+                callback();
+            }
+        }
+    }
 
     for (size_t i = 0, sz = syncCallbacks.size(); i < sz;)
     {
