863f2abd5fd014d94c5ccd0c794740e23d95cdc4
Igor Solovey <i_solovey@wargaming.net>
2015-09-21 13:05:44 +0300

    DF-6367 revert mistake changes
    Former-commit-id: 95d8797b32d23ac75fa8e29b11efb1ad5ed69a4c

--- Projects/SceneViewer/Classes/GameCore.cpp
+++ Projects/SceneViewer/Classes/GameCore.cpp
@@ -80,8 +80,8 @@ void GameCore::OnAppStarted()
     viewSceneScreen = new ViewSceneScreen();
 
     //SetScenePath( "~doc:/GB/Cromwell-test.sc2" );
-//    SetScenePath("~doc:/effect.sc2");    
-    SetScenePath("~res:/3d/Maps/14_port_pt/14_port_pt.sc2");
+//    SetScenePath("~doc:/effect.sc2");
+    SetScenePath("~doc:/karelia/karelia.sc2");
 //    SetScenePath("~doc:/scene_viewer/test_box/box.sc2");
 //      SetScenePath("~doc:/amigosville/amigosville.sc2");
 //      SetScenePath("~doc:/fort/fort.sc2");
