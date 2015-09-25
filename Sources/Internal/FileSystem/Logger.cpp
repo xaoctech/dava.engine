0499110983ea93dda9ec3a83ccbfa20a41b430e8
Andrey Derkach <a_derkach2@wargaming.net>
2015-09-25 17:32:03 +0300

    [DF-7307] Method for getting log level from string added
    Former-commit-id: c0cbe7d8085eb5b519da3d7fe544a2e8b0365282

--- Sources/Internal/FileSystem/Logger.cpp
+++ Sources/Internal/FileSystem/Logger.cpp
@@ -150,6 +150,18 @@ const char8 * Logger::GetLogLevelString(eLogLevel ll) const
     return logLevelString[ll];
 }
 
+Optional<Logger::eLogLevel> Logger::GetLogLevelFromString(const char8* ll) const
+{
+    for (size_t i = 0; i < logLevelString.size(); ++i)
+    {
+        if (strcmp(ll, logLevelString[i]) == 0)
+        {
+            return static_cast<eLogLevel>(i);
+        }
+    }
+    return EmptyOptional();
+}
+
 void Logger::SetLogLevel(eLogLevel ll)
 {
     logLevel = ll;
