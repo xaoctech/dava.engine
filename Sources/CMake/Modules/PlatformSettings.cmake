4007e645b9cd498b16a2dda7267a60da50701527
Dzmitry-art <dartsukevichwork@gmail.com>
2015-09-25 18:27:50 +0300

    [DF-6367] - small fix
    Former-commit-id: 81ad141e104da262664ddb7d6c5779151b9573e1

--- Sources/CMake/Modules/PlatformSettings.cmake
+++ Sources/CMake/Modules/PlatformSettings.cmake
@@ -143,8 +143,8 @@ if( WARNINGS_AS_ERRORS )
         set( LOCAL_DISABLED_WARNINGS "${LOCAL_DISABLED_WARNINGS} \
 -Wno-reserved-id-macro \
 -Wno-unused-local-typedef \
--Wno-inconsistent-missing-override") \
--Wno-unknown-pragmas") \
+-Wno-inconsistent-missing-override \
+-Wno-unknown-pragmas")
         set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${LOCAL_DISABLED_WARNINGS}" ) # warnings as errors
     elseif( APPLE )
         set( LOCAL_DISABLED_WARNINGS "${LOCAL_DISABLED_WARNINGS} \
