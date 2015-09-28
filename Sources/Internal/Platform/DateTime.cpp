9f004c583049a4e11f3d5c972763474492c6529c
Leanid Chaika <leanid.chaika@gmail.com>
2015-09-28 14:29:18 +0300

    DF-7464 fix padding in clang on mac os
    Former-commit-id: 9a191f9fa2d123771f0ffc753b03f20c9c02a970

--- Sources/Internal/Platform/DateTime.cpp
+++ Sources/Internal/Platform/DateTime.cpp
@@ -49,11 +49,6 @@ static const char *months[] = { "jan","feb","mar","apr","may","jun","jul","aug",
 
 namespace DAVA
 {
-DateTime::DateTime()
-{
-    std::uninitialized_fill_n(&localTime.tm_sec, sizeof(tm) / sizeof(localTime.tm_sec), 0);
-}
-
 DateTime::DateTime(Timestamp timeStamp, int32 _timeZoneOffset):
 innerTime(timeStamp),
 timeZoneOffset(_timeZoneOffset)
