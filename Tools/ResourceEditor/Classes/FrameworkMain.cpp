ef509370022bca8cd7b5375cc9568f2d32954c13
Kernel <Kernel@tut.by>
2015-09-29 16:33:26 +0300

    DF-6367 pools increased fro resource editor
    Former-commit-id: 53f4372bdf69eda0de360ff9c0cfde591cf95fc9

--- Tools/ResourceEditor/Classes/FrameworkMain.cpp
+++ Tools/ResourceEditor/Classes/FrameworkMain.cpp
@@ -42,8 +42,13 @@ void FrameworkDidLaunched()
     appOptions->SetInt32("renderer", rhi::RHI_GLES2);
     appOptions->SetInt32("width", 1024);
     appOptions->SetInt32("height", 768);
-	
-	GameCore * core = new GameCore();
+
+    appOptions->SetInt32("max_index_buffer_count", 8192);
+    appOptions->SetInt32("max_vertex_buffer_count", 8192);
+    appOptions->SetInt32("max_const_buffer_count", 32767);
+    appOptions->SetInt32("max_texture_count", 2048);
+
+    GameCore * core = new GameCore();
 	DAVA::Core::SetApplicationCore(core);
 	DAVA::Core::Instance()->SetOptions(appOptions);
     DAVA::VirtualCoordinatesSystem::Instance()->EnableReloadResourceOnResize(false);
