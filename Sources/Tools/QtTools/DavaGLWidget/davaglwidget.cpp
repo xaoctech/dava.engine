f29005ddfdc4d2872fed81eb4d29a920ba88e4ef
klesch <victor@davaconsulting.com>
2015-10-13 16:55:57 +0300

    [DF-8225] Changed includes
    Former-commit-id: de58e90bd8128f78ddbb80d59b78ab5b0219d3c3

--- Sources/Tools/QtTools/DavaGLWidget/davaglwidget.cpp
+++ Sources/Tools/QtTools/DavaGLWidget/davaglwidget.cpp
@@ -27,8 +27,6 @@
 =====================================================================================*/
 
 
-#include "DAVAEngine.h"
-
 #include "Platform/Qt5/QtLayer.h"
 
 #include "Davaglwidget.h"
