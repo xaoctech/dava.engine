a12a852f8a564014aa2699559783d4d5f0ea2894
Andrey Derkach <a_derkach2@wargaming.net>
2015-10-13 15:09:44 +0300

    [DF-8233] Support of multiplatform for CMake
    Former-commit-id: 6df478b4d37e4d275e30166233d1974030477c31

--- Sources/CMake/Toolchains/win_uap.toolchain.cmake
+++ Sources/CMake/Toolchains/win_uap.toolchain.cmake
@@ -7,3 +7,4 @@ include ( GlobalVariables )
 #define system name and version for windows universal application
 set ( CMAKE_SYSTEM_NAME "${WINDOWS_UAP_PLATFORM_NAME}" )
 set ( CMAKE_SYSTEM_VERSION "${WINDOWS_UAP_PLATRORM_VERSION}" )
+set ( CMAKE_VS_EFFECTIVE_PLATFORMS "Win32;ARM;x64" )
--- Tools/WinStoreCMake/update_cmake.bat
+++ Tools/WinStoreCMake/update_cmake.bat
@@ -2,13 +2,13 @@
 set WORK_DIR_REPO=cmake_repo
 if exist %WORK_DIR_REPO% (
 cd %WORK_DIR_REPO%
-git pull https://github.com/Microsoft/cmake feature/Win10
+git pull https://github.com/Umkerius/cmake feature/Win10Multiplatform
 git status
 cd ..
 ) else (
-git clone https://github.com/Microsoft/cmake %WORK_DIR_REPO%
+git clone https://github.com/Umkerius/cmake %WORK_DIR_REPO%
 cd %WORK_DIR_REPO%
-git checkout feature/Win10
+git checkout feature/Win10Multiplatform
 git status
 cd ..
 )
