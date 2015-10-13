8da6e23671d7208dfcc3949f1c0c3bd23baf3680
Andrey Derkach <a_derkach2@wargaming.net>
2015-10-13 20:06:38 +0300

    [DF-8233] Branch name fix
    Former-commit-id: a3d00ff06a7ab5e918e0c14a6098710bc47745c0

--- Tools/WinStoreCMake/update_cmake.bat
+++ Tools/WinStoreCMake/update_cmake.bat
@@ -2,14 +2,13 @@
 set WORK_DIR_REPO=cmake_repo
 if exist %WORK_DIR_REPO% (
 cd %WORK_DIR_REPO%
-git pull https://github.com/Umkerius/cmake feature/Win10Multiplatform
+git pull https://github.com/Umkerius/cmake feature/Win10MultiPlatform
 git status
 cd ..
 ) else (
 git clone https://github.com/Umkerius/cmake %WORK_DIR_REPO%
 cd %WORK_DIR_REPO%
-git checkout feature/Win10Multiplatform
+git checkout feature/Win10MultiPlatform
 git status
 cd ..
 )
-
