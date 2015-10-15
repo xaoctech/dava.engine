39541fc7a041919da0809c4b4ab31fb1fbc7a842
Mikhail Polubisok <mpolubisok@gmail.com>
2015-10-15 13:46:20 +0300

    DF-7593 Destroy previous screen on screenshot comlete event
    Former-commit-id: 839c017aae76c36bc077dfdf8a768af5f0116df3

--- Sources/Internal/UI/UIScreenTransition.cpp
+++ Sources/Internal/UI/UIScreenTransition.cpp
@@ -89,11 +89,7 @@ void UIScreenTransition::StartTransition(UIScreen * _prevScreen, UIScreen * _nex
     nextScreen = _nextScreen;
     prevScreen = _prevScreen;
 
-    UIControlSystem::Instance()->GetScreenshoter()->MakeScreenshot(prevScreen, renderTargetPrevScreen->GetTexture());
-
-    // If prevScreen is visible we should notify it that it will comes to invisible state
-    if (prevScreen->IsOnScreen())
-        prevScreen->SystemWillBecomeInvisible();
+    UIControlSystem::Instance()->GetScreenshoter()->MakeScreenshot(prevScreen, renderTargetPrevScreen->GetTexture(), MakeFunction(this, &UIScreenTransition::OnPrevScreenScreenshotComplete));
 
     nextScreen->LoadGroup();
     nextScreen->SystemWillAppear();
@@ -112,22 +108,14 @@ void UIScreenTransition::Update(float32 timeElapsed)
     {
         currentTime = duration;
         
-        if (prevScreen)
-        {
-            if (prevScreen->IsOnScreen())
-                prevScreen->SystemWillBecomeInvisible();
-            prevScreen->SystemWillDisappear();
-            if (prevScreen->GetGroupId() != nextScreen->GetGroupId())
-                prevScreen->UnloadGroup();
-            prevScreen->SystemDidDisappear();
-            SafeRelease(prevScreen);
-        }
-        
         UIControlSystem::Instance()->ReplaceScreen(nextScreen);
+        
         nextScreen->SystemDidAppear();
         if (nextScreen->IsOnScreen())
             nextScreen->SystemWillBecomeVisible();
+        
         ReleaseRenderTargets();
+        
         // go to next screen
         UIControlSystem::Instance()->UnlockInput();
         UIControlSystem::Instance()->UnlockSwitch();
@@ -189,6 +177,20 @@ UI3DView* UIScreenTransition::FindFirst3dView(UIControl* control)
         }
     }
     return nullptr;
+}
+    
+void UIScreenTransition::OnPrevScreenScreenshotComplete(Texture* texture)
+{
+    if (prevScreen)
+    {
+        if (prevScreen->IsOnScreen())
+            prevScreen->SystemWillBecomeInvisible();
+        prevScreen->SystemWillDisappear();
+        if (nextScreen == nullptr || prevScreen->GetGroupId() != nextScreen->GetGroupId())
+            prevScreen->UnloadGroup();
+        prevScreen->SystemDidDisappear();
+        SafeRelease(prevScreen);
+    }
 }
 
 };
