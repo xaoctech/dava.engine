47f46a0bbad145cae77cea77d5f898e7abb5e677
Andrey Derkach <a_derkach2@wargaming.net>
2015-10-17 15:07:03 +0300

    [DF-8233] Switch on MS cmake repo
    Former-commit-id: ccf6defb831a4cee104968f3616f27c25ce17b57

--- Tools/WinStoreCMake/update_cmake.bat
+++ Tools/WinStoreCMake/update_cmake.bat
@@ -2,11 +2,11 @@
 set WORK_DIR_REPO=cmake_repo
 if exist %WORK_DIR_REPO% (
 cd %WORK_DIR_REPO%
-git pull https://github.com/Umkerius/cmake feature/Win10MultiPlatform
+git pull https://github.com/Microsoft/cmake feature/Win10MultiPlatform
 git status
 cd ..
 ) else (
-git clone https://github.com/Umkerius/cmake %WORK_DIR_REPO%
+git clone https://github.com/Microsoft/cmake %WORK_DIR_REPO%
 cd %WORK_DIR_REPO%
 git checkout feature/Win10MultiPlatform
 git status
