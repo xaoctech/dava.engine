a8e5bfe6132c3c934a29f77a29bf77c382ff18ac
y_rakhuba <y_rakhuba@wargaming.net>
2015-10-22 15:07:14 +0300

    [DF-8331] setup RE as dynamic lib and run it inside NGT enviroment.
    Former-commit-id: 44eee451f0ab3d3ea81a3263815492abe9b15896

--- Sources/CMake/Modules/NGTMacro.cmake
+++ Sources/CMake/Modules/NGTMacro.cmake
@@ -33,3 +33,21 @@ macro (append_ngt_plugin LIB_NAME)
     SET_PROPERTY( TARGET ${LIB_NAME} PROPERTY FOLDER "NGT Plugins")
     append_deploy_dependency(${LIB_NAME})
 endmacro()
+
+# FEATURE_LEVEL can be equal: "INTERFACE", "CORE", "UI"
+
+function (get_ngt_modules LIB_LIST_VAR PLG_LIST_VAR QTLIBS_LIST_VAR)
+    list(APPEND NGT_LIBS  wg_types core_command_system core_common core_data_model
+                                               core_generic_plugin core_dependency_system core_generic_plugin_manager
+                                               core_logging core_logging_system core_qt_common core_qt_script core_reflection
+                                               core_reflection_utils core_serialization core_string_utils core_ui_framework core_variant)
+
+    list(APPEND NGT_PLUGINS  plg_alert_ui plg_reflection plg_variant plg_command_system plg_copy_paste
+                                                       plg_serialization plg_file_system plg_qt_common plg_editor_interaction plg_history_ui
+                                                       plg_macros_ui plg_panel_manager plg_qt_app)
+    list(APPEND QT_LIBS Core Gui Widgets Concurrent Qml Network Quick QuickWidgets UiTools Xml)
+
+    set (${LIB_LIST_VAR} ${NGT_LIBS} PARENT_SCOPE)
+    set (${PLG_LIST_VAR} ${NGT_PLUGINS} PARENT_SCOPE)
+    set (${QTLIBS_LIST_VAR} ${QT_LIBS} PARENT_SCOPE)
+endfunction()
