63f1a81806c5d30f13df62bf8d82a05257098977
klesch <victor.kleschenko@gmail.com>
2015-11-12 15:19:30 +0300

    [DF-185] Added Reload Shader button
    Former-commit-id: 5314d94c66edf9e0cc30e8c386cf829a5951baef

--- Projects/SceneViewer/Classes/ViewSceneScreen.cpp
+++ Projects/SceneViewer/Classes/ViewSceneScreen.cpp
@@ -36,19 +36,29 @@ using namespace DAVA;
 
 ViewSceneScreen::ViewSceneScreen()
     : BaseScreen()
-    , info(NULL)
-    , frameCounter(0)
-    , framesTime(0.f)
-    , drawTime(0)
-    , updateTime(0)
-    , cursorSize(.1f)
 {
+}
+
+ViewSceneScreen::~ViewSceneScreen()
+{
+    if (scene)
+    {
+        scene->RemoveSystem(wasdSystem);
+        scene->RemoveSystem(rotationControllerSystem);
+    }
+    SafeDelete(wasdSystem);
+    SafeDelete(rotationControllerSystem);
+
+    SafeRelease(scene);
+    SafeRelease(info);
+    SafeRelease(moveJoyPAD);
 }
 
 void ViewSceneScreen::LoadResources()
 {
     BaseScreen::LoadResources();
 
+    DVASSERT(scene == nullptr);
     scene = new Scene();
     scene->LoadScene(GameCore::Instance()->GetScenePath());
 
@@ -110,12 +120,11 @@ void ViewSceneScreen::LoadResources()
     scene->AddCamera(camera);
     scene->SetCurrentCamera(camera);
 
-    Entity* cameraEntity = new Entity();
+    ScopedPtr<Entity> cameraEntity(new Entity());
     cameraEntity->AddComponent(new CameraComponent(camera));
     cameraEntity->AddComponent(new WASDControllerComponent());
     cameraEntity->AddComponent(new RotationControllerComponent());
     scene->AddNode(cameraEntity);
-    cameraEntity->Release();
 
     rotationControllerSystem = new RotationControllerSystem(scene);
     scene->AddSystem(rotationControllerSystem, MAKE_COMPONENT_MASK(Component::CAMERA_COMPONENT) | MAKE_COMPONENT_MASK(Component::ROTATION_CONTROLLER_COMPONENT),
@@ -139,18 +148,22 @@ void ViewSceneScreen::LoadResources()
     backButton->SetDebugDraw(true);
     AddControl(backButton);
 
-    DVASSERT(info == NULL);
+    ScopedPtr<UIButton> reloadShadersButton(CreateButton(Rect(100, 0, 190, 30), L"Reload Shaders"));
+    reloadShadersButton->AddEvent(UIControl::EVENT_TOUCH_UP_INSIDE, Message(this, &ViewSceneScreen::OnReloadShaders));
+    reloadShadersButton->SetDebugDraw(true);
+    AddControl(reloadShadersButton);
+
+    DVASSERT(info == nullptr);
     info = new UIStaticText(Rect(0, 0, 35, 30.f));
     info->SetFont(font);
     info->SetTextColor(Color::White);
     info->SetTextAlign(ALIGN_VCENTER | ALIGN_RIGHT);
-    
     AddControl(info);
 
+    DVASSERT(moveJoyPAD == nullptr);
     moveJoyPAD = new UIJoypad(Rect(0, screenRect.dy - 200.f, 200.f, 200.f));
     moveJoyPAD->SetDebugDraw(true);
     AddControl(moveJoyPAD);
-    moveJoyPAD->Release();
 }
 
 void ViewSceneScreen::UnloadResources()
@@ -165,13 +178,55 @@ void ViewSceneScreen::UnloadResources()
 
     SafeRelease(scene);
     SafeRelease(info);
-    
+    SafeRelease(moveJoyPAD);
+
     BaseScreen::UnloadResources();
 }
 
 void ViewSceneScreen::OnBack(BaseObject *caller, void *param, void *callerData)
 {
     SetPreviousScreen();
+}
+
+void ViewSceneScreen::OnReloadShaders(DAVA::BaseObject* caller, void* param, void* callerData)
+{
+    ShaderDescriptorCache::RelaoadShaders();
+
+    List<NMaterial*> materials;
+    scene->GetDataNodes(materials);
+    for (auto material : materials)
+    {
+        material->InvalidateRenderVariants();
+    }
+
+    const Map<uint64, NMaterial*>& particleInstances = scene->particleEffectSystem->GetMaterialInstances();
+    for (auto material : particleInstances)
+    {
+        material.second->InvalidateRenderVariants();
+    }
+
+    DAVA::Set<DAVA::NMaterial*> materialList;
+    scene->foliageSystem->CollectFoliageMaterials(materialList);
+    for (auto material : materialList)
+    {
+        if (material)
+            material->InvalidateRenderVariants();
+    }
+
+    scene->renderSystem->GetDebugDrawer()->InvalidateMaterials();
+    scene->renderSystem->SetForceUpdateLights();
+    
+#define INVALIDATE_2D_MATERIAL(material) \
+if (RenderSystem2D::material)        \
+RenderSystem2D::material->InvalidateRenderVariants();
+
+    INVALIDATE_2D_MATERIAL(DEFAULT_2D_COLOR_MATERIAL)
+    INVALIDATE_2D_MATERIAL(DEFAULT_2D_TEXTURE_MATERIAL)
+    INVALIDATE_2D_MATERIAL(DEFAULT_2D_TEXTURE_NOBLEND_MATERIAL)
+    INVALIDATE_2D_MATERIAL(DEFAULT_2D_TEXTURE_ALPHA8_MATERIAL)
+    INVALIDATE_2D_MATERIAL(DEFAULT_2D_TEXTURE_GRAYSCALE_MATERIAL)
+    
+#undef INVALIDATE_2D_MATERIAL
 }
 
 void ViewSceneScreen::Draw(const DAVA::UIGeometricData &geometricData)
