2b1e8c073edb632cc9d62817e0f0c0a92fb4aed8
Mikhail Polubisok <m_polubisok@wargaming.net>
2015-11-14 18:33:10 +0300

    DF-8564 Change sync hiding native text field to async
    Former-commit-id: 34d08fba7552503394db6fb4bee94b3dfa01fb70

--- Sources/Internal/Platform/TemplateWin32/PrivateTextFieldWinUAP.cpp
+++ Sources/Internal/Platform/TemplateWin32/PrivateTextFieldWinUAP.cpp
@@ -269,7 +269,7 @@ void PrivateTextFieldWinUAP::SetVisible(bool isVisible)
         properties.anyPropertyChanged = true;
         if (!isVisible)
         { // Immediately hide native control if it has been already created
-            core->RunOnUIThreadBlocked([this]() {
+            core->RunOnUIThread([this]() {
                 if (nativeControl != nullptr)
                 {
                     SetNativeVisible(false);
