95c5ea39552d7faff2beb45dc0d3739a563a9809
leanid chaika <leanid.chaika@gmail.com>
2015-11-17 14:07:17 +0300

    DF-8526 fix gamepad switch
    Former-commit-id: 18110d5a56fa61e012dce8e77228ff84a304ddee

--- Sources/Internal/Platform/TemplateAndroid/CorePlatformAndroid.cpp
+++ Sources/Internal/Platform/TemplateAndroid/CorePlatformAndroid.cpp
@@ -303,20 +303,27 @@ void CorePlatformAndroid::OnGamepadElement(int32 elementKey, float32 value, bool
 {
     GamepadDevice& gamepadDevice = InputSystem::Instance()->GetGamepadDevice();
 
-    int32 davaKey = GamepadDevice::INVALID_DAVAKEY;
+    uint32 davaKey = GamepadDevice::INVALID_DAVAKEY;
     if (isKeycode)
+    {
         davaKey = gamepadDevice.GetDavaEventIdForSystemKeycode(elementKey);
+    }
     else
+    {
         davaKey = gamepadDevice.GetDavaEventIdForSystemAxis(elementKey);
+    }
 
     if (davaKey == GamepadDevice::INVALID_DAVAKEY)
+    {
         return;
+    }
 
     UIEvent newEvent;
     newEvent.tid = davaKey;
     newEvent.physPoint.x = value;
     newEvent.point.x = value;
     newEvent.phase = DAVA::UIEvent::Phase::JOYSTICK;
+    newEvent.device = DAVA::UIEvent::Device::GAMEPAD;
 
     gamepadDevice.SystemProcessElement(static_cast<GamepadDevice::eDavaGamepadElement>(davaKey), value);
     InputSystem::Instance()->ProcessInputEvent(&newEvent);
