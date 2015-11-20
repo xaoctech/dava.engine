a8a216acbfc2c51d6cda9710f4d9885db0f170ff
Maksim Lazarev <m_lazarev@wargaming.net>
2015-11-20 18:09:57 +0300

    DF-8614 set TestBed minimum window size
    Former-commit-id: 28442805663da8e9a3312361b0326ea7ba3bbc54

--- Projects/TestBed/Classes/Infrastructure/FrameworkMain.cpp
+++ Projects/TestBed/Classes/Infrastructure/FrameworkMain.cpp
@@ -76,8 +76,8 @@ void FrameworkDidLaunched()
     appOptions->SetInt32("bpp", 32);
 
     // For testing purpose limit minimum window size to 400x300
-    appOptions->SetInt32("min-width", 400);
-    appOptions->SetInt32("min-height", 300);
+    appOptions->SetInt32("min-width", 960);
+    appOptions->SetInt32("min-height", 640);
 
     DAVA::VirtualCoordinatesSystem::Instance()->SetProportionsIsFixed(false);
 #else
