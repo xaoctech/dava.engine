e15e71b55112498e0bceec9358f943406ab9b6c6
Andrey Derkach <a_derkach2@wargaming.net>
2015-12-01 21:59:22 +0300

    Revert of renaming UIScreenManagerAndroid -> UIScreenManagerDefault
    Former-commit-id: 4646a2890172a15a6d6e0f3f05457584564e0a5d

--- Sources/Internal/UI/UIScreenManagerAndroid.h
+++ Sources/Internal/UI/UIScreenManagerAndroid.h
@@ -27,19 +27,54 @@
 =====================================================================================*/
 
 
-#ifndef __DAVAENGINE_SCREENMANAGER_H__
-#define __DAVAENGINE_SCREENMANAGER_H__
+#ifndef __DAVAENGINE_DEFAULT_SCREENMANAGER_C_H__
+#define __DAVAENGINE_DEFAULT_SCREENMANAGER_C_H__
 
-#include "Base/BaseTypes.h"
+#include "DAVAEngine.h"
 
-#if defined(__DAVAENGINE_MACOS__) || defined(__DAVAENGINE_WINDOWS__)
-    #include "UI/UIScreenManagerDefault.h"
-#elif defined(__DAVAENGINE_IPHONE__)
-    #include "UI/UIScreenManageriPhone.h"
-#elif defined(__DAVAENGINE_ANDROID__)
-    #include "UI/UIScreenManagerAndroid.h"
-#else //PLATFORMS
-	//other platforms
-#endif //PLATFORMS
+namespace DAVA
+{
+class UIScreenManager : public Singleton<UIScreenManager>
+{
+public:
+    UIScreenManager();
+    virtual ~UIScreenManager();
 
-#endif // __DAVAENGINE_SCREENMANAGER_C_H__
+    void RegisterScreen(int screenId, UIScreen* screen);
+
+    void SetFirst(int screenId);
+    void SetScreen(int screenId, UIScreenTransition* transition = 0);
+
+    UIScreen* GetScreen(int screenId);
+    UIScreen* GetScreen();
+    int32 GetScreenId();
+
+    void ScreenSizeChanged();
+
+private:
+    void ActivateGLController();
+
+    struct Screen
+    {
+        enum eType
+        {
+            TYPE_NULL = 0,
+            TYPE_SCREEN,
+        };
+        Screen(eType _type = TYPE_NULL, void* _value = 0)
+        {
+            type = _type;
+            value = _value;
+        }
+        eType type;
+        void* value;
+    };
+
+    Map<int, Screen> screens;
+    int glControllerId;
+    int activeControllerId;
+    int activeScreenId;
+};
+};
+
+#endif // __DAVAENGINE_ANDROID_SCREENMANAGER_C_H__
