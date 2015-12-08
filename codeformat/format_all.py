d9b0fe51e2ebc4ab92555f7a78b523e8c6fc7533
Ivan Petrochenko <i_petrochenko@wargaming.net>
2015-12-08 12:55:09 +0300

    DF-185 codeformat scripts win32 fix
    Former-commit-id: 922568f8d6a63193e195de0dcf1cb092f9e0890a

--- codeformat/format_all.py
+++ codeformat/format_all.py
@@ -6,13 +6,17 @@ import subprocess
 import errno
 
 cwd = os.getcwd()
+if os.name == 'nt':
+	execName = 'clang-format.exe'
+else:
+	execName = 'clang-format'
 sources = ['../Sources/Internal', '../Projects', '../Tools']
 for source in sources:
 	for root, dirnames, filenames in os.walk(source):
 		for ext in ['cpp', 'h', 'c', 'mm']:
 			for filename in fnmatch.filter(filenames, '*.'+ext):
 				try:
-					proc = subprocess.Popen([cwd+'/clang-format', '-i', '--style=file', os.path.join(root, filename)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					proc = subprocess.Popen([cwd+'/'+execName, '-i', '--style=file', os.path.join(root, filename)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				except OSError as e:
 					if e.errno == errno.ENOENT:
 						exit('cannot find executable clang-format')
--- codeformat/teamcity_check_format.py
+++ codeformat/teamcity_check_format.py
@@ -7,13 +7,17 @@ import errno
 
 formatOK = True
 cwd = os.getcwd()
+if os.name == 'nt':
+	execName = 'clang-format.exe'
+else:
+	execName = 'clang-format'
 sources = ['../Sources/Internal', '../Projects', '../Tools']
 for source in sources:
 	for root, dirnames, filenames in os.walk(source):
 		for ext in ['cpp', 'h', 'c', 'mm']:
 			for filename in fnmatch.filter(filenames, '*.'+ext):
 				try:
-					proc = subprocess.Popen([cwd+'/clang-format', '-output-replacements-xml', '--style=file', os.path.join(root, filename)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					proc = subprocess.Popen([cwd+'/'+execName, '-output-replacements-xml', '--style=file', os.path.join(root, filename)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				except OSError as e:
 					if e.errno == errno.ENOENT:
 						exit('cannot find executable clang-format')
@@ -21,7 +25,7 @@ for source in sources:
 						raise
 				stdout, stderr = proc.communicate()
 				if stdout.find("<replacement ") > -1:
-					formatOK = false
+					formatOK = False
 					errorMsg = "##teamcity[message text=\'" + "%s not formatted" % os.path.join(root, filename) + "\' errorDetails=\'\' status=\'" + "ERROR" + "\']\n"
 					print errorMsg
 
