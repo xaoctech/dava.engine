9690fbc5955b68d6ea4751a894276b3efc29a60e
Andrey Derkach <a_derkach2@wargaming.net>
2015-12-11 13:48:21 +0300

    [DF-185] Prebuilt cmake added
    Former-commit-id: ce86f44c3a7497b3b5b8367ceb0088ce67b076ae

--- Tools/WinStoreCMake/make_prebuilt.bat
+++ Tools/WinStoreCMake/make_prebuilt.bat
@@ -6,19 +6,19 @@ set REPO_DIR=%SCRIPT_DIR%cmake_repo
 
 ::pre-build cleanup
 rmdir /s /q %PREBUILT_DIR%
-::rmdir /s /q %BUILD_DIR%
-::rmdir /s /q %REPO_DIR%
+rmdir /s /q %BUILD_DIR%
+rmdir /s /q %REPO_DIR%
 
 ::build cmake
-::call %SCRIPT_DIR%rebuild_cmake.bat
+call %SCRIPT_DIR%rebuild_cmake.bat
 
 ::make prebuilt
 mkdir %PREBUILT_DIR%
 
-xcopy /Q /I /E %BUILD_DIR%\bin %PREBUILT_DIR%\bin
-xcopy /Q /I /E %REPO_DIR%\Modules %PREBUILT_DIR%\bin\share\cmake-3.3\Modules
-xcopy /Q /I /E %REPO_DIR%\Templates %PREBUILT_DIR%\bin\share\cmake-3.3\Templates
+xcopy /Q /I /E %BUILD_DIR%\bin\Release %PREBUILT_DIR%\bin
+xcopy /Q /I /E %REPO_DIR%\Modules %PREBUILT_DIR%\share\cmake-3.3\Modules
+xcopy /Q /I /E %REPO_DIR%\Templates %PREBUILT_DIR%\share\cmake-3.3\Templates
 
 ::post-build cleanup
-::rmdir /s /q %BUILD_DIR%
-::rmdir /s /q %REPO_DIR%
+rmdir /s /q %BUILD_DIR%
+rmdir /s /q %REPO_DIR%
