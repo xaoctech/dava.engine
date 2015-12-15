f0e8a127bb720bc2354b66f3868c9eb7bc335ebd
Andrey Derkach <a_derkach2@wargaming.net>
2015-12-15 13:06:38 +0300

    [DF-185] Prebuilt cmake moved to zip
    Former-commit-id: 21dc83acccd33a0ab92163bc5dc43f00ba331138

--- Tools/WinStoreCMake/make_prebuilt.bat
+++ Tools/WinStoreCMake/make_prebuilt.bat
@@ -3,6 +3,7 @@ set SCRIPT_DIR=%~dp0
 set PREBUILT_DIR=%SCRIPT_DIR%cmake_prebuilt
 set BUILD_DIR=%SCRIPT_DIR%cmake_build
 set REPO_DIR=%SCRIPT_DIR%cmake_repo
+set CMAKE_SCRIPTS=%SCRIPT_DIR%\..\..\Sources\CMake\Scripts
 
 ::pre-build cleanup
 rmdir /s /q %PREBUILT_DIR%
@@ -19,6 +20,11 @@ xcopy /Q /I /E %BUILD_DIR%\bin\Release %PREBUILT_DIR%\bin
 xcopy /Q /I /E %REPO_DIR%\Modules %PREBUILT_DIR%\share\cmake-3.3\Modules
 xcopy /Q /I /E %REPO_DIR%\Templates %PREBUILT_DIR%\share\cmake-3.3\Templates
 
+::pack binaries
+del /Q /F %SCRIPT_DIR%\cmake_prebuilt.zip
+call python.exe %CMAKE_SCRIPTS%\zip.py %PREBUILT_DIR% %SCRIPT_DIR%\cmake_prebuilt.zip
+
 ::post-build cleanup
 rmdir /s /q %BUILD_DIR%
 rmdir /s /q %REPO_DIR%
+rmdir /s /q %PREBUILT_DIR%
