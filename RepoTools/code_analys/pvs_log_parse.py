ce05918838d6c394d57cfdb9b92bd44202707838
Aleksei Kanash <aleksei.kanash@gmail.com>
2015-12-29 15:33:46 +0300

    [DF-7487] Code style improvements, could return success.
    Former-commit-id: 1150b88708ac40dbddf3b4a7d4309f5c10dbdc9c

--- RepoTools/code_analys/pvs_log_parse.py
+++ RepoTools/code_analys/pvs_log_parse.py
@@ -4,6 +4,7 @@ from optparse import OptionParser
 from sets import Set
 import xml.etree.ElementTree
 
+
 class TeamCityOutput:
     @staticmethod
     def log_test_start(message):
@@ -15,19 +16,20 @@ class TeamCityOutput:
 
     @staticmethod
     def log(message):
-        output = '##teamcity[testFailed name=\'\' message=\'' + message + '\' details=\'\']'
-        print output
+        print '##teamcity[testFailed name=\'\' message=\'' + message + '\' details=\'\']'
 
 
 class LogEntry:
     """Log Message Entry Representer"""
-    error_level = 0
-    line_no = 0
-    error_code = ''
-    file_path = ''
-    message = ''
 
     error_status_list = ['HIGH', 'MEDIUM', 'LOW']
+
+    def __init__(self):
+        self.error_level = 0
+        self.line_no = 0
+        self.error_code = ''
+        self.file_path = ''
+        self.message = ''
 
     def get_status(self):
         return LogEntry.error_status_list[self.error_level - 1]
@@ -51,6 +53,7 @@ class LogEntry:
 
 
 g_folders_to_report = ['sources\\internal', 'tools', 'projects']
+
 
 def get_entries(log_file):
     global g_folders_to_report
@@ -95,14 +98,7 @@ def get_error_level_count(level, log_entries):
     return count
 
 
-def main():
-    options = OptionParser()
-    options.add_option("--plog", dest="log_xml_file", default="Log/Log.plog", help="PVS studio report file.")
-
-    (options, args) = options.parse_args(sys.argv)
-
-    log_entries = get_entries(options.log_xml_file)
-
+def report_status_to_teamcity(log_entries):
     counters = [0, 0, 0]
     for error_level in range(len(LogEntry.error_status_list)):
         error_level_entries_count = get_error_level_count(error_level, log_entries)
@@ -115,22 +111,46 @@ def main():
     TeamCityOutput.log('PVS Check Failed. ' + str(len(log_entries)) + ' Errors found:')
     TeamCityOutput.log(description_string)
 
-    log_entries.sort(LogEntry.less_than_by_file)
 
-    current_file = ''
-    for entry in log_entries:
-        if current_file != entry.file_path:
-            if current_file != '':
-                TeamCityOutput.log_test_end(current_file)
-            current_file = entry.file_path
-            TeamCityOutput.log_test_start('In file ' + current_file)
+def report_errors_to_teamcity(log_entries):
+        current_file = ''
 
-        entry.print_self()
+        for entry in log_entries:
+            if current_file != entry.file_path:
+                if current_file != '':
+                    TeamCityOutput.log_test_end('')
 
-    TeamCityOutput.log_test_end(current_file)
+                current_file = entry.file_path
+                # log filename with one or couple of errors
+                TeamCityOutput.log_test_start('In file ' + current_file)
+
+            entry.print_self()
+
+        TeamCityOutput.log_test_end('')
+
+
+def main():
+    options = OptionParser()
+    options.add_option("--plog", dest="log_xml_file", default="Log/Log.plog", help="PVS studio report file.")
+
+    (options, args) = options.parse_args(sys.argv)
+
+    log_entries = get_entries(options.log_xml_file)
 
     if len(log_entries) > 0:
+        # report error and some details
+        report_status_to_teamcity(log_entries)
+
+        # sort entries by filename to group them
+        log_entries.sort(LogEntry.less_than_by_file)
+
+        # report logs grouped by filename
+        report_errors_to_teamcity(log_entries)
+
         sys.exit(1)
+    else:
+        sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
