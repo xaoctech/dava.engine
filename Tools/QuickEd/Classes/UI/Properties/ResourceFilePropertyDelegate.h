0ac51f9bc3c478d4b2ed9f882e696de65770a2f7
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-01-15 17:21:37 +0300

    [DF-7484] added validation for files and resources
    Former-commit-id: e2f3b84e3677303b0b06158e942242ee412a4581

--- Tools/QuickEd/Classes/UI/Properties/ResourceFilePropertyDelegate.h
+++ Tools/QuickEd/Classes/UI/Properties/ResourceFilePropertyDelegate.h
@@ -36,7 +36,7 @@ class ResourceFilePropertyDelegate : public BasePropertyDelegate
 {
     Q_OBJECT
 public:
-    explicit ResourceFilePropertyDelegate(const QString &resourcefilter, const QString &resourceDir, PropertiesTreeItemDelegate *delegate);
+    explicit ResourceFilePropertyDelegate(const QString& extension, const QString& resourceDir, PropertiesTreeItemDelegate* delegate);
     ~ResourceFilePropertyDelegate();
 
     QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) override;
@@ -47,10 +47,13 @@ public:
 private slots:
     void selectFileClicked();
     void clearFileClicked();
-    void valueChanged();
+    void OnEditingFinished();
+    void OnTextChanged(const QString& text);
 
 private:
-    QString resourcefilter;
+    bool IsPathValid(const QString& path);
+    QPointer<QLineEdit> lineEdit = nullptr;
+    QString resourceExtension;
     QString resourceDir;
 };
 
