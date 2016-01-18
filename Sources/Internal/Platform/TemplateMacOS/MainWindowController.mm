2b8ac8f40edc9cd7f59649883cd77906525e237f
kkdaemon <kkdaemon@gmail.com>
2016-01-18 17:34:55 +0300

    DF-9171 macos: smallest allowed minimum size is 64x64
    Former-commit-id: eee8c75ac48b0900503d31b7abb25c06467f0fcd

--- Sources/Internal/Platform/TemplateMacOS/MainWindowController.mm
+++ Sources/Internal/Platform/TemplateMacOS/MainWindowController.mm
@@ -182,13 +182,15 @@ namespace DAVA
     [mainWindow setDelegate:self];
     [mainWindow setContentView: openGLView];
     [mainWindow setContentSize: NSMakeSize(width, height)];
+    mainWindow.contentMinSize = NSMakeSize(width, height);
+    [mainWindowController setMinimumWindowSize: 0.0f height: 0.0f];
+    
     if (minWidth > 0 && minHeight > 0)
     {
         // Call Core::SetWindowMinimumSize to save minimum width and height and limit window size
         // Such a strange way due to my little knowledge of Objective-C
         Core::Instance()->SetWindowMinimumSize(minWidth, minHeight);
     }
-    
 
     core = Core::GetApplicationCore();
     Core::Instance()->SetNativeView(openGLView);
@@ -214,6 +216,14 @@ namespace DAVA
 
 -(void)setMinimumWindowSize:(DAVA::float32)width height:(DAVA::float32)height
 {
+    const float32 MIN_WIDTH = 64.0f;
+    const float32 MIN_HEIGHT = 64.0f;
+ 
+    // Always limit minimum window size to 64x64, as application crashes
+    // when resizing window to zero height (stack overflow occures)
+    // It seems that NSOpenGLView is responsible for that
+    if (width < MIN_WIDTH) width = MIN_WIDTH;
+    if (height < MIN_HEIGHT) height = MIN_HEIGHT;
     mainWindow.contentMinSize = NSMakeSize(width, height);
 }
 
