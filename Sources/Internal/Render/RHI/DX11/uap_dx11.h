5c74b0196aea9dea2e4309183b612bb18709ee69
Igor Solovey <i_solovey@wargaming.net>
2016-01-19 16:21:25 +0300

    DF-9137 fix WinUAP after removing scale from render init/reset params
    Former-commit-id: ade5d5222f21b4d502536f43aabada70a2ea7c20

--- Sources/Internal/Render/RHI/DX11/uap_dx11.h
+++ Sources/Internal/Render/RHI/DX11/uap_dx11.h
@@ -2,6 +2,6 @@
 #define __DAVAFRAMEWORK_UAP_DX11__
 
 void init_device_and_swapchain_uap(void* panel);
-void resize_swapchain(int32 width, int32 height, float32 sx, float32 sy);
+void resize_swapchain(int32 width, int32 height);
 
 #endif
