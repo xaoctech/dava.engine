6a5d8d3e0400fecfa5b4cbe6b991ce7eba3a0f44
Aleksei Kanash <aleksei.kanash@gmail.com>
2016-02-08 09:19:24 +0300

    [DF-8584] Horizontal scroll works on win10 by pressing any shift key.
    Former-commit-id: 5acc368ecddbefe38b3fddc56539d34fecbc35f8

--- Sources/Internal/Platform/TemplateWin32/WinUAPXamlApp.cpp
+++ Sources/Internal/Platform/TemplateWin32/WinUAPXamlApp.cpp
@@ -45,6 +45,7 @@
 #include "FileSystem/Logger.h"
 
 #include "Utils/Utils.h"
+#include "Input/InputSystem.h"
 
 #include "WinUAPXamlApp.h"
 #include "DeferredEvents.h"
@@ -652,8 +653,17 @@ void WinUAPXamlApp::OnSwapChainPanelPointerWheel(Platform::Object ^ /*sender*/, 
 
     core->RunOnMainThread([this, wheelDelta, physPoint, type]() {
         UIEvent ev;
-
-        ev.wheelDelta.y = wheelDelta / static_cast<float32>(WHEEL_DELTA);
+        auto delta = wheelDelta / static_cast<float32>(WHEEL_DELTA);
+        KeyboardDevice & keybDev = InputSystem::Instance()->GetKeyboard();
+        if (keybDev.IsKeyPressed(Key::LSHIFT) || keybDev.IsKeyPressed(Key::RSHIFT))
+        {
+            ev.wheelDelta.x = delta;
+        }
+        else
+        {
+            ev.wheelDelta.y = delta;
+        }
+        
         ev.phase = UIEvent::Phase::WHEEL;
         ev.device = ToDavaDeviceId(type);
         ev.physPoint = physPoint;
