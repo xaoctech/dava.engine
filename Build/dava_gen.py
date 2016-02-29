8878279381d21fb4efe4c05a8da0e13113699614
Igor.Bratchik <i_bratchik@wargaming.net>
2016-02-29 23:55:29 +0300

    DF-185 fix dava_gen for windows
    Former-commit-id: 58447a64f085a440b05952553d371c3d83111c4b

--- Build/dava_gen.py
+++ Build/dava_gen.py
@@ -40,16 +40,16 @@ def search_program(program):
             if is_exe(exe_file):
                 return exe_file
 
-return False
+    return False
 
 def parse_additional_params(additional):
     global g_is_console
     global g_is_uap
     global g_is_unity_build
-    
+
     if not additional:
         return True;
-    
+
     for param in additional:
         param = param.lower()
         if "console" == param:
@@ -57,12 +57,12 @@ def parse_additional_params(additional):
         elif "uap" == param:
             g_is_uap = True
         elif "ub" == param:
-            g_is_unity_build = True
+            g_is_unity_build = True                
         else:
             print "Unsupported additional parameter " + "'" + param + "'" + " Use combination of " + str(g_supported_additional_parameters)
             return False
 
-return True
+    return True
 
 
 def setup_framework_env():
@@ -70,30 +70,30 @@ def setup_framework_env():
     global g_toolchains_full_path
     global g_toolchains_relative_framework_path
     global g_cmake_file_path
-    
+
     # take current path and make it easier understandable
     path = os.path.realpath(sys.argv[0])
     path = path.replace('\\', '/')
     path = os.path.normpath(path)
     path = path if path.endswith('/') else path + '/'
-    
+
     # take path like "some/folder/dava.framework.something/inside/"
     # and split it to "some/folder/" + "dava.framework.something/" + "inside/"
     regex = r"(dava\.framework[^/]*/)"
     tail = re.split(regex, path)
-    
+
     # determine full path to dava.framework folder
     g_framework_path = ""
     if len(tail) >= 3:
         g_framework_path = tail[0] + tail[1]
-    
+
     if "" == g_framework_path:
         return False
 
     g_toolchains_full_path = os.path.realpath( g_framework_path + g_toolchains_relative_framework_path )
     g_toolchains_full_path = g_toolchains_full_path + '/'
 
-return True
+    return True
 
 
 def get_project_type(dst_platform, is_console):
@@ -105,10 +105,10 @@ def get_project_type(dst_platform, is_console):
         project_string += "Visual Studio 12"
     if "android" == dst_platform:
         current_platform = platform.system()
-        
+
         if not is_console:
             project_string += "Eclipse CDT4 - "
-        
+
         if "MinGW" == current_platform:
             project_string += "Mingw Makefiles"
         elif "Windows" == current_platform:
@@ -116,64 +116,64 @@ def get_project_type(dst_platform, is_console):
         else:
             project_string += "Unix Makefiles"
 
-return project_string
+    return project_string
 
 
 def get_toolchain(dst_platform):
     global g_ios_toolchain
     global g_android_toolchain
-    
+
     toolchain_base = "-DCMAKE_TOOLCHAIN_FILE=" + g_toolchains_full_path;
     toolchain_string = ""
-    
+
     if "ios" == dst_platform:
         toolchain_string = toolchain_base + g_ios_toolchain
     if "android" == dst_platform:
         toolchain_string = toolchain_base + g_android_toolchain
-    
+
     return toolchain_string
 
 def main():
     global g_supported_additional_parameters
     global g_supported_platforms
-    
+
     parser = argparse.ArgumentParser(description='Dava.Framework projects genarator')
     parser.add_argument('platform_name', help='One of ' + str(g_supported_platforms))
     parser.add_argument('additional_params', nargs='*', help= 'One of ' + str(g_supported_additional_parameters))
     parser.add_argument('cmake_path', help='relative path to cmake list')
     parser.add_argument('--generation_dir', default="", help="path to generation cmake list" )
     parser.add_argument('--add_definitions', '-defs', default="", help="add definitions" )
-    
+
     options = parser.parse_args()
-    
+
     if not setup_framework_env():
         print "Couldn't configure environment. Make sure that you runs this script from dava.framework subfolder."
         exit()
 
     destination_platform = ""
 
-if options.platform_name not in g_supported_platforms:
-    print "Wrong destination OS name " + "'" + options.platform_name + "'"
+    if options.platform_name not in g_supported_platforms:
+        print "Wrong destination OS name " + "'" + options.platform_name + "'"
         parser.print_help()
         exit();
     else:
         destination_platform = options.platform_name.lower()
 
-project_type = get_project_type(destination_platform, g_is_console)
+    project_type = get_project_type(destination_platform, g_is_console)
     if project_type == "":
         print "Unknown project type. Seems get_project_type() works wrong."
         exit()
 
-if False == parse_additional_params(options.additional_params):
-    parser.print_help()
+    if False == parse_additional_params(options.additional_params):
+        parser.print_help()
         exit()
 
     toolchain = get_toolchain(destination_platform)
 
-g_cmake_file_path = os.path.realpath(options.cmake_path)
+    g_cmake_file_path = os.path.realpath(options.cmake_path)
     g_generation_dir  = options.generation_dir
     g_add_definitions = options.add_definitions.replace(',',' ')
-    
+
     if len(g_generation_dir) :
         if not os.path.exists(g_generation_dir):
             os.makedirs(g_generation_dir)
@@ -181,23 +181,23 @@ g_cmake_file_path = os.path.realpath(options.cmake_path)
 
     cmake_program = search_program("cmake")
 
-if False == cmake_program:
-    print "cmake command not found."
+    if False == cmake_program:
+        print "cmake command not found."
         exit()
 
     call_string = [cmake_program, '-G', project_type, toolchain, g_cmake_file_path]
 
-if len(g_add_definitions) :
-    call_string +=  g_add_definitions.split(' ')
+    if len(g_add_definitions) :
+        call_string +=  g_add_definitions.split(' ') 
     
     if g_is_unity_build:
         call_string.append("-DUNITY_BUILD=true")
-print call_string
-    
+    print call_string
+
     subprocess.check_output(call_string)
     
     if "android" == destination_platform:
-        subprocess.check_output(call_string)
+        subprocess.check_output(call_string)    
 
 if __name__ == '__main__':
     main()
