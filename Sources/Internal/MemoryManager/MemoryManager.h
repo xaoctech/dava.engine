45cbc768c6fae3d7193ebf434f6d86e01b75931f
Maksim Lazarev <m_lazarev@wargaming.net>
2016-03-01 10:22:26 +0300

    DF-9625 win32: memory profiler: finish symbol collector thread before exiting main
    Former-commit-id: f5a9a8e6f188259c07c6fdd546958f38f9bde739

--- Sources/Internal/MemoryManager/MemoryManager.h
+++ Sources/Internal/MemoryManager/MemoryManager.h
@@ -101,6 +101,7 @@ public:
     void EnableLightWeightMode();
     void SetCallbacks(Function<void()> updateCallback, Function<void(uint32, bool)> tagCallback);
     void Update();
+    void Finish();
 
     DAVA_NOINLINE void* Allocate(size_t size, uint32 poolIndex);
     DAVA_NOINLINE void* AlignedAllocate(size_t size, size_t align, uint32 poolIndex);
@@ -199,6 +200,7 @@ private:
     Mutex symbolCollectorMutex;
     size_t bktraceGrowDelta = 0;
     bool lightWeightMode = false; // Flag enabling lightweight mode: no backtrace and symbols, should increase performance
+    bool finishSymbolThread = false;
 
     Function<void()> updateCallback;
     Function<void(uint32, bool)> tagCallback;
