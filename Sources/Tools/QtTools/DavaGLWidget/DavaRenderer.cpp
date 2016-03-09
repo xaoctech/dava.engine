9ff3ff7152e3b18669caf9158aec6a08f268f5c4
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-03-09 17:14:43 +0300

    [DF-9741] extra default destructor removed
    Former-commit-id: 9c8d0ef50ad3806b99e6cd86ef980ed542c1be86

--- Sources/Tools/QtTools/DavaGLWidget/DavaRenderer.cpp
+++ Sources/Tools/QtTools/DavaGLWidget/DavaRenderer.cpp
@@ -25,6 +25,8 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
+
+
 #include "Render/RenderBase.h"
 
 #include "DavaRenderer.h"
@@ -61,8 +63,6 @@ DavaRenderer::DavaRenderer()
     DAVA::QtLayer::Instance()->AppStarted();
     DAVA::QtLayer::Instance()->OnResume();
 }
-
-DavaRenderer::~DavaRenderer() = default;
 
 void DavaRenderer::paint()
 {
