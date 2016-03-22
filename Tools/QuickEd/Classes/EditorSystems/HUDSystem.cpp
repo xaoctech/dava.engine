cea52e0c6afd382453cf07608f1c7bcc63296915
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-03-22 16:44:54 +0300

    [DF-8152] review fix
    Former-commit-id: 6c62bff7dd2fe71d8b3b4133d892e8c2b9d45ecc

--- Tools/QuickEd/Classes/EditorSystems/HUDSystem.cpp
+++ Tools/QuickEd/Classes/EditorSystems/HUDSystem.cpp
@@ -136,6 +136,7 @@ class HUDControl : public UIControl
     void Draw(const UIGeometricData& geometricData) override
     {
         UpdateLayout();
+        UIControl::Draw(geometricData);
     }
 };
 
