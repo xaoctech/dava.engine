7f616466045136fa2615aed0591d32b6f56a6a97
m_kiselev <m_kiselev@wargaming.net>
2016-03-28 16:44:31 +0300

    [DF-9924] remarks fix
    Former-commit-id: 3cda4bedb6f5a236a9bde416c653f9047bab9e25

--- Sources/Internal/Core/Core.cpp
+++ Sources/Internal/Core/Core.cpp
@@ -897,18 +897,17 @@ void Core::InitWindowSize(void* nativeView, float32 width, float32 height, float
 
 void Core::WindowSizeChanged(float32 width, float32 height, float32 scaleX, float32 scaleY)
 {
-    DVASSERT(scaleX * scaleY);
+    if ((width == 0.f) || (height == 0.f) || (scaleX == 0.f) || (scaleY != 0.f))
+    {
+        return;
+    }
     bool equal = true;
     equal &= FLOAT_EQUAL(width, screenMetrics.width);
     equal &= FLOAT_EQUAL(height, screenMetrics.height);
     equal &= FLOAT_EQUAL(scaleX, screenMetrics.scaleX);
     equal &= FLOAT_EQUAL(scaleY, screenMetrics.scaleY);
-    bool validSize = true;
-    validSize &= (width != 0.f);
-    validSize &= (height != 0.f);
-    validSize &= (scaleX != 0.f);
-    validSize &= (scaleY != 0.f);
-    if (!equal && validSize)
+
+    if (!equal)
     {
         screenMetrics.width = width;
         screenMetrics.height = height;
