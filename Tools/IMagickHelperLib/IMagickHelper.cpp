57b61ca6fd2e13e271bde3873338e4b4c163ccbb
Sergey Reznik <s_reznik@wargaming.net>
2016-03-31 15:04:40 +0300

    [DF-9968] ImageMagick replaced with libpsd
    Former-commit-id: 3710af0a0c364a2da08aad80d9e3211ce264bb26

--- Tools/IMagickHelperLib/IMagickHelper.cpp
+++ Tools/IMagickHelperLib/IMagickHelper.cpp
@@ -1,8 +1,8 @@
 #include "IMagickHelper.h"
-#include <FileTools.h>
-#include <Magick++.h>
+#include "FileTools.h"
+#include <libpsd.h>
+#include <png.h>
 #include <cstdlib>
-#include <cstring>
 
 using namespace std;
 
@@ -37,66 +37,7 @@ void CroppedData::Reset()
     delete[] layers;
 }
 
-namespace
-{
-inline bool ReadImage(std::vector<Magick::Image>& layers, const char* inName)
-{
-    try
-    {
-        Magick::readImages(&layers, inName);
-    }
-    catch (Magick::Warning& warning_)
-    {
-        printf("\n[IMAGE-MAGICK WARNING] Reading file %s\n: %s\n", inName, warning_.what());
-    }
-    catch (Magick::Error& error_)
-    {
-        printf("\n[IMAGE-MAGICK ERROR] Reading file %s\n: %s\n", inName, error_.what());
-        return false;
-    }
-    catch (Magick::Exception& other_)
-    {
-        printf("\n[IMAGE-MAGICK EXCEPTION] Reading file %s\n: %s\n", inName, other_.what());
-        return false;
-    }
-
-    if (layers.empty())
-    {
-        printf("[IMAGE-MAGICK ERROR]: Image being read does not contain any layers:\n%s", inName);
-        return false;
-    }
-
-    return true;
-}
-
-inline bool WritePNGImage(Magick::Image& image, const std::string& outName, const char* inName)
-{
-    try
-    {
-        image.magick("PNG");
-        image.write(outName);
-    }
-    catch (Magick::Warning&)
-    {
-        // silently ignore warnings
-        // usual case - warning about RGB profile in grayscale image
-        // image still will be saved correctly
-    }
-    catch (Magick::Error& error_)
-    {
-        printf("\n[IMAGE-MAGICK ERROR] %s\nWriting file %s\n:from source: %s", error_.what(), outName.c_str(), inName);
-        return false;
-    }
-    catch (Magick::Exception& other_)
-    {
-        printf("\n[IMAGE-MAGICK EXCEPTION] %s\nWriting file %s\n:from source: %s", other_.what(), outName.c_str(), inName);
-        return false;
-    }
-
-    return true;
-}
-
-inline bool CreateOutputDirectoryForOutputPath(const char* out_path, size_t& out_path_len)
+bool CreateOutputDirectoryForOutputPath(const char* out_path, size_t& out_path_len)
 {
     out_path_len = (out_path == nullptr) ? 0 : strlen(out_path);
     if ((out_path_len > 0) && !FileTool::CreateDir(out_path))
@@ -107,33 +48,82 @@ inline bool CreateOutputDirectoryForOutputPath(const char* out_path, size_t& out
 
     return true;
 }
+
+bool WritePNGImage(int width, int height, char* imageData, const char* outName, int channels, int bit_depth)
+{
+    FILE* fp = fopen(outName, "wb");
+    if (fp == nullptr)
+    {
+        return false;
+    }
+    
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    
+    if (png_ptr == nullptr)
+        return false;
+    
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == nullptr)
+    {
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+        return false;
+    }
+    
+    png_init_io(png_ptr, fp);
+    
+    png_byte colorType = 0;
+    switch (channels)
+    {
+        case 1:
+        {
+            colorType = PNG_COLOR_TYPE_GRAY;
+            break;
+        }
+        case 2:
+        {
+            colorType = PNG_COLOR_TYPE_GRAY_ALPHA;
+            break;
+        }
+            
+        case 3:
+        {
+            colorType = PNG_COLOR_TYPE_RGB;
+            break;
+        }
+        case 4:
+        {
+            colorType = PNG_COLOR_TYPE_RGBA;
+            break;
+        }
+            
+        default:
+        {
+            printf("Invalid image configuration: %d channels, %d bit depth", channels, bit_depth);
+            png_destroy_info_struct(png_ptr, &info_ptr);
+            png_destroy_write_struct(&png_ptr, &info_ptr);
+            return false;
+        }
+    }
+    
+    int rowSize = width * channels * bit_depth / 8;
+    
+    png_set_IHDR(png_ptr, info_ptr, width, height, bit_depth, colorType,
+        PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_write_info(png_ptr, info_ptr);
+    for (int y = 0; y < height; ++y)
+        png_write_row(png_ptr, (png_bytep)(&imageData[y * rowSize]));
+    png_write_end(png_ptr, info_ptr);
+    png_destroy_info_struct(png_ptr, &info_ptr);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    
+    fclose(fp);
+    return true;
 }
 
 bool ConvertToPNG(const char* in_image_path, const char* out_path)
 {
-    size_t out_path_len = 0;
-    if (CreateOutputDirectoryForOutputPath(out_path, out_path_len) == false)
-    {
-        return false;
-    }
-
-    std::vector<Magick::Image> layers;
-    if (ReadImage(layers, in_image_path) == false)
-    {
-        return false;
-    }
-
-    string out_image_path = FileTool::WithNewExtension(in_image_path, ".png");
-    if (out_path_len > 0)
-    {
-        out_image_path = FileTool::ReplaceDirectory(out_image_path, out_path);
-    }
-
-    Magick::Image& image = layers.front();
-    int width = (int)image.columns();
-    int height = (int)image.rows();
-    image.crop(Magick::Geometry(width, height, 0, 0));
-    return WritePNGImage(image, out_image_path, in_image_path);
+    abort();
+    return false;
 }
 
 bool ConvertToPNGCroppedGeometry(const char* in_image_path, const char* out_path, CroppedData& out_cropped_data, bool skipCompositeLayer)
@@ -143,13 +133,7 @@ bool ConvertToPNGCroppedGeometry(const char* in_image_path, const char* out_path
     {
         return false;
     }
-
-    std::vector<Magick::Image> layers;
-    if (ReadImage(layers, in_image_path) == false)
-    {
-        return false;
-    }
-
+    
     std::string out_image_base_path = FileTool::WithNewExtension(in_image_path, ".png");
     std::string out_image_basename = FileTool::GetBasename(out_image_base_path);
     std::string out_image_path = out_image_base_path;
@@ -157,43 +141,49 @@ bool ConvertToPNGCroppedGeometry(const char* in_image_path, const char* out_path
     {
         out_image_path = FileTool::ReplaceDirectory(out_image_path, out_path);
     }
-
-    size_t width = layers.front().columns();
-    size_t height = layers.front().rows();
-    if (skipCompositeLayer && layers.size() > 1)
+    
+    psd_context* psd = nullptr;
+    auto status = psd_image_load(&psd, (psd_char*)in_image_path);
+    if ((psd == nullptr) || (status != psd_status_done))
     {
-        layers.erase(layers.begin());
+        printf("Unable to load PSD from file %s", in_image_path);
+        return false;
     }
-
-    int layerIndex = 0;
-    for (auto& currentLayer : layers)
+    
+    out_cropped_data.Reset();
+    out_cropped_data.layer_width = psd->width;
+    out_cropped_data.layer_height = psd->height;
+    out_cropped_data.layers_count = psd->layer_count;
+    out_cropped_data.layers = new IMagickHelper::Layer[psd->layer_count]();
+    
+    for (int i = 0, e = psd->layer_count; i < e; ++i)
     {
         char c_buf[32] = {};
-        sprintf(c_buf, "%d", layerIndex);
+        sprintf(c_buf, "%d", static_cast<int>(i));
         out_image_path = FileTool::ReplaceBasename(out_image_path, out_image_basename + "_" + c_buf);
-        currentLayer.crop(Magick::Geometry(width, height, 0, 0));
-        if (WritePNGImage(currentLayer, out_image_path, in_image_path) == false)
+        
+        auto& layer = psd->layer_records[i];
+        auto& outLayer = out_cropped_data.layers[i];
+        outLayer.x = layer.left;
+        outLayer.y = layer.top;
+        outLayer.dx = layer.width;
+        outLayer.dy = layer.height;
+        memcpy(outLayer.name, layer.layer_name, 256);
+        uint32_t* p = reinterpret_cast<uint32_t*>(layer.image_data);
+        for (int i = 0, e = layer.width * layer.height; i < e; ++i)
         {
+            p[i] = (p[i] & 0x000000ff) << 16 | (p[i] & 0x0000ff00) | (p[i] & 0xff0000) >> 16 | (p[i] & 0xff000000);
+        }
+        
+        auto data = reinterpret_cast<char*>(layer.image_data);
+        if (WritePNGImage(layer.width, layer.height, data, out_image_path.c_str(), 4, 8) == false)
+        {
+            printf("Failed to write PNG for file %s, layer %d", in_image_path, int(i));
             return false;
         }
-        ++layerIndex;
     }
-
-    out_cropped_data.Reset();
-    out_cropped_data.layer_width = static_cast<int>(width);
-    out_cropped_data.layer_height = static_cast<int>(height);
-    out_cropped_data.layers_count = layers.size();
-    out_cropped_data.layers = new Layer[layers.size()]();
-
-    Layer* layer = out_cropped_data.layers;
-    for (const auto& currentLayer : layers)
-    {
-        Magick::Geometry bbox = currentLayer.page();
-        int xOff = static_cast<int>(bbox.xOff()) * (bbox.xNegative() ? -1 : 1);
-        int yOff = static_cast<int>(bbox.yOff()) * (bbox.yNegative() ? -1 : 1);
-        *layer++ = Layer(xOff, yOff, (int)bbox.width(), (int)bbox.height(), currentLayer.label().c_str());
-    }
-
+    
+    psd_image_free(psd);
     return true;
 }
 }
