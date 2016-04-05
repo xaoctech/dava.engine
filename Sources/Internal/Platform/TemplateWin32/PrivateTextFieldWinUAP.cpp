e79487ab179939c8ab59381dc7f997c4ddc02884
Dmitry Belsky <d_belsky@wargaming.net>
2016-04-05 12:58:30 +0300

    [DF-9068] focus: fixed 10 text fields behaviour
    Former-commit-id: 31654c11352d066cc41f0dcb1d9f5033ce0488d7

--- Sources/Internal/Platform/TemplateWin32/PrivateTextFieldWinUAP.cpp
+++ Sources/Internal/Platform/TemplateWin32/PrivateTextFieldWinUAP.cpp
@@ -680,6 +680,20 @@ void PrivateTextFieldWinUAP::OnGotFocus()
 
 void PrivateTextFieldWinUAP::OnLostFocus()
 {
+    if (uiTextField != nullptr && UIControlSystem::Instance()->GetFocusedControl() == uiTextField && uiTextField->IsEditing() && 
+        uiTextField->GetStopEditPolicy() == UITextField::STOP_EDIT_WHEN_FOCUS_LOST) // don't lose system focus if ui wants to stay in focus
+    {
+        if (nativeText != nullptr)
+        {
+            nativeText->Focus(FocusState::Programmatic);
+        }
+        else if (nativePassword != nullptr)
+        {
+            nativePassword->Focus(FocusState::Programmatic);
+        }
+        return;
+    }
+
     if (!IsMultiline())
     {
         waitRenderToTextureComplete = true;
