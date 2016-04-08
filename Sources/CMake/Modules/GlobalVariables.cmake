91ba938acc9e6790d92d9894f3daaf3a448e6706
Igor.Bratchik <i_bratchik@wargaming.net>
2016-04-08 15:04:55 +0300

    DF-10067 fix android building
    Former-commit-id: eec55b0aa824249d622550fd57cb272ad0d02658

--- Sources/CMake/Modules/GlobalVariables.cmake
+++ Sources/CMake/Modules/GlobalVariables.cmake
@@ -86,6 +86,7 @@ set( DAVA_THIRD_PARTY_INCLUDES_PATH     "${DAVA_THIRD_PARTY_ROOT_PATH}/include"
                                         "${DAVA_THIRD_PARTY_ROOT_PATH}/glew/include" 
                                         "${DAVA_THIRD_PARTY_ROOT_PATH}/fmod/include" 
                                         "${DAVA_THIRD_PARTY_ROOT_PATH}/lua/include" 
+                                        "${DAVA_ROOT_DIR}/Libs/include"
                                         "${DAVA_THIRD_PARTY_ROOT_PATH}/icucommon/source/common" 
                                       ) 
 
