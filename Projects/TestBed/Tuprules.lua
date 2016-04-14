b2ba54c4a35c0aa46da91fd2812d6d238cc23bd8
U-CORP\s_zdanevich <s_zdanevich@BY1-WW-351.corp.wargaming.local>
2016-04-14 19:02:41 +0300

    [DF-9682] Added packs structure db generation
    Former-commit-id: 3129f3a1ed88381bb68b9a7eab7d949231b45b73

--- Projects/TestBed/Tuprules.lua
+++ Projects/TestBed/Tuprules.lua
@@ -4,12 +4,24 @@ require("dava")
 dava.config.output_dir = ".tupOutput"
 dava.config.packlist_output_dir = ".tupOutput/packlists"
 
+dava.add_pack_rule("vpack",
+{
+    depends = { "pack1", "pack2" }
+})
+
 dava.add_pack_rule("pack1",
 {
+    exclusive = true,
     { "Data",  "%.aaa" }
 })
 
 dava.add_pack_rule("pack2",
 {
---    { "Data", "%.sc2" }
+    exclusive = true,
+    { "Data", "%.sc2" }
+})
+
+dava.add_pack_rule("pack3",
+{
+    { "Data", "" }
 })
--- Tools/Bin/tup/dava.lua
+++ Tools/Bin/tup/dava.lua
@@ -17,6 +17,7 @@ dava.current_dir = tup.getrelativedir(dava.project_dir)
 --
 dava.config = { }
 dava.config.output_dir = "Output"
+dava.config.output_db = "packs.db"
 dava.config.packlist_output_dir = "Output/packlist"
 dava.config.packlist_ext = "list"
 dava.config.packlist_default = "__else__"
@@ -27,10 +28,17 @@ dava.config.packlist_default = "__else__"
 dava.init = function()
     if dava.initialized ~= true then
         dava.im.output_dir = dava.project_dir .. "/" .. dava.config.output_dir
+        dava.im.packs_db_output = dava.im.output_dir .. "/" .. dava.config.output_db
         dava.im.packlist_output_dir = dava.project_dir .. "/" .. dava.config.packlist_output_dir
         dava.im.packlist_ext = dava.config.packlist_ext
         dava.im.packlist_default = dava.config.packlist_default
-        dava.im.packs = { __else__ = { } }
+        
+        dava.im.packs = { 
+            { 
+                name = dava.im.packlist_default,
+                rules = {" "}
+            }
+        }
 
         -- get directory path where this dava.lua file is located 
         local info = debug.getinfo(1, 'S');
@@ -50,6 +58,7 @@ dava.init = function()
         dava.im.cmd_cat = "cat"
         dava.im.cmd_fwdep = fwpath .. "dep"
         dava.im.cmd_fwzip = fwpath .. "../7za"
+        dava.im.cmd_fwsql = fwpath .. "../sqlite3"
 
         -- edit commands for win32
         if dava.platform == "win32" then
@@ -86,7 +95,12 @@ dava.add_pack_rule = function(pack_name, pack_rules)
 	assert(dava.im.packs[pack_name] == nil, "Pack '" .. pack_name .. "' is already defined")
 
 	for k, v in pairs(pack_rules) do
-        if type(v) ~= "string" then
+        if type(k) ~= "number" then
+            assert(type(k) == "string" and (k == "depends" or k == "exclusive"), "Pack dependencies should be declared with 'depends' key.")
+            if k == "depends" then
+                assert(type(v) == "table", "Pack dependencies should be declared as array")
+            end
+        elseif type(v) ~= "string" then
             if type(v) == "table" then
                 assert(#v >= 2 and type(v[1]) == "string" and type(v[2]) == "string",
                     "Pack rule #" .. k .. " table should be defined as { 'dir pattern', 'file pattern', exclusive = false }")
@@ -96,7 +110,7 @@ dava.add_pack_rule = function(pack_name, pack_rules)
         end
 	end
 
-	dava.im.packs[pack_name] = pack_rules
+	dava.im.packs[#dava.im.packs + 1] = { name = pack_name, rules = pack_rules }
 end
 
 
@@ -126,17 +140,27 @@ dava.create_lists = function()
         local pack_found = false
 
         -- go through pack
-        for pack_name, pack_rules in pairs(dava.im.packs) do
+        for pai, pack in pairs(dava.im.packs) do
+            local is_exclusive = false
 
             -- each pack has multiple rules
-            for kk, rule in pairs(pack_rules) do
+            for kk, rule in pairs(pack.rules) do
+
+                -- dependency specification
+                if kk == "depends" then
+
+                    -- nothing to do here
+
+                elseif kk == "exclusive" then
+                    -- mark that we found exclusive pack
+                    is_exclusive = true
 
                 -- when pack rule is a simple string
                 -- we should just compare it to match
-                if type(rule) == "string" then
+                elseif type(rule) == "string" then
 
-                    if pack_rules == file_full_path then
-                        pack_found = add_to_affected_packs(pack_name, file)
+                    if pack.rules == file_full_path then
+                        pack_found = add_to_affected_packs(pack.name, file)
                     end
 
                 -- when pack rule is a table
@@ -159,7 +183,7 @@ dava.create_lists = function()
                     --
 
                     if cur_dir:match(dir_rule) and file:match(file_rule) then
-                        pack_found = add_to_affected_packs(pack_name, file)
+                        pack_found = add_to_affected_packs(pack.name, file)
                     end
 
                 -- when pack rule is a function
@@ -167,13 +191,18 @@ dava.create_lists = function()
                 elseif type(rule) == "function" then
 
                     if rule(cur_dir, file) == true then
-                        pack_found = add_to_affected_packs(pack_name, file)
+                        pack_found = add_to_affected_packs(pack.name, file)
                     end
 
                 -- this should never happend
                 else
                     assert(false, "this should never happend, type(pack_rule) = " .. type(pack_rule))
                 end
+            end
+
+            if is_exclusive and pack_found then
+                -- exclusive rule will break matching no other rules
+                break
             end
         end
 
@@ -212,34 +241,64 @@ end
 dava.create_packs = function()
     dava.init()
 
-    for pack_name, pack_rules in pairs(dava.im.packs) do
-	    local pack_output = dava.im.output_dir .. "/" .. pack_name .. ".pack.zip"
-        local pack_merged_list_output = dava.im.output_dir .. "/" .. pack_name .. "." .. dava.im.packlist_ext
+    local packs_merged_sql_input = {}
+    local packs_merged_sql_output = dava.project_dir .. "/" .. dava.config.packlist_output_dir .. "/merged/merged.sql"
+    local packs_db_output = dava.project_dir .. "/" .. dava.config.output_dir .. "/" .. dava.config.output_db
 
-	    local pack_in_group = dava.project_dir .. "/<"  .. pack_name .. ">"
-        local pack_in_mask = dava.im.packlist_output_dir .. "/" .. pack_name .. "_*"
-        local empty_pack = dava.im.packlist_output_dir .. "/" .. pack_name .. "_empty." .. dava.im.packlist_ext
+    for pai, pack in pairs(dava.im.packs) do
+	    local pack_output = dava.im.output_dir .. "/" .. pack.name .. ".pack"
+        local pack_output_hash = dava.im.output_dir .. "/" .. pack.name .. ".hash"
+        local pack_dependencies = ""
+        local pack_merged_list_output = dava.im.packlist_output_dir .. "/merged/" .. pack.name .. ".merged"
+        local pack_sql_output = dava.im.packlist_output_dir .. "/" .. pack.name .. ".sql"
+
+	    local pack_in_group = dava.project_dir .. "/<"  .. pack.name .. ">"
+        local pack_in_mask = dava.im.packlist_output_dir .. "/" .. pack.name .. "_*"
+        local empty_pack = dava.im.packlist_output_dir .. "/" .. pack.name .. "_empty." .. dava.im.packlist_ext
 
         if dava.platform == "win32" then
             pack_in_mask = pack_in_mask:gsub("/", "\\")
             empty_pack = empty_pack:gsub("/", "\\")
         end
 
+        -- check if pack has dependencies
+        if type(pack.rules.depends) == "table" then
+            for i, dep in pairs(pack.rules.depends) do
+                pack_dependencies = pack_dependencies .. " " .. dep
+            end
+        end
+
         -- generate emply lists for each pack
         -- this will allow cat/type command not fail
         -- if no lists were generated for pack 
-        tup.rule("^ Touching " .. pack_name .. "^ " .. dava.im.cmd_fwdep .. " echo > %o", { empty_pack })
+        tup.rule("^ Touching " .. pack.name .. "^ " .. dava.im.cmd_fwdep .. " echo > %o", { empty_pack })
 
         -- merge final pack list 
 	    tup.frule{
 	        inputs = { pack_in_mask, pack_in_group, empty_pack },
-	        command = "^ Gen big " .. pack_name .. " list^ " .. dava.im.cmd_cat .." " .. pack_in_mask .. " > %o",
+	        command = "^ Gen big " .. pack.name .. " list^ " .. dava.im.cmd_cat .." " .. pack_in_mask .. " > %o",
 	        outputs = { pack_merged_list_output }
 	    }
 
-        -- archivate
-	    if pack_name ~= dava.im.packlist_default then
-	        tup.rule(pack_merged_list_output, "^ Pack " .. pack_name .. "^ " .. dava.im.cmd_fwzip .. " a -bd -bso0 -- %o @%f", pack_output)
+	    if pack.name ~= dava.im.packlist_default then
+            -- archivate
+	        tup.rule(pack_merged_list_output, "^ Pack " .. pack.name .. "^ " .. dava.im.cmd_fwzip .. " a -bd -bso0 -- %o @%f", pack_output)
+
+            -- generate pack crc
+            tup.rule(pack_output, "^ Pack " .. pack.name .. " HASH^ " .. dava.im.cmd_fwdep .. " hash %f > %o", pack_output_hash)
+
+            -- generate pack sql
+            tup.rule({ pack_merged_list_output, pack_output_hash } , "^ SQL for " .. pack.name .. "^ " .. dava.im.cmd_fwdep .. " sql -l " .. pack_merged_list_output .. " -h " .. pack_output_hash .. " " .. pack.name .. " " .. pack_dependencies .. " > %o", pack_sql_output)
+
+            -- add sql into commin database input list
+            packs_merged_sql_input[#packs_merged_sql_input + 1] = pack_sql_output
 	    end
 	end
+
+    -- merge all sql
+    tup.rule(packs_merged_sql_input, "^ Gen big SQL^ " .. dava.im.cmd_cat .. " %\"f > %o", packs_merged_sql_output)
+
+    -- generate final packs DB
+    print(dava.im.output_db)
+    tup.rule({ packs_merged_sql_output }, "^ Gen final packs DB^ " .. dava.im.cmd_fwsql .. " -cmd \".read " .. packs_merged_sql_output .. "\" -cmd \".save " .. packs_db_output .. "\" \"\" \"\"", dava.im.packs_db_output)
 end
