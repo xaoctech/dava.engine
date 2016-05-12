cf7622c6c02b32cb28cc2fb3f18380754e639b3d
Aleksei Kanash <aleksei.kanash@gmail.com>
2016-05-12 17:00:28 +0300

    [DF-9566] Win10 build fix.
    Former-commit-id: cbe94d4e8e07b59960384ed57f87d34827a16554

--- Sources/Internal/Platform/TemplateWin32/UAPNetworkHelper.h
+++ Sources/Internal/Platform/TemplateWin32/UAPNetworkHelper.h
@@ -33,27 +33,26 @@
 #include "Base/BaseTypes.h"
 
 #if defined(__DAVAENGINE_WIN_UAP__)
-{
 
 #include "Network/Base/Endpoint.h"
 #include "Network/NetworkCommon.h"
 
-    namespace DAVA
-    {
-    class UAPNetworkHelper
-    {
-    public:
-        static const uint16 UAP_DESKTOP_TCP_PORT = 7777;
-        static const uint16 UAP_MOBILE_TCP_PORT = 1911;
-        static const char* UAP_IP_ADDRESS;
+namespace DAVA
+{
+class UAPNetworkHelper
+{
+public:
+    static const uint16 UAP_DESKTOP_TCP_PORT = 7777;
+    static const uint16 UAP_MOBILE_TCP_PORT = 1911;
+    static const char* UAP_IP_ADDRESS;
 
-        static Net::eNetworkRole GetCurrentNetworkRole();
-        static Net::Endpoint GetCurrentEndPoint();
-        static Net::Endpoint GetEndPoint(Net::eNetworkRole role);
-    };
+    static Net::eNetworkRole GetCurrentNetworkRole();
+    static Net::Endpoint GetCurrentEndPoint();
+    static Net::Endpoint GetEndPoint(Net::eNetworkRole role);
+};
 
-    } // namespace DAVA
-}
+} // namespace DAVA
+
 
 #endif
 #endif // __DAVAENGINE_UAP_NETWORK_HELPER_H__
