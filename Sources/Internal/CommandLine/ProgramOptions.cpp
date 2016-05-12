84226f27e19e01276e87c9f728edece14504f7d5
klesch <victor.kleschenko@gmail.com>
2016-05-12 17:53:22 +0300

    [DF-10421] Fixed parcing of command line for OSX+Debug
    Former-commit-id: b54887e09678ae27487dd0999ed17df8eaec37d0

--- Sources/Internal/CommandLine/ProgramOptions.cpp
+++ Sources/Internal/CommandLine/ProgramOptions.cpp
@@ -49,6 +49,10 @@ void ProgramOptions::Option::SetValue(const VariantType& value)
 ProgramOptions::ProgramOptions(const String& _commandName)
     : commandName(_commandName)
 {
+#if defined(__DAVAENGINE_DEBUG__) && defined(__DAVAENGINE_MACOS__)
+    AddOption("-NSDocumentRevisionsDebugMode", VariantType(false), "-dummy-");
+    AddOption("YES", VariantType(false), "-dummy-");
+#endif //#if defined (__DAVAENGINE_DEBUG__) && defined(__DAVAENGINE_MACOS__)
 }
 
 void ProgramOptions::AddOption(const String& optionName, const VariantType& defaultValue, const String& description, bool canBeMultiple)
