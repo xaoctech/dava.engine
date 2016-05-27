7339e1db33ffbd39cb46a59a34eedd818989bf4b
Andrey Derkach <a_derkach2@wargaming.net>
2016-05-28 02:56:36 +0300

    [DF-10434] CEF for Win64
    Former-commit-id: b7e74aca2fca2d818ae321b5f5948a1a56ca7072

--- Tools/CEFHelperProcess2/make_cef_helper_process.bat
+++ Tools/CEFHelperProcess2/make_cef_helper_process.bat
@@ -1,11 +1,13 @@
 ::clean build directory
 rmdir /s /q _build
+rmdir /s /q _build_x64
 mkdir _build
-cd _build
+mkdir _build_x64
 
 ::generate project and build
-cmake -G"Visual Studio 12" .. -DUNITY_BUILD=true && cmake --build . --config Release
+cd _build && cmake -G"Visual Studio 12" .. -DUNITY_BUILD=true && cmake --build . --config Release && cd ..
+cd _build_x64 && cmake -G"Visual Studio 12 Win64" .. -DUNITY_BUILD=true && cmake --build . --config Release && cd ..
 
 ::leave directory and copy artifacts to Tools/Bin/cef
-cd ..
 copy /Y _build\Release\CEFHelperProcess.exe ..\Bin\cef
+copy /Y _build_x64\Release\CEFHelperProcess.exe ..\Bin\x64\cef
