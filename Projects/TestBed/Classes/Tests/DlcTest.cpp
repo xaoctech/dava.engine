173bbe4b7b8fd660a0a0214a47396c335e223de3
Maksim Lazarev <m_lazarev@wargaming.net>
2016-06-08 18:57:50 +0300

    DF-10781 fix TestBed crash when exiting with active DlcTest screen
    Former-commit-id: 4c70faa93edef338f518603d1fc8967394026f6a

--- Projects/TestBed/Classes/Tests/DlcTest.cpp
+++ Projects/TestBed/Classes/Tests/DlcTest.cpp
@@ -271,7 +271,14 @@ void DlcTest::UnloadResources()
     BaseScreen::UnloadResources();
 
     options->SaveToYamlFile(optionsPath);
-    DownloadManager::Instance()->SetDownloadSpeedLimit(0);
+
+    // App crashes when exiting with active DlcTest screen as DownloadManager destroyed
+    // before UIControlSystem in Core::ReleaseSingletons
+    // So add this strange check
+    if (DownloadManager::Instance() != nullptr)
+    {
+        DownloadManager::Instance()->SetDownloadSpeedLimit(0);
+    }
 
     SafeRelease(dlSpeedIn);
     SafeRelease(gameVersionIn);
