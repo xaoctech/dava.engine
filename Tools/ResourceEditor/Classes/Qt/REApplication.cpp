2d4cf709b83c0c6c428956fd7be29107b9f5b3c3
y_rakhuba <y_rakhuba@wargaming.net>
2016-06-16 14:31:46 +0300

    [DF-10923] variant more basic library.
    Former-commit-id: c9c406de9532aa20eedba60eb909b549a3bfb6ca

--- Tools/ResourceEditor/Classes/Qt/REApplication.cpp
+++ Tools/ResourceEditor/Classes/Qt/REApplication.cpp
@@ -47,8 +47,8 @@ void REApplication::Run()
 
 void REApplication::GetPluginsForLoad(DAVA::Vector<DAVA::WideString>& names) const
 {
-    names.push_back(L"plg_reflection");
     names.push_back(L"plg_variant");
+    names.push_back(L"plg_reflection");
     names.push_back(L"plg_command_system");
     names.push_back(L"plg_serialization");
     names.push_back(L"plg_file_system");
