b64a8dde3df42cd5d05aaf53ce199efb2496d3ac
Andrey Derkach <a_derkach2@wargaming.net>
2016-06-17 19:21:56 +0300

    [DF-10868] Unity build fix
    Former-commit-id: 4433b1f225b89a3b392bf0b59895653ee7308509

--- Sources/CMake/Modules/UnityBuild.cmake
+++ Sources/CMake/Modules/UnityBuild.cmake
@@ -1,6 +1,7 @@
 macro( generated_unity_sources SOURCE_FILES )  
 
-    if( SOURCE_FILES )
+    list ( LENGTH ${SOURCE_FILES} SOURCE_FILES_LIST_LEN )
+    if ( ${SOURCE_FILES_LIST_LEN} GREATER "0" )
        list( REMOVE_DUPLICATES ${SOURCE_FILES} )
     endif()
 
