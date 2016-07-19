d96b7ed35bc97c0135c118617e104aa525f9da65
Igor.Bratchik <i_bratchik@wargaming.net>
2016-07-19 19:26:33 +0300

    DF-7819 coverage html report modified
    Former-commit-id: 388e42a83802e2a95ab8d94be4b669ce5bf8d686

--- Sources/CMake/Modules/PlatformSettings.cmake
+++ Sources/CMake/Modules/PlatformSettings.cmake
@@ -113,7 +113,7 @@ elseif ( WIN32 )
     add_definitions ( -DUNICODE )
 endif  ()
 
-if( APPLE AND COVERAGE )
+if( MACOS AND COVERAGE AND NOT DAVA_MEGASOLUTION )
     set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage -fprofile-instr-generate -fcoverage-mapping" ) 
     set( CMAKE_XCODE_ATTRIBUTE_GCC_INSTRUMENT_PROGRAM_FLOW_ARCS YES )
     set( CMAKE_XCODE_ATTRIBUTE_GCC_GENERATE_TEST_COVERAGE_FILES YES )
