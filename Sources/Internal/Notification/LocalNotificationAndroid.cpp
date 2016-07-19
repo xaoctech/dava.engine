09492478e560b4578452633edf37756e1c42f9ca
Andrey Derkach <a_derkach2@wargaming.net>
2016-07-19 20:47:44 +0300

    [DF-11074] JNI function declarations fix
    Former-commit-id: 547bd4bf7e43fd6d564585ae8728a237e0cd86f9

--- Sources/Internal/Notification/LocalNotificationAndroid.cpp
+++ Sources/Internal/Notification/LocalNotificationAndroid.cpp
@@ -105,7 +105,7 @@ LocalNotificationImpl* LocalNotificationImpl::Create(const String& _id)
 
 extern "C"
 {
-void Java_com_dava_framework_JNINotificationProvider_onNotificationPressed(JNIEnv* env, jobject classthis, jstring uid)
+JNIEXPORT void JNICALL Java_com_dava_framework_JNINotificationProvider_onNotificationPressed(JNIEnv* env, jobject classthis, jstring uid)
 {
     const char* str = env->GetStringUTFChars(uid, 0);
     DAVA::LocalNotificationController::Instance()->OnNotificationPressed(DAVA::String(str));
