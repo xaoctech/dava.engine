22a7729e3e2d42264993485982b58c002fdff8ab
Stanislav Madytsky <s_madytskiy@wargaming.net>
2016-07-19 17:09:00 +0300

    DF-11189 splitter fix
    Former-commit-id: 0edc56530d888c60baf910d5ca6f4fb135e22487

--- Tools/ResourceEditor/Classes/Qt/ImageSplitterDialog/ImageArea.h
+++ Tools/ResourceEditor/Classes/Qt/ImageSplitterDialog/ImageArea.h
@@ -1,5 +1,4 @@
-#ifndef __QT_IMAGE_AREA_H__
-#define __QT_IMAGE_AREA_H__
+#pragma once
 
 #include <QLabel>
 #include "DAVAEngine.h"
@@ -12,10 +11,9 @@ class ImageArea : public QLabel
 
 public:
     explicit ImageArea(QWidget* parent = 0);
-    ~ImageArea();
     void SetImage(const DAVA::FilePath& filePath);
-    void SetImage(DAVA::Image* image);
-    inline DAVA::Image* GetImage() const;
+    void SetImage(const DAVA::ScopedPtr<DAVA::Image>& image);
+    inline const DAVA::ScopedPtr<DAVA::Image>& GetImage() const;
     DAVA::Vector2 GetAcceptableSize() const;
     const DAVA::FilePath& GetImagePath() const;
 
@@ -40,16 +38,14 @@ private:
     void ConnectSignals();
     DAVA::String GetDefaultPath() const;
 
-    DAVA::Image* image;
+    DAVA::ScopedPtr<DAVA::Image> image;
     DAVA::Vector2 acceptableSize;
     DAVA::FilePath imagePath;
 
     DAVA::PixelFormat requestedFormat;
 };
 
-inline DAVA::Image* ImageArea::GetImage() const
+inline const DAVA::ScopedPtr<DAVA::Image>& ImageArea::GetImage() const
 {
     return image;
 }
-
-#endif /* defined(__QT_IMAGE_AREA_H__) */
