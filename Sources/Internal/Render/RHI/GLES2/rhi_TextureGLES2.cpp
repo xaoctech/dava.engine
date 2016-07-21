7ce32382220aabeff1b799e8d5153c66dc42e530
Sergey Reznik <s_reznik@wargaming.net>
2016-07-21 14:28:36 +0300

    [DF-11147] Fixed iOS / Mac compilation
    Former-commit-id: 338b6ad947846cb8a7368083c97b7809b4f7057c

--- Sources/Internal/Render/RHI/GLES2/rhi_TextureGLES2.cpp
+++ Sources/Internal/Render/RHI/GLES2/rhi_TextureGLES2.cpp
@@ -797,10 +797,10 @@ void SetToRHI(Handle tex, unsigned unit_i, uint32 base_i)
         GL_CALL(glTexParameteri(target, GL_TEXTURE_WRAP_T, _AddrModeGLES2(TextureAddrMode(sampler->addrV))));
 
 #if defined(GL_TEXTURE_MAX_ANISOTROPY_EXT)
-        if (_GLES2_DeviceCaps.isAnisotropicFilteringSupported)
+        if (rhi::DeviceCaps().isAnisotropicFilteringSupported)
         {
             DVASSERT(sampler->anisotropyLevel >= 1);
-            uint32 targetLevel = std::min(sampler->anisotropyLevel, _GLES2_DeviceCaps.maxAnisotropy);
+            uint32 targetLevel = std::min(sampler->anisotropyLevel, rhi::DeviceCaps().maxAnisotropy);
             GL_CALL(glTexParameteri(target, GL_TEXTURE_MAX_ANISOTROPY_EXT, targetLevel));
         }
 #endif
