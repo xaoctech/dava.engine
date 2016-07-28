10c23b33ede427e3b5810d5b4e3b10bffd9b88b6
Maksim Lazarev <m_lazarev@wargaming.net>
2016-07-28 11:00:01 +0300

    DF-9998 rename mistyped class SignalStrengthListener
    Former-commit-id: fbea084bd71b057a1d5d2aa766a538337f852e69

--- Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/SignalStrengthListener.java
+++ Sources/Internal/Platform/TemplateAndroid/Java/com/dava/framework/SignalStrengthListener.java
@@ -3,17 +3,17 @@ package com.dava.framework;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 
-public class SingalStrengthListner extends PhoneStateListener {
-	
-	private int singalStrength = 0;
-	
-	@Override
-	public void onSignalStrengthsChanged(SignalStrength signalStrength) {
-		singalStrength = signalStrength.getGsmSignalStrength();
-		super.onSignalStrengthsChanged(signalStrength);
-	}
+public class SignalStrengthListener extends PhoneStateListener {
+    
+    private int signalStrength = 0;
+    
+    @Override
+    public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+        this.signalStrength = signalStrength.getGsmSignalStrength();
+        super.onSignalStrengthsChanged(signalStrength);
+    }
 
-	public int GetSignalStrength() {
-		return singalStrength;
-	}
+    public int GetSignalStrength() {
+        return signalStrength;
+    }
 }
