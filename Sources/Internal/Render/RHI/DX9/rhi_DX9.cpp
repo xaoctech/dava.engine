f09411c40a674ee0a802d1d04252bd273f7daaf3
Igor Solovey <i_solovey@wargaming.net>
2016-08-09 14:41:40 +0300

    DF-11306 remove driver version from DX9 device description
    Former-commit-id: 192c87ce3c75d47741630e79c9577af90d39be78

--- Sources/Internal/Render/RHI/DX9/rhi_DX9.cpp
+++ Sources/Internal/Render/RHI/DX9/rhi_DX9.cpp
@@ -321,8 +321,7 @@ void _InitDX9()
         {
             if (SUCCEEDED(_D3D9->GetAdapterIdentifier(D3DADAPTER_DEFAULT, 0, &info)))
             {
-                DAVA::String deviceDesc = DAVA::Format("%s v%u.%u.%u.%u", info.Description, HIWORD(info.DriverVersion.HighPart), LOWORD(info.DriverVersion.HighPart), HIWORD(info.DriverVersion.LowPart), LOWORD(info.DriverVersion.LowPart));
-                Memcpy(_DeviceCapsDX9.deviceDescription, deviceDesc.c_str(), DAVA::Min(countof(_DeviceCapsDX9.deviceDescription), deviceDesc.length() + 1));
+                Memcpy(_DeviceCapsDX9.deviceDescription, info.Description, DAVA::Min(countof(_DeviceCapsDX9.deviceDescription), strlen(info.Description) + 1));
 
                 Logger::Info("Adapter[%u]:\n  %s \"%s\"\n", adapter, info.DeviceName, info.Description);
                 Logger::Info("  Driver %u.%u.%u.%u\n",
