436d7799d15861828f283030c940f3aeda39cc8e
klesch <victor.kleschenko@gmail.com>
2016-08-10 13:50:13 +0300

    [DF-11317] Code format only
    Former-commit-id: 63892d576c42f5b3272a052129accffe911dff67

--- Projects/UnitTests/Classes/Tests/ImageTest.cpp
+++ Projects/UnitTests/Classes/Tests/ImageTest.cpp
@@ -9,7 +9,7 @@ using namespace DAVA;
 
 DAVA_TESTCLASS (ImageTest)
 {
-    DAVA_TEST(DownscaleTest)
+    DAVA_TEST (DownscaleTest)
     {
         struct TestData
         {
@@ -22,28 +22,28 @@ DAVA_TESTCLASS (ImageTest)
 
         static Vector<TestData> tests =
         {
-            { PixelFormat::FORMAT_RGBA8888, 8, 8, true, true },
-            { PixelFormat::FORMAT_RGBA5551, 8, 8, true, true },
-            { PixelFormat::FORMAT_RGBA4444, 8, 8, false, false },
-            { PixelFormat::FORMAT_RGB888, 8, 8, true, true },
-            { PixelFormat::FORMAT_RGB565, 8, 8, false, false },
-            { PixelFormat::FORMAT_A8, 8, 8, true, true },
-            { PixelFormat::FORMAT_A16, 8, 8, false, false },
-            { PixelFormat::FORMAT_PVR4, 8, 8, false, false },
-            { PixelFormat::FORMAT_PVR2, 8, 8, false, false },
-            { PixelFormat::FORMAT_RGBA16161616, 8, 8, true, true },
-            { PixelFormat::FORMAT_RGBA32323232, 8, 8, true, false }, //sometimes this code works bad because of uint32 overflow
-            { PixelFormat::FORMAT_DXT1, 8, 8, false, false },
-            { PixelFormat::FORMAT_DXT1A, 8, 8, false, false },
-            { PixelFormat::FORMAT_DXT3, 8, 8, false, false },
-            { PixelFormat::FORMAT_DXT5, 8, 8, false, false },
-            { PixelFormat::FORMAT_DXT5NM, 8, 8, false, false },
-            { PixelFormat::FORMAT_ETC1, 8, 8, false, false },
-            { PixelFormat::FORMAT_ATC_RGB, 8, 8, false, false },
-            { PixelFormat::FORMAT_ATC_RGBA_EXPLICIT_ALPHA, 8, 8, false, false },
-            { PixelFormat::FORMAT_ATC_RGBA_INTERPOLATED_ALPHA, 8, 8, false, false },
-            { PixelFormat::FORMAT_RGBA16F, 8, 8, true, true },
-            { PixelFormat::FORMAT_RGBA32F, 8, 8, true, true }
+          { PixelFormat::FORMAT_RGBA8888, 8, 8, true, true },
+          { PixelFormat::FORMAT_RGBA5551, 8, 8, true, true },
+          { PixelFormat::FORMAT_RGBA4444, 8, 8, false, false },
+          { PixelFormat::FORMAT_RGB888, 8, 8, true, true },
+          { PixelFormat::FORMAT_RGB565, 8, 8, false, false },
+          { PixelFormat::FORMAT_A8, 8, 8, true, true },
+          { PixelFormat::FORMAT_A16, 8, 8, false, false },
+          { PixelFormat::FORMAT_PVR4, 8, 8, false, false },
+          { PixelFormat::FORMAT_PVR2, 8, 8, false, false },
+          { PixelFormat::FORMAT_RGBA16161616, 8, 8, true, true },
+          { PixelFormat::FORMAT_RGBA32323232, 8, 8, true, false }, //sometimes this code works bad because of uint32 overflow
+          { PixelFormat::FORMAT_DXT1, 8, 8, false, false },
+          { PixelFormat::FORMAT_DXT1A, 8, 8, false, false },
+          { PixelFormat::FORMAT_DXT3, 8, 8, false, false },
+          { PixelFormat::FORMAT_DXT5, 8, 8, false, false },
+          { PixelFormat::FORMAT_DXT5NM, 8, 8, false, false },
+          { PixelFormat::FORMAT_ETC1, 8, 8, false, false },
+          { PixelFormat::FORMAT_ATC_RGB, 8, 8, false, false },
+          { PixelFormat::FORMAT_ATC_RGBA_EXPLICIT_ALPHA, 8, 8, false, false },
+          { PixelFormat::FORMAT_ATC_RGBA_INTERPOLATED_ALPHA, 8, 8, false, false },
+          { PixelFormat::FORMAT_RGBA16F, 8, 8, true, true },
+          { PixelFormat::FORMAT_RGBA32F, 8, 8, true, true }
         };
 
         for (const TestData& td : tests)
@@ -63,7 +63,7 @@ DAVA_TESTCLASS (ImageTest)
                 TEST_VERIFY(destination->GetPixelFormat() == td.format);
                 TEST_VERIFY(destination->GetWidth() == (td.width / 2));
                 TEST_VERIFY(destination->GetHeight() == (td.height / 2));
-                
+
                 if (td.shouldTestData)
                 {
                     bool isDataEqual = true;
@@ -81,4 +81,3 @@ DAVA_TESTCLASS (ImageTest)
         }
     }
 };
-
