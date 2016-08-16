79731cc0b5a056f9bb93bff9a998d6620b7ef7e0
Andrey Derkach <a_derkach2@wargaming.net>
2016-08-16 16:09:57 +0300

    [DF-11342] Crypto & SSL for android (build by NDK r10e & engine support in ssl)
    Former-commit-id: 21c3cd48fc8c1e864727309dc199bb299b583920

--- Libs/openssl/includes/openssl/opensslconf.h
+++ Libs/openssl/includes/openssl/opensslconf.h
@@ -20,9 +20,6 @@ extern "C" {
 #ifndef OPENSSL_NO_EC_NISTP_64_GCC_128
 # define OPENSSL_NO_EC_NISTP_64_GCC_128
 #endif
-#ifndef OPENSSL_NO_ENGINE
-# define OPENSSL_NO_ENGINE
-#endif
 #ifndef OPENSSL_NO_GMP
 # define OPENSSL_NO_GMP
 #endif
@@ -94,9 +91,6 @@ extern "C" {
 # endif
 # if defined(OPENSSL_NO_EC_NISTP_64_GCC_128) && !defined(NO_EC_NISTP_64_GCC_128)
 #  define NO_EC_NISTP_64_GCC_128
-# endif
-# if defined(OPENSSL_NO_ENGINE) && !defined(NO_ENGINE)
-#  define NO_ENGINE
 # endif
 # if defined(OPENSSL_NO_GMP) && !defined(NO_GMP)
 #  define NO_GMP
