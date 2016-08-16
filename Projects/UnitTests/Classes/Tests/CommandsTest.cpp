e590ddae559122c100b0ec1bd3aef7a72aba04b8
klesch <victor.kleschenko@gmail.com>
2016-08-16 14:03:37 +0300

    [DF-10768] Updated UnitTest
    Former-commit-id: 41884a2d7e470520f0b4ff037561b4f62f2d46c2

--- Projects/UnitTests/Classes/Tests/CommandsTest.cpp
+++ Projects/UnitTests/Classes/Tests/CommandsTest.cpp
@@ -17,7 +17,7 @@ public:
         ++redoCounter;
     }
 
-    void Undo()
+    void Undo() override
     {
         ++undoCounter;
     }
@@ -41,6 +41,12 @@ public:
 
 DAVA_TESTCLASS (CommandsTest)
 {
+    BEGIN_CLASSES_COVERED_BY_TESTS()
+    DECLARE_COVERED_FILES("Command")
+    DECLARE_COVERED_FILES("CommandBatch")
+    DECLARE_COVERED_FILES("CommandStack")
+    END_FILES_COVERED_BY_TESTS()
+
     DAVA_TEST (SimpleTest)
     {
         std::unique_ptr<DAVA::Command> command(new TestCommand("TestCommand"));
@@ -124,6 +130,14 @@ DAVA_TESTCLASS (CommandsTest)
         TEST_VERIFY(stack.IsClean() == false);
         TEST_VERIFY(stack.CanUndo() == true);
         TEST_VERIFY(stack.CanRedo() == false);
+
+        stack.Undo();
+        stack.Exec(std::unique_ptr<DAVA::Command>(new TestCommand("TestCommand3")));
+        TEST_VERIFY(testCommand->redoCounter == 3);
+        TEST_VERIFY(testCommand->undoCounter == 2);
+        TEST_VERIFY(stack.IsClean() == false);
+        TEST_VERIFY(stack.CanUndo() == true);
+        TEST_VERIFY(stack.CanRedo() == false);
     }
 
     DAVA_TEST (CommandStackCleanTest)
@@ -133,6 +147,56 @@ DAVA_TESTCLASS (CommandsTest)
         TEST_VERIFY(stack.IsClean() == true);
         TEST_VERIFY(stack.CanUndo() == true);
         TEST_VERIFY(stack.CanRedo() == false);
+
+        stack.Exec(std::unique_ptr<DAVA::Command>(new TestCommand()));
+        TEST_VERIFY(stack.IsClean() == false);
+        TEST_VERIFY(stack.CanUndo() == true);
+        TEST_VERIFY(stack.CanRedo() == false);
+
+        stack.SetClean();
+        TEST_VERIFY(stack.IsClean() == true);
+        TEST_VERIFY(stack.CanUndo() == true);
+        TEST_VERIFY(stack.CanRedo() == false);
+
+        stack.Undo();
+        TEST_VERIFY(stack.IsClean() == false);
+        TEST_VERIFY(stack.CanUndo() == true);
+        TEST_VERIFY(stack.CanRedo() == true);
+    }
+
+    DAVA_TEST (CommandBatchSimpleTest)
+    {
+        std::unique_ptr<DAVA::CommandBatch> testBatch(new DAVA::CommandBatch("TestBatch", 2));
+        TEST_VERIFY(DAVA::IsCommandBatch(testBatch.get()) == true);
+        TEST_VERIFY(testBatch->Size() == 0);
+        TEST_VERIFY(testBatch->IsEmpty());
+
+        std::unique_ptr<DAVA::Command> batchTestCommand(new TestCommand("BatchTestCommand"));
+        TestCommand* testCommandPtr = static_cast<TestCommand*>(batchTestCommand.get());
+        TEST_VERIFY(DAVA::IsCommandBatch(testCommandPtr) == false);
+
+        std::unique_ptr<DAVA::Command> batchTestCommand2(new TestCommand("BatchTestCommand2"));
+        TestCommand* testCommand2Ptr = static_cast<TestCommand*>(batchTestCommand2.get());
+
+        testBatch->AddAndRedo(std::move(batchTestCommand));
+        testBatch->Add(std::move(batchTestCommand2));
+
+        TEST_VERIFY(testCommandPtr->redoCounter == 1);
+        TEST_VERIFY(testCommandPtr->undoCounter == 0);
+        TEST_VERIFY(testCommand2Ptr->redoCounter == 0);
+        TEST_VERIFY(testCommand2Ptr->undoCounter == 0);
+
+        testBatch->Redo();
+        TEST_VERIFY(testCommandPtr->redoCounter == 2);
+        TEST_VERIFY(testCommandPtr->undoCounter == 0);
+        TEST_VERIFY(testCommand2Ptr->redoCounter == 1);
+        TEST_VERIFY(testCommand2Ptr->undoCounter == 0);
+
+        testBatch->Undo();
+        TEST_VERIFY(testCommandPtr->redoCounter == 2);
+        TEST_VERIFY(testCommandPtr->undoCounter == 1);
+        TEST_VERIFY(testCommand2Ptr->redoCounter == 1);
+        TEST_VERIFY(testCommand2Ptr->undoCounter == 1);
     }
 
     DAVA_TEST (CommandBatchTest)
