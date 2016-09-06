bf492c281d32a93aa7d1e3a4b5ad0d204ef056c5
Ivan Radkevich <i_radkevich@wargaming.net>
2016-09-06 18:26:05 +0300

    DF-10308 finnaly fixed sw command buffers on metal
    Former-commit-id: f36e08b3c9f634baf73b74f0a14f0f207f5b8a7c

--- Sources/Internal/Core/Core.cpp
+++ Sources/Internal/Core/Core.cpp
@@ -276,7 +276,6 @@ void Core::CreateRenderer()
 {
     DVASSERT(options->IsKeyExists("renderer"));
     rhi::Api renderer = static_cast<rhi::Api>(options->GetInt32("renderer"));
-    renderer = rhi::RHI_GLES2;
     if (options->IsKeyExists("rhi_threaded_frame_count"))
     {
         rendererParams.threadedRenderEnabled = true;
