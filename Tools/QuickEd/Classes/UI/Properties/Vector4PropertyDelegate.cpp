dd0b6d384e7ac28231ad4c1c19371827fdd65776
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-09-16 17:29:12 +0300

    [DF-11591] string parser rewrited to regex
    Former-commit-id: 882662aa408026bd8c31fa78a14a258e900c9742

--- Tools/QuickEd/Classes/UI/Properties/Vector4PropertyDelegate.cpp
+++ Tools/QuickEd/Classes/UI/Properties/Vector4PropertyDelegate.cpp
@@ -53,23 +53,10 @@ bool Vector4PropertyDelegate::setModelData(QWidget* rawEditor, QAbstractItemMode
 
     QLineEdit* editor = rawEditor->findChild<QLineEdit*>("lineEdit");
 
-    DAVA::VariantType variantType;
-
-    QStringList tokens = editor->text().split(";");
-    if (tokens.length() == 1) // try to split by space
-    {
-        tokens = editor->text().split(" ", QString::SkipEmptyParts);
-    }
-
-    Vector4 val;
-    int count = Min(tokens.size(), 4);
-    for (int i = 0; i < count; i++)
-    {
-        val.data[i] = tokens[i].toFloat();
-    }
+    Vector4 vector = StringToVector<Vector4>(editor->text());
 
     QVariant variant;
-    variant.setValue<DAVA::VariantType>(VariantType(val));
+    variant.setValue<DAVA::VariantType>(VariantType(vector));
 
     return model->setData(index, variant, Qt::EditRole);
 }
--- Tools/QuickEd/Classes/Utils/QtDavaConvertion.h
+++ Tools/QuickEd/Classes/Utils/QtDavaConvertion.h
@@ -5,6 +5,9 @@
 #include "FileSystem/VariantType.h"
 #include "qmetatype.h"
 
+#include <QString>
+#include <QRegularExpression>
+
 class QString;
 class QColor;
 class QVector2D;
@@ -22,5 +25,40 @@ QString QColorToHex(const QColor& color);
 
 DAVA::Vector2 QVector2DToVector2(const QVector2D& vector);
 QVector2D Vector2ToQVector2D(const DAVA::Vector2& vector);
+
+template <typename T>
+T StringToVector(const QString& str);
+
+template <typename T>
+T StringToVector(const QString& str)
+{
+    static_assert(std::is_same<T, DAVA::Vector2>::value ||
+                  std::is_same<T, DAVA::Vector3>::value ||
+                  std::is_same<T, DAVA::Vector4>::value,
+                  "this function works only for types Vector2, Vector3 and Vector4"
+                  );
+
+    QRegularExpression expr("[\\d*]*[.]?[\\d*]+");
+    QRegularExpressionMatchIterator iter = expr.globalMatch(str);
+    QList<float> floatList;
+    while (iter.hasNext())
+    {
+        QRegularExpressionMatch match(iter.next());
+        if (match.hasMatch())
+        {
+            QString matchedStr = match.captured(0);
+            bool ok;
+            floatList << matchedStr.toFloat(&ok);
+            DVASSERT(ok);
+        }
+    }
+    T vector;
+    int count = Min(floatList.size(), static_cast<int32>(T::AXIS_COUNT));
+    for (int i = 0; i < count; i++)
+    {
+        vector.data[i] = floatList[i];
+    }
+    return vector;
+}
 
 #endif // __QTDAVACONVERTION_H__
