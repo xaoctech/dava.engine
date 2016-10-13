36f414a64f9551697bab83162a97e9eaf7e34020
Ivan Petrochenko <i_petrochenko@wargaming.net>
2016-10-13 12:08:43 +0300

    DF-11838 dynamic module
    Former-commit-id: e594b6749657960177087e1b9eb8e30b201c84f0

---
+++ Sources/Internal/ModuleManager/Private/DynamicModuleInitializer.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Base/Platform.h"
+
+#ifdef DAVA_IMPLEMENT_DYNAMIC_MODULE
+    #ifdef __DAVAENGINE_WIN32__
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+}
+    #endif
+#endif
