d8fc54a3fa1b97a35320f38ac306947afe033379
Sergey Kiryushichev <s_kiryushichev@sergeys-mbp.corp.wargaming.local>
2016-10-14 16:50:06 +0300

    DF-11700: libogg android removed
    Former-commit-id: eecc85f62f7a4e828bf572ff4d0216abaa0162c8

--- Thirdparty/libogg/build.py
+++ Thirdparty/libogg/build.py
@@ -6,7 +6,7 @@ def get_supported_targets_for_build_platform(platform):
 	if platform == 'win32':
 		return []
 	else:
-		return ['macos', 'ios', 'android']
+		return ['macos', 'ios']
 
 def get_dependencies_for_target(target):
 	return []
@@ -23,8 +23,6 @@ def build_for_target(target, working_directory_path, root_project_path):
 		return _build_macos(working_directory_path, root_project_path)
 	elif target == 'ios':
 		return _build_ios(working_directory_path, root_project_path)
-	elif target == 'android':
-		return _build_android(working_directory_path, root_project_path)
 
 def get_download_url():
 	return 'http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz'
@@ -48,13 +46,4 @@ def _build_ios(working_directory_path, root_project_path):
 	install_dir_ios = os.path.join(working_directory_path, 'gen/install_ios')
 	build_utils.build_with_autotools(source_folder_path, ['--host=armv7-apple-darwin', '--disable-shared', '--enable-static'], install_dir_ios, env=build_utils.get_autotools_ios_env())
 	
-def _build_android(working_directory_path, root_project_path):
-	source_folder_path = _download_and_extract(working_directory_path)
-
-	install_dir_android_arm = os.path.join(working_directory_path, 'gen/install_android_arm')
-	build_utils.build_with_autotools(source_folder_path, ['--host=arm-linux-androideabi', '--disable-shared', '--enable-static'], install_dir_android_arm, env=build_utils.get_autotools_android_arm_env(root_project_path, enable_stl=True))
-
-	install_dir_android_x86 = os.path.join(working_directory_path, 'gen/install_android_x86')
-	build_utils.build_with_autotools(source_folder_path, ['--host=i686-linux-android', '--disable-shared', '--enable-static'], install_dir_android_x86, env=build_utils.get_autotools_android_x86_env(root_project_path, enable_stl=True))
-
 # TODO: Add copying headers when switching to new directories structure
