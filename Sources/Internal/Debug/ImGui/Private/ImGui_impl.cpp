afc197313c74e575b7aac87f183f4cd17658f98b
Igor Solovey <i_solovey@wargaming.net>
2016-10-16 15:53:38 +0300

    DF-185 ImGui integration WIP
    Former-commit-id: 1a57b3b4cdedd857e2bf9772876e24643f67cd35

---
+++ Sources/Internal/Debug/ImGui/Private/ImGui_impl.cpp
@@ -0,0 +1,377 @@
+#include "Base/BaseTypes.h"
+#include "Render/RenderBase.h"
+#include "Render/Renderer.h"
+#include "UI/UIEvent.h"
+#include "Debug/ImGui/ImGui.h"
+#include "Render/RHI/rhi_Public.h"
+#include "Render/RHI/rhi_ShaderCache.h"
+#include "Render/RHI/rhi_ShaderSource.h"
+#include "Render/DynamicBufferAllocator.h"
+#include "Debug/ProfilerCPU.h"
+#include "Debug/ProfilerGPU.h"
+#include "Platform/SystemTimer.h"
+
+namespace ImGuiImplDetails
+{
+static bool initialized = false;
+
+static rhi::HPipelineState pipelineStatePC, pipelineStatePTC;
+static rhi::HConstBuffer constBufferPC, constBufferPTC;
+static rhi::HDepthStencilState depthState;
+static DAVA::uint32 vertexLayoutPC = 0, vertexLayoutPTC = 0;
+
+static DAVA::Size2i framebufferSize = { 0, 0 };
+
+static const char* IMGUI_RENDER_PASS_MARKER_NAME = "ImGuiRenderPass";
+
+static const char* vprogPC =
+"VPROG_IN_BEGIN\n"
+"    VPROG_IN_TEXCOORD0(2)\n"
+"    VPROG_IN_COLOR\n"
+"VPROG_IN_END\n"
+"\n"
+"VPROG_OUT_BEGIN\n"
+"    VPROG_OUT_POSITION\n"
+"    VPROG_OUT_COLOR0(color,4)\n"
+"VPROG_OUT_END\n"
+"\n"
+"property float4x4   XForm : unique,dynamic : ;\n"
+"\n"
+"VPROG_BEGIN\n"
+"\n"
+"    float3  in_position = float3(VP_IN_TEXCOORD0.x, -VP_IN_TEXCOORD0.y, 0.5);\n"
+"    float4  in_color    = VP_IN_COLOR;\n"
+"\n"
+"    VP_OUT_POSITION     = mul( XForm, float4(in_position,1.0) );\n"
+"    VP_OUT(color)       = in_color;\n"
+"\n"
+"VPROG_END\n";
+
+static const char* fprogPC =
+"FPROG_IN_BEGIN\n"
+"    FPROG_IN_COLOR0(color,4)\n"
+"FPROG_IN_END\n"
+"\n"
+"FPROG_OUT_BEGIN\n"
+"    FPROG_OUT_COLOR\n"
+"FPROG_OUT_END\n"
+"\n"
+"FPROG_BEGIN\n"
+"\n"
+"    FP_OUT_COLOR = FP_IN(color);\n"
+"\n"
+"FPROG_END\n"
+"BLEND_MODE(alpha)\n";
+
+static const char* vprogPTC =
+"VPROG_IN_BEGIN\n"
+"    VPROG_IN_TEXCOORD0(2)\n"
+"    VPROG_IN_TEXCOORD1(2)\n"
+"    VPROG_IN_COLOR\n"
+"VPROG_IN_END\n"
+"\n"
+"VPROG_OUT_BEGIN\n"
+"    VPROG_OUT_POSITION\n"
+"    VPROG_OUT_TEXCOORD0(texcoord,2)\n"
+"    VPROG_OUT_COLOR0(color,4)\n"
+"VPROG_OUT_END\n"
+"\n"
+"property float4x4   XForm : unique,dynamic : ;\n"
+"\n"
+"VPROG_BEGIN\n"
+"\n"
+"    float3  in_position = float3(VP_IN_TEXCOORD0.x, -VP_IN_TEXCOORD0.y, 0.5);\n"
+"    float2  in_texcoord = VP_IN_TEXCOORD1;\n"
+"    float4  in_color    = VP_IN_COLOR;\n"
+"\n"
+"    VP_OUT_POSITION     = mul( XForm, float4(in_position, 1.0) );\n"
+"    VP_OUT(texcoord)    = in_texcoord;\n"
+"    VP_OUT(color)       = in_color;\n"
+"\n"
+"VPROG_END\n";
+
+static const char* fprogPTC =
+"FPROG_IN_BEGIN\n"
+"    FPROG_IN_TEXCOORD0(texcoord,2)\n"
+"    FPROG_IN_COLOR0(color,4)\n"
+"FPROG_IN_END\n"
+"\n"
+"FPROG_OUT_BEGIN\n"
+"    FPROG_OUT_COLOR\n"
+"FPROG_OUT_END\n"
+"\n"
+"DECL_FP_SAMPLER2D(tex)\n"
+"\n"
+"FPROG_BEGIN\n"
+"\n"
+"    float4 sample = FP_TEXTURE2D( tex, FP_IN(texcoord) );\n"
+"\n"
+"    FP_OUT_COLOR = sample * FP_IN(color);\n"
+"\n"
+"FPROG_END\n"
+"BLEND_MODE(alpha)\n";
+
+void ImGuiDrawFn(ImDrawData* data)
+{
+    if (!data->Valid)
+        return;
+
+    rhi::RenderPassConfig passConfig;
+    passConfig.colorBuffer[0].loadAction = rhi::LOADACTION_LOAD;
+    passConfig.colorBuffer[0].storeAction = rhi::STOREACTION_STORE;
+    passConfig.depthStencilBuffer.loadAction = rhi::LOADACTION_NONE;
+    passConfig.depthStencilBuffer.storeAction = rhi::STOREACTION_NONE;
+    passConfig.priority = DAVA::PRIORITY_MAIN_2D - 10;
+    passConfig.viewport.x = 0;
+    passConfig.viewport.y = 0;
+    passConfig.viewport.width = DAVA::uint32(framebufferSize.dx);
+    passConfig.viewport.height = DAVA::uint32(framebufferSize.dy);
+    DAVA_PROFILER_GPU_RENDER_PASS(passConfig, IMGUI_RENDER_PASS_MARKER_NAME);
+
+    DAVA::Matrix4 ortho(
+    2.0f / framebufferSize.dx, 0.0f, 0.0f, -1.0f,
+    0.0f, 2.0f / framebufferSize.dy, 0.0f, 1.0f,
+    0.0f, 0.0f, -(0.0f) / (0.0f - 1.0f), 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f);
+
+    rhi::UpdateConstBuffer4fv(constBufferPC, 0, ortho.data, 4);
+    rhi::UpdateConstBuffer4fv(constBufferPTC, 0, ortho.data, 4);
+
+    rhi::HPacketList packetList;
+    rhi::HRenderPass pass = rhi::AllocateRenderPass(passConfig, 1, &packetList);
+    rhi::BeginRenderPass(pass);
+    rhi::BeginPacketList(packetList);
+
+    for (DAVA::int32 i = 0; i < data->CmdListsCount; ++i)
+    {
+        ImDrawList* cmdList = data->CmdLists[i];
+
+        DAVA::DynamicBufferAllocator::AllocResultIB ib = DAVA::DynamicBufferAllocator::AllocateIndexBuffer(cmdList->IdxBuffer.size());
+        Memcpy(ib.data, cmdList->IdxBuffer.Data, cmdList->IdxBuffer.size() * sizeof(DAVA::uint16));
+
+        DAVA::DynamicBufferAllocator::AllocResultVB vb = DAVA::DynamicBufferAllocator::AllocateVertexBuffer(sizeof(ImDrawVert), cmdList->VtxBuffer.size());
+        Memcpy(vb.data, cmdList->VtxBuffer.Data, cmdList->VtxBuffer.size() * sizeof(ImDrawVert));
+
+        rhi::Packet packet;
+        packet.vertexStreamCount = 1;
+        packet.vertexStream[0] = vb.buffer;
+        packet.vertexCount = vb.allocatedVertices;
+        packet.baseVertex = vb.baseVertex;
+        packet.startIndex = ib.baseIndex;
+        packet.indexBuffer = ib.buffer;
+        packet.cullMode = rhi::CULL_NONE;
+        packet.depthStencilState = depthState;
+        packet.vertexConstCount = 1;
+        packet.fragmentConstCount = 0;
+        packet.primitiveType = rhi::PRIMITIVE_TRIANGLELIST;
+
+        for (ImDrawCmd& cmd : cmdList->CmdBuffer)
+        {
+            if (cmd.TextureId)
+            {
+                packet.renderPipelineState = pipelineStatePTC;
+                packet.vertexConst[0] = constBufferPTC;
+
+                DAVA::Texture* texture = reinterpret_cast<DAVA::Texture*>(cmd.TextureId);
+                packet.textureSet = texture->singleTextureSet;
+                packet.samplerState = texture->samplerStateHandle;
+                packet.vertexLayoutUID = ImGuiImplDetails::vertexLayoutPTC;
+            }
+            else
+            {
+                packet.renderPipelineState = pipelineStatePC;
+                packet.vertexConst[0] = constBufferPC;
+                packet.textureSet = rhi::HTextureSet();
+                packet.samplerState = rhi::HSamplerState();
+                packet.vertexLayoutUID = ImGuiImplDetails::vertexLayoutPC;
+            }
+
+            packet.primitiveCount = cmd.ElemCount / 3;
+            packet.scissorRect.x = DAVA::uint16(cmd.ClipRect.x);
+            packet.scissorRect.y = DAVA::uint16(cmd.ClipRect.y);
+            packet.scissorRect.width = DAVA::uint16(cmd.ClipRect.z - cmd.ClipRect.x);
+            packet.scissorRect.height = DAVA::uint16(cmd.ClipRect.w - cmd.ClipRect.y);
+
+            rhi::AddPacket(packetList, packet);
+        }
+    }
+
+    rhi::EndPacketList(packetList);
+    rhi::EndRenderPass(pass);
+}
+
+} //ns Details
+
+namespace ImGui
+{
+using DAVA::uint8;
+using DAVA::int32;
+using DAVA::Key;
+using DAVA::UIEvent;
+using DAVA::float32;
+
+void EnsureInited()
+{
+    if (!ImGuiImplDetails::initialized)
+    {
+        ImGuiImplDetails::framebufferSize.dx = int32(DAVA::Renderer::GetFramebufferWidth());
+        ImGuiImplDetails::framebufferSize.dy = int32(DAVA::Renderer::GetFramebufferHeight());
+
+        ImGuiIO& io = ImGui::GetIO();
+        io.RenderDrawListsFn = ImGuiImplDetails::ImGuiDrawFn;
+        ImGui::GetIO().DisplaySize.x = float32(ImGuiImplDetails::framebufferSize.dx);
+        ImGui::GetIO().DisplaySize.y = float32(ImGuiImplDetails::framebufferSize.dy);
+
+        io.IniFilename = nullptr;
+        io.LogFilename = nullptr;
+
+        io.KeyMap[ImGuiKey_Tab] = int32(Key::TAB);
+        io.KeyMap[ImGuiKey_LeftArrow] = int32(Key::LEFT);
+        io.KeyMap[ImGuiKey_RightArrow] = int32(Key::RIGHT);
+        io.KeyMap[ImGuiKey_UpArrow] = int32(Key::UP);
+        io.KeyMap[ImGuiKey_DownArrow] = int32(Key::DOWN);
+        io.KeyMap[ImGuiKey_PageUp] = int32(Key::PGUP);
+        io.KeyMap[ImGuiKey_PageDown] = int32(Key::PGDN);
+        io.KeyMap[ImGuiKey_Home] = int32(Key::HOME);
+        io.KeyMap[ImGuiKey_End] = int32(Key::END);
+        io.KeyMap[ImGuiKey_Delete] = int32(Key::DELETE);
+        io.KeyMap[ImGuiKey_Backspace] = int32(Key::BACKSPACE);
+        io.KeyMap[ImGuiKey_Enter] = int32(Key::ENTER);
+        io.KeyMap[ImGuiKey_Escape] = int32(Key::ESCAPE);
+        io.KeyMap[ImGuiKey_A] = int32(Key::KEY_A);
+        io.KeyMap[ImGuiKey_C] = int32(Key::KEY_C);
+        io.KeyMap[ImGuiKey_V] = int32(Key::KEY_V);
+        io.KeyMap[ImGuiKey_X] = int32(Key::KEY_X);
+        io.KeyMap[ImGuiKey_Y] = int32(Key::KEY_Y);
+        io.KeyMap[ImGuiKey_Z] = int32(Key::KEY_Z);
+
+        //vertex layouts
+        rhi::VertexLayout vLayout;
+        vLayout.AddElement(rhi::VS_TEXCOORD, 0, rhi::VDT_FLOAT, 2);
+        vLayout.AddElement(rhi::VS_COLOR, 0, rhi::VDT_UINT8N, 4);
+        ImGuiImplDetails::vertexLayoutPC = rhi::VertexLayout::UniqueId(vLayout);
+
+        vLayout.Clear();
+        vLayout.AddElement(rhi::VS_TEXCOORD, 0, rhi::VDT_FLOAT, 2);
+        vLayout.AddElement(rhi::VS_TEXCOORD, 1, rhi::VDT_FLOAT, 2);
+        vLayout.AddElement(rhi::VS_COLOR, 0, rhi::VDT_UINT8N, 4);
+        ImGuiImplDetails::vertexLayoutPTC = rhi::VertexLayout::UniqueId(vLayout);
+
+        //font texture
+        uint8* pixels;
+        int32 width, height, bytes_per_pixel;
+        io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height, &bytes_per_pixel);
+        DAVA::Texture* fontTexture = DAVA::Texture::CreateFromData(DAVA::FORMAT_RGBA8888, pixels, width, height, true);
+        fontTexture->SetWrapMode(rhi::TEXADDR_WRAP, rhi::TEXADDR_WRAP, rhi::TEXADDR_WRAP);
+        io.Fonts->TexID = fontTexture;
+
+        //depth state
+        rhi::DepthStencilState::Descriptor ds_desc;
+        ds_desc.depthTestEnabled = false;
+        ds_desc.depthWriteEnabled = false;
+        ImGuiImplDetails::depthState = rhi::AcquireDepthStencilState(ds_desc);
+
+        //pc pipeline state
+        rhi::ShaderSource vp_pc;
+        rhi::ShaderSource fp_pc;
+
+        if (vp_pc.Construct(rhi::PROG_VERTEX, ImGuiImplDetails::vprogPC) && fp_pc.Construct(rhi::PROG_FRAGMENT, ImGuiImplDetails::fprogPC))
+        {
+            rhi::PipelineState::Descriptor ps_desc;
+
+            ps_desc.vertexLayout = vp_pc.ShaderVertexLayout();
+            ps_desc.vprogUid = DAVA::FastName("imgui.vp.pc");
+            ps_desc.fprogUid = DAVA::FastName("imgui.fp.pc");
+            ps_desc.blending = fp_pc.Blending();
+
+            rhi::ShaderCache::UpdateProg(rhi::HostApi(), rhi::PROG_VERTEX, ps_desc.vprogUid, vp_pc.SourceCode());
+            rhi::ShaderCache::UpdateProg(rhi::HostApi(), rhi::PROG_FRAGMENT, ps_desc.fprogUid, fp_pc.SourceCode());
+
+            ImGuiImplDetails::pipelineStatePC = rhi::AcquireRenderPipelineState(ps_desc);
+            rhi::CreateVertexConstBuffers(ImGuiImplDetails::pipelineStatePC, 1, &ImGuiImplDetails::constBufferPC);
+        }
+
+        //ptc pipeline-state
+        rhi::ShaderSource vp_ptc;
+        rhi::ShaderSource fp_ptc;
+
+        if (vp_ptc.Construct(rhi::PROG_VERTEX, ImGuiImplDetails::vprogPTC) && fp_ptc.Construct(rhi::PROG_FRAGMENT, ImGuiImplDetails::fprogPTC))
+        {
+            rhi::PipelineState::Descriptor ps_desc;
+
+            ps_desc.vertexLayout = vp_ptc.ShaderVertexLayout();
+            ps_desc.vprogUid = DAVA::FastName("imgui.vp.ptc");
+            ps_desc.fprogUid = DAVA::FastName("imgui.fp.ptc");
+            ps_desc.blending = fp_ptc.Blending();
+
+            rhi::ShaderCache::UpdateProg(rhi::HostApi(), rhi::PROG_VERTEX, ps_desc.vprogUid, vp_ptc.SourceCode());
+            rhi::ShaderCache::UpdateProg(rhi::HostApi(), rhi::PROG_FRAGMENT, ps_desc.fprogUid, fp_ptc.SourceCode());
+
+            ImGuiImplDetails::pipelineStatePTC = rhi::AcquireRenderPipelineState(ps_desc);
+            rhi::CreateVertexConstBuffers(ImGuiImplDetails::pipelineStatePTC, 1, &ImGuiImplDetails::constBufferPTC);
+        }
+
+        ImGuiImplDetails::initialized = true;
+    }
+}
+
+void BeginFrame()
+{
+    ImGui::GetIO().DeltaTime = DAVA::SystemTimer::Instance()->FrameDelta();
+}
+
+void OnInput(UIEvent* input)
+{
+    ImGuiIO& io = ImGui::GetIO();
+
+    switch (input->phase)
+    {
+    case UIEvent::Phase::MOVE:
+    case UIEvent::Phase::DRAG:
+        io.MousePos.x = input->physPoint.x;
+        io.MousePos.y = input->physPoint.y;
+        break;
+
+    case UIEvent::Phase::WHEEL:
+        io.MouseWheel = input->wheelDelta.x;
+        break;
+
+    case UIEvent::Phase::KEY_DOWN:
+    case UIEvent::Phase::KEY_UP:
+
+        io.KeysDown[int32(input->key)] = (input->phase == UIEvent::Phase::KEY_DOWN);
+
+        if (input->key == Key::LCTRL || input->key == Key::RCTRL)
+            io.KeyCtrl = (input->phase == UIEvent::Phase::KEY_DOWN);
+
+        if (input->key == Key::LALT || input->key == Key::RALT)
+            io.KeyAlt = (input->phase == UIEvent::Phase::KEY_DOWN);
+
+        if (input->key == Key::LCMD || input->key == Key::RCMD)
+            io.KeySuper = (input->phase == UIEvent::Phase::KEY_DOWN);
+
+        break;
+
+    case UIEvent::Phase::BEGAN:
+    case UIEvent::Phase::ENDED:
+        io.MouseDown[int32(input->mouseButton) - 1] = (input->phase == UIEvent::Phase::BEGAN);
+        break;
+
+    case UIEvent::Phase::CHAR:
+        io.AddInputCharacter(input->keyChar);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Uninitialize()
+{
+    DAVA::Texture* fontTexture = reinterpret_cast<DAVA::Texture*>(ImGui::GetIO().Fonts->TexID);
+    DAVA::SafeRelease(fontTexture);
+
+    ImGui::Shutdown();
+}
+
+} //ns ImGui
