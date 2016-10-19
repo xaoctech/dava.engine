8409df7f182d89cb7a8f9a5d41b37318d4a7a0d9
Sergey Reznik <s_reznik@wargaming.net>
2016-10-19 16:13:35 +0300

    [DF-11520] Fixed potential error
    Former-commit-id: 15934390ecd4cd04ee8bd66c57f362c37679f7db

--- Sources/Internal/Render/RHI/DX11/_dx11.h
+++ Sources/Internal/Render/RHI/DX11/_dx11.h
@@ -53,7 +53,7 @@
     { \
         DAVA::Logger::Error("DX11 Device is not ready, therefor call %s is not possible\nat %s [%u]", #F, __FILE__, __LINE__);\
         _D3D11_DeviceLock.Unlock(); \
-        do { DAVA::Thread::Sleep(1); } while (_D3D11_Device == nullptr); \
+        do { DAVA::Thread::Sleep(1); } while (true); \
     } \
 }
 
