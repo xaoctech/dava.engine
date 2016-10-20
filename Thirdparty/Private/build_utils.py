29d44183c2493e29cc12685bd525ef616a9c2db1
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-10-20 17:19:33 +0300

    DF-11700: fixed downloading sources from github
    Former-commit-id: feb4d3157e523ff5a44aa1652691a09b8ed1d46e

--- Thirdparty/Private/build_utils.py
+++ Thirdparty/Private/build_utils.py
@@ -22,7 +22,15 @@ def download(url, file_name):
     u = urllib2.urlopen(url)
     f = open(file_name, 'wb')
     meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
+    print meta
+    print meta.getheaders("Content-Length")
+
+    content_length = meta.getheaders("Content-Length")
+
+    if content_length:
+        file_size = int(content_length[0])
+    else:
+        file_size = 0
 
     print "Downloading %s (%s bytes) from %s ..." % (file_name, file_size, url)
 
@@ -35,9 +43,11 @@ def download(url, file_name):
 
         file_size_dl += len(buffer)
         f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
+
+        if file_size > 0:
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8)*(len(status)+1)
+            print status,
 
     f.close()
 
