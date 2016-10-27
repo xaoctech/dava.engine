481303dc065e85561b1f1b694a5f587e7fc1383b
Andrey Derkach <a_derkach2@wargaming.net>
2016-10-27 19:51:54 +0300

    [DF-11929] Doxygen comment
    Former-commit-id: 12a58f965c5d3bdfa79a4d520687ea6810c0b9d2

--- Sources/Internal/Platform/Steam.h
+++ Sources/Internal/Platform/Steam.h
@@ -1,5 +1,4 @@
-#ifndef STEAM_H
-#define STEAM_H
+#pragma once
 
 #if defined(__DAVAENGINE_STEAM__)
 #include "Base/BaseTypes.h"
@@ -19,7 +18,11 @@ public:
     static bool IsInited();
     static void Update();
 
-    // Get code of language, ex "ru" for "russian"
+    /** 
+    Return language code from Steam (for example: "ru" for russian, "en" for english).
+    Try to return set language for app. 
+    If fails, return language of Steam app or empty string if language is unsupported.
+    */
     static String GetSteamLanguage();
 
     static ISteamRemoteStorage* CreateStorage();
@@ -28,5 +31,4 @@ private:
     static bool isInited;
 };
 }
-#endif
 #endif
