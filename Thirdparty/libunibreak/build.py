b9cd9acd8aae6326fbb030cfda2feedecb27538b
Mikhail Polubisok <m_polubisok@wargaming.net>
2016-11-03 17:54:38 +0300

    DF-11905 Update libunibreak
    Former-commit-id: e9c7784c47d40a9b65db6c0c20b4889fdbdd373f

--- Thirdparty/libunibreak/build.py
+++ Thirdparty/libunibreak/build.py
@@ -91,11 +91,14 @@ def _build_macos(working_directory_path, root_project_path):
     install_dir_macos = os.path.join(
         working_directory_path, 'gen/install_macos')
     build_utils.run_process(['autoreconf', '-i'], process_cwd=source_folder_path)
+    env=build_utils.get_autotools_macos_env()
+    env['CFLAGS'] += ' -DNDEBUG'
+    env['CXXFLAGS'] = env['CFLAGS']
     build_utils.build_with_autotools(
         source_folder_path,
         ['--host=x86_64-apple-darwin', '--disable-shared', '--enable-static'],
         install_dir_macos,
-        env=build_utils.get_autotools_macos_env())
+        env=env)
 
     libunibreak_lib_path = os.path.join(install_dir_macos, 'lib/libunibreak.a')
     shutil.copyfile(
@@ -113,11 +116,14 @@ def _build_ios(working_directory_path, root_project_path):
 
     install_dir_ios = os.path.join(working_directory_path, 'gen/install_ios')
     build_utils.run_process(['autoreconf', '-i'], process_cwd=source_folder_path)
+    env=build_utils.get_autotools_ios_env()
+    env['CFLAGS'] += ' -DNDEBUG'
+    env['CXXFLAGS'] = env['CFLAGS']
     build_utils.build_with_autotools(
         source_folder_path,
         ['--host=armv7-apple-darwin', '--disable-shared', '--enable-static'],
         install_dir_ios,
-        env=build_utils.get_autotools_ios_env())
+        env=env)
 
     libunibreak_lib_path = os.path.join(install_dir_ios, 'lib/libunibreak.a')
     shutil.copyfile(
@@ -136,13 +142,16 @@ def _build_android(working_directory_path, root_project_path):
     install_dir_android_arm = os.path.join(
         working_directory_path, 'gen/install_android_arm')
     build_utils.run_process(['autoreconf', '-i'], process_cwd=source_folder_path)
+    env=build_utils.get_autotools_android_arm_env(root_project_path)
+    env['CFLAGS'] += ' -DNDEBUG'
+    env['CXXFLAGS'] = env['CFLAGS']
     build_utils.build_with_autotools(
         source_folder_path,
         ['--host=arm-linux-androideabi',
          '--disable-shared',
          '--enable-static'],
         install_dir_android_arm,
-        env=build_utils.get_autotools_android_arm_env(root_project_path))
+        env=env)
 
     install_dir_android_x86 = os.path.join(
         working_directory_path, 'gen/install_android_x86')
