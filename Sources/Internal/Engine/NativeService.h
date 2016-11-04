df5789b6ba9938891d4879b43c7d6b042e0f429d
Sergey Zdanevich <s_zdanevich@wargaming.net>
2016-11-04 13:29:17 +0300

    [DF-11781] Reverting back accasionally modified file
    Former-commit-id: 43db6eb50a1f78357081e450724260cb82f79319

--- Sources/Internal/Engine/NativeService.h
+++ Sources/Internal/Engine/NativeService.h
@@ -1,112 +1,21 @@
 #pragma once
 
-#if defined(__DAVAENGINE_COREV2__)
-#include "Engine/Private/EnginePrivateFwd.h"
-
-namespace DAVA
-{
-// Select right header for NativeService class depending on platfrom
+// Select right header for NativeService class depending on platform
 #if defined(__DAVAENGINE_QT__)
-
-class QApplication;
-class RenderWidget;
-class NativeService final
-{
-public:
-    QApplication* GetApplication();
-    RenderWidget* GetRenderWidget();
-
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/Qt/NativeServiceQt.h"
 #elif defined(__DAVAENGINE_WIN32__)
-
-class NativeService final
-{
-public:
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/Win32/NativeServiceWin32.h"
 #elif defined(__DAVAENGINE_WIN_UAP__)
-
-class NativeService final
-{
-public:
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/UWP/NativeServiceUWP.h"
 #elif defined(__DAVAENGINE_MACOS__)
-
-class NativeService final
-{
-public:
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/OsX/NativeServiceOsX.h"
 #elif defined(__DAVAENGINE_IPHONE__)
-
-class NativeService final
-{
-public:
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/iOS/NativeServiceiOS.h"
 #elif defined(__DAVAENGINE_ANDROID__)
-
-class NativeService final
-{
-public:
-private:
-    NativeService(Private::PlatformCore* c);
-
-private:
-    Private::PlatformCore* core = nullptr;
-
-    // Friends
-    friend Private::PlatformCore;
-};
-
+#include "Engine/Android/NativeServiceAndroid.h"
 #else
 #if defined(__DAVAENGINE_COREV2__)
 // Do not emit error when building with old core implementation
 #error "NativeService is not implemented"
 #endif
-#endif
-}
-
 #endif
