99fd430b1da90d64b66340cd4917daac37935048
Ivan Radkevich <i_radkevich@wargaming.net>
2016-11-11 17:28:28 +0300

    DF-11754 comment fix )
    Former-commit-id: db43f75f62d534d38504a5f425fe32df838f585c

--- Sources/Internal/Render/RHI/Metal/rhi_Metal.mm
+++ Sources/Internal/Render/RHI/Metal/rhi_Metal.mm
@@ -188,7 +188,7 @@ void metal_Initialize(const InitParam& param)
     int ringBufferSize = 4 * 1024 * 1024;
     if (param.shaderConstRingBufferSize)
         ringBufferSize = param.shaderConstRingBufferSize;
-    ConstBufferMetal::InitializeRingBuffer(ringBufferSize * 2); //TODO: 2 is for release 3.1 only, in 3.2 we will decrease this in game configuration and set corresponding multiplier here (supposed 3)
+    ConstBufferMetal::InitializeRingBuffer(ringBufferSize * 2); //TODO: 2 is for release 3.1 only, in 3.2 we will decrease this in game configuration and set corresponding multiplier here (supposed 3) (now supposed 4 as metal now can work in render thread as well)
 
     stat_DIP = StatSet::AddStat("rhi'dip", "dip");
     stat_DP = StatSet::AddStat("rhi'dp", "dp");
