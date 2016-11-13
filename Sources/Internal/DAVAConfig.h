b84e79e9d63f3994553a0a0837ae93aa95dbe1d3
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-11-13 16:30:26 +0300

    DF-11489: changed DAVAConfig.h to match previous default behaviours
    Former-commit-id: 493663512e34812f59e18e21d6af05fc2570688b

--- Sources/Internal/DAVAConfig.h
+++ Sources/Internal/DAVAConfig.h
@@ -17,9 +17,9 @@
 // Enable memory profiling - should be defined for non-cmake projects
 //#define DAVA_MEMORY_PROFILING_ENABLE
 
-//#define ENABLE_BASE_OBJECT_CHECKS // separate thing to check if you release BaseObjects properly. Need to be disabled for release configurations
+//#define ENABLE_BASE_OBJECT_CHECKS // separate thing to check if you release BaseObjects properly. Needs to be disabled for release configurations
 
-//#define ENABLE_CONTROL_EDIT //allows to drug'n'drop controls for position editing
+//#define ENABLE_CONTROL_EDIT //allows to drag'n'drop controls for position editing
 
 //#define SHOW_FRAME_TIME	// shows milliseconds per fame
 
@@ -29,7 +29,7 @@
 //#define __DAVAENGINE_DIRECTX9__
 #define __DAVAENGINE_OPENGL__
 
-// This flag allow to enable profiling stats
+// This flag allows to enable profiling stats
 //#define __DAVAENGINE_ENABLE_DEBUG_STATS__
 #if defined(__DAVAENGINE_ENABLE_DEBUG_STATS__)
 //    #define __DAVAENGINE_ENABLE_FRAMEWORK_STATS__
@@ -41,8 +41,13 @@
 
 #if defined(__DAVAENGINE_DEBUG__)
 //#define DAVA_ENGINE_DEBUG_FPU_EXCEPTIONS // throw std::runtime_error on div_by_zero, overflow, underflow
+#define ENABLE_ASSERT_LOGGING
+#define ENABLE_ASSERT_MESSAGE
+#define ENABLE_ASSERT_BREAK
 #else
-// #define __DAVAENGINE_ENABLE_ASSERTS__ // uncomment if DVASSERT macro should not be stripped in release mode
+#define __DAVAENGINE_ENABLE_ASSERTS__ // comment if DVASSERT macro should be stripped in release mode
+#define ENABLE_ASSERT_LOGGING
+#define ENABLE_ASSERT_MESSAGE
 #endif
 
 #define USE_FILEPATH_IN_MAP
@@ -52,7 +57,7 @@
 #define FILEPATH_MAP_KEY(key) key.GetAbsolutePathname()
 #endif //#ifdef USE_FILEPATH_IN_MAP
 
-//Uncomment this define to using C++11 concurrency instead native
+//Uncomment this define to use C++11 concurrency instead of native
 //#define USE_CPP11_CONCURRENCY
 
 #endif // __DAVAENGINE_CONFIG_H__
