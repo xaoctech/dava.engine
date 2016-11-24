bf4508fe889717d1d02676cb37ff98647f0709e7
a_shpakovskiy <a_shpakovskiy@wargaming.net>
2016-11-24 17:10:24 +0300

    DF-12147 review notes fix: add warning for null type
    Former-commit-id: 4a5d15f07a3df250a5a3183072aeff0b1549e584

--- Sources/Internal/Database/MongodbClient.cpp
+++ Sources/Internal/Database/MongodbClient.cpp
@@ -476,6 +476,12 @@ void MongodbClient::ReadData(KeyedArchive* archive, void* bsonObj)
             break;
         }
 
+        case BSON_NULL:
+        {
+            Logger::Warning("[MongodbClient::ReadData] Get NULL type by key: %s", key.c_str());
+            break;
+        }
+
         case BSON_OID:
             //TODO: add 12-bytes array
             //bson_append_oid(object, key, bson_iterator_oid(&it));
@@ -483,7 +489,7 @@ void MongodbClient::ReadData(KeyedArchive* archive, void* bsonObj)
 
         default:
             Logger::Error("[MongodbClient::ReadData] Not implemented type: %d", type);
-            //DVASSERT(false);
+            DVASSERT(false);
             break;
         }
     }
