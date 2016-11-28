bd97d0130ec61bd43c2ce1d1eccc438df48e4eb5
m_kiselev <m_kiselev@wargaming.net>
2016-11-28 17:22:27 +0300

    [DF-11951] delete hack for fullscreen, change PreferredLaunchWindowingMode = Auto
    Former-commit-id: cbd2c5400c3b27fbb0071f9a93a871187782014c

--- Sources/Internal/Platform/TemplateWin32/WinUAPXamlApp.cpp
+++ Sources/Internal/Platform/TemplateWin32/WinUAPXamlApp.cpp
@@ -1026,15 +1026,11 @@ void WinUAPXamlApp::PrepareScreenSize()
     }
     if (!isPhoneApiDetected)
     {
+        ApplicationView::PreferredLaunchWindowingMode = ApplicationViewWindowingMode::Auto;
         if (!isFull)
         {
             // in units of effective (view) pixels
             ApplicationView::GetForCurrentView()->PreferredLaunchViewSize = Size(static_cast<float32>(windowedMode.width), static_cast<float32>(windowedMode.height));
-            ApplicationView::PreferredLaunchWindowingMode = ApplicationViewWindowingMode::PreferredLaunchViewSize;
-        }
-        else
-        {
-            ApplicationView::PreferredLaunchWindowingMode = ApplicationViewWindowingMode::FullScreen;
         }
         SetFullScreen(isFull);
     }
