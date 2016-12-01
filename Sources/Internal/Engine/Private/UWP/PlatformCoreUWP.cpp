ce42c16dce4541c39e0d25225628e335a55d3927
Maksim Lazarev <m_lazarev@wargaming.net>
2016-12-01 10:15:20 +0300

    DF-11514 win10: enable DeviceInfo to watch input device addition/removal
    Former-commit-id: 92c0efe12acf90125cd88f50edd89c2e19fd08ad

--- Sources/Internal/Engine/Private/UWP/PlatformCoreUWP.cpp
+++ Sources/Internal/Engine/Private/UWP/PlatformCoreUWP.cpp
@@ -10,12 +10,12 @@
 #include "Engine/Private/UWP/Window/WindowBackendUWP.h"
 #include "Engine/Private/UWP/DllImportUWP.h"
 
-#include "Debug/Backtrace.h"
-#include "Platform/SystemTimer.h"
 #include "Concurrency/Thread.h"
+#include "Debug/Backtrace.h"
 #include "Logger/Logger.h"
-#include "Utils/Utils.h"
 #include "Platform/DeviceInfo.h"
+#include "Platform/SystemTimer.h"
+#include "Utils/Utils.h"
 
 extern int DAVAMain(DAVA::Vector<DAVA::String> cmdline);
 
@@ -85,12 +85,16 @@ void PlatformCore::OnLaunchedOrActivated(::Windows::ApplicationModel::Activation
 {
     using namespace ::Windows::ApplicationModel::Activation;
 
+    // Force DeviceInfo instantiation for early initialization (due to static nature of DeviceInfo)
+    Logger::FrameworkDebug("%s", DeviceInfo::GetPlatformString().c_str());
+
     ApplicationExecutionState prevExecState = args->PreviousExecutionState;
     if (prevExecState != ApplicationExecutionState::Running && prevExecState != ApplicationExecutionState::Suspended)
     {
         Thread* gameThread = Thread::Create(MakeFunction(this, &PlatformCore::GameThread));
         gameThread->Start();
         gameThread->BindToProcessor(0);
+        gameThread->SetPriority(Thread::PRIORITY_HIGH);
         // TODO: make Thread detachable
         //gameThread->Detach();
         //gameThread->Release();
