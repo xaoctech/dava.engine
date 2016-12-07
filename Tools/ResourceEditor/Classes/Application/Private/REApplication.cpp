65e1d8b7889dbedf1d134498fcb6a6837431dc53
Sergey Reznik <s_reznik@wargaming.net>
2016-12-07 17:39:57 +0300

    RE feature: create camera by duplicating current one
    Former-commit-id: b88619ba5489e932facc3e80bd7421884c8c81b3

--- Tools/ResourceEditor/Classes/Application/Private/REApplication.cpp
+++ Tools/ResourceEditor/Classes/Application/Private/REApplication.cpp
@@ -4,6 +4,7 @@
 #include "Classes/Application/LaunchModule.h"
 #include "Classes/Project/ProjectManagerModule.h"
 #include "Classes/SceneManager/SceneManagerModule.h"
+#include "Classes/SceneManager/EntityProducerModule.h"
 
 #include "TextureCompression/PVRConverter.h"
 #include "Settings/SettingsManager.h"
@@ -171,6 +172,7 @@ void REApplication::CreateGUIModules(DAVA::TArc::Core* tarcCore) const
     tarcCore->CreateModule<REModule>();
     tarcCore->CreateModule<ProjectManagerModule>();
     tarcCore->CreateModule<SceneManagerModule>();
+    tarcCore->CreateModule<EntityProducerModule>();
     tarcCore->CreateModule<LaunchModule>();
 }
 
