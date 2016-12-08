be8370452cda3e9007b4dae7235072e5a3fafbd4
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-12-08 09:37:32 +0300

    DF-12166: fixed missing import in java
    Former-commit-id: a72c930c5a5d72256d10bc1e79b7292e41846b97

--- Sources/Internal/Engine/Private/Android/Java/com/dava/engine/DavaSurfaceView.java
+++ Sources/Internal/Engine/Private/Android/Java/com/dava/engine/DavaSurfaceView.java
@@ -1,6 +1,7 @@
 package com.dava.engine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
