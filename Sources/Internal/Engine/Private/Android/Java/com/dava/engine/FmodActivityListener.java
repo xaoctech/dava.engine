deadb477f7f49f5706ad6e6e9462e90730ead96c
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-12-09 13:58:34 +0300

    DF-12166: switched to spaces for new java files
    Former-commit-id: 9987245b9af1ac22d5767c37da33d3f1d7d065c7

--- Sources/Internal/Engine/Private/Android/Java/com/dava/engine/FmodActivityListener.java
+++ Sources/Internal/Engine/Private/Android/Java/com/dava/engine/FmodActivityListener.java
@@ -4,50 +4,50 @@ import org.fmod.FMODAudioDevice;
 
 public class FmodActivityListener extends DavaActivity.ActivityListenerImpl
 {
-	private FMODAudioDevice fmodDevice;
-	
-	public FmodActivityListener()
-	{
-		final FmodActivityListener instance = this;
-		DavaActivity.instance().runOnUiThread(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				fmodDevice = new FMODAudioDevice();
-				
-				DavaActivity activity = DavaActivity.instance();
-				
-				activity.registerActivityListener(instance);
-				
-				// Handle a case when FmodActivityListener is being created while activity is already visible
-				// We need to start fmod device in this case since onStart won't be called
-				if (activity.isVisible())
-				{
-					fmodDevice.start();
-				}
-			}
-		});
-	}
-	
-	public void unregister()
-	{
-		DavaActivity activity = DavaActivity.instance();
-		if (activity != null)
-		{
-			activity.unregisterActivityListener(this);
-		}
-	}
-	
-	@Override
-	public void onStart()
-	{	
-		fmodDevice.start();
-	}
-	
-	@Override
-	public void onStop()
-	{
-		fmodDevice.stop();
-	}
+    private FMODAudioDevice fmodDevice;
+    
+    public FmodActivityListener()
+    {
+        final FmodActivityListener instance = this;
+        DavaActivity.instance().runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                fmodDevice = new FMODAudioDevice();
+                
+                DavaActivity activity = DavaActivity.instance();
+                
+                activity.registerActivityListener(instance);
+                
+                // Handle a case when FmodActivityListener is being created while activity is already visible
+                // We need to start fmod device in this case since onStart won't be called
+                if (activity.isVisible())
+                {
+                    fmodDevice.start();
+                }
+            }
+        });
+    }
+    
+    public void unregister()
+    {
+        DavaActivity activity = DavaActivity.instance();
+        if (activity != null)
+        {
+            activity.unregisterActivityListener(this);
+        }
+    }
+    
+    @Override
+    public void onStart()
+    {   
+        fmodDevice.start();
+    }
+    
+    @Override
+    public void onStop()
+    {
+        fmodDevice.stop();
+    }
 }
--- Sources/Internal/Engine/Private/Android/Java/com/dava/engine/Utils.java
+++ Sources/Internal/Engine/Private/Android/Java/com/dava/engine/Utils.java
@@ -11,65 +11,65 @@ import android.content.ActivityNotFoundException;
 
 public class Utils
 {
-	private Utils()
-	{
-		
-	}
-	
-	public static void disableSleepTimer()
-	{
-		final Activity activity = DavaActivity.instance();
-		activity.runOnUiThread(new Runnable()
-		{
-			@Override
-			public void run() 
-			{
-				DavaActivity.instance().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-			}
-		});
-	}
-	
-	public static void enableSleepTimer()
-	{
-		final Activity activity = DavaActivity.instance();
-		activity.runOnUiThread(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				DavaActivity.instance().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-			}
-		});
-	}
+    private Utils()
+    {
+        
+    }
+    
+    public static void disableSleepTimer()
+    {
+        final Activity activity = DavaActivity.instance();
+        activity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run() 
+            {
+                DavaActivity.instance().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
+    }
+    
+    public static void enableSleepTimer()
+    {
+        final Activity activity = DavaActivity.instance();
+        activity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                DavaActivity.instance().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
+    }
 
-	public static void openURL(final String url)
-	{
-		final Activity activity = DavaActivity.instance();
-		activity.runOnUiThread(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				try
-				{
-					final DavaActivity activity = DavaActivity.instance();
-					if (null == activity || activity.isPaused())
-					{
-						return;
-					}
+    public static void openURL(final String url)
+    {
+        final Activity activity = DavaActivity.instance();
+        activity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    final DavaActivity activity = DavaActivity.instance();
+                    if (null == activity || activity.isPaused())
+                    {
+                        return;
+                    }
 
-					Log.i(DavaActivity.LOG_TAG, "[OpenURL] " + url);
+                    Log.i(DavaActivity.LOG_TAG, "[OpenURL] " + url);
 
-					Intent exWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-					activity.startActivity(exWeb);
-				}
-				catch(ActivityNotFoundException e)
-				{
-					Log.e(DavaActivity.LOG_TAG, "[OpenURL] failed with exeption: " + e.toString());
-				}
-			}
-		});
-	}
+                    Intent exWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    activity.startActivity(exWeb);
+                }
+                catch(ActivityNotFoundException e)
+                {
+                    Log.e(DavaActivity.LOG_TAG, "[OpenURL] failed with exeption: " + e.toString());
+                }
+            }
+        });
+    }
 
     public static String generateGUID()
     {
