cd0fed9360a9f9315bd558535436691951348b62
Mikhail Polubisok <m_polubisok@wargaming.net>
2016-12-12 16:04:54 +0300

    DF-12091 Fix StaticText unit test
    Former-commit-id: 46cf429e96ca66f397f2f9005b695f9f7b631e4f

--- Projects/UnitTests/Classes/Tests/StaticTextTest.cpp
+++ Projects/UnitTests/Classes/Tests/StaticTextTest.cpp
@@ -2,6 +2,7 @@
 #include "UnitTests/UnitTests.h"
 
 #include "Utils/StringUtils.h"
+#include "Utils/UTF8Utils.h"
 
 using namespace DAVA;
 
@@ -12,16 +13,17 @@ static struct FittingTestInfo
     int32 align;
     WideString result;
 } testData[] = {
-    { 0, ALIGN_LEFT, L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTIN" },
-    { 0, ALIGN_HCENTER, L"DAVA CONSULTING, LLC AND CONTRIBUTORS AS IS AND" },
-    { 0, ALIGN_RIGHT, L"ORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES" },
-    { TextBlock::FITTING_POINTS, ALIGN_LEFT, L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTI..." },
-    { TextBlock::FITTING_POINTS, ALIGN_HCENTER, L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTI..." },
-    { TextBlock::FITTING_POINTS, ALIGN_RIGHT, L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTI..." },
+    { 0, ALIGN_LEFT, L"THIS SOFTWARE" },
+    { 0, ALIGN_HCENTER, L"LLC AND" },
+    { 0, ALIGN_RIGHT, L"IMPLIED WARRANTIES" },
+    { TextBlock::FITTING_POINTS, ALIGN_LEFT, L"..." },
+    { TextBlock::FITTING_POINTS, ALIGN_HCENTER, L"..." },
+    { TextBlock::FITTING_POINTS, ALIGN_RIGHT, L"..." },
     { TextBlock::FITTING_REDUCE, ALIGN_LEFT, TEST_DATA },
     { TextBlock::FITTING_REDUCE, ALIGN_HCENTER, TEST_DATA },
     { TextBlock::FITTING_REDUCE, ALIGN_RIGHT, TEST_DATA },
 };
+static int32 TEST_WIDTH = 400;
 
 DAVA_TESTCLASS (StaticTextTest)
 {
@@ -30,9 +32,8 @@ DAVA_TESTCLASS (StaticTextTest)
 
     StaticTextTest()
     {
-        staticText = new UIStaticText();
-        staticText->SetRect(Rect(10.f, 10.f, 400.f, 200.f));
         font = FTFont::Create("~res:/Fonts/korinna.ttf");
+        staticText = new UIStaticText(Rect(10.f, 10.f, static_cast<float32>(TEST_WIDTH), 200.f));
         staticText->SetFont(font);
     }
 
@@ -42,198 +43,54 @@ DAVA_TESTCLASS (StaticTextTest)
         SafeRelease(font);
     }
 
+    void CheckLinesWidth()
+    {
+        Vector<WideString> strings = staticText->GetMultilineStrings();
+        for (const WideString& line : strings)
+        {
+            Font::StringMetrics rmetrics = font->GetStringMetrics(line);
+            TEST_VERIFY_WITH_MESSAGE(rmetrics.width <= TEST_WIDTH, Format("Line width %d > limit %d in line: '%s'", rmetrics.width, TEST_WIDTH, UTF8Utils::EncodeToUTF8(line).c_str()));
+        }
+    }
+
     DAVA_TEST (SplitByWords)
     {
-        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTING, LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
-        Vector<WideString> resultStrings;
-        resultStrings.push_back(L"THIS SOFTWARE IS PROVIDED BY THE DAVA");
-        resultStrings.push_back(L"CONSULTING, LLC AND CONTRIBUTORS AS IS AND ANY");
-        resultStrings.push_back(L"EXPRESS OR IMPLIED WARRANTIES");
-
+        staticText->SetText(TEST_DATA);
         staticText->SetMultiline(true);
 
-        Vector<WideString> strings = staticText->GetMultilineStrings();
-        bool testSuccess = true;
-
-        if (strings.size() != resultStrings.size())
-        {
-            Logger::Error("SplitByWords: strings.size() != resultStrings.size(): %d != %d", strings.size(), resultStrings.size());
-            testSuccess = false;
-        }
-        else
-        {
-            Vector<WideString>::iterator it = strings.begin();
-            Vector<WideString>::iterator itResult = resultStrings.begin();
-            while (it != strings.end() && itResult != resultStrings.end())
-            {
-                if (*it != *itResult)
-                {
-                    Logger::Error("SplitByWords: *it != *itResult: %ls != %ls", it->c_str(), itResult->c_str());
-                    testSuccess = false;
-                }
-                ++it;
-                ++itResult;
-            }
-        }
-
-        TEST_VERIFY(testSuccess);
+        CheckLinesWidth();
     }
 
     DAVA_TEST (SplitBySymbols)
     {
-        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTING, LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
-        Vector<WideString> resultStrings;
-        resultStrings.push_back(L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTIN");
-        resultStrings.push_back(L"G, LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR");
-        resultStrings.push_back(L"IMPLIED WARRANTIES");
-
+        staticText->SetText(TEST_DATA);
         staticText->SetMultiline(true, true);
 
-        Vector<WideString> strings = staticText->GetMultilineStrings();
-
-        bool testSuccess = true;
-
-        if (strings.size() != resultStrings.size())
-        {
-            Logger::Error("SplitBySymbols: strings.size() != resultStrings.size(): %d != %d", strings.size(), resultStrings.size());
-            testSuccess = false;
-        }
-        else
-        {
-            Vector<WideString>::iterator it = strings.begin();
-            Vector<WideString>::iterator itResult = resultStrings.begin();
-            while (it != strings.end() && itResult != resultStrings.end())
-            {
-                if (*it != *itResult)
-                {
-                    Logger::Error("SplitBySymbols: *it != *itResult: %ls != %ls", it->c_str(), itResult->c_str());
-                    testSuccess = false;
-                }
-                ++it;
-                ++itResult;
-            }
-        }
-
-        TEST_VERIFY(testSuccess);
+        CheckLinesWidth();
     }
 
     DAVA_TEST (SplitByWordsWithNewLine)
     {
-        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY\nTHE DAVA CONSULTING\nLLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
-        Vector<WideString> resultStrings;
-        resultStrings.push_back(L"THIS SOFTWARE IS PROVIDED BY");
-        resultStrings.push_back(L"THE DAVA CONSULTING");
-        resultStrings.push_back(L"LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR");
-        resultStrings.push_back(L"IMPLIED WARRANTIES");
-
+        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY\nTHE DAVA CONSULTING,\nLLC AND\nCONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
         staticText->SetMultiline(true);
 
-        Vector<WideString> strings = staticText->GetMultilineStrings();
-
-        bool testSuccess = true;
-
-        if (strings.size() != resultStrings.size())
-        {
-            Logger::Error("SplitByWordsWithNewLine: strings.size() != resultStrings.size(): %d != %d", strings.size(), resultStrings.size());
-            testSuccess = false;
-        }
-        else
-        {
-            Vector<WideString>::iterator it = strings.begin();
-            Vector<WideString>::iterator itResult = resultStrings.begin();
-            while (it != strings.end() && itResult != resultStrings.end())
-            {
-                if (*it != *itResult)
-                {
-                    Logger::Error("SplitByWordsWithNewLine: *it != *itResult: %ls != %ls", it->c_str(), itResult->c_str());
-                    testSuccess = false;
-                }
-                ++it;
-                ++itResult;
-            }
-        }
-
-        TEST_VERIFY(testSuccess);
+        CheckLinesWidth();
     }
 
     DAVA_TEST (SplitBySymbolsWithNewLine)
     {
-        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTING,\nLLC AND\nCONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
-        Vector<WideString> resultStrings;
-        resultStrings.push_back(L"THIS SOFTWARE IS PROVIDED BY THE DAVA CONSULTIN");
-        resultStrings.push_back(L"G,");
-        resultStrings.push_back(L"LLC AND");
-        resultStrings.push_back(L"CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED W");
-        resultStrings.push_back(L"ARRANTIES");
-
+        staticText->SetText(L"THIS SOFTWARE IS PROVIDED BY\nTHE DAVA CONSULTING,\nLLC AND\nCONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
         staticText->SetMultiline(true, true);
 
-        Vector<WideString> strings = staticText->GetMultilineStrings();
-
-        bool testSuccess = true;
-
-        if (strings.size() != resultStrings.size())
-        {
-            Logger::Error("SplitBySymbolsWithNewLine: strings.size() != resultStrings.size(): %d != %d", strings.size(), resultStrings.size());
-            testSuccess = false;
-        }
-        else
-        {
-            Vector<WideString>::iterator it = strings.begin();
-            Vector<WideString>::iterator itResult = resultStrings.begin();
-            while (it != strings.end() && itResult != resultStrings.end())
-            {
-                if (*it != *itResult)
-                {
-                    Logger::Error("SplitBySymbolsWithNewLine: *it != *itResult: %ls != %ls", it->c_str(), itResult->c_str());
-                    testSuccess = false;
-                }
-                ++it;
-                ++itResult;
-            }
-        }
-
-        TEST_VERIFY(testSuccess);
+        CheckLinesWidth();
     }
 
     DAVA_TEST (SplitAndTrimTest)
     {
         staticText->SetText(L"  THIS SOFTWARE IS   \u00A0   PROVIDED BY   \u00A0   \n  THE DAVA CONSULTING\n LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES");
-        Vector<WideString> resultStrings;
-        resultStrings.push_back(L"  THIS SOFTWARE IS       PROVIDED BY");
-        resultStrings.push_back(L"  THE DAVA CONSULTING");
-        resultStrings.push_back(L" LLC AND CONTRIBUTORS AS IS AND ANY EXPRESS OR");
-        resultStrings.push_back(L"IMPLIED WARRANTIES");
-
         staticText->SetMultiline(true);
 
-        Vector<WideString> strings = staticText->GetMultilineStrings();
-
-        bool testSuccess = true;
-
-        if (strings.size() != resultStrings.size())
-        {
-            Logger::Error("SplitAndTrimTest: strings.size() != resultStrings.size(): %d != %d", strings.size(), resultStrings.size());
-            testSuccess = false;
-        }
-        else
-        {
-            Vector<WideString>::iterator it = strings.begin();
-            Vector<WideString>::iterator itResult = resultStrings.begin();
-            while (it != strings.end() && itResult != resultStrings.end())
-            {
-                if (*it != *itResult)
-                {
-                    Logger::Error("SplitAndTrimTest: *it != *itResult: %ls != %ls", it->c_str(), itResult->c_str());
-                    testSuccess = false;
-                    break;
-                }
-                ++it;
-                ++itResult;
-            }
-        }
-
-        TEST_VERIFY(testSuccess);
+        CheckLinesWidth();
     }
 
     DAVA_TEST (CleanLineTest)
@@ -256,19 +113,21 @@ DAVA_TESTCLASS (StaticTextTest)
 
     DAVA_TEST (TestFitting)
     {
+        const float32 originalFontSize = font->GetSize();
+        staticText->SetText(TEST_DATA);
         staticText->SetMultiline(false);
-
         for (const auto& data : testData)
         {
             staticText->SetFittingOption(data.fitting);
             staticText->SetTextAlign(data.align);
-            staticText->SetText(TEST_DATA);
+
             const WideString& result = staticText->GetVisualText();
-            if (result != data.result)
-            {
-                Logger::Error("TestFitting: %ls != %ls", result.c_str(), data.result.c_str());
-            }
-            TEST_VERIFY(result == data.result);
+            TEST_VERIFY_WITH_MESSAGE(result.find(data.result) != String::npos, Format("Line '%s' doesn't contain '%s'", UTF8Utils::EncodeToUTF8(result).c_str(), UTF8Utils::EncodeToUTF8(data.result).c_str()));
+
+            font->SetSize(staticText->GetFontSize());
+            Font::StringMetrics rmetrics = font->GetStringMetrics(result);
+            font->SetSize(originalFontSize);
+            TEST_VERIFY_WITH_MESSAGE(rmetrics.width <= TEST_WIDTH, Format("Line width %d > limit %d in line: %s", rmetrics.width, TEST_WIDTH, UTF8Utils::EncodeToUTF8(result).c_str()));
         }
     }
 };
