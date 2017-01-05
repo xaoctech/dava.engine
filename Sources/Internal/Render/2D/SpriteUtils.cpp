d99fd03b89cb31a78c39b30824308167464ddbe0
Viktor Kleschenko <v_kleschenko@wargaming.net>
2017-01-05 17:00:54 +0300

    [DF-12450] Removed using of psd at SceneViewer
    Former-commit-id: 070c67e3c379631b56b8391365bc8d292e32e26c

---
+++ Sources/Internal/Render/2D/SpriteUtils.cpp
@@ -0,0 +1,21 @@
+#include "Render/2D/SpriteUtils.h"
+#include "Render/2D/Sprite.h"
+
+#include "FileSystem/FilePath.h"
+
+#include "Render/Image/Image.h"
+#include "Render/Image/ImageSystem.h"
+
+namespace DAVA
+{
+namespace SpriteUtils
+{
+
+Sprite* CreateFromImagePath(const FilePath& imagepath)
+{
+    ScopedPtr<Image> image(ImageSystem::LoadSingleMip(imagepath));
+    return  Sprite::CreateFromImage(image);
+}
+
+}
+}
---
+++ Sources/Internal/Render/2D/SpriteUtils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace DAVA
+{
+class Sprite;
+class FilePath;
+
+namespace SpriteUtils
+{
+
+Sprite* CreateFromImagePath(const FilePath& imagepath);
+
+}
+
+}
