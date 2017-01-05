f508735214fbbb76cdbd6a4b9795f9bcbd714b8a
Igor.Bratchik <stinvi@rambler.ru>
2017-01-05 18:38:02 +0300

    DF-12450 fix build ToolSet
    Former-commit-id: dd53b2646b6c154ee6544c8689fbe0f571ed6ab9

--- Sources/CMake/Modules/FindDavaTools.cmake
+++ Sources/CMake/Modules/FindDavaTools.cmake
@@ -2,6 +2,7 @@ if ( DAVA_TOOLS_FOUND )
     return ()
 endif ()
 set ( DAVA_TOOLS_FOUND 1 )
+include ( ModuleHelper  )
 
 get_filename_component( CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH CACHE )
 
