594f9923006d42db5be6b96df989d3383b335bea
klesch <victor.kleschenko@gmail.com>
2017-01-10 15:02:29 +0300

    [DF-12450] Added build script for toolset
    Former-commit-id: c81d52f811e3f40dee594dfaa6b813ab637aec61

---
+++ build_tools.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+
+import sys
+import os
+import platform
+import subprocess
+import shutil
+
+def create_folder(toolsetBuildPath):
+    if os.path.exists(toolsetBuildPath):
+        shutil.rmtree(toolsetBuildPath)
+    os.makedirs(toolsetBuildPath)
+
+def get_cmake_generator():
+    platformName = platform.system()
+    if platformName == "Darwin":
+        return "Xcode"
+    elif platformName == "win32":
+        return "Visual Studio 12 Win64"
+
+def get_cmake_executable_path(frameworkPath):
+    platformName = platform.system()
+    if platformName == "Darwin":
+        return frameworkPath + "/Bin/CMake.app/Contents/bin/cmake"
+    elif platformName == "win32":
+        return frameworkPath + "Bin/cmake/bin/cmake.exe"
+
+def create_toolset(cmakePath, toolsetPath, toolsetBuildPath, cmakeGenerator, toolsetBinaryPath):
+    commandLine = [cmakePath, "-G", cmakeGenerator, toolsetPath, "-DUNITY_BUILD=true", 
+                       "-B" + toolsetBuildPath, "-DDEPLOY=true", "-DONLY_CONTENT_TOOLS=true",
+                       "-DDEPLOY_DIR=" + toolsetBinaryPath]
+    print "create_toolset: ", commandLine
+    subprocess.call(commandLine)
+
+def build_toolset(cmakePath, toolsetBuildPath):
+    # commandLine = ["cmake", "--build", toolsetBuildPath, "--config", "Release"]
+    configuration = ""
+    platformName = platform.system()
+    if platformName == "Darwin":
+        configuration = "Release"
+    elif platformName == "win32":
+        configuration = "RelWithDebinfo"
+
+    commandLine = ["cmake", "--build", toolsetBuildPath, "--config", configuration]
+
+    print "build_toolset: ", commandLine
+    subprocess.call(commandLine)
+
+if __name__ == '__main__':
+    executablePath = os.path.dirname(sys.argv[0])        
+    frameworkPath = os.path.abspath(executablePath)
+    toolsetPath = frameworkPath + "/Programs/Toolset"
+    toolsetBuildPath = toolsetPath + "/_build"
+    toolsetBinaryPath = frameworkPath + "/Bin/Toolset"
+    cmakePath = get_cmake_executable_path(frameworkPath)
+
+    create_folder(toolsetBuildPath)
+    create_folder(toolsetBinaryPath)
+
+    cmakeGenerator = get_cmake_generator()
+    create_toolset(cmakePath, toolsetPath, toolsetBuildPath, cmakeGenerator, toolsetBinaryPath)
+
+    build_toolset(cmakePath, toolsetBuildPath)
