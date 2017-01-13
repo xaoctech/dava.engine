a3401e053e0af918dbbae6393d2ae45632723cd9
Sergey Reznik <s_reznik@wargaming.net>
2017-01-13 16:47:09 +0300

    [DF-12347] Loading flipped .HDR files, skipping unrelated parameters in file
    Former-commit-id: 32b722e3eccb256feb226fa19ec162bddb8c79b7

--- Sources/Internal/Render/Image/LibHDRHelper.cpp
+++ Sources/Internal/Render/Image/LibHDRHelper.cpp
@@ -10,7 +10,7 @@ namespace DAVA
 namespace LibHDRDetails
 {
 const String kRadianceHeader = "#?RADIANCE";
-const String kRadianceFormatEntry = "FORMAT=";
+const String kRadianceFormatEntry = "FORMAT";
 const String kRadiance32Bit_RLE_RGBE = "32-BIT_RLE_RGBE";
 
 struct RGBE
@@ -42,7 +42,7 @@ bool LibHDRHelper::CanProcessFileInternal(File* infile) const
     return strcmp(buffer.c_str(), LibHDRDetails::kRadianceHeader.c_str()) == 0;
 }
 
-ImageInfo LibHDRHelper::GetImageInfo(File* infile) const
+ImageInfo LibHDRHelper::GetImageInfo(File* infile, Size2i& flip) const
 {
     String buffer(1024, 0);
     infile->ReadLine(&buffer[0], static_cast<uint32>(buffer.size()));
@@ -52,86 +52,80 @@ ImageInfo LibHDRHelper::GetImageInfo(File* infile) const
         return ImageInfo();
     }
 
-    std::fill(buffer.begin(), buffer.end(), 0);
-    infile->ReadLine(&buffer[0], static_cast<uint32>(buffer.size()));
-    ;
-    while (!infile->IsEof() && ((buffer.front() == 0) || (buffer.find('#') == 0)))
+    Size2i size(0, 0);
+    bool dimensionsFound = false;
+    bool validFormatFound = false;
+    while (!infile->IsEof() && !(validFormatFound && dimensionsFound))
     {
         std::fill(buffer.begin(), buffer.end(), 0);
         infile->ReadLine(&buffer[0], static_cast<uint32>(buffer.size()));
-    }
-    std::transform(buffer.begin(), buffer.end(), buffer.begin(), toupper);
+        if (buffer.empty() || (buffer.front() == 0)) // skip empty lines
+            continue;
 
-    if (buffer.find(LibHDRDetails::kRadianceFormatEntry) != 0)
-    {
-        Logger::Error("HDR file contain invalid format signature: %s", buffer.c_str());
-        return ImageInfo();
-    }
+        // Remove whitespace and transform to upper
+        buffer.erase(std::remove_if(buffer.begin(), buffer.end(), [](int8 c) { return std::isspace(c) != 0; }), buffer.end());
+        std::transform(buffer.begin(), buffer.end(), buffer.begin(), toupper);
 
-    String format = buffer.substr(LibHDRDetails::kRadianceFormatEntry.size(), LibHDRDetails::kRadiance32Bit_RLE_RGBE.size());
-    if (format != LibHDRDetails::kRadiance32Bit_RLE_RGBE)
-    {
-        Logger::Error("Invalid (or unsupported format) in HDR: %s", buffer.c_str());
-        return ImageInfo();
-    }
+        if (buffer.front() == '#') // skip comments
+            continue;
 
-    std::fill(buffer.begin(), buffer.end(), 0);
-    infile->ReadLine(&buffer[0], static_cast<uint32>(buffer.size()));
-    while (!infile->IsEof() && ((buffer.front() == 0) || (buffer.find('#') == 0)))
-    {
-        std::fill(buffer.begin(), buffer.end(), 0);
-        infile->ReadLine(&buffer[0], static_cast<uint32>(buffer.size()));
-    }
-    buffer.erase(std::remove_if(buffer.begin(), buffer.end(), [](int8 c) {
-                     return std::isspace(c) != 0;
-                 }),
-                 buffer.end());
-    std::transform(buffer.begin(), buffer.end(), buffer.begin(), toupper);
-
-    size_t xpos = buffer.find('X');
-    size_t ypos = buffer.find('Y');
-    if ((xpos == String::npos) || (ypos == String::npos))
-    {
-        Logger::Error("No dimension specified (or can not be parsed) in HDR file: %s", buffer.c_str());
-        return ImageInfo();
-    }
-
-    String ws;
-    String hs;
-    if (xpos < ypos)
-    {
-        if (ypos < xpos + 2)
+        size_t eqPos = buffer.find('=');
+        if (eqPos != String::npos) // parse property
         {
-            Logger::Error("Invalid dimension format in HDR file: %s", buffer.c_str());
+            String param = buffer.substr(0, eqPos);
+            String value = buffer.substr(eqPos + 1, LibHDRDetails::kRadiance32Bit_RLE_RGBE.length());
+            if (param == LibHDRDetails::kRadianceFormatEntry)
+            {
+                if (value == LibHDRDetails::kRadiance32Bit_RLE_RGBE)
+                {
+                    validFormatFound = true;
+                }
+                else
+                {
+                    Logger::Error("Invalid (or unsupported format) in HDR: %s = %s", param.c_str(), value.c_str());
+                    return ImageInfo();
+                }
+            }
+            continue;
+        }
+
+        size_t xpos = buffer.find('X');
+        size_t ypos = buffer.find('Y');
+        if ((xpos == String::npos) || (ypos == String::npos) || (xpos == 0) || (ypos == 0))
+        {
+            Logger::Error("Unsupported line found in HDR file: %s", buffer.c_str());
             return ImageInfo();
         }
-        ws = buffer.substr(xpos + 1, ypos - xpos - 2);
-        hs = buffer.substr(ypos + 1);
-    }
-    else
-    {
-        if (xpos < ypos + 2)
-        {
-            Logger::Error("Invalid dimension format in HDR file: %s", buffer.c_str());
-            return ImageInfo();
-        }
-        hs = buffer.substr(ypos + 1, xpos - ypos - 2);
-        ws = buffer.substr(xpos + 1);
+
+        String xSize = buffer.substr(xpos + 1);
+        String ySize = buffer.substr(ypos + 1);
+        flip.dx = (buffer[xpos - 1] == '-') ? -1 : 1;
+        flip.dy = (buffer[ypos - 1] == '-') ? -1 : 1;
+        size.dx = ParseStringTo<int32>(xSize);
+        size.dy = ParseStringTo<int32>(ySize);
+        dimensionsFound = true;
     }
 
     ImageInfo result;
     result.format = PixelFormat::FORMAT_RGBA32F;
     result.faceCount = 1;
     result.mipmapsCount = 1;
-    result.width = ParseStringTo<uint32>(ws);
-    result.height = ParseStringTo<uint32>(hs);
+    result.width = static_cast<uint32>(size.dx);
+    result.height = static_cast<uint32>(size.dy);
     result.dataSize = 4 * sizeof(float32) * result.width * result.height;
     return result;
 }
 
+ImageInfo LibHDRHelper::GetImageInfo(File* infile) const
+{
+    Size2i flip(1, 1);
+    return GetImageInfo(infile, flip);
+}
+
 eErrorCode LibHDRHelper::ReadFile(File* infile, Vector<Image*>& imageSet, const ImageSystem::LoadingParams& loadingParams) const
 {
-    ImageInfo desc = GetImageInfo(infile);
+    Size2i flip(1, 1);
+    ImageInfo desc = GetImageInfo(infile, flip);
 
     if ((desc.width < 8) || (desc.width > 0x7fff))
         return eErrorCode::ERROR_READ_FAIL;
@@ -146,7 +140,8 @@ eErrorCode LibHDRHelper::ReadFile(File* infile, Vector<Image*>& imageSet, const 
         uint8* ptr = inData.data();
         for (uint32 y = 0; y < desc.height; ++y)
         {
-            LibHDRDetails::RGBE* rowPtr = rgbeData.data() + desc.width * (desc.height - 1 - y);
+            uint32 row = (flip.dy == -1) ? y : (desc.height - 1 - y);
+            LibHDRDetails::RGBE* rowPtr = rgbeData.data() + desc.width * row;
             ptr = LibHDRDetails::ReadScanline(ptr, desc.width, rowPtr);
         }
     }
@@ -163,13 +158,13 @@ eErrorCode LibHDRHelper::ReadFile(File* infile, Vector<Image*>& imageSet, const 
 eErrorCode LibHDRHelper::WriteFileAsCubeMap(const FilePath& fileName, const Vector<Vector<Image*>>& imageSet, PixelFormat pixelFormat, ImageQuality quality) const
 {
     Logger::Error("[%s] Writing is not yet supported for HDR", __FUNCTION__);
-    return DAVA::eErrorCode::ERROR_WRITE_FAIL;
+    return eErrorCode::ERROR_WRITE_FAIL;
 }
 
 eErrorCode LibHDRHelper::WriteFile(const FilePath& fileName, const Vector<Image*>& imageSet, PixelFormat piexelFormat, ImageQuality quality) const
 {
     Logger::Error("[%s] Writing is not yet supported for HDR", __FUNCTION__);
-    return DAVA::eErrorCode::ERROR_WRITE_FAIL;
+    return eErrorCode::ERROR_WRITE_FAIL;
 }
 
 /*
