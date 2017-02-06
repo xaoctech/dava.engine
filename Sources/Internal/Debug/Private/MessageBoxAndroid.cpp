d5fced5e3ddf9edff1d785b44c59a8e1c27c606d
Maksim Lazarev <m_lazarev@wargaming.net>
2017-02-06 16:23:59 +0300

    DF-12174 android: MessageBox improvements
    Former-commit-id: 8b9ab1b777ea0852bce68cdf6c607610a8fcf754

--- Sources/Internal/Debug/Private/MessageBoxAndroid.cpp
+++ Sources/Internal/Debug/Private/MessageBoxAndroid.cpp
@@ -21,31 +21,23 @@ int MessageBox(const String& title, const String& message, const Vector<String>&
 
     try
     {
-        // TODO: make use JNI::ObjectRef after merging corev2_android branch to avoid jobject leaks on exception
         JNI::JavaClass msgboxClass("com/dava/engine/MessageBox");
         Function<jint(jstring, jstring, jstringArray)> showModal = msgboxClass.GetStaticMethod<jint, jstring, jstring, jstringArray>("messageBox");
 
-        jstring jtitle = JNI::StringToJavaString(title);
-        jstring jmessage = JNI::StringToJavaString(message);
-
         JNIEnv* env = JNI::GetEnv();
-        jclass stringClass = JNI::LoadJavaClass("java/lang/String", true, env);
+        JNI::LocalRef<jstring> jtitle = JNI::StringToJavaString(title, env);
+        JNI::LocalRef<jstring> jmessage = JNI::StringToJavaString(message, env);
+
+        JNI::GlobalRef<jclass> stringClass = JNI::LoadJavaClass("java/lang/String", true, env);
         jsize n = static_cast<jsize>(buttons.size());
-        jstringArray jbuttons = static_cast<jstringArray>(env->NewObjectArray(n, stringClass, nullptr));
+        JNI::LocalRef<jstringArray> jbuttons = env->NewObjectArray(n, stringClass, nullptr); // TODO: should I delete array elements explicitly?
         for (jsize i = 0; i < n; ++i)
         {
-            jstring jbuttonName = JNI::StringToJavaString(buttons[i]);
+            JNI::LocalRef<jstring> jbuttonName = JNI::StringToJavaString(buttons[i], env);
             env->SetObjectArrayElement(jbuttons, i, jbuttonName);
             JNI::CheckJavaException(env, true);
-            env->DeleteLocalRef(jbuttonName);
         }
-
-        jint r = showModal(jtitle, jmessage, jbuttons);
-
-        env->DeleteLocalRef(jtitle);
-        env->DeleteLocalRef(jmessage);
-        env->DeleteLocalRef(jbuttons); // TODO: should I delete array elements explicitly?
-        return static_cast<int>(r);
+        return static_cast<int>(showModal(jtitle, jmessage, jbuttons));
     }
     catch (const JNI::Exception& e)
     {
