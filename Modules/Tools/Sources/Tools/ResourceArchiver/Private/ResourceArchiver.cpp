422c1216e434a363390da0a926b46bd39183284e
leanid <leanid.chaika@gmail.com>
2017-02-20 17:46:21 +0300

    DF-12956 fix ResourceArchiver remove old core v1 code
    Former-commit-id: 5f5e8b6a46bf470f58f998e6aa4796a27d28b7d2

--- Modules/Tools/Sources/Tools/ResourceArchiver/Private/ResourceArchiver.cpp
+++ Modules/Tools/Sources/Tools/ResourceArchiver/Private/ResourceArchiver.cpp
@@ -252,11 +252,7 @@ bool CollectFilesFromDB(const FilePath& baseDirPath, const FilePath& metaDbPath,
         collectedFiles.clear();
         collectedFiles.reserve(numFiles);
 
-#ifdef __DAVAENGINE_COREV2__
         FileSystem* fs = GetEngineContext()->fileSystem;
-#else
-        FileSystem* fs = FileSystem::Instance();
-#endif
 
         db << "SELECT path, pack_index FROM files"
         >> [&](String path, int64 /*packIndex*/) // HACK we have to do request as "SELECT path, pack_index FROM files" to save order
