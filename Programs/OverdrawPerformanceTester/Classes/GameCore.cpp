159ec24d38d8786ce87b88b8c6436518f5390ab7
Aleksandr Vorontsov <a_vorontsov@aleksandrs-mbp.corp.wargaming.local>
2017-02-21 18:17:53 +0300

    DF-12948 Metal api
    Former-commit-id: 4d8a399087f040acb8247abe0ba8231be6795fe6

--- Programs/OverdrawPerformanceTester/Classes/GameCore.cpp
+++ Programs/OverdrawPerformanceTester/Classes/GameCore.cpp
@@ -185,8 +185,8 @@ KeyedArchive* CreateOptions()
     KeyedArchive* appOptions = new KeyedArchive();
 
 #if defined(__DAVAENGINE_IPHONE__) || defined(__DAVAENGINE_ANDROID__)
-    appOptions->SetInt32("renderer", rhi::RHI_GLES2);
-    //appOptions->SetInt32("renderer", rhi::RHI_METAL);
+    //appOptions->SetInt32("renderer", rhi::RHI_GLES2);
+    appOptions->SetInt32("renderer", rhi::RHI_METAL);
     appOptions->SetInt32("rhi_threaded_frame_count", 2);
     appOptions->SetBool("iPhone_autodetectScreenScaleFactor", true);
 
