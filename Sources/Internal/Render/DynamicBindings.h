5268cd8667488810ffb937716bfacc47da386957
Igor Solovey <i_solovey@wargaming.net>
2017-02-28 10:06:34 +0300

    DF-13037 add global time associated with scene
    Former-commit-id: eeb35e1fe285336f76f18f6b49e9111b426a318f

--- Sources/Internal/Render/DynamicBindings.h
+++ Sources/Internal/Render/DynamicBindings.h
@@ -118,7 +118,6 @@ private:
     void ComputeWorldViewInvTransposeMatrixIfRequired();
     void ComputeLocalBoundingBoxSizeIfRequired();
     void ComputeWorldViewObjectCenterIfRequired();
-    void UpdateGlobalTimeIfRequired();
 
     void ComputeInvWorldMatrixIfRequired();
     void ComputeWorldInvTransposeMatrixIfRequired();
