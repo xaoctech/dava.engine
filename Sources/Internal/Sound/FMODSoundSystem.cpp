252b26ec5e9faff48af3ccf118c1fcb180e09da7
kkdaemon <kkdaemon@gmail.com>
2017-03-01 17:00:14 +0300

    DF-12986 log food version on sound system initialisation
    Former-commit-id: faefe9a0238324874125761a3803593aa50db127

--- Sources/Internal/Sound/FMODSoundSystem.cpp
+++ Sources/Internal/Sound/FMODSoundSystem.cpp
@@ -100,6 +100,13 @@ SoundSystem::SoundSystem()
 
     FMOD_VERIFY(FMOD::EventSystem_Create(&fmodEventSystem));
     FMOD_VERIFY(fmodEventSystem->getSystemObject(&fmodSystem));
+
+    {
+        unsigned int version = 0;
+        fmodSystem->getVersion(&version);
+        Logger::Info("FMOD version %X", version);
+    }
+    
 #ifdef __DAVAENGINE_ANDROID__
     FMOD_VERIFY(fmodSystem->setOutput(FMOD_OUTPUTTYPE_AUDIOTRACK));
 
