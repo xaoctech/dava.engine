ae0db3d8f0e5e06eb555f269c85d59236ad45aa0
Viktor Kleschenko <v_kleschenko@wargaming.net>
2017-03-07 11:33:31 +0300

    [DF-13117] Fixed compilation on win32
    Former-commit-id: 458cab6d412c584f7c21d27080ec925696b9e91b

--- Modules/TArc/Sources/TArc/DataProcessing/AnySupport/AnyQStringCompare.h
+++ Modules/TArc/Sources/TArc/DataProcessing/AnySupport/AnyQStringCompare.h
@@ -6,6 +6,12 @@
 namespace DAVA
 {
 template <>
-bool AnyCompare<QString>::IsEqual(const Any& v1, const Any& v2);
-extern template struct AnyCompare<QString>;
+struct AnyCompare<QString>
+{
+    static bool IsEqual(const Any& v1, const Any& v2)
+    {
+        return v1.Get<QString>() == v2.Get<QString>();
+    }
+};
+
 }
