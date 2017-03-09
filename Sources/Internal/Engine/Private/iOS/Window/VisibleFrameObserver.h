685311a15ebc59e67cad52964f3b35e79925acc9
Mikhail Polubisok <m_polubisok@wargaming.net>
2017-03-09 18:16:34 +0300

    DF-12898 Fix compilation error
    Former-commit-id: 7e927537adfe27a6c54616384c834e6f6baedb20

--- Sources/Internal/Engine/Private/iOS/Window/VisibleFrameObserver.h
+++ Sources/Internal/Engine/Private/iOS/Window/VisibleFrameObserver.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#if defined(__DAVAENGINE_COREV2__)
+#if defined(__DAVAENGINE_IPHONE__)
+
 #import <Foundation/NSObject.h>
 #import <Foundation/NSNotification.h>
 
@@ -21,3 +24,6 @@ class WindowNativeBridge;
 - (void)keyboardFrameDidChange:(NSNotification*)notification;
 
 @end
+
+#endif // __DAVAENGINE_IPHONE__
+#endif // __DAVAENGINE_COREV2__
