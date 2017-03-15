4fd236c8edad5f94c2304c79e63a742bb9f4be4f
Mikhail Polubisok <m_polubisok@wargaming.net>
2017-03-15 19:01:52 +0300

    DF-11834 Fix review issue
    Former-commit-id: d0219540a24a4d042532c044602e5a9ed0eac265

--- Sources/Internal/UI/UIPackageLoader.cpp
+++ Sources/Internal/UI/UIPackageLoader.cpp
@@ -236,7 +236,7 @@ void UIPackageLoader::LoadStyleSheets(const YamlNode* styleSheetsNode, AbstractU
                 {
                     uint32 index = propertyDB->GetStyleSheetPropertyIndex(propertyName);
                     const UIStyleSheetPropertyDescriptor& propertyDescr = propertyDB->GetStyleSheetPropertyByIndex(index);
-                    if (propertyDescr.field_s != nullptr)
+                    if (propertyDescr.field != nullptr)
                     {
                         const YamlNode* propertyNode = properties->Get(propertyIndex);
                         const YamlNode* valueNode = propertyNode;
@@ -245,7 +245,7 @@ void UIPackageLoader::LoadStyleSheets(const YamlNode* styleSheetsNode, AbstractU
 
                         if (valueNode)
                         {
-                            Any value(valueNode->AsAny(propertyDescr.field_s));
+                            Any value(valueNode->AsAny(propertyDescr.field));
 
                             UIStyleSheetProperty property{ index, value };
 
