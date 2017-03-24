c47903161e75122b1e120165b604266f149ce1c5
Stanislav Madytsky <s_madytskiy@wargaming.net>
2017-03-24 14:35:14 +0300

    DF-13000 module include fix
    Former-commit-id: e32b740bbe8b3d693de1f1d386251c2e652f5957

--- Sources/CMake/Modules/FindMemoryProfilerService.cmake
+++ Sources/CMake/Modules/FindMemoryProfilerService.cmake
@@ -6,4 +6,4 @@ set ( MEMORY_PROFILER_SERVICE_FOUND 1 )
 include (GlobalVariables)
 append_property( DAVA_COMPONENTS  "MemoryProfilerService" )
 
-add_subdirectory ( "${DAVA_MODULES_DIR}/NetworkServices/MemoryProfilerService" ${CMAKE_CURRENT_BINARY_DIR}/NetworkServices/MemoryProfilerService )
+add_module_subdirectory ( MemoryProfilerService "${DAVA_MODULES_DIR}/NetworkServices/MemoryProfilerService" )
