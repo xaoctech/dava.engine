28bfe684380658a5bad6b1b082f729d58fce8e3b
a_lebedev <a_lebedev@wargaming.net>
2017-04-14 11:28:55 +0300

    DF-13493 Review fix
    Former-commit-id: 0f10c2cd45e56de2f0358702d7edb1544361e983

--- Sources/Internal/Render/2D/Systems/RenderSystem2D.cpp
+++ Sources/Internal/Render/2D/Systems/RenderSystem2D.cpp
@@ -2000,15 +2000,17 @@ void StretchDrawData::GenerateTransformData()
         transformedVertices[index] = vertices[index] * transformMatr;
     }
 
+    RenderSystem2D *renderSystem = RenderSystem2D::Instance();
+
     if (usePerPixelAccuracy)
     {
-        Vector2 ppaShift = RenderSystem2D::Instance()->GetAlignedVertex(transformedVertices[0]) - transformedVertices[0];
+        Vector2 ppaShift = renderSystem->GetAlignedVertex(transformedVertices[0]) - transformedVertices[0];
         transformedVertices[0] += ppaShift;
         transformedVertices[1] += ppaShift;
         transformedVertices[4] += ppaShift;
         transformedVertices[5] += ppaShift;
 
-        ppaShift = RenderSystem2D::Instance()->GetAlignedVertex(transformedVertices[2]) - transformedVertices[2];
+        ppaShift = renderSystem->GetAlignedVertex(transformedVertices[2]) - transformedVertices[2];
         transformedVertices[2] += ppaShift;
         transformedVertices[3] += ppaShift;
         transformedVertices[6] += ppaShift;
@@ -2016,13 +2018,13 @@ void StretchDrawData::GenerateTransformData()
 
         if (transformedVertices.size() > 8)
         {
-            ppaShift = RenderSystem2D::Instance()->GetAlignedVertex(transformedVertices[8]) - transformedVertices[8];
+            ppaShift = renderSystem->GetAlignedVertex(transformedVertices[8]) - transformedVertices[8];
             transformedVertices[8] += ppaShift;
             transformedVertices[9] += ppaShift;
             transformedVertices[12] += ppaShift;
             transformedVertices[13] += ppaShift;
 
-            ppaShift = RenderSystem2D::Instance()->GetAlignedVertex(transformedVertices[10]) - transformedVertices[10];
+            ppaShift = renderSystem->GetAlignedVertex(transformedVertices[10]) - transformedVertices[10];
             transformedVertices[10] += ppaShift;
             transformedVertices[11] += ppaShift;
             transformedVertices[14] += ppaShift;
