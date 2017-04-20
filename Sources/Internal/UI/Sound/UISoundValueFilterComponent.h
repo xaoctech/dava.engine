8f08d5addb5026256331a832ee0adaebabaaa694
Mikhail Polubisok <m_polubisok@wargaming.net>
2017-04-20 13:08:50 +0300

    DF-13571 Fix UISoundValueFilterComponent reflection
    Former-commit-id: aa7526b19b4e3a4815d6f1d677c17c682e1d6285

--- Sources/Internal/UI/Sound/UISoundValueFilterComponent.h
+++ Sources/Internal/UI/Sound/UISoundValueFilterComponent.h
@@ -6,6 +6,8 @@ namespace DAVA
 {
 class UISoundValueFilterComponent : public UIBaseComponent<UIComponent::SOUND_VALUE_FILTER_COMPONENT>
 {
+    DAVA_VIRTUAL_REFLECTION(UISoundValueFilterComponent, UIBaseComponent<UIComponent::SOUND_VALUE_FILTER_COMPONENT>);
+
 public:
     UISoundValueFilterComponent();
     UISoundValueFilterComponent(const UISoundValueFilterComponent& src);
@@ -29,11 +31,5 @@ private:
     float32 step = 1.0f;
     float32 deadZone = 0.0f;
     int32 normalizedValue = -1;
-
-public:
-    INTROSPECTION_EXTEND(UISoundValueFilterComponent, UIComponent,
-                         PROPERTY("step", "Step", GetStep, SetStep, I_SAVE | I_VIEW | I_EDIT | I_LOAD)
-                         PROPERTY("deadZone", "Dead zone", GetDeadZone, SetDeadZone, I_SAVE | I_VIEW | I_EDIT | I_LOAD)
-                         );
 };
 }
