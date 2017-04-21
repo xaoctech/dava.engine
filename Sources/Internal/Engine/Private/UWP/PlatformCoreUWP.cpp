ddf4cafff2f9d766002b78c5aaf540b30f5defe8
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2017-04-21 13:03:53 +0300

    DF-12874: removed useless comment
    Former-commit-id: 5ebccdba97c714bc7c70be74e833bd09e17d198f

--- Sources/Internal/Engine/Private/UWP/PlatformCoreUWP.cpp
+++ Sources/Internal/Engine/Private/UWP/PlatformCoreUWP.cpp
@@ -99,8 +99,6 @@ void PlatformCore::Quit()
 void PlatformCore::SetScreenTimeoutEnabled(bool enabled)
 {
     engineBackend->GetPrimaryWindow()->RunOnUIThreadAsync([this, enabled]() {
-        // If user wants to disable screen timeout and display request is already active - do nothing
-        // If user wants to enable screen timeout and display request is not active - do nothing
         if (displayRequestActive != enabled)
         {
             return;
