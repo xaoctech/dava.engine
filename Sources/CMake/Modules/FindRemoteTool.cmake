934dc9845b642af6521af0692cd68ed0ca7361cb
Igor.Bratchik <i_bratchik@wargaming.net>
2017-04-30 14:44:22 +0300

    DF-13589 minor fix
    Former-commit-id: 8b5d1c2b92e243df66733d8664d842f0d69aaab5

--- Sources/CMake/Modules/FindRemoteTool.cmake
+++ Sources/CMake/Modules/FindRemoteTool.cmake
@@ -3,8 +3,6 @@ if ( REMOTE_TOOL_FOUND )
 endif ()
 set ( REMOTE_TOOL_FOUND 1 )
 
-add_definitions(-DWITH_SCENE_PERFORMANCE_TESTS)
-
 include (GlobalVariables)
 
 add_module_subdirectory ( RemoteTool "${DAVA_MODULES_DIR}/RemoteTool" )
