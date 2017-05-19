01f096e67efa92fce1ab430185de936e9b373dad
Ivan Petrochenko <i_petrochenko@wargaming.net>
2017-05-19 19:30:07 +0300

    DF-3543 error fix
    Former-commit-id: 4eb5aa2930df872b698d63d07d2e328d3ced7b5f

--- Sources/Internal/Scene3D/Components/SingleComponents/Private/TransformSingleComponent.cpp
+++ Sources/Internal/Scene3D/Components/SingleComponents/Private/TransformSingleComponent.cpp
@@ -14,6 +14,7 @@ void RemoveEntitiesFromVector(Vector<Entity*>& vector, const Entity* entity)
         {
             vector[k] = vector[size - 1];
             vector.pop_back();
+            size--;
         }
     }
 }
