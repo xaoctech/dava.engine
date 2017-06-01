7bab69d105af7db9ba3ae1b970e47c4025cf3b5d
o_strokachuk <o_strokachuk@wargaming.net>
2017-06-01 11:26:26 +0300

    DF-13931 Кидать DAVA::Exception вместо utf-8 исключений
    Former-commit-id: 37233ea112a48f86ca869f9249b898f5555c185b

--- Sources/Internal/Utils/UTF8Utils.cpp
+++ Sources/Internal/Utils/UTF8Utils.cpp
@@ -1,5 +1,6 @@
 #include "Utils/UTF8Utils.h"
 #include "Debug/DVAssert.h"
+#include "Base/Exception.h"
 
 #include <utf8.h>
 
@@ -7,44 +8,50 @@ namespace DAVA
 {
 
 #ifdef __DAVAENGINE_WINDOWS__
-
 static_assert(sizeof(char16) == 2, "check size of wchar_t on current platform");
+#else
+static_assert(sizeof(char16) == 4, "check size of wchar_t on current platform");
+#endif
 
 void UTF8Utils::EncodeToWideString(const uint8* string, size_t size, WideString& result)
 {
     DVASSERT(nullptr != string);
     result.clear();
     result.reserve(size); // minimum they will be same
-    utf8::utf8to16(string, string + size, std::back_inserter(result));
-};
 
-String UTF8Utils::EncodeToUTF8(const WideString& wstring)
-{
-    String result;
-    result.reserve(wstring.size()); // minimum they will be same
-    utf8::utf16to8(wstring.begin(), wstring.end(), std::back_inserter(result));
-    return result;
-};
-
+    try
+    {
+#ifdef __DAVAENGINE_WINDOWS__
+        utf8::utf8to16(string, string + size, std::back_inserter(result));
 #else
-
-static_assert(sizeof(char16) == 4, "check size of wchar_t on current platform");
-
-void UTF8Utils::EncodeToWideString(const uint8* string, size_type size, WideString& result)
-{
-    DVASSERT(nullptr != string);
-    result.clear();
-    result.reserve(size); // minimum they will be same
-    utf8::utf8to32(string, string + size, std::back_inserter(result));
+        utf8::utf8to32(string, string + size, std::back_inserter(result));
+#endif
+    }
+    catch (const utf8::exception& exception)
+    {
+        DAVA_THROW(DAVA::Exception, "UTF8->WideString Conversion error: " + String(exception.what()));
+    }
 };
 
 String UTF8Utils::EncodeToUTF8(const WideString& wstring)
 {
     String result;
     result.reserve(wstring.size()); // minimum they will be same
-    utf8::utf32to8(wstring.begin(), wstring.end(), std::back_inserter(result));
+
+    try
+    {
+#ifdef __DAVAENGINE_WINDOWS__
+        utf8::utf16to8(wstring.begin(), wstring.end(), std::back_inserter(result));
+#else
+        utf8::utf32to8(wstring.begin(), wstring.end(), std::back_inserter(result));
+#endif
+    }
+    catch (const utf8::exception& exception)
+    {
+        DAVA_THROW(DAVA::Exception, "WideString->UTF8 Conversion error: " + String(exception.what()));
+    }
+
     return result;
 };
-#endif
 
 } // namespace DAVA
