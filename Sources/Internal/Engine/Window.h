6fb36dbc1f974f8b0f4bf24c95e55c2848fe3d18
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2017-06-06 12:13:02 +0300

    DF-12708: added a note to backNavigation comment
    Former-commit-id: 1b9018eaa82bee39ad1a015abc8a74325abcb870

--- Sources/Internal/Engine/Window.h
+++ Sources/Internal/Engine/Window.h
@@ -299,7 +299,7 @@ public:
     Signal<Window*, float32> update; //!< Emitted on each frame if window is visible.
     Signal<Window*> draw; //!< Emited after `update` signal after `UIControlSystem::Draw`
     Signal<Window*, Rect /*visibleFrameRect*/> visibleFrameChanged; //!< Emitted when window visible frame changed (showed virtual keyboard over window).
-    Signal<Window*> backNavigation; //!< Emitted when user presses a back button.
+    Signal<Window*> backNavigation; //!< Emitted when user presses a back button or its alternative (like Win + Backspace on UWP).
 
 private:
     /// Initialize platform specific render params, e.g. acquire/release context functions for Qt platform
