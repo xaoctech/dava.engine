78885fcf8e96d756b1810b9376d56f89d7786aa5
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2017-06-15 14:12:47 +0300

    DF-14000: added comment to DavaLog
    Former-commit-id: 6d460227b243597268517c09391f9bd702eeb318

--- Sources/Internal/Engine/Private/Android/Java/com/dava/engine/DavaLog.java
+++ Sources/Internal/Engine/Private/Android/Java/com/dava/engine/DavaLog.java
@@ -4,6 +4,32 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+    Logger class which allows for better extensibility compared to `android.utils.Log`.
+    It can be used in case some additional actions need to be performed for each log entry, for example sending it to Crashlytics, saving to file etc.
+    
+    Each log entry is processed in two steps:
+        - First, it's passed to `java.util.logging.Logger` object
+        - Then passed to according `android.util.Log` function (i.e. `DavaLog.e` calls `android.utils.Log.e` method)
+
+    A user can get a `java.util.logging.Logger` object, every log entry is passed to, via `DavaLog.getLogger` method and setup as required.
+    As an example, a custom handler can be added to it, which will send every record with level bigger than INFO to Crashlytics:
+    \code
+    DavaLog.getLogger().addHandler(
+                new Handler() {
+                    @Override
+                    public void publish(LogRecord record)
+                    {
+                        if (isLoggable(record) && record.getLevel().intValue() >= Level.INFO.intValue())
+                        {
+                            Crashlytics.log(record.getMessage());
+                        }
+                    }
+
+                    ...
+                });
+    \endcode
+*/
 public class DavaLog
 {
     private static Logger logger;
