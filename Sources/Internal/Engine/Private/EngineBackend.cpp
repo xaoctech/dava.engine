091055c428f1ca809fc931edf86bd43753138639
Ivan Petrochenko <i_petrochenko@wargaming.net>
2017-06-19 10:56:10 +0300

    DF-12839 fix after merge
    Former-commit-id: 787638567670e5ce0e15ee886bbee327c2753cce

--- Sources/Internal/Engine/Private/EngineBackend.cpp
+++ Sources/Internal/Engine/Private/EngineBackend.cpp
@@ -211,7 +211,6 @@ void EngineBackend::Init(eEngineRunMode engineRunMode, const Vector<String>& mod
     }
 
     Thread::InitMainThread();
-    RegisterReflectionForBaseTypes();
     // For now only next subsystems/modules are created on demand:
     //  - LocalizationSystem
     //  - JobManager
