c128878dd4d0b10fc93e1d0f42926168d0d6047d
Ivan Petrochenko <i_petrochenko@wargaming.net>
2017-07-07 12:14:15 +0300

    DF-14237 compilation fix
    Former-commit-id: a9e29b682c6e64b5dfd60aad104a4e803e65efa5

--- Programs/QuickEd/Classes/UI/Properties/CompletionsProviderForUIReflection.h
+++ Programs/QuickEd/Classes/UI/Properties/CompletionsProviderForUIReflection.h
@@ -3,10 +3,10 @@
 #include "UI/Properties/CompletionsProvider.h"
 #include <Base/String.h>
 #include <Base/FastName.h>
+#include <Base/Type.h>
 
 class PackageBaseNode;
 
-class DAVA::Type;
 class CompletionsProviderForUIReflection : public CompletionsProvider
 {
 public:
