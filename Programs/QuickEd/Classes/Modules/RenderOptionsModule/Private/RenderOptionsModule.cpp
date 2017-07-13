cbbb5fd002aeb43afb30f57fcdabc38eeb9457cc
Mikhail Polubisok <m_polubisok@wargaming.net>
2017-07-13 23:41:34 +0300

    DF-14308 Add Render Options dialog to QuickEd
    Former-commit-id: 8cf6213ec579cdda088fc79a32e2a034a4ba70aa

--- Programs/QuickEd/Classes/Modules/RenderOptionsModule/Private/RenderOptionsModule.cpp
+++ Programs/QuickEd/Classes/Modules/RenderOptionsModule/Private/RenderOptionsModule.cpp
@@ -1,15 +1,13 @@
 #include "Modules/RenderOptionsModule/RenderOptionsModule.h"
-#include "Modules/RenderOptionsModule/Private/OptionWrapper.h"
-#include "Modules/RenderOptionsModule/Private/RenderOptionsData.h"
 
 #include <Engine/EngineContext.h>
 #include <Render/RenderOptions.h>
 #include <Render/Renderer.h>
 
 #include <TArc/Controls/CheckBox.h>
-#include <TArc/Controls/QtBoxLayouts.h>
 #include <TArc/Controls/ReflectedButton.h>
 #include <TArc/Core/ContextAccessor.h>
+#include <TArc/DataProcessing/DataNode.h>
 #include <TArc/Utils/ModuleCollection.h>
 #include <TArc/WindowSubSystem/ActionUtils.h>
 #include <TArc/WindowSubSystem/UI.h>
@@ -21,14 +19,105 @@
 #include <QScrollArea>
 #include <QWidget>
 
-const QString RenderOptionsModule::renderOptionsMenuItemName = QString("Render Options");
-
-void RenderOptionsModule::OnContextCreated(DAVA::TArc::DataContext* context)
+namespace RenderOptionsDetails
 {
+namespace Properties
+{
+static const DAVA::String GlobalName = "RenderOptionsDialogProperties";
+static const DAVA::String Geometry = "Geometry";
 }
 
-void RenderOptionsModule::OnContextDeleted(DAVA::TArc::DataContext* context)
+class OptionWrapper : public DAVA::ReflectionBase
 {
+    DAVA_VIRTUAL_REFLECTION_IN_PLACE(OptionWrapper, DAVA::ReflectionBase)
+    {
+        using namespace DAVA;
+        ReflectionRegistrator<OptionWrapper>::Begin()
+        .Field("title", &OptionWrapper::GetTitle, nullptr)
+        .Field("enabled", &OptionWrapper::GetEnabled, &OptionWrapper::SetEnabled)
+        .End();
+    }
+
+public:
+    OptionWrapper(RenderOptions::RenderOption option)
+        : option(option)
+    {
+    }
+
+    String GetTitle() const
+    {
+        using namespace DAVA;
+        RenderOptions* options = Renderer::GetOptions();
+        if (options)
+        {
+            return String(options->GetOptionName(option).c_str());
+        }
+        return String();
+    }
+
+    bool GetEnabled() const
+    {
+        using namespace DAVA;
+        RenderOptions* options = Renderer::GetOptions();
+        if (options)
+        {
+            return options->IsOptionEnabled(option);
+        }
+        return false;
+    }
+
+    void SetEnabled(bool value)
+    {
+        using namespace DAVA;
+        RenderOptions* options = Renderer::GetOptions();
+        if (options)
+        {
+            options->SetOption(option, value);
+        }
+    }
+
+private:
+    RenderOptions::RenderOption option;
+};
+
+class RenderOptionsData : public DAVA::TArc::DataNode
+{
+    DAVA_VIRTUAL_REFLECTION_IN_PLACE(RenderOptionsData, DAVA::TArc::DataNode)
+    {
+        DAVA::ReflectionRegistrator<RenderOptionsData>::Begin()
+        .Field("isEnabled", &RenderOptionsData::IsEnabled, nullptr)
+        .End();
+    }
+
+public:
+    bool IsEnabled() const
+    {
+        return DAVA::Renderer::IsInitialized();
+    }
+
+    DAVA::Vector<std::shared_ptr<OptionWrapper>> optionsRefs;
+    std::unique_ptr<QDialog> optionsDialog;
+};
+}
+
+const QString RenderOptionsModule::renderOptionsMenuItemName = QString("Render Options");
+
+RenderOptionsModule::~RenderOptionsModule()
+{
+    using namespace DAVA;
+    using namespace DAVA::TArc;
+
+    ContextAccessor* accessor = GetAccessor();
+    if (accessor)
+    {
+        DataContext* globalContext = accessor->GetGlobalContext();
+        RenderOptionsDetails::RenderOptionsData* data = globalContext->GetData<RenderOptionsDetails::RenderOptionsData>();
+        DVASSERT(data);
+        DVASSERT(data->optionsDialog);
+
+        PropertiesItem properties = accessor->CreatePropertiesNode(RenderOptionsDetails::Properties::GlobalName);
+        properties.Set(RenderOptionsDetails::Properties::Geometry, data->optionsDialog->geometry());
+    }
 }
 
 void RenderOptionsModule::PostInit()
@@ -36,16 +125,14 @@ void RenderOptionsModule::PostInit()
     using namespace DAVA;
     using namespace DAVA::TArc;
 
+    std::unique_ptr<RenderOptionsDetails::RenderOptionsData> data(new RenderOptionsDetails::RenderOptionsData());
+    for (uint32 option = 0; option < RenderOptions::OPTIONS_COUNT; ++option)
+    {
+        data->optionsRefs.emplace_back(std::make_shared<RenderOptionsDetails::OptionWrapper>(static_cast<RenderOptions::RenderOption>(option)));
+    }
+
     ContextAccessor* accessor = GetAccessor();
     UI* ui = GetUI();
-
-    // Data node
-
-    {
-        std::unique_ptr<RenderOptionsData> data(new RenderOptionsData());
-        DataContext* globalContext = accessor->GetGlobalContext();
-        globalContext->CreateData(std::move(data));
-    }
 
     // Menu item
 
@@ -53,7 +140,7 @@ void RenderOptionsModule::PostInit()
         QtAction* action = new QtAction(accessor, QIcon(":/Icons/render_options.png"), renderOptionsMenuItemName);
 
         FieldDescriptor fieldIsEnabled;
-        fieldIsEnabled.type = ReflectedTypeDB::Get<RenderOptionsData>();
+        fieldIsEnabled.type = ReflectedTypeDB::Get<RenderOptionsDetails::RenderOptionsData>();
         fieldIsEnabled.fieldName = FastName("isEnabled");
         action->SetStateUpdationFunction(QtAction::Enabled, fieldIsEnabled, [](const Any& fieldValue) -> Any {
             return fieldValue.Cast<bool>(false);
@@ -70,40 +157,67 @@ void RenderOptionsModule::PostInit()
     // Dialog
 
     {
-        for (uint32 option = 0; option < RenderOptions::OPTIONS_COUNT; ++option)
-        {
-            optionsRefs.emplace_back(std::make_shared<OptionWrapper>(static_cast<RenderOptions::RenderOption>(option)));
-        }
+        PropertiesItem properties = accessor->CreatePropertiesNode(RenderOptionsDetails::Properties::GlobalName);
+        QRect dialogRect = properties.Get<QRect>(RenderOptionsDetails::Properties::Geometry);
 
-        optionsDialog = std::make_unique<QDialog>();
-        optionsDialog->setWindowTitle("Render Options");
-        optionsDialog->resize(300, 400);
+        data->optionsDialog = std::make_unique<QDialog>();
+        data->optionsDialog->setWindowTitle("Render Options");
+
+        if (dialogRect.isValid())
+        {
+            data->optionsDialog->setGeometry(dialogRect);
+            data->optionsDialog->move(dialogRect.topLeft());
+        }
+        else
+        {
+            data->optionsDialog->resize(400, 300);
+        }
 
         QScrollArea* scrollArea = new QScrollArea();
         scrollArea->setWidgetResizable(true);
         scrollArea->setWidget(new QWidget());
 
-        QVBoxLayout* layout = new QtVBoxLayout(optionsDialog.get());
+        QVBoxLayout* layout = new QVBoxLayout(data->optionsDialog.get());
         layout->setMargin(5);
         layout->addWidget(scrollArea);
 
-        QtVBoxLayout* scrollLayout = new QtVBoxLayout(scrollArea->widget());
+        QGridLayout* scrollLayout = new QGridLayout(scrollArea->widget());
         scrollLayout->setSpacing(5);
 
-        for (std::shared_ptr<OptionWrapper>& option : optionsRefs)
+        uint32 index = 0;
+        for (std::shared_ptr<RenderOptionsDetails::OptionWrapper>& option : data->optionsRefs)
         {
             CheckBox::Params params(accessor, ui, DAVA::TArc::mainWindowKey);
             params.fields[CheckBox::Fields::Checked] = "enabled";
             params.fields[CheckBox::Fields::TextHint] = "title";
-            scrollLayout->AddControl(new CheckBox(params, accessor, DAVA::Reflection::Create(DAVA::ReflectedObject(option.get())), optionsDialog.get()));
+            CheckBox* cb = new CheckBox(params, accessor, DAVA::Reflection::Create(DAVA::ReflectedObject(option.get())), data->optionsDialog.get());
+            scrollLayout->addWidget(cb->ToWidgetCast(), index / 2, index & 1);
+            index++;
         }
+    }
+
+    // Store data node
+
+    {
+        DataContext* globalContext = accessor->GetGlobalContext();
+        globalContext->CreateData(std::move(data));
     }
 }
 
 void RenderOptionsModule::ShowRenderOptionsDialog()
 {
-    DVASSERT(optionsDialog);
-    optionsDialog->exec();
+    using namespace DAVA;
+    using namespace DAVA::TArc;
+
+    ContextAccessor* accessor = GetAccessor();
+    if (accessor)
+    {
+        DataContext* globalContext = accessor->GetGlobalContext();
+        RenderOptionsDetails::RenderOptionsData* data = globalContext->GetData<RenderOptionsDetails::RenderOptionsData>();
+        DVASSERT(data);
+        DVASSERT(data->optionsDialog);
+        data->optionsDialog->exec();
+    }
 }
 
 DAVA_VIRTUAL_REFLECTION_IMPL(RenderOptionsModule)
--- Programs/QuickEd/Classes/Modules/RenderOptionsModule/RenderOptionsModule.h
+++ Programs/QuickEd/Classes/Modules/RenderOptionsModule/RenderOptionsModule.h
@@ -3,12 +3,9 @@
 #include <TArc/Core/ClientModule.h>
 #include <TArc/Utils/QtConnections.h>
 
-#include <Base/BaseTypes.h>
-#include <Base/Vector.h>
 #include <Reflection/Reflection.h>
 
-class OptionWrapper;
-class QDialog;
+#include <QString>
 
 class RenderOptionsModule : public DAVA::TArc::ClientModule
 {
@@ -18,14 +15,12 @@ public:
     /** Menu item name in Tools menu. */
     static const QString renderOptionsMenuItemName;
 
+    ~RenderOptionsModule() override;
+
 protected:
-    void OnContextCreated(DAVA::TArc::DataContext* context) override;
-    void OnContextDeleted(DAVA::TArc::DataContext* context) override;
     void PostInit() override;
 
 private:
     void ShowRenderOptionsDialog();
-    DAVA::Vector<std::shared_ptr<OptionWrapper>> optionsRefs;
-    std::unique_ptr<QDialog> optionsDialog;
     DAVA::TArc::QtConnections connections;
 };
