18b4269b4e51a7ff06144e29f0a390bc0cf81ba4
Igor.Bratchik <i_bratchik@wargaming.net>
2017-08-06 13:44:23 +0300

    DF-14540 minor fix
    Former-commit-id: 66f19ac10610f91164ff9ff4894249da76232829

---
+++ Sources/Internal/ModuleManager/ModuleManagerStub.cpp
@@ -0,0 +1,11 @@
+#include "ModuleManager/ModuleManager.h"
+#include "ModuleManager/IModule.h"
+
+namespace DAVA
+{
+Vector<IModule*> CreateModuleInstances(Engine* engine)
+{
+    Vector<IModule*> modules;
+    return modules;
+}
+} // namespace DAVA
