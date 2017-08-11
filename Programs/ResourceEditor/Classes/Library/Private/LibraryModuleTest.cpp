29205019af4259ab356c57c60f5a36984fcf32f3
klesch <victor.kleschenko@gmail.com>
2017-08-11 11:32:23 +0300

    [DF-14598] CodeFormat only
    Former-commit-id: 23b9294863361e84c7bab8f9277048249c3246dc

--- Programs/ResourceEditor/Classes/Library/Private/LibraryModuleTest.cpp
+++ Programs/ResourceEditor/Classes/Library/Private/LibraryModuleTest.cpp
@@ -81,134 +81,136 @@ protected:
 };
 }
 
+// clang-format off
 DAVA_TARC_TESTCLASS(LibraryModuleTests)
 {
-  void ModifyScene()
-  {
-  using namespace DAVA;
-FilePath scenePathname = FilePath::CreateWithNewExtension(LibraryTest::testDAEPathname, ".sc2");
-
-ScopedPtr<Scene> scene(new Scene());
-TEST_VERIFY(scene->LoadScene(scenePathname) == SceneFileV2::eError::ERROR_NO_ERROR);
-
-Vector<Entity*> lodEntities;
-scene->GetChildEntitiesWithComponent(lodEntities, Component::LOD_COMPONENT);
-TEST_VERIFY(lodEntities.empty() == false)
-
-for (Entity* e : lodEntities)
-{
-    uint32 count = e->GetComponentCount(Component::LOD_COMPONENT);
-    TEST_VERIFY(count > 0)
-    for (uint32 ic = 0; ic < count; ++ic)
+    void ModifyScene()
     {
-        LodComponent* lod = static_cast<LodComponent*>(e->GetComponent(Component::LOD_COMPONENT, ic));
-        for (int32 layer = 0; layer < LodComponent::MAX_LOD_LAYERS; ++layer)
+        using namespace DAVA;
+        FilePath scenePathname = FilePath::CreateWithNewExtension(LibraryTest::testDAEPathname, ".sc2");
+
+        ScopedPtr<Scene> scene(new Scene());
+        TEST_VERIFY(scene->LoadScene(scenePathname) == SceneFileV2::eError::ERROR_NO_ERROR);
+
+        Vector<Entity*> lodEntities;
+        scene->GetChildEntitiesWithComponent(lodEntities, Component::LOD_COMPONENT);
+        TEST_VERIFY(lodEntities.empty() == false);
+
+        for (Entity* e : lodEntities)
         {
-            lod->SetLodLayerDistance(layer, 10.f + 10.f * layer);
-        }
-    }
-
-    //add CP
-    CustomPropertiesComponent* cp = new CustomPropertiesComponent();
-    cp->GetArchive()->SetString("editor.referenceToOwner", scenePathname.GetAbsolutePathname());
-    cp->GetArchive()->SetFastName("name", e->GetName());
-    e->AddComponent(cp);
-}
-
-Vector<Entity*> renderEntities;
-scene->GetChildEntitiesWithComponent(renderEntities, Component::RENDER_COMPONENT);
-for (Entity* e : renderEntities)
-{
-    uint32 count = e->GetComponentCount(Component::RENDER_COMPONENT);
-    TEST_VERIFY(count > 0)
-    for (uint32 ic = 0; ic < count; ++ic)
-    {
-        RenderComponent* rc = static_cast<RenderComponent*>(e->GetComponent(Component::RENDER_COMPONENT, ic));
-        RenderObject* ro = rc->GetRenderObject();
-        TEST_VERIFY(ro->GetRenderBatchCount() > 0);
-        for (uint32 ib = 0; ib < ro->GetRenderBatchCount(); ++ib)
-        {
-            RenderBatch* batch = ro->GetRenderBatch(ib);
-            NMaterial* material = batch->GetMaterial();
-
-            TEST_VERIFY(material->HasLocalFlag(NMaterialFlagName::FLAG_FLATCOLOR) == false);
-            material->AddFlag(NMaterialFlagName::FLAG_FLATCOLOR, ib % 2);
-        }
-    }
-}
-
-TEST_VERIFY(scene->SaveScene(scenePathname, false) == SceneFileV2::eError::ERROR_NO_ERROR);
-}
-
-void TestScene()
-{
-    using namespace DAVA;
-    FilePath scenePathname = FilePath::CreateWithNewExtension(LibraryTest::testDAEPathname, ".sc2");
-
-    ScopedPtr<Scene> scene(new Scene());
-    TEST_VERIFY(scene->LoadScene(scenePathname) == SceneFileV2::eError::ERROR_NO_ERROR);
-
-    Vector<Entity*> lodEntities;
-    scene->GetChildEntitiesWithComponent(lodEntities, Component::LOD_COMPONENT);
-    TEST_VERIFY(lodEntities.empty() == false)
-
-    for (Entity* e : lodEntities)
-    {
-        uint32 count = e->GetComponentCount(Component::LOD_COMPONENT);
-        TEST_VERIFY(count > 0)
-        for (uint32 ic = 0; ic < count; ++ic)
-        {
-            LodComponent* lod = static_cast<LodComponent*>(e->GetComponent(Component::LOD_COMPONENT, ic));
-            for (int32 layer = 0; layer < LodComponent::MAX_LOD_LAYERS; ++layer)
+            uint32 count = e->GetComponentCount(Component::LOD_COMPONENT);
+            TEST_VERIFY(count > 0);
+            for (uint32 ic = 0; ic < count; ++ic)
             {
-                TEST_VERIFY(lod->GetLodLayerDistance(layer) - (10.f + 10.f * layer) < DAVA::EPSILON);
+                LodComponent* lod = static_cast<LodComponent*>(e->GetComponent(Component::LOD_COMPONENT, ic));
+                for (int32 layer = 0; layer < LodComponent::MAX_LOD_LAYERS; ++layer)
+                {
+                    lod->SetLodLayerDistance(layer, 10.f + 10.f * layer);
+                }
+            }
+
+            //add CP
+            CustomPropertiesComponent* cp = new CustomPropertiesComponent();
+            cp->GetArchive()->SetString("editor.referenceToOwner", scenePathname.GetAbsolutePathname());
+            cp->GetArchive()->SetFastName("name", e->GetName());
+            e->AddComponent(cp);
+        }
+
+        Vector<Entity*> renderEntities;
+        scene->GetChildEntitiesWithComponent(renderEntities, Component::RENDER_COMPONENT);
+        for (Entity* e : renderEntities)
+        {
+            uint32 count = e->GetComponentCount(Component::RENDER_COMPONENT);
+            TEST_VERIFY(count > 0);
+            for (uint32 ic = 0; ic < count; ++ic)
+            {
+                RenderComponent* rc = static_cast<RenderComponent*>(e->GetComponent(Component::RENDER_COMPONENT, ic));
+                RenderObject* ro = rc->GetRenderObject();
+                TEST_VERIFY(ro->GetRenderBatchCount() > 0);
+                for (uint32 ib = 0; ib < ro->GetRenderBatchCount(); ++ib)
+                {
+                    RenderBatch* batch = ro->GetRenderBatch(ib);
+                    NMaterial* material = batch->GetMaterial();
+
+                    TEST_VERIFY(material->HasLocalFlag(NMaterialFlagName::FLAG_FLATCOLOR) == false);
+                    material->AddFlag(NMaterialFlagName::FLAG_FLATCOLOR, ib % 2);
+                }
             }
         }
 
-        CustomPropertiesComponent* cp = static_cast<CustomPropertiesComponent*>(e->GetComponent(Component::CUSTOM_PROPERTIES_COMPONENT, 0));
-        TEST_VERIFY(cp != nullptr);
-        if (cp != nullptr)
-        {
-            TEST_VERIFY(cp->GetArchive()->GetString("editor.referenceToOwner") == scenePathname.GetAbsolutePathname());
-            TEST_VERIFY(cp->GetArchive()->GetFastName("name") == e->GetName());
-        }
+        TEST_VERIFY(scene->SaveScene(scenePathname, false) == SceneFileV2::eError::ERROR_NO_ERROR);
     }
 
-    Vector<Entity*> renderEntities;
-    scene->GetChildEntitiesWithComponent(renderEntities, Component::RENDER_COMPONENT);
-    for (Entity* e : renderEntities)
+    void TestScene()
     {
-        uint32 count = e->GetComponentCount(Component::RENDER_COMPONENT);
-        TEST_VERIFY(count > 0)
-        for (uint32 ic = 0; ic < count; ++ic)
-        {
-            RenderComponent* rc = static_cast<RenderComponent*>(e->GetComponent(Component::RENDER_COMPONENT, ic));
-            RenderObject* ro = rc->GetRenderObject();
-            TEST_VERIFY(ro->GetRenderBatchCount() > 0);
-            for (uint32 ib = 0; ib < ro->GetRenderBatchCount(); ++ib)
-            {
-                RenderBatch* batch = ro->GetRenderBatch(ib);
-                NMaterial* material = batch->GetMaterial();
+        using namespace DAVA;
+        FilePath scenePathname = FilePath::CreateWithNewExtension(LibraryTest::testDAEPathname, ".sc2");
 
-                TEST_VERIFY(material->HasLocalFlag(NMaterialFlagName::FLAG_FLATCOLOR) == true);
-                TEST_VERIFY(material->GetLocalFlagValue(NMaterialFlagName::FLAG_FLATCOLOR) == (ib % 2));
+        ScopedPtr<Scene> scene(new Scene());
+        TEST_VERIFY(scene->LoadScene(scenePathname) == SceneFileV2::eError::ERROR_NO_ERROR);
+
+        Vector<Entity*> lodEntities;
+        scene->GetChildEntitiesWithComponent(lodEntities, Component::LOD_COMPONENT);
+        TEST_VERIFY(lodEntities.empty() == false);
+
+        for (Entity* e : lodEntities)
+        {
+            uint32 count = e->GetComponentCount(Component::LOD_COMPONENT);
+            TEST_VERIFY(count > 0);
+            for (uint32 ic = 0; ic < count; ++ic)
+            {
+                LodComponent* lod = static_cast<LodComponent*>(e->GetComponent(Component::LOD_COMPONENT, ic));
+                for (int32 layer = 0; layer < LodComponent::MAX_LOD_LAYERS; ++layer)
+                {
+                    TEST_VERIFY(lod->GetLodLayerDistance(layer) - (10.f + 10.f * layer) < DAVA::EPSILON);
+                }
+            }
+
+            CustomPropertiesComponent* cp = static_cast<CustomPropertiesComponent*>(e->GetComponent(Component::CUSTOM_PROPERTIES_COMPONENT, 0));
+            TEST_VERIFY(cp != nullptr);
+            if (cp != nullptr)
+            {
+                TEST_VERIFY(cp->GetArchive()->GetString("editor.referenceToOwner") == scenePathname.GetAbsolutePathname());
+                TEST_VERIFY(cp->GetArchive()->GetFastName("name") == e->GetName());
+            }
+        }
+
+        Vector<Entity*> renderEntities;
+        scene->GetChildEntitiesWithComponent(renderEntities, Component::RENDER_COMPONENT);
+        for (Entity* e : renderEntities)
+        {
+            uint32 count = e->GetComponentCount(Component::RENDER_COMPONENT);
+            TEST_VERIFY(count > 0);
+            for (uint32 ic = 0; ic < count; ++ic)
+            {
+                RenderComponent* rc = static_cast<RenderComponent*>(e->GetComponent(Component::RENDER_COMPONENT, ic));
+                RenderObject* ro = rc->GetRenderObject();
+                TEST_VERIFY(ro->GetRenderBatchCount() > 0);
+                for (uint32 ib = 0; ib < ro->GetRenderBatchCount(); ++ib)
+                {
+                    RenderBatch* batch = ro->GetRenderBatch(ib);
+                    NMaterial* material = batch->GetMaterial();
+
+                    TEST_VERIFY(material->HasLocalFlag(NMaterialFlagName::FLAG_FLATCOLOR) == true);
+                    TEST_VERIFY(material->GetLocalFlagValue(NMaterialFlagName::FLAG_FLATCOLOR) == (ib % 2));
+                }
             }
         }
     }
-}
 
-DAVA_TEST (ConvertDAETest)
-{
-    TEST_VERIFY(DAEConverter::Convert(LibraryTest::testDAEPathname) == true);
-    ModifyScene();
+    DAVA_TEST (ConvertDAETest)
+    {
+        TEST_VERIFY(DAEConverter::Convert(LibraryTest::testDAEPathname) == true);
+        ModifyScene();
 
-    TEST_VERIFY(DAEConverter::Convert(LibraryTest::testDAEPathname) == true);
-    TestScene();
-}
+        TEST_VERIFY(DAEConverter::Convert(LibraryTest::testDAEPathname) == true);
+        TestScene();
+    }
 
-BEGIN_TESTED_MODULES()
-DECLARE_TESTED_MODULE(LibraryTest::ProjectManagerDummyModule)
-DECLARE_TESTED_MODULE(LibraryModule)
-END_TESTED_MODULES()
-}
-;
+    BEGIN_TESTED_MODULES()
+    DECLARE_TESTED_MODULE(LibraryTest::ProjectManagerDummyModule)
+    DECLARE_TESTED_MODULE(LibraryModule)
+    END_TESTED_MODULES()
+};
+
+    // clang-format on
