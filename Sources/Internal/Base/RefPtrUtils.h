67324938733d54365c6ea85ee3c0bb856c832b28
a_strokachuk <a_strokachuk@wargaming.net>
2017-08-18 14:58:45 +0300

    [DF-14670] remove DAVA::
    Former-commit-id: ebe33e829b2733664ba74bedc1f6246844cbeba6

--- Sources/Internal/Base/RefPtrUtils.h
+++ Sources/Internal/Base/RefPtrUtils.h
@@ -15,7 +15,7 @@ template <class T1, class T2>
 RefPtr<T1> DynamicTypeCheckRef(const RefPtr<T2>& rp)
 {
     RefPtr<T1> p;
-    p = DAVA::DynamicTypeCheck<T1*>(rp.Get());
+    p = DynamicTypeCheck<T1*>(rp.Get());
     return p;
 }
 }
