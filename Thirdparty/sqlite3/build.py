2488850c07bb83463e51543768103721e7935c35
Maksim Lazarev <m_lazarev@wargaming.net>
2017-08-25 15:06:02 +0300

    DF-14624 rebuild spine and sqlite3 with msvc2017 using dynamic runtime
    Former-commit-id: 5d55436c9182e299245a5ba94da40a8f7200d4ee

--- Thirdparty/sqlite3/build.py
+++ Thirdparty/sqlite3/build.py
@@ -67,7 +67,7 @@ def _build_win32(working_directory_path, root_project_path):
         'sqlite3.lib', 'sqlite3.lib',
         'sqlite3.lib', 'sqlite3.lib',
         'sqlite3.lib', 'sqlite3.lib',
-        static_runtime=True)
+        static_runtime=False)
 
     _copy_headers(source_folder_path, root_project_path)
 
