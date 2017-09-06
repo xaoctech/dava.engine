f52f02cc4cf3af470db9e10d2fb92ebcb23ea068
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2017-09-06 10:54:04 +0300

    DF-14332: removed unneeded includes
    Former-commit-id: 22e103719cf25a0bb09463dbcada1d671aaf334e

--- Modules/Physics/Sources/Physics/Private/CollisionShapeComponent.cpp
+++ Modules/Physics/Sources/Physics/Private/CollisionShapeComponent.cpp
@@ -1,9 +1,6 @@
 #include "Physics/CollisionShapeComponent.h"
 #include "Physics/Private/PhysicsMath.h"
-#include "Physics/PhysicsModule.h"
 
-#include <Engine/Engine.h>
-#include <ModuleManager/ModuleManager.h>
 #include <Scene3D/Scene.h>
 #include <Reflection/ReflectionRegistrator.h>
 
