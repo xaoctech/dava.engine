7ce1408b4890ae6e5933eaa9e4c0c9778b795f61
k_lyashkevich <k_lyashkevich@wargaming.net>
2017-09-11 17:00:40 +0300

    [DF-14771] after merge fixes
    Former-commit-id: b2b51967717d378f2c019a8f5062bc88169ad417

--- Programs/QuickEd/Classes/UI/Preview/PreviewWidgetSettings.h
+++ Programs/QuickEd/Classes/UI/Preview/PreviewWidgetSettings.h
@@ -8,9 +8,9 @@
 class PreviewWidgetSettings : public DAVA::TArc::SettingsNode
 {
 public:
-    DAVA::Color backgroundColor0 = DAVA::Color::Transparent;
-    DAVA::Color backgroundColor1 = DAVA::Color(1.0f, 1.0f, 1.0f, 1.0f);
-    DAVA::Color backgroundColor2 = DAVA::Color(0.0f, 0.0f, 0.0f, 1.0f);
+    DAVA::Color backgroundColor0 = DAVA::Color(0.0f, 0.0f, 0.0f, 0.5f);
+    DAVA::Color backgroundColor1 = DAVA::Color(0.242f, 0.242f, 0.242f, 1.0f);
+    DAVA::Color backgroundColor2 = DAVA::Color(0.159f, 0.159f, 0.159f, 1.0f);
     DAVA::uint32 backgroundColorIndex = 0;
 
     DAVA_VIRTUAL_REFLECTION(PreviewWidgetSettings, DAVA::TArc::SettingsNode);
