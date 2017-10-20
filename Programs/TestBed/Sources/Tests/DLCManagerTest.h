a03a9cdbeac612491c03e5a70938aecc1ecb7bdf
leanid.chaika <leanid.chaika@gmail.com>
2017-10-20 16:15:11 +0300

    DF-15390 better DLCManagerTest screen
    Former-commit-id: 0a88470bb664c079a2d6a63fd55f5cb23d854f90

--- Programs/TestBed/Sources/Tests/DLCManagerTest.h
+++ Programs/TestBed/Sources/Tests/DLCManagerTest.h
@@ -20,13 +20,14 @@ private:
     void UnloadResources() override;
 
     void Update(DAVA::float32 timeElapsed) override;
+    void UpdateProgress(float32 progress);
 
-    void OnStartInitClicked(BaseObject* sender, void* data, void* callerData);
+    void OnInitClicked(BaseObject* sender, void* data, void* callerData);
     void OnIOErrorClicked(BaseObject* sender, void* data, void* callerData);
-    void OnClearDocsClicked(BaseObject* sender, void* data, void* callerData);
+    void OnDeleteClicked(BaseObject* sender, void* data, void* callerData);
     void OnListPacksClicked(BaseObject* sender, void* data, void* callerData);
     void OnOffRequestingClicked(BaseObject* sender, void* data, void* callerData);
-    void OnStartDownloadClicked(BaseObject* sender, void* data, void* callerData);
+    void OnLoadClicked(BaseObject* sender, void* data, void* callerData);
     void OnExitButton(BaseObject* obj, void* data, void* callerData) override;
 
     void OnRequestUpdated(const DAVA::DLCManager::IRequest& request);
@@ -38,23 +39,16 @@ private:
     DAVA::ProfilerCPU profiler;
 
     DAVA::FilePath folderWithDownloadedPacks;
-    // TODO quick and dirty way to test download on all platforms, in future replace with local http server
+    // quick and dirty way to test download on all platforms
     DAVA::String urlToServerSuperpack = "http://by1-builddlc-01.corp.wargaming.local/DLC_Blitz/superpack_dx11.dvpk";
-    //"http://dl-wotblitz.wargaming.net/dlc/r11608713/3.7.0.236.dvpk";
-    //"http://by1-builddlc-01.corp.wargaming.local/DLC_Blitz/smart_dlc/3.7.0.236.dvpk";
-    //"http://dl-wotblitz.wargaming.net/dlc/r11608713/3.7.0.236.dvpk";
-    //"http://127.0.0.1:8080/superpack_for_unittests.dvpk";
-    //"http://by1-builddlc-01.corp.wargaming.local/DLC_Blitz/superpack_for_unittests.dvpk";
-    //"http://127.0.0.1:2424/superpack.dvpk";
 
-    DAVA::UIStaticText* textProperties = nullptr;
+    DAVA::UIStaticText* textStatusOutput = nullptr;
     DAVA::UIButton* buttonLoadPack = nullptr;
 
     DAVA::UITextField* editPackName = nullptr;
     DAVA::UIControl* progressRed = nullptr;
     DAVA::UIControl* progressGreen = nullptr;
-    DAVA::UIStaticText* textDescription = nullptr;
     DAVA::UITextField* editUrl = nullptr;
     DAVA::UIButton* buttonInitDLC = nullptr;
-    DAVA::UIButton* buttonRemoveDoc = nullptr;
+    DAVA::UIButton* buttonRemovePack = nullptr;
 };
