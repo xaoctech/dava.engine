0bb9c985d8ebfda75683c41cff240170fbbc6cd0
Evgeniy Veretennikov <e_veretennikov@wargaming.net>
2017-10-24 13:54:17 +0300

    DF-15368 ui script component. More test coverage.
    Former-commit-id: 9fd161cbdaa2b9c072aaaf8210c261a94b392351

--- Sources/Internal/UI/Script/Private/UIScrtiptComponent.cpp
+++ Sources/Internal/UI/Script/Private/UIScrtiptComponent.cpp
@@ -28,12 +28,14 @@ UIScriptComponent* UIScriptComponent::Clone() const
 void UIScriptComponent::SetReflectionTypeName(const String& typeName)
 {
     reflectionTypeName = typeName;
+    luaScriptPath = "";
     SetModifiedScripts(true);
 }
 
 void UIScriptComponent::SetLuaScriptPath(const FilePath& filePath)
 {
     luaScriptPath = filePath;
+    reflectionTypeName = "";
     SetModifiedScripts(true);
 }
 
