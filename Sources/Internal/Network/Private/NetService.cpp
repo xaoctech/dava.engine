eb6aaa920f9b5fad969f525c515f01d962ecf6d9
Stanislav Madytski <s_madytskiy@wargaming.net>
2017-11-16 11:55:29 +0300

    DF-13798 review fix
    Former-commit-id: 30ff6e72b096c118a7cbbb9f159a9fdfdd39b68a

--- Sources/Internal/Network/Private/NetService.cpp
+++ Sources/Internal/Network/Private/NetService.cpp
@@ -9,37 +9,37 @@ namespace Net
 IChannelListener::~IChannelListener() = default;
 IChannel::~IChannel() = default;
 
-void NetService::OnChannelOpen(const std::shared_ptr<IChannel>& aChannel)
+void NetService::OnChannelOpen(const std::shared_ptr<IChannel>& channel_)
 {
     DVASSERT(NULL == channel);
-    channel = aChannel;
+    channel = channel_;
     ChannelOpen();
 }
 
-void NetService::OnChannelClosed(const std::shared_ptr<IChannel>& aChannel, const char8* message)
+void NetService::OnChannelClosed(const std::shared_ptr<IChannel>& channel_, const char8* message)
 {
     // OnChannelClosed can be called without corresponding OnChannelOpen, e.g. when remote service is unavailable
-    DVASSERT(NULL == channel || channel == aChannel);
+    DVASSERT(NULL == channel || channel == channel_);
     channel = NULL;
     ChannelClosed(message);
 }
 
-void NetService::OnPacketReceived(const std::shared_ptr<IChannel>& aChannel, const void* buffer, size_t length)
+void NetService::OnPacketReceived(const std::shared_ptr<IChannel>& channel_, const void* buffer, size_t length)
 {
-    DVASSERT(channel == aChannel);
+    DVASSERT(channel == channel_);
     PacketReceived(buffer, length);
 }
 
-void NetService::OnPacketSent(const std::shared_ptr<IChannel>& aChannel, const void* buffer, size_t length)
+void NetService::OnPacketSent(const std::shared_ptr<IChannel>& channel_, const void* buffer, size_t length)
 {
     // If channel is NULL then OnChannelClosed has been called already
-    DVASSERT(NULL == channel || channel == aChannel);
+    DVASSERT(NULL == channel || channel == channel_);
     PacketSent();
 }
 
-void NetService::OnPacketDelivered(const std::shared_ptr<IChannel>& aChannel, uint32 packetId)
+void NetService::OnPacketDelivered(const std::shared_ptr<IChannel>& channel_, uint32 packetId)
 {
-    DVASSERT(channel == aChannel);
+    DVASSERT(channel == channel_);
     PacketDelivered();
 }
 
