1d8d96ad62abf33e9281830280a6f03647f12f83
i_bratchik <i_bratchik@wargaming.net>
2017-11-17 16:49:06 +0300

    DF-15640  fixed inheritage of child definitions
    Former-commit-id: 9714b40c0824a5a0e7e0ad6eea759a96fd16123f

--- Sources/CMake/Modules/DavaTemplateModules.cmake
+++ Sources/CMake/Modules/DavaTemplateModules.cmake
@@ -292,6 +292,9 @@ macro( setup_main_module )
         get_property( MAIN_MODULES_FIND_FIRST_CALL_LIST GLOBAL PROPERTY MAIN_MODULES_FIND_FIRST_CALL_LIST )
         if( NOT MAIN_MODULES_FIND_FIRST_CALL_LIST )  
             # "root call" 
+            set( MODULE_STACK_DEFINITION )
+        else()
+            set( MODULE_STACK_DEFINITION ${MODULE_STACK_DEFINITION} ${DEFINITIONS} ${DEFINITIONS_${DAVA_PLATFORM_CURRENT}} PARENT_SCOPE)
         endif()
 
         list( APPEND MAIN_MODULES_FIND_FIRST_CALL_LIST "call" )
@@ -325,7 +328,6 @@ macro( setup_main_module )
 
         endif()
 
-        set( DEFINITIONS_BACKUP ${DEFINITIONS} ${DEFINITIONS_${DAVA_PLATFORM_CURRENT}} )
 
         #"FIND LIBRARY"
         foreach( NAME ${FIND_SYSTEM_LIBRARY} ${FIND_SYSTEM_LIBRARY_${DAVA_PLATFORM_CURRENT}} )
@@ -358,17 +360,8 @@ macro( setup_main_module )
             list ( APPEND STATIC_LIBRARIES_SYSTEM_${DAVA_PLATFORM_CURRENT} ${PACKAGE_${NAME}_STATIC_LIBRARIES} )
         endforeach()
 
-
-        load_property( PROPERTY_LIST 
-                DEFINITIONS
-                DEFINITIONS_${DAVA_PLATFORM_CURRENT} )
-
-        set( PACKAGES_DEFINITIONS  ${DEFINITIONS} ${DEFINITIONS_${DAVA_PLATFORM_CURRENT}} )
-
-        if( DEFINITIONS_BACKUP )
-            list(REMOVE_ITEM PACKAGES_DEFINITIONS "${DEFINITIONS_BACKUP}" )
-        endif()
-
+        list( APPEND DEFINITIONS ${MODULE_STACK_DEFINITION} )
+        
 #####
         if( CPP_FILES_EXECUTE )
             get_filename_component( CPP_FILES_EXECUTE ${CPP_FILES_EXECUTE} ABSOLUTE )
@@ -388,16 +381,11 @@ macro( setup_main_module )
             set( MODULE_CACHE   ${COVERAGE_STRING}
                                 ${MODULE_COMPONENTS}  )
             
-            if( USE_PARENT_DEFINITIONS  )
-
-                list( APPEND MODULE_CACHE ${DEFINITIONS} 
-                                          ${DEFINITIONS_${DAVA_PLATFORM_CURRENT}} 
-                                        )
-            else()
-                list( APPEND MODULE_CACHE ${PACKAGES_DEFINITIONS} 
-                                          ${PACKAGES_DEFINITIONS_${DAVA_PLATFORM_CURRENT}} 
-                                        )
-            endif()
+    
+            list( APPEND MODULE_CACHE ${DEFINITIONS} 
+                                      ${DEFINITIONS_${DAVA_PLATFORM_CURRENT}} 
+                                    )
+ 
 
             if( MODULE_CACHE )
                 list( REMOVE_DUPLICATES MODULE_CACHE )
