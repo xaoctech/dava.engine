f28f0efda41c3f2b4f28f969306e90840380a5f2
l_chayka <l_chayka@wargaming.net>
2017-11-28 10:51:02 +0300

    DF-15592 implement iOS GetStorageList
    Former-commit-id: 20dda5410464ba9dc63da1a779e2f46a528ffc0c

--- Sources/Internal/Platform/TemplateiOS/DeviceInfoIos.mm
+++ Sources/Internal/Platform/TemplateiOS/DeviceInfoIos.mm
@@ -7,11 +7,15 @@
 #import "Utils/NSStringUtils.h"
 #include "Base/GlobalEnum.h"
 #include "Base/TemplateHelpers.h"
+#include "Logger/Logger.h"
 
 #import <UIKit/UIDevice.h>
 #import <UIKit/UIKit.h>
 #import <Foundation/NSLocale.h>
-#import <sys/utsname.h>
+
+#include <sys/utsname.h>
+#include <sys/statvfs.h>
+
 #import <AdSupport/ASIdentifierManager.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #import <CoreTelephony/CTCarrier.h>
@@ -410,6 +414,35 @@ DeviceInfo::NetworkInfo DeviceInfoPrivate::GetNetworkInfo()
 List<DeviceInfo::StorageInfo> DeviceInfoPrivate::GetStoragesList()
 {
     List<DeviceInfo::StorageInfo> l;
+    DeviceInfo::StorageInfo info;
+
+    info.type = DeviceInfo::STORAGE_TYPE_INTERNAL;
+
+    const char* home = ::getenv("HOME");
+    if (nullptr == home)
+    {
+        Logger::Error("HOME env not found");
+        return l;
+    }
+
+    struct statvfs stat_data;
+
+    if (0 != ::statvfs(home, &stat_data))
+    {
+        Logger::Error("failed get filesystem info for path: %s, error: %s", home, ::strerror(errno));
+        return l;
+    }
+
+    info.totalSpace = stat_data.f_frsize * stat_data.f_blocks;
+    info.freeSpace = stat_data.f_frsize * stat_data.f_bavail;
+
+    info.readOnly = false;
+    info.removable = false;
+    info.emulated = false;
+
+    info.path = home;
+
+    l.push_back(info);
     return l;
 }
 
