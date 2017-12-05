a3a431f1ece736a1219102810bcedec979b850eb
leanid.chaika <leanid.chaika@gmail.com>
2017-12-05 18:04:46 +0300

    DF-15616 add selftest for ResourceArchiver on teamcity
    Former-commit-id: ef3c755cb29749fca58d82ac20f23b367e022e29

--- .teamcity/dava_framework_NewBuilds_ToolSet/buildTypes/dava_framework_NewBuilds_ToolSet_ToolSetWin.kt
+++ .teamcity/dava_framework_NewBuilds_ToolSet/buildTypes/dava_framework_NewBuilds_ToolSet_ToolSetWin.kt
@@ -86,6 +86,7 @@ object dava_framework_NewBuilds_ToolSet_ToolSetWin : BuildType({
             scriptContent = """
                 ResourceEditor.exe --selftest
                 QuickEd.exe --selftest
+                ResourceArchiver.exe --selftest
             """.trimIndent()
         }
         script {
