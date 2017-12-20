12bbf4f709a2f1673e4d731e234336f5cfd823be
Maksim Molokovskih <m_molokovskih@wargaming.net>
2017-12-20 15:36:12 +0300

    DF-14151 Removed old vcs root, from unit tests.
    Former-commit-id: ceb1e82352bc6c1d7ca9ada507bf2c5ab147c045

--- .teamcity/dava_framework_NewBuilds_Tests_UnitTests/buildTypes/dava_framework_NewBuilds_Tests_UnitTests_UnitTestsWin102.kt
+++ .teamcity/dava_framework_NewBuilds_Tests_UnitTests/buildTypes/dava_framework_NewBuilds_Tests_UnitTests_UnitTestsWin102.kt
@@ -35,7 +35,6 @@ object dava_framework_NewBuilds_Tests_UnitTests_UnitTestsWin102 : BuildType({
 
     vcs {
         root("dava_DavaFrameworkStash")
-        root("dava_framework_UIEditor_BuildmachineWargamingNetTools", "+:Teamcity => Teamcity")
 
         checkoutMode = CheckoutMode.ON_AGENT
         cleanCheckout = true
